6f1fca800118339822a785d823b0f5d560ee384a
Dmytro Hryhoruk <dimamyhgrygoruk@gmail.com>
2019-12-06 14:30:17 +0200

    5.3 is done

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,5 +1,11 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 /**
  * <p>Дано файл, потрібно прочитати його вміст і вибрати всі слова що починаються на `w`.
  * Результат повернути у вигляді відсортованого масиву (за замовчуванням). Всі слова повинні
@@ -9,6 +15,28 @@ package core.basesyntax;
  */
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        return null;
+        File file = new File(fileName);
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            String result = "";
+            String st;
+            while ((st = bufferedReader.readLine()) != null) {
+                String[] singleLine = st
+                        .replaceAll("[^a-zA-Z ]", "")
+                        .split("\\s");
+                for (String words : singleLine) {
+                    if (words.toLowerCase().charAt(0) == 'w') {
+                        result += words.toLowerCase() + " ";
+                    }
+                }
+            }
+            if (result.length() == 0) {
+                return new String[0];
+            }
+            String[] arrayResult = result.split("\\s");
+            Arrays.sort(arrayResult);
+            return arrayResult;
+        } catch (IOException e) {
+            return new String[0];
+        }
     }
 }
