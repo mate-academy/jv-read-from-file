4c96f4bc009636cf96ce52d9a3a478e0f1510883
Daniil Hovorun <hovorun.d@gmail.com>
2019-12-06 19:56:19 +0200

    hw:5.3-readFromFile solved

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,5 +1,11 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 /**
  * <p>Дано файл, потрібно прочитати його вміст і вибрати всі слова що починаються на `w`.
  * Результат повернути у вигляді відсортованого масиву (за замовчуванням). Всі слова повинні
@@ -9,6 +15,32 @@ package core.basesyntax;
  */
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        return null;
+        File file = new File(fileName);
+        String[] result = new String[(int) file.length()];
+        int counter = 0;
+
+        try (FileReader fileReader = new FileReader(file);
+                BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+
+            String fileLine;
+            while ((fileLine = bufferedReader.readLine()) != null) {
+                String[] lineWords = fileLine.replaceAll("[^\\w]", " ").split(" ");
+                for (String word : lineWords) {
+                    word = word.toLowerCase();
+                    if (word.startsWith("w")) {
+                        result[counter] = word;
+                        counter++;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        result = Arrays.copyOf(result, counter);
+        Arrays.sort(result);
+
+        return result;
     }
+
 }
+
