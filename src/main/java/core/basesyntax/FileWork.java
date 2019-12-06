f2983ce803c7092e08fb75addf5962667ece7ee5
SergiyAgeev <sergiyageev@gmail.com>
2019-12-06 20:51:42 +0200

    hw 5 3

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
 /**
  * <p>Дано файл, потрібно прочитати його вміст і вибрати всі слова що починаються на `w`.
  * Результат повернути у вигляді відсортованого масиву (за замовчуванням). Всі слова повинні
@@ -8,7 +17,31 @@ package core.basesyntax;
  * Результат: web wide width world</p>
  */
 public class FileWork {
+
     public String[] readFromFile(String fileName) {
-        return null;
+        List<String> resultList = new ArrayList<>();
+
+        try {
+            File file = new File(fileName);
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String temp;
+
+            while ((temp = bufferedReader.readLine()) != null) {
+                String[] result = temp.toLowerCase().replaceAll("[^a-z]", " ").split(" ");
+                for (String s : result) {
+                    if (s.startsWith("w")) {
+                        resultList.add(s);//
+                    }
+
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println("problem with IO");
+        }
+        Collections.sort(resultList);
+        String[] results = new String[resultList.size()];
+        results = resultList.toArray(results);
+        return results;
     }
 }
