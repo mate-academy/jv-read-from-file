04a685c2a33beaa75c25d913ba0aeac8eca813a0
Yaroslava Bondarchuk <yaroslava7bondarchuk@gmail.com>
2019-12-06 23:25:51 +0200

    added readFromFile method

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,5 +1,11 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * <p>Дано файл, потрібно прочитати його вміст і вибрати всі слова що починаються на `w`.
  * Результат повернути у вигляді відсортованого масиву (за замовчуванням). Всі слова повинні
@@ -8,7 +14,31 @@ package core.basesyntax;
  * Результат: web wide width world</p>
  */
 public class FileWork {
-    public String[] readFromFile(String fileName) {
-        return null;
+
+    public static String[] readFromFile(String fileName) {
+        StringBuilder words = new StringBuilder();
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(fileName));
+            if (lines.isEmpty()) {
+                return new String[0];
+            }
+
+            for (String line : lines) {
+                String[] lineWords = line.toLowerCase().split(" ");
+                for (String word : lineWords) {
+                    if (word.charAt(0) == 'w') {
+                        words.append(word.replaceAll("[^a-z]+", "")).append(" ");
+                    }
+                }
+            }
+            if (words.toString().isBlank()) {
+                return new String[0];
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String[] result = words.toString().trim().split(" ");
+        Arrays.sort(result);
+        return result;
     }
 }
