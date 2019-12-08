0060c87c64f04f5178ff1657c2535a0dd13cb43f
Grave <graveman13@gmail.com>
2019-12-08 16:52:10 +0300

    Home work 5.3

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,5 +1,10 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 /**
  * <p>Дано файл, потрібно прочитати його вміст і вибрати всі слова що починаються на `w`.
  * Результат повернути у вигляді відсортованого масиву (за замовчуванням). Всі слова повинні
@@ -9,6 +14,29 @@ package core.basesyntax;
  */
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        return null;
+        String[] wordsWithSym = {};
+        String text = "";
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String readline = "";
+            while ((readline = reader.readLine()) != null) {
+                text += readline;
+            }
+        } catch (IOException ex) {
+            ex.getStackTrace();
+        }
+        wordsWithSym = getWordsBeginningWhithSym(text, 'w');
+        Arrays.sort(wordsWithSym);
+        return wordsWithSym;
+    }
+
+    public String[] getWordsBeginningWhithSym(String line, char sym) {
+        String[] result = {};
+        if (line.length() != 0) {
+            result = Arrays.stream(line.split(" "))
+                    .map(word -> word.toLowerCase()).map(word -> word.replaceAll("[^a-z]", ""))
+                    .filter(word -> word.charAt(0) == sym)
+                    .toArray(String[]::new);
+        }
+        return result;
     }
 }
