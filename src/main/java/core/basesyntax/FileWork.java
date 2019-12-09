e3e5b2a8bc0a0a687dd0bdf1a8c829468d0e639f
Mykhailo Kramar <3805033@gmail.com>
2019-12-09 08:32:21 +0200

    hw5.3

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,5 +1,11 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  * <p>Дано файл, потрібно прочитати його вміст і вибрати всі слова що починаються на `w`.
  * Результат повернути у вигляді відсортованого масиву (за замовчуванням). Всі слова повинні
@@ -9,6 +15,35 @@ package core.basesyntax;
  */
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        return null;
+        ArrayList<String> wordsArray = new ArrayList<>();
+        StringBuilder sb = new StringBuilder(" ");
+        File file = new File(fileName);
+        if (file.length() == 0) {
+            return new String[0];
+        }
+        String line = "";
+        try {
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            while ((line = bufferedReader.readLine()) != null) {
+                sb.append(line);
+            }
+            bufferedReader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        String[] lineArray = sb.toString().toLowerCase().replaceAll("[^a-z]", " ").split(" ");
+        for (String i : lineArray) {
+            i.split(" ");
+            for (int j = 0; j < i.split(" ").length; j++) {
+                if (i.split(" ")[j].startsWith("w")) {
+                    wordsArray.add(i.split(" ")[j]);
+                }
+            }
+        }
+        Collections.sort(wordsArray);
+        String[] textFromFile = new String[wordsArray.size()];
+        return (String[]) wordsArray.toArray(textFromFile);
     }
 }
+
