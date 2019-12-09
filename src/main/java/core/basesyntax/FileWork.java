24a4eb948fb0395d57df82eca377415fa5e544a0
AlinaPodoliaka <57722396+AlinaPodoliaka@users.noreply.github.com>
2019-12-09 11:40:56 +0200

    added hw-5-3-solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * <p>Дано файл, потрібно прочитати його вміст і вибрати всі слова що починаються на `w`.
  * Результат повернути у вигляді відсортованого масиву (за замовчуванням). Всі слова повинні
@@ -9,6 +18,36 @@ package core.basesyntax;
  */
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        return null;
+        String str = "";
+        try {
+            File file = new File(fileName);
+            FileReader fileReader = new FileReader(file);
+            BufferedReader reader = new BufferedReader(fileReader);
+            while (reader.ready()) {
+                str += reader.readLine();
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        str = str.replaceAll("[^\\s a-z A-Z]", "");
+        String[] words = str.split(" ");
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].toLowerCase().startsWith("w")) {
+                list.add(words[i].toLowerCase());
+            }
+        }
+        if (list.isEmpty()) {
+            return new String[0];
+        }
+        Collections.sort(list);
+        String[] result = new String[list.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = list.get(i);
+        }
+        return result;
     }
 }
