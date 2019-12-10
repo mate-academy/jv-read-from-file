2f95b590f474bb5c8e324f7622e8f2d0b99a309f
MrNopi <Smart.like1998@gmail.com>
2019-12-10 12:16:00 +0300

    Remove path variable and Collections.sort, also rename W as START_OF_WORDS

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -2,9 +2,10 @@ package core.basesyntax;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * <p>Дано файл, потрібно прочитати його вміст і вибрати всі слова що починаються на `w`.
@@ -15,19 +16,17 @@ import java.util.*;
  */
 public class FileWork {
 
-    private static final String W = "w";
+    private static final String START_OF_WORDS = "w";
     private static final String REGEX_NON_WORDS = "[^a-z+]";
 
     public String[] readFromFile(String fileName) {
         List<String> arr = new ArrayList<>();
-
-        Path path = Paths.get(fileName);
         try {
-            List<String> array = Files.readAllLines(path);
+            List<String> array = Files.readAllLines(Paths.get(fileName));
             for (String value : array) {
                 String[] tmp = value.toLowerCase().split(" ");
                 for (String s : tmp) {
-                    if (s.startsWith(W)) {
+                    if (s.startsWith(START_OF_WORDS)) {
                         arr.add(s.replaceAll(REGEX_NON_WORDS, ""));
                     }
                 }
@@ -35,8 +34,8 @@ public class FileWork {
         } catch (IOException e) {
             System.out.println("Failed to upload file, make sure it's exist");
         }
-        Collections.sort(arr);
         String[] newArr = arr.toArray(new String[0]);
+        Arrays.sort(newArr);
         return newArr;
     }
 }
