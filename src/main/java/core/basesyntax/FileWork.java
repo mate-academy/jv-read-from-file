3ba763cb74aba2632faea74ae238750380b73fea
NikolayAbysov <nikolay.abysov@ukr.net>
2020-04-02 19:29:20 +0300

    Fixed issues: 1 Added constant for indexOf search 2 Added runtime exception 3 List to array conversion accepted :)

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -15,21 +15,23 @@ import java.util.List;
  * Результат: web wide width world</p>
  */
 public class FileWork {
+
+    private static final char CHAR_MASK = 'w';
+
     public String[] readFromFile(String fileName) {
-        String[] arr = new String[0];
+        String[] arr;
         try {
             arr = Files.readString(Path.of(fileName)).toLowerCase().split("\\W+");
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException("No file");
         }
         List<String> list = new ArrayList<>();
         for (String s : arr) {
-            if (s.indexOf('w') == 0) {
+            if (s.indexOf(CHAR_MASK) == 0) {
                 list.add(s);
             }
         }
         Collections.sort(list);
-        String[] output = new String[list.size()];
-        return list.toArray(output);
+        return list.toArray(String[]::new);
     }
 }
