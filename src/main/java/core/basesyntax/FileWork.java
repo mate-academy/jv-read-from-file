3d55eabce674dcf6f95329d64c39e7f3a8d173d7
andriy <maziarandriy@gmail.com>
2020-04-02 18:24:10 +0300

    hw5.3 solution problem with maven i do not know how to fix

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,5 +1,10 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
 /**
  * <p>Дано файл, потрібно прочитати його вміст і вибрати всі слова що починаються на `w`.
  * Результат повернути у вигляді відсортованого масиву (за замовчуванням). Всі слова повинні
@@ -9,6 +14,25 @@ package core.basesyntax;
  */
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        return null;
+        String[] result;
+        try {
+            String line = Files.readString(Paths.get(fileName));
+            String[] lineList = line.toLowerCase().split(" ");
+            StringBuilder wList = new StringBuilder();
+            for (String word: lineList) {
+                if (word.toLowerCase().startsWith("w")) {
+                    wList.append(word.replaceAll("[^a-zA-Z0-9]","")).append(" "); // please help i have problem with mvn
+                } //  ileWork.java:21:27: Local variable name 'wList' must match pattern '^[a-z]([a-z0-9][a-zA-Z0-9]*)?$'. [LocalVariableName]
+            }
+            if (wList.length() > 0) {
+                result = wList.toString().split(" ");
+            } else {
+                result = new String[0];
+            }
+            Arrays.sort(result);
+            return result;
+        } catch (IOException e) {
+            throw new RuntimeException("No such file");
+        }
     }
 }
