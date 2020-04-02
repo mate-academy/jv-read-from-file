b9c65de0c4e5c3309a7681fd01150818f348f6c5
xeddin <61269748+xeddin@users.noreply.github.com>
2020-04-02 22:27:40 +0300

    Added solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,5 +1,10 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
 /**
  * <p>Дано файл, потрібно прочитати його вміст і вибрати всі слова що починаються на `w`.
  * Результат повернути у вигляді відсортованого масиву (за замовчуванням). Всі слова повинні
@@ -8,7 +13,23 @@ package core.basesyntax;
  * Результат: web wide width world</p>
  */
 public class FileWork {
+    private static final String PATTERN = "w";
+
     public String[] readFromFile(String fileName) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        try {
+            String string = Files.readString(Paths.get(fileName));
+            String[] stringArray = string.toLowerCase().split(" ");
+            for (String s : stringArray) {
+                if (s.startsWith(PATTERN)) {
+                    sb.append(s.replaceAll("\\W", "")).append(" ");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sb.length() > 0
+                ? Arrays.stream(sb.toString().trim().split(" ")).sorted().toArray(String[]::new)
+                : new String[0];
     }
 }
