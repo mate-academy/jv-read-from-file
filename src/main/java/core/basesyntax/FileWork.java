9beaf6c24b75eec477a6322d3e569b2d1aad81cf
Serhii Tsyhankov <40259977+fruts@users.noreply.github.com>
2020-04-02 22:02:24 +0300

    edit solutrion to normal

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,40 +1,38 @@
 package core.basesyntax;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Scanner;
 
 /**
  * <p>Дано файл, потрібно прочитати його вміст і вибрати всі слова що починаються на `w`.
  * Результат повернути у вигляді відсортованого масиву (за замовчуванням). Всі слова повинні
- * бути в нижньому регістрі. У випадку якщо таких слів не знайдено повернути пустий масив.
- * Приклад: Width world Wide web
- * Результат: web wide width world</p>
+ @@ -8,7 +13,26 @@
+  * Результат: web wide width world</p>
  */
 
 public class FileWork {
-    public static final String CHARACTER = "w";
+    private static final String START_LETTER = "w";
 
     public String[] readFromFile(String fileName) {
-        File file = new File(fileName);
-        StringBuilder scannedWords = new StringBuilder();
-        String[] result;
-        try (Scanner scan = new Scanner(file)) {
-            while (scan.hasNext()) {
-                String oneString = scan.next().toLowerCase();
-                if (oneString.startsWith(CHARACTER)) {
-                    scannedWords.append(oneString.replaceAll("\\W", "")).append(" ");
-                }
+        StringBuilder stringBuilder = new StringBuilder();
+        String line = "";
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            while ((line = br.readLine()) != null) {
+                stringBuilder.append(line);
             }
-            result = scannedWords.length() > 0
-                    ? Arrays.stream(scannedWords.toString().trim()
-                    .split(" ")).sorted().toArray(String[]::new)
-                    : new String[0];
         } catch (IOException e) {
-            throw new RuntimeException("Неверно указано имя файла, или его не существует", e);
+            throw new RuntimeException(e);
         }
+        String[] result = stringBuilder
+                .toString()
+                .toLowerCase()
+                .replaceAll("[^a-z\\s]", "")
+                .split(" ");
         Arrays.sort(result);
-        return result;
+        return Arrays.stream(result)
+                .filter(s -> s.startsWith(START_LETTER))
+                .toArray(String[]::new);
     }
 }
