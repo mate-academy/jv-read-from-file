16940671e48d2c70710d94b509eb21e7adf2675b
vladyslav <vlad.bes0711@gmail.com>
2020-04-02 18:08:10 +0300

    added solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,14 +1,32 @@
 package core.basesyntax;
 
-/**
- * <p>Дано файл, потрібно прочитати його вміст і вибрати всі слова що починаються на `w`.
- * Результат повернути у вигляді відсортованого масиву (за замовчуванням). Всі слова повинні
- * бути в нижньому регістрі. У випадку якщо таких слів не знайдено повернути пустий масив.
- * Приклад: Width world Wide web
- * Результат: web wide width world</p>
- */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static char CHECKED_LATTER = 'w';
+
     public String[] readFromFile(String fileName) {
-        return null;
+        StringBuilder fullText = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String readLine;
+            while ((readLine = br.readLine()) != null) {
+                fullText.append(readLine);
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        String[] result = fullText
+                .toString()
+                .toLowerCase()
+                .replaceAll("[^A-Za-zА-Яа-я ]", "")
+                .split(" ");
+        return fullText.length() == 0 ? new String[0] : Arrays
+                .stream(result)
+                .filter(word -> word.charAt(0) == CHECKED_LATTER)
+                .sorted()
+                .toArray(String[]::new);
     }
 }
