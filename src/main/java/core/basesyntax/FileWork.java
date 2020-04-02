7ab4690205c19717f6ca9b3d507028de39eacc10
Yaroslav-Serhii <u.vusochanskiy@gmail.com>
2020-04-02 16:30:24 +0300

    Added solution.

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,5 +1,10 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 /**
  * <p>Дано файл, потрібно прочитати його вміст і вибрати всі слова що починаються на `w`.
  * Результат повернути у вигляді відсортованого масиву (за замовчуванням). Всі слова повинні
@@ -7,8 +12,35 @@ package core.basesyntax;
  * Приклад: Width world Wide web
  * Результат: web wide width world</p>
  */
+
 public class FileWork {
+
+    public static final char WORD_START_AT = 'w';
+
     public String[] readFromFile(String fileName) {
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName));) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line.toLowerCase().replaceAll("\\p{Punct}", ""));
+                stringBuilder.append(" ");
+            }
+        } catch (IOException e) {
+            return new String[0];
+        }
+        StringBuilder result = new StringBuilder();
+        while (stringBuilder.length() != 0) {
+            if (stringBuilder.charAt(0) == WORD_START_AT) {
+                result.append(stringBuilder.subSequence(0, stringBuilder.indexOf(" "))).append(" ");
+            }
+            stringBuilder.delete(0, stringBuilder.indexOf(" ") + 1);
+        }
+        if (result.length() != 0) {
+            String[] finalResult = result.toString().split(" ");
+            Arrays.sort(finalResult);
+            return finalResult;
+        } else {
+            return new String[0];
+        }
     }
 }
