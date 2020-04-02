66d8b41d60773c21b2e298156f01ea613a9c151e
vladpetrovskyi <v.petrovskyi98@gmail.com>
2020-04-02 19:40:55 +0300

    added solution for homework 5.3

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * <p>Дано файл, потрібно прочитати його вміст і вибрати всі слова що починаються на `w`.
  * Результат повернути у вигляді відсортованого масиву (за замовчуванням). Всі слова повинні
@@ -8,7 +18,33 @@ package core.basesyntax;
  * Результат: web wide width world</p>
  */
 public class FileWork {
+    private static final String REGEX = "\\b[wW]\\w*\\b";
+
     public String[] readFromFile(String fileName) {
-        return null;
+        Path file = Paths.get(fileName);
+        List<String> allLines = null;
+        try {
+            allLines = Files.readAllLines(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        List<String> answerList = new ArrayList<>();
+        Pattern pattern = Pattern.compile(REGEX);
+        if (allLines != null) {
+            for (String s : allLines) {
+                Matcher matcher = pattern.matcher(s);
+                while (matcher.find()) {
+                    answerList.add(s.substring(matcher.start(), matcher.end()).toLowerCase());
+                }
+            }
+        }
+
+        String[] answer = new String[answerList.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = answerList.get(i);
+        }
+        Arrays.sort(answer);
+        return answer;
     }
 }
