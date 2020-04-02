0259960c441fac08cdf35a0408a61a6e964d8c51
Vinglish <khan.ildar94@gmail.com>
2020-04-02 16:18:14 +0300

    read from file

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * <p>Дано файл, потрібно прочитати його вміст і вибрати всі слова що починаються на `w`.
  * Результат повернути у вигляді відсортованого масиву (за замовчуванням). Всі слова повинні
@@ -9,6 +19,23 @@ package core.basesyntax;
  */
 public class FileWork {
     public String[] readFromFile(String fileName) {
+        try {
+            Stream<String> lines = Files.lines(Paths.get(fileName));
+            List<String> content = lines.collect(Collectors.toList());
+            Pattern p = Pattern.compile("(?i)\\b(w\\w*)\\b");
+            Matcher m = p.matcher(content.toString());
+            List<String> list = new ArrayList<>();
+
+            while (m.find()) {
+                list.add(m.group().toLowerCase());
+            }
+            String[] arr = new String[list.size()];
+            list.toArray(arr);
+            Arrays.sort(arr);
+            return arr;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
         return null;
     }
 }
