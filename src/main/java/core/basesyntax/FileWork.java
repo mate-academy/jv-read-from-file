e937bbc00ab613d2971961f837f555f4dfa3fc4d
IrinaDyakonova <dyakonova.irina.v@gmail.com>
2020-04-02 22:33:25 +0300

    hw_5_3_

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -4,9 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 
 /**
  * <p>Дано файл, потрібно прочитати його вміст і вибрати всі слова що починаються на `w`.
@@ -16,27 +14,27 @@ import java.util.List;
  * Результат: web wide width world</p>
  */
 public class FileWork {
-    private static final char FIRST_LETTER = 'w';
+    private static final String FIRST_LETTER = "w";
 
     public String[] readFromFile(String fileName) {
         String [] arrayResult = new String[0];
+        StringBuilder allDocument = new StringBuilder();
         try (BufferedReader reader = Files.newBufferedReader(Paths.get(fileName))) {
             String myStringBuffer = new String();
-            List<String> listResult = new ArrayList<String>();
             while ((myStringBuffer = reader.readLine()) != null) {
-                myStringBuffer = myStringBuffer.toLowerCase().replaceAll("[^a-zA-Z\\s]","");
-                String[] arrayWords = myStringBuffer.split(" ");
-                for (String tempWord: arrayWords) {
-                    if (tempWord.indexOf(FIRST_LETTER) == 0) {
-                        listResult.add(tempWord);
-                    }
-                }
-                Collections.sort(listResult);
-                arrayResult = listResult.toArray(new String[listResult.size()]);
+                allDocument.append(myStringBuffer);
             }
         } catch (IOException e) {
             throw new RuntimeException("There is no such file", e);
         }
-        return arrayResult;
+        arrayResult = allDocument
+                .toString()
+                .toLowerCase()
+                .replaceAll("[^a-z\\s]","")
+                .split(" ");
+        Arrays.sort(arrayResult);
+        return Arrays.stream(arrayResult)
+                .filter(s -> s.startsWith(FIRST_LETTER))
+                .toArray(String[]::new);
     }
 }
