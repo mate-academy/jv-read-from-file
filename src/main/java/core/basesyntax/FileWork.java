cb2c62b7ffae723b53fe04099c6fe135d20d4f6e
Andrey Tanchak <tanchak12@gmail.com>
2020-04-03 13:22:52 +0300

    added final solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -8,34 +8,32 @@ import java.util.Arrays;
 import java.util.List;
 
 public class FileWork {
+    public static final char MAGIC_NUMBER = 'w';
 
     public static String[] readFromFile(String fileName) {
         final StringBuilder sb = new StringBuilder();
         List<String> reader;
         Path filePath = Paths.get(fileName);
-        String [] split = new String[0];
+        String[] split;
         try {
             reader = Files.readAllLines(filePath);
         } catch (IOException e) {
             throw new RuntimeException("Причина в том, что " + e);
         }
-        if (reader.size() > 0) {
-            for (String a : reader) {
-                sb.append(a.toLowerCase() + " ");
-            }
-            split = sb.toString().split(" ");
-            String worlds = "";
+        for (String a : reader) {
+            split = a.toLowerCase().split(" ");
             for (String s : split) {
-                if (s.charAt(0) == ('w')) {
-                    worlds = worlds + s.replaceAll("\\W","") + " ";
+                if (s.charAt(0) == MAGIC_NUMBER) {
+                    sb.append(s.toLowerCase().replaceAll("\\W", "") + " ");
                 }
-                split = worlds.split(" ");
             }
-            if (split.length == 1) {
-                split = new String [0];
-            }
-            Arrays.sort(split);
         }
+        if (sb.length() == 0) {
+            split = new String[0];
+            return split;
+        }
+        split = sb.toString().split(" ");
+        Arrays.sort(split);
         return split;
     }
 }
