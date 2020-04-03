a228f36626758641140fa36fcb970245aa34a51d
Andrew <tolstuy1996@gmail.com>
2020-04-03 10:49:08 +0300

    done

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,5 +1,10 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * <p>Дано файл, потрібно прочитати його вміст і вибрати всі слова що починаються на `w`.
  * Результат повернути у вигляді відсортованого масиву (за замовчуванням). Всі слова повинні
@@ -9,6 +14,24 @@ package core.basesyntax;
  */
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        return null;
+        StringBuilder words = new StringBuilder();
+        try (Scanner scan = new Scanner(new File(fileName))) {
+            while (scan.hasNext()) {
+                String word = scan.next().toLowerCase();
+                if (word.startsWith("w")) {
+                    words.append(word.replaceAll("[^a-z]", "")).append(" ");
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("file not found");
+            e.printStackTrace();
+        }
+
+        if (words.length() <= 0) {
+            return new String[0];
+        }
+        String[] text = words.toString().trim().split(" ");
+        Arrays.sort(text);
+        return text;
     }
 }
