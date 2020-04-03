93bf811c0cedf8b3d79fa02e4751bde19f2476d6
Vitamin-oht <61590116+Vitamin-oht@users.noreply.github.com>
2020-04-03 13:37:31 +0300

    Array stars w created

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,5 +1,10 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
 /**
  * <p>Дано файл, потрібно прочитати його вміст і вибрати всі слова що починаються на `w`.
  * Результат повернути у вигляді відсортованого масиву (за замовчуванням). Всі слова повинні
@@ -9,6 +14,23 @@ package core.basesyntax;
  */
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        return null;
+        String[] wordsStartWithW;
+        try {
+            String lineFileName = Files.readString(Paths.get(fileName));
+            String[] arrayFile = lineFileName.toLowerCase().split(" ");
+            StringBuilder builder = new StringBuilder();
+            for (String temp : arrayFile) {
+                if (temp.toLowerCase().startsWith("w")) {
+                    builder.append(temp.replaceAll("\\W", "")).append(" ");
+                }
+            }
+            wordsStartWithW = builder.toString().split(" ");
+            Arrays.sort(wordsStartWithW);
+
+            return builder.length() > 0 ? wordsStartWithW : new String[0];
+        } catch (IOException e) {
+            throw new RuntimeException("No such file", e);
+        }
+
     }
 }
