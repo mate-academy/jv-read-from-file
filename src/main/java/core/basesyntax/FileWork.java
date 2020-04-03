65c58ef31c6d206ae94aaf140ddf14b8515bd7c4
VovkAA <vovkandrew@gmail.com>
2020-04-03 16:24:12 +0300

    Done PS I could make some unintentional corrections at the Test file, sorry, tried to fine out why it asked me to throw exceptions

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,5 +1,13 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * <p>Дано файл, потрібно прочитати його вміст і вибрати всі слова що починаються на `w`.
  * Результат повернути у вигляді відсортованого масиву (за замовчуванням). Всі слова повинні
@@ -9,6 +17,26 @@ package core.basesyntax;
  */
 public class FileWork {
     public String[] readFromFile(String fileName) {
+        try {
+            Path filePath = Paths.get(fileName);
+            List<String> listOfAllLines = Files.readAllLines(filePath);
+            StringBuilder readText = new StringBuilder();
+            for (String i : listOfAllLines) {
+                readText.append(i).append(" ");
+            }
+            String newText = readText.toString().trim().toLowerCase().replaceAll("[^a-z]", " ");
+            String[] split = newText.split(" ");
+            ArrayList<String> listOfWords = new ArrayList<>();
+            for (String i : split) {
+                if (i.startsWith("w")) {
+                    listOfWords.add(i);
+                }
+            }
+            Collections.sort(listOfWords);
+            return listOfWords.toArray(new String[0]);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
