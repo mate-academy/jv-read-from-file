d418f25844cce97148a874955919a242b90a3bc5
olehedza <olehedza@gmail.com>
2020-04-03 16:12:11 +0300

    solution with stream & StringBuilder

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,5 +1,12 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * <p>Дано файл, потрібно прочитати його вміст і вибрати всі слова що починаються на `w`.
  * Результат повернути у вигляді відсортованого масиву (за замовчуванням). Всі слова повинні
@@ -8,7 +15,25 @@ package core.basesyntax;
  * Результат: web wide width world</p>
  */
 public class FileWork {
-    public String[] readFromFile(String fileName) {
-        return null;
+
+    public static final String STARTS_WITH = "w";
+
+    public static String[] readFromFile(String fileName) {
+        StringBuilder result = new StringBuilder();
+
+        try (Stream<String> lines = Files.lines(Paths.get(fileName))) {
+            lines.forEach(line -> {
+                Arrays.stream(line.split("\\s+"))
+                        .map(String::toLowerCase)
+                        .filter(w -> w.startsWith(STARTS_WITH))
+                        .map(w -> w.replaceAll("\\W", ""))
+                        .forEach(w -> result.append(w).append(" "));
+            });
+            return result.length() == 0 ? new String[] {}
+                    : Arrays.stream(result.toString().split(" "))
+                    .sorted().collect(Collectors.toList()).toArray(String[]::new);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
