7a42cc4a720c32c0ab6d5aff2e0a660fd1ed71c8
Maksym Vakuliuk <vakuliukmaksym@mail.com>
2020-04-03 14:25:28 +0300

    implenented readFromFile(fileName)

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,5 +1,12 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * <p>Дано файл, потрібно прочитати його вміст і вибрати всі слова що починаються на `w`.
  * Результат повернути у вигляді відсортованого масиву (за замовчуванням). Всі слова повинні
@@ -8,7 +15,22 @@ package core.basesyntax;
  * Результат: web wide width world</p>
  */
 public class FileWork {
+
+    private static final String WORDS_START_WITH = "w";
+
     public String[] readFromFile(String fileName) {
-        return null;
+        Stream<String> allLines = null;
+        try {
+            allLines = Files.lines(Paths.get(fileName));
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+
+        return Arrays.stream(allLines.collect(Collectors.joining(" ")).split(" "))
+                .map(String::toLowerCase)
+                .filter(word -> word.startsWith(WORDS_START_WITH))
+                .map(word -> word.replaceAll("\\W", ""))
+                .sorted()
+                .toArray(String[]::new);
     }
 }
