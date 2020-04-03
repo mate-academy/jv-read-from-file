105e743e156520bd07b1991b4f2b251f4216d147
NikolayAbysov <nikolay.abysov@ukr.net>
2020-04-03 10:52:47 +0300

    Fixed issues: 1 String[] arr changed to String[] words 2 CHAR_MASK changed to String and .startsWith method is used

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -16,18 +16,18 @@ import java.util.List;
  */
 public class FileWork {
 
-    private static final char CHAR_MASK = 'w';
+    private static final String CHAR_MASK = "w";
 
     public String[] readFromFile(String fileName) {
-        String[] arr;
+        String[] words;
         try {
-            arr = Files.readString(Path.of(fileName)).toLowerCase().split("\\W+");
+            words = Files.readString(Path.of(fileName)).toLowerCase().split("\\W+");
         } catch (IOException e) {
             throw new RuntimeException("No file");
         }
         List<String> list = new ArrayList<>();
-        for (String s : arr) {
-            if (s.indexOf(CHAR_MASK) == 0) {
+        for (String s : words) {
+            if (s.startsWith(CHAR_MASK)) {
                 list.add(s);
             }
         }
