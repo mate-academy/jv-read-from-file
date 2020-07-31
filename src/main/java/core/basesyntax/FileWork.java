0749beb3f4bb049dbbc2553c185648ccec6f1bcf
Your Name <i.dymytreyeva@gmail.com>
2020-07-31 16:24:37 +0300

    a small fix added

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -2,7 +2,9 @@ package core.basesyntax;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * <p>Дано файл, потрібно прочитати його вміст і вибрати всі слова що починаються на `w`.
@@ -14,14 +16,14 @@ import java.util.*;
 public class FileWork {
     public String[] readFromFile(String fileName) {
         File file = new File(fileName);
-        ArrayList <String> textFromFile = new ArrayList<>();
-        final char STARTSWITH = 'w';
+        ArrayList<String> textFromFile = new ArrayList<>();
+        final char StartsWith = 'w';
         try {
             Scanner sc = new Scanner(file);
             while (sc.hasNextLine()) {
                 String[] words = sc.nextLine().toLowerCase().split(" ");
                 for (int i = 0; i < words.length; i++) {
-                    if (words[i].startsWith(String.valueOf(STARTSWITH))) {
+                    if (words[i].startsWith(String.valueOf(StartsWith))) {
                         textFromFile.add(words[i].replaceAll("[, '!.)?-]", ""));
                     }
                 }
