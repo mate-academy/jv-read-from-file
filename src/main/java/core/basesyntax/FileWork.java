a717bc305053a31025fd4670aab68007deed948d
tkhambir <tatiana.khambir@gmail.com>
2020-07-31 22:09:11 +0300

    homework 5.3 solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,5 +1,12 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * <p>Дано файл, потрібно прочитати його вміст і вибрати всі слова що починаються на `w`.
  * Результат повернути у вигляді відсортованого масиву (за замовчуванням). Всі слова повинні
@@ -9,6 +16,23 @@ package core.basesyntax;
  */
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        return null;
+        List<String> text = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            while (br.ready()) {
+                String[] allText = br.readLine().split(" ");
+
+                for (String s : allText) {
+                    if (s.startsWith("w") || s.startsWith("W")) {
+                        text.add(s.toLowerCase().replaceAll("[^a-z]", ""));
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+
+        Collections.sort(text);
+        return text.toArray(new String[0]);
     }
 }
