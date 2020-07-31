52c8e27ea9125068615e2535d7026515601e2dd3
ROMASHSX <andriiromash@gmail.com>
2020-07-31 14:45:11 +0300

    Task 3 is done

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,14 +1,32 @@
 package core.basesyntax;
 
-/**
- * <p>Дано файл, потрібно прочитати його вміст і вибрати всі слова що починаються на `w`.
- * Результат повернути у вигляді відсортованого масиву (за замовчуванням). Всі слова повинні
- * бути в нижньому регістрі. У випадку якщо таких слів не знайдено повернути пустий масив.
- * Приклад: Width world Wide web
- * Результат: web wide width world</p>
- */
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        return null;
+        List<String> result = new ArrayList<>();
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(fileName));
+            for (String line : lines) {
+                line = line.toLowerCase();
+                line = line.replaceAll("[^a-z]", " ");
+                String[] separateWords = line.split("\\s+");
+                for (String word : separateWords) {
+                    if (word.startsWith("w")) {
+                        result.add(word);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("No such a file");
+        }
+        String[] newResult = result.toArray(new String[0]);
+        Arrays.sort(newResult);
+        return newResult;
     }
 }
