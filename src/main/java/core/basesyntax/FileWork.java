b9df19ab853701a4d67876a324524d7f0c88557b
Vlad Kuzmin <vladkuzmin22@gmail.com>
2020-07-31 17:00:59 +0300

    hw5-3_done

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,5 +1,11 @@
 package core.basesyntax;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * <p>Дано файл, потрібно прочитати його вміст і вибрати всі слова що починаються на `w`.
  * Результат повернути у вигляді відсортованого масиву (за замовчуванням). Всі слова повинні
@@ -7,8 +13,40 @@ package core.basesyntax;
  * Приклад: Width world Wide web
  * Результат: web wide width world</p>
  */
+
 public class FileWork {
-    public String[] readFromFile(String fileName) {
-        return null;
+    public static final char LETTER = 'w';
+
+    public static String[] readFromFile(String fileName) {
+        try (FileReader fileReader = new FileReader(fileName)) {
+            Scanner scanner = new Scanner(fileReader);
+
+            String text = "";
+            while (scanner.hasNextLine()) {
+                text += scanner.nextLine() + " ";
+            }
+            String[] words = text.split(" ");
+            String resultString = "";
+
+            int index = 0;
+            for (int i = 0; i < words.length; i++) {
+                if (words.length > 1) {
+                    if (words[i].toLowerCase().charAt(0) == LETTER) {
+                        resultString += words[i].toLowerCase().replaceAll("[.), \\-'!?]", "") + " ";
+                    }
+                } else {
+                    return new String[0];
+                }
+            }
+            String[] result = resultString.trim().split(" ");
+            Arrays.sort(result);
+
+            return result[0] != "" ? result : new String[0];
+        } catch (FileNotFoundException e) {
+            System.out.println("File with name " + fileName + " not found!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new String[0];
     }
 }
