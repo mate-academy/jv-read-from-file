399f378f40a1da42342faf855b8852edfdb76ef0
Alkey <Sirj777sirj@gmail.com>
2020-07-31 20:26:01 +0300

    homework5.3

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,5 +1,12 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
 /**
  * <p>Дано файл, потрібно прочитати його вміст і вибрати всі слова що починаються на `w`.
  * Результат повернути у вигляді відсортованого масиву (за замовчуванням). Всі слова повинні
@@ -8,7 +15,40 @@ package core.basesyntax;
  * Результат: web wide width world</p>
  */
 public class FileWork {
+
+    private static final String CONSTANTECHAR = "w";
+
     public String[] readFromFile(String fileName) {
-        return null;
+        StringBuilder builder = new StringBuilder();
+        try {
+            Path paths = Paths.get(fileName);
+            File file = paths.toFile();
+            InputStream inputStream = new FileInputStream(file);
+
+            while (inputStream.available() > 0) {
+                char tmp = (char) inputStream.read();
+                builder.append(tmp);
+            }
+            inputStream.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        String[] words = builder.toString().toLowerCase().split(" ");
+        int count = 0;
+        for (String word : words) {
+            if (word.startsWith(CONSTANTECHAR)) {
+                ++count;
+            }
+        }
+        String[] result = new String[count];
+        count = 0;
+        for (String word : words) {
+            if (word.startsWith(CONSTANTECHAR)) {
+                result[count] = word.replaceAll("\\W", "");
+                ++count;
+            }
+        }
+        Arrays.sort(result);
+        return result;
     }
 }
