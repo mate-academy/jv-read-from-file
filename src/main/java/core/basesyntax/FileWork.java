72e58af5131ac50e04c21e752621effd98ff4a3d
Borisko130 <krimenyukb@gmail.com>
2020-08-01 16:48:30 +0300

    Homework 5.3 completed

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * <p>Дано файл, потрібно прочитати його вміст і вибрати всі слова що починаються на `w`.
  * Результат повернути у вигляді відсортованого масиву (за замовчуванням). Всі слова повинні
@@ -8,7 +17,30 @@ package core.basesyntax;
  * Результат: web wide width world</p>
  */
 public class FileWork {
+
+    static final char WORDS_START_FROM = 'w';
+
     public String[] readFromFile(String fileName) {
-        return null;
+        Path path = Paths.get(fileName);
+        List<String> selectedWords = new ArrayList<String>();
+
+        try (BufferedReader reader = Files.newBufferedReader(path)) {
+            String buffer;
+            while ((buffer = reader.readLine()) != null) {
+                for (String s : buffer.split(" ")) {
+                    if (s.toLowerCase().charAt(0) == WORDS_START_FROM) {
+                        selectedWords.add(s.toLowerCase().replaceAll("[^a-z]", ""));
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("No such file");
+        }
+
+        if (selectedWords.size() == 0) {
+            return new String[0];
+        }
+        selectedWords.sort(Comparator.naturalOrder());
+        return selectedWords.toArray(new String[0]);
     }
 }
