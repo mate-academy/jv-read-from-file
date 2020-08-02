117c7d34e8d78c368f9266b05057e20bd6ad27cb
aleksandr.hrankin <aleksandr.hrankin@gmail.com>
2020-08-02 20:34:45 +0300

    added solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * <p>Дано файл, потрібно прочитати його вміст і вибрати всі слова що починаються на `w`.
  * Результат повернути у вигляді відсортованого масиву (за замовчуванням). Всі слова повинні
@@ -8,7 +17,28 @@ package core.basesyntax;
  * Результат: web wide width world</p>
  */
 public class FileWork {
-    public String[] readFromFile(String fileName) {
-        return null;
+
+    private static final String SEARCH_WORD = "\\b[w]\\w*\\b";
+
+    public static String[] readFromFile(String fileName) {
+        List<String> wordList = new ArrayList<>();
+        Pattern pattern = Pattern.compile(SEARCH_WORD);
+        Matcher matcher;
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                matcher = pattern.matcher(line.toLowerCase());
+                while (matcher.find()) {
+                    wordList.add(matcher.group());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String[] words = wordList.toArray(new String[0]);
+        Arrays.sort(words);
+
+        return words;
     }
 }
