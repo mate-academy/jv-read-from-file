947b72c80203e3b81c65e5fbd644bfbb4726028b
YaTut1901 <59802427+YaTut1901@users.noreply.github.com>
2020-08-03 12:31:02 +0300

    hw

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,5 +1,12 @@
 package core.basesyntax;
 
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * <p>Дано файл, потрібно прочитати його вміст і вибрати всі слова що починаються на `w`.
  * Результат повернути у вигляді відсортованого масиву (за замовчуванням). Всі слова повинні
@@ -9,6 +16,25 @@ package core.basesyntax;
  */
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        return null;
+        final char W = 'w';
+        ArrayList<String> wordsWithWList = new ArrayList<String>();
+        String text = "";
+        try {
+            text = new String(Files.readAllBytes((Paths.get(fileName)))).toLowerCase().replaceAll(
+                    "[^A-Za-zА-Яа-я ]", "");
+        } catch (IOException e) {
+            System.out.println("Такого файла не существует");
+        }
+        if (text.length() == 0) {
+            return new String[0];
+        }
+        for (String word : text.split(" ")) {
+            if(word.charAt(0) == W) {
+                wordsWithWList.add(word);
+            }
+        }
+        String[] wordsWithWArray = wordsWithWList.toArray(String[]::new);
+        Arrays.sort(wordsWithWArray);
+        return wordsWithWArray;
     }
 }
