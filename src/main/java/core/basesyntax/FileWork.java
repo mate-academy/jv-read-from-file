16eb04a318b6470bc02ac8a8102d30e123fa17f1
romanHreits <romagreic@gmail.com>
2020-08-03 12:17:50 +0300

    aded simpler solution hw

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -4,9 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 /**
  * <p>Дано файл, потрібно прочитати його вміст і вибрати всі слова що починаються на `w`.
@@ -18,31 +16,15 @@ import java.util.List;
 public class FileWork {
     public String[] readFromFile(String fileName) {
         Path pathToFile = Paths.get(fileName);
-        List<String> stringsFromFile = new ArrayList<>();
         if (Files.exists(pathToFile) & Files.isReadable(pathToFile)) {
             try {
-                Files.lines(pathToFile)
-                        .map(x -> Arrays.stream(x.split("\\W"))
-                                .map(String::toLowerCase)
-                                .filter(z -> {
-                                    if (z.startsWith("w")) {
-                                        stringsFromFile.add(z);
-                                        return true;
-                                    }
-                                    return false;
-                                }).toArray(String[]::new)).count();
-                return arrayCreator(stringsFromFile);
+                return Arrays.stream(Files.readString(pathToFile)
+                        .split("\\W")).map(String::toLowerCase)
+                        .filter(x -> x.startsWith("w")).sorted().toArray(String[]::new);
             } catch (IOException e) {
                 throw new RuntimeException("Exception when reading a file !!!");
             }
         }
         return new String[0];
-    }
-
-    private String[] arrayCreator(List<String> list) {
-        String[] stringStartWithW = new String[list.size()];
-        list.toArray(stringStartWithW);
-        Arrays.sort(stringStartWithW);
-        return stringStartWithW;
     }
 }
