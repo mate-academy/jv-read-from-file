e010a5f0fc8385dc0105e25a3a4eff07bb69e366
Vitalik Kopach <apple_air@ukr.net>
2020-12-05 00:18:49 +0200

    implement method readFromFile

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,7 +1,33 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class FileWork {
+
+    private static final String SPECIFIED_CHARACTER = "w";
+
     public String[] readFromFile(String fileName) {
-        return null;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));) {
+            List<String> arrayList = new ArrayList<>();
+            String line = null;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] split = line.split(" ");
+                for (String s : split) {
+                    if (s.toLowerCase().startsWith(SPECIFIED_CHARACTER)) {
+                        arrayList.add(s.replaceAll("\\W", "").toLowerCase());
+                    }
+                }
+            }
+            String[] resultArray = arrayList.toArray(new String[arrayList.size()]);
+            Arrays.sort(resultArray);
+            return resultArray;
+        } catch (IOException e) {
+            throw new RuntimeException("");
+        }
     }
 }
