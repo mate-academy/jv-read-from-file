83ba2ac62d37ced140c6aa2a06ab512d325bf59c
Yura <yuramitryahin@gmail.com>
2020-12-04 17:01:32 +0200

    Fixed bags

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,7 +1,6 @@
 package core.basesyntax;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -12,29 +11,28 @@ public class FileWork {
 
     public String[] readFromFile(String fileName) {
         StringBuilder allText = new StringBuilder();
-        File file = new File(fileName);
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
             String line;
             while ((line = bufferedReader.readLine()) != null) {
                 allText.append(line).append(" ");
             }
         } catch (FileNotFoundException e) {
-            new RuntimeException("Can't find file with this name!", e);
+            throw new RuntimeException("Can't find file with this name!", e);
         } catch (IOException e) {
-            new RuntimeException("Error with reading a file!", e);
+            throw new RuntimeException("Error with reading a file!", e);
         }
         return findWordsWithCharacterW(allText.toString());
     }
 
     private String[] findWordsWithCharacterW(String allText) {
         StringBuilder lineWordsWithCharW = new StringBuilder();
-        for (String word : allText.replaceAll("[!?,.]", "").toLowerCase().split(" ")) {
+        for (String word : allText.toLowerCase().split("\\W+")) {
             if (word.startsWith(CHARACTER_W)) {
                 lineWordsWithCharW.append(word).append(" ");
             }
         }
         String[] wordsWithCharW = new String[0];
-        if (lineWordsWithCharW.length() != 0) {
+        if (!lineWordsWithCharW.toString().isEmpty()) {
             wordsWithCharW = lineWordsWithCharW.toString().split(" ");
             Arrays.sort(wordsWithCharW);
         }
