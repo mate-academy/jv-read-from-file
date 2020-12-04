b10dfde98cfafd68d3517491ae5bded654308851
Tonitrus <grom4er@gmail.com>
2020-12-04 16:49:57 +0200

    Complete and test pass mvn package pass

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,38 @@
 package core.basesyntax;
 
-public class FileWork {
-    public String[] readFromFile(String fileName) {
-        return null;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
+public class FileWork {
+    private static final char SYMBOL_LOWER_CASE = 'w';
+    private static final char SYMBOL_UPPER_CASE = 'W';
+
+    public String[] readFromFile(String fileName) {
+        List<String> list = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            while (reader.ready()) {
+                String[] line = reader.readLine().split(" ");
+                for (String value : line) {
+                    if (value.charAt(0) == SYMBOL_LOWER_CASE
+                            || value.charAt(0) == SYMBOL_UPPER_CASE) {
+                        list.add(value.chars().filter(Character::isLetter)
+                                .mapToObj(Character::toString).collect(Collectors.joining())
+                                .toLowerCase());
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("File not found. Try again", e);
+        } catch (IOException e) {
+            throw new RuntimeException("Some problem with file. Try again", e);
+        }
+        Collections.sort(list);
+        return list.toArray(new String[0]);
     }
 }
