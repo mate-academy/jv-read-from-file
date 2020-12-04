df1c1129086b9c5abbb2cff4a1d9d0c794be5797
Serhii Semenykhin <semyonich@users.noreply.github.com>
2020-12-04 17:09:26 +0200

    all warnings fixed

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -18,20 +18,17 @@ public class FileWork {
             while (newLine != null) {
                 String[] lineWords = newLine.toLowerCase().split("\\W+");
                 for (String item : lineWords) {
-                    if (item.toLowerCase().charAt(0) == FIRST_CHARACTER_TO_FILTER) {
+                    if (item.charAt(0) == FIRST_CHARACTER_TO_FILTER) {
                         stringBuilder.append(item.toLowerCase()).append(" ");
                     }
                 }
                 newLine = bufferedReader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't read from file", e);
-        } finally {
-            String[] output = stringBuilder.toString().split(" ");
-            Arrays.sort(output);
-            System.out.println(output);
-            return (output[0].equals("")) ? new String[] {} : output;
+            throw new RuntimeException("Can't read from file:" + fileName, e);
         }
-
+        String[] output = stringBuilder.toString().split(" ");
+        Arrays.sort(output);
+        return (output[0].isEmpty()) ? new String[] {} : output;
     }
 }
