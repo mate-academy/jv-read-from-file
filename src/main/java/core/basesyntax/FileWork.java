a2222e79eb2a508da87a27d44d8e8e647d2683ad
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-04 18:05:16 +0200

    Solution for readFromFile added

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,7 +1,30 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.List;
+
 public class FileWork {
+
     public String[] readFromFile(String fileName) {
-        return null;
+        File fileToRead = new File(fileName);
+        List<String> readData = null;
+        if (fileToRead.exists()) {
+            try {
+                readData = Files.readAllLines(fileToRead.toPath());
+            } catch (IOException e) {
+                throw new RuntimeException("I wasn't able to read the file", e);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (String line : readData) {
+            sb.append(line).append(" ");
+        }
+        String[] finalArray = sb.toString().trim().toLowerCase()
+                .replaceAll("[^ a-zA-Z]", "").split(" ");
+        return Arrays.stream(finalArray).sorted()
+                .filter(i -> i.startsWith("w")).toArray(String[]::new);
     }
 }
