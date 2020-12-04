db6411aa2602d70ff050572ea37d3e85a886df1a
Okulov Vadim <Vedroidua@gmail.com>
2020-12-04 21:16:50 +0200

    Fix

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -4,36 +4,29 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class FileWork {
-    public static final String SYMBOL_W = "w";
-    public static final String SYMBOL_W_UPPER_CASE = "W";
-    
     public String[] readFromFile(String fileName) {
         StringBuilder words = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-            StringBuilder word = new StringBuilder();
-            int symbol = reader.read();
-            while (symbol > 0) {
-                if (Character.isAlphabetic(symbol)) {
-                    word.append((char) symbol);
-                } else {
-                    if (word.toString().startsWith(SYMBOL_W_UPPER_CASE)
-                            || word.toString().startsWith(SYMBOL_W)) {
-                        words.append(word).append(" ");
-                    }
-                    word.setLength(0);
+            String line = reader.readLine();
+            while (line != null) {
+                Matcher matcher = Pattern.compile("\\b[w][a-z]*").matcher(line.toLowerCase());
+                while (matcher.find()) {
+                    words.append(matcher.group()).append(" ");
                 }
-                symbol = reader.read();
+                line = reader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        String result = words.toString();
-        if (result.equals("")) {
+        String result = words.toString().trim();
+        if (result.isEmpty()) {
             return new String[]{};
         }
-        String[] resultArray = result.toLowerCase().split(" ");
+        String[] resultArray = result.split(" ");
         Arrays.sort(resultArray);
         return resultArray;
     }
