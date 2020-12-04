8c9374d4616508ca7e64efdcbbf59208053e18e4
bogdan <b.kostevich@gmail.com>
2020-12-04 15:11:46 +0200

    Realized array which looking for each word with first letter as "W"/"w".

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,7 +1,40 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class FileWork {
+    private List<String> words = new ArrayList<>();
+
     public String[] readFromFile(String fileName) {
-        return null;
+        File file = new File(new File(fileName).getAbsolutePath());
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                String[] arrOfWords = value.split(" ");
+                for (int i = 0; i < arrOfWords.length; i++) {
+                    if (arrOfWords[i].charAt(0) == 'w' || arrOfWords[i].charAt(0) == 'W') {
+                        words.add(arrOfWords[i].toLowerCase().replaceAll("\\W", ""));
+                    }
+                }
+                value = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read the file", e);
+        }
+
+        Collections.sort(words);
+
+        String[] finalResult = new String[words.size()];
+        for (int i = 0; i < finalResult.length; i++) {
+            finalResult[i] = words.get(i);
+        }
+        return finalResult;
     }
 }
