f0739f3f3aeb8c6a6bcae2ec82d9f725e2bfd487
Volodymyr Mazuryk <mzrkvolodymyr@gmail.com>
2020-12-05 20:08:43 +0200

    add solution (all tests passed);

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,7 +1,36 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String SPECIFIED_CHARACTER = "w";
+
     public String[] readFromFile(String fileName) {
-        return null;
+        File file = new File(fileName);
+        ArrayList<String> arrayList = new ArrayList<>();
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            StringBuilder stringBuilder = new StringBuilder();
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                stringBuilder.append(value).append(" ");
+                value = bufferedReader.readLine();
+            }
+            String[] allWords = stringBuilder.toString().toLowerCase().split(" ");
+            for (String word: allWords) {
+                if (word.startsWith(SPECIFIED_CHARACTER)) {
+                    arrayList.add(word.replaceAll("[.?!]", ""));
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        String[] result = arrayList.toArray(new String[arrayList.size()]);
+        Arrays.sort(result);
+        return result;
     }
 }
