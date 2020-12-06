1eca91ec82be6f62aa1ec8ea1b5a0802288da132
RuslanIbIsh <ruslan8ishankulov@gmail.com>
2020-12-06 12:42:20 +0200

    first commit

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,7 +1,41 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class FileWork {
+    private static final String STRING_START_WITH = "w";
+
     public String[] readFromFile(String fileName) {
-        return null;
+        File file = new File(fileName);
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            StringBuilder stringBuilder = new StringBuilder();
+            String lineSeparator = System.getProperty("line.separator");
+            String lineFromFile = bufferedReader.readLine();
+            while (lineFromFile != null) {
+                stringBuilder.append(lineFromFile).append(lineSeparator);
+                lineFromFile = bufferedReader.readLine();
+            }
+            String[] splitedStringBuilderResult = stringBuilder
+                    .toString()
+                    .toLowerCase()
+                    .split(" |\\.|,|:|;|'|!|\\?|\r|\n");
+            List<String> wordsList = new ArrayList<>();
+            for (String eachWord : splitedStringBuilderResult) {
+                if (eachWord.startsWith(STRING_START_WITH)) {
+                    wordsList.add(eachWord);
+                }
+            }
+            Collections.sort(wordsList);
+            String[] sortedWords = new String[wordsList.size()];
+            return wordsList.toArray(sortedWords);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
     }
 }
