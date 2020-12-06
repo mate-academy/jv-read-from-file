f00ed7dd195fc2fb877a01a527b7d09af2825f2f
KostyantynProskura <kostyantyn.proskura@gmail.com>
2020-12-06 19:35:49 +0200

    1) Commit

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,7 +1,48 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.List;
+
 public class FileWork {
+    private static final String SPECIFIED_CHARACTER = "w";
+
     public String[] readFromFile(String fileName) {
-        return null;
+        File file = new File(fileName);
+        try {
+            List<String> listOfWords = Files.readAllLines(new File(fileName).toPath());
+            return wordsFromListLowerCase(listOfWords);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file " + fileName, e);
+        }
+    }
+
+    private String[] wordsFromListLowerCase(List<String> listOfWords) {
+        StringBuilder textBuilder = new StringBuilder();
+
+        for (String word : listOfWords) {
+            textBuilder.append(word.toLowerCase()).append(System.lineSeparator());
+            if (textBuilder.length() <= 0) {
+                return new String[0];
+            }
+        }
+
+        String[] wordsArray = textBuilder.toString().toLowerCase().split("[^\\w]");
+        textBuilder = new StringBuilder();
+
+        for (String word : wordsArray) {
+            if (startWithLetter(word)) {
+                textBuilder.append(word).append(" ");
+            }
+        }
+        wordsArray = textBuilder.toString().split(" ");
+        Arrays.sort(wordsArray);
+        return wordsArray.length == 1 ? new String[0] : wordsArray;
+    }
+
+    public boolean startWithLetter(String word) {
+        return word.startsWith(SPECIFIED_CHARACTER);
     }
 }
