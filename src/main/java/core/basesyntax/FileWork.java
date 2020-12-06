a59b6bf4fed70c2f3cb103c03e799bca3468a6ac
Ivan Marchenko <51205960+IwalkInSilence@users.noreply.github.com>
2020-12-06 20:04:16 +0200

    Implement readFromFile method

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,7 +1,38 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String MY_CHARACTER = "w";
+    private static final String DELIMITERS = "\\s*[^a-zA-Z]+\\s*";
+
     public String[] readFromFile(String fileName) {
-        return null;
+        StringBuilder allText = new StringBuilder();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            String value = reader.readLine();
+            while (value != null) {
+                allText.append(value).append(" ");
+                value = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file!");
+        }
+        String[] words = allText.toString().toLowerCase().split(DELIMITERS);
+        StringBuilder myWords = new StringBuilder();
+        for (String word : words) {
+            if (word.startsWith(MY_CHARACTER)) {
+                myWords.append(word).append(" ");
+            }
+        }
+        if (myWords.length() == 0) {
+            return new String[]{};
+        }
+        String[] wordsStartedOnMyCharacter = myWords.toString().split(DELIMITERS);
+        Arrays.sort(wordsStartedOnMyCharacter);
+        return wordsStartedOnMyCharacter;
     }
 }
