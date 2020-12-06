4e083643d67e49164c1abad636958e0617116800
egorka <entertainment.veks@gmail.com>
2020-12-06 17:03:01 +0200

    yegor sereda 1st commit

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,7 +1,42 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        return null;
+
+        File file = new File(fileName);
+        List<String> data;
+        List<String> returnStrings = new ArrayList<>();
+        try {
+            data = Files.readAllLines(file.toPath());
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+
+        for (int i = 0; i < data.size(); i++) {
+            String[] strings = data.get(i).split(" ");
+            for (String str : strings) {
+                if (str.charAt(0) == 'w' || str.charAt(0) == 'W') {
+                    int lastSymbol = str.length() - 1;
+                    if (str.charAt(lastSymbol) == '!'
+                            || str.charAt(lastSymbol) == '.'
+                            || str.charAt(lastSymbol) == '?'
+                            || str.charAt(lastSymbol) == ',') {
+                        returnStrings.add(str.toLowerCase().substring(0, lastSymbol));
+                    } else {
+                        returnStrings.add(str.toLowerCase());
+                    }
+                }
+            }
+        }
+
+        returnStrings = returnStrings.stream().sorted().collect(Collectors.toList());
+        return returnStrings.toArray(new String[0]);
     }
 }
