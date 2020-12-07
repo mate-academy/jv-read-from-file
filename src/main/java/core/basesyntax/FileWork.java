f2d8d957b913090ffb04e6632fdada0717ab58f4
Yaroslav <yaroslav8oroboro@gmail.com>
2020-12-07 11:22:07 +0200

    added corrections

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,7 +1,6 @@
 package core.basesyntax;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -10,18 +9,15 @@ import java.util.List;
 
 public class FileWork {
     private static final String SPECIFIED_CHARACTER_LOWER_CASE = "w";
-    private static final String SPECIFIED_CHARACTER_UPPER_CASE = "W";
-    private List<String> notFormattedWords = new ArrayList<>();
 
-    public String[] readFromFile(String fileName) throws FileNotFoundException {
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+    public String[] readFromFile(String fileName) {
+        List<String> notFormattedWords = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String value = reader.readLine();
             while (value != null) {
-                String[] notSortedList = value.split(" ");
+                String[] notSortedList = value.toLowerCase().split(" ");
                 for (String word : notSortedList) {
-                    if (word.startsWith(SPECIFIED_CHARACTER_LOWER_CASE)
-                            || word.startsWith(SPECIFIED_CHARACTER_UPPER_CASE)) {
+                    if (word.startsWith(SPECIFIED_CHARACTER_LOWER_CASE)) {
                         notFormattedWords.add(word.toLowerCase().replaceAll("\\W", ""));
                     }
                 }
