a3ced9f48312ba6d172a004e5d537d91cacdb43d
Tonitrus <grom4er@gmail.com>
2020-12-07 11:00:41 +0200

    Fix: change logix to find first symbol. Now only one check. Delete not use field

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -7,20 +7,18 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 public class FileWork {
     private static final char SYMBOL_LOWER_CASE = 'w';
-    private static final char SYMBOL_UPPER_CASE = 'W';
-
+    
     public String[] readFromFile(String fileName) {
         List<String> list = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             while (reader.ready()) {
                 String[] line = reader.readLine().split(" ");
                 for (String value : line) {
-                    if (value.charAt(0) == SYMBOL_LOWER_CASE
-                            || value.charAt(0) == SYMBOL_UPPER_CASE) {
+                    if (Character.toLowerCase(value.charAt(0)) == SYMBOL_LOWER_CASE) {
                         list.add(value.replaceAll("[^A-z]","").toLowerCase());
                     }
                 }
