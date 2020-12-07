615b5916c29623ab07d6160dbd055d4a45060f53
Andrii Bardin <andrii.bardin@gmail.com>
2020-12-07 15:55:22 +0200

    created logic, that reads from file and returns result

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,7 +1,39 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        return null;
+        String[] empty = new String[]{};
+        StringBuilder readStrings = new StringBuilder();
+        File test = new File(fileName);
+
+        if (test.length() == 0) {
+            return empty;
+        } else {
+            try {
+                List<String> lines = Files.readAllLines(Path.of(fileName));
+                String[] text = lines.toArray(new String[0]);
+                for (String line : text) {
+                    String[] temp = line.split(" ");
+                    for (String word : temp) {
+                        if (word.toLowerCase().startsWith("w")) {
+                            readStrings.append(word).append(",");
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                throw new RuntimeException("Can't read file");
+            }
+            String[] result = Stream.of(readStrings.toString().toLowerCase()
+                    .replaceAll("[^a-z,]", "")
+                    .split(",")).sorted().toArray(String[]::new);
+            return (readStrings.length() == 0) ? empty : result;
+        }
     }
 }
