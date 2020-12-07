1fc6f39ae7dab028c142d244b87c7c8f6e757486
Andrii Bardin <andrii.bardin@gmail.com>
2020-12-07 17:02:24 +0200

    removed redundant code, fixer error messages

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,6 +1,5 @@
 package core.basesyntax;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -8,32 +7,29 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class FileWork {
+    private static final String WORDS_START = "w";
+
     public String[] readFromFile(String fileName) {
         String[] empty = new String[]{};
         StringBuilder readStrings = new StringBuilder();
-        File test = new File(fileName);
 
-        if (test.length() == 0) {
-            return empty;
-        } else {
-            try {
-                List<String> lines = Files.readAllLines(Path.of(fileName));
-                String[] text = lines.toArray(new String[0]);
-                for (String line : text) {
-                    String[] temp = line.split(" ");
-                    for (String word : temp) {
-                        if (word.toLowerCase().startsWith("w")) {
-                            readStrings.append(word).append(",");
-                        }
+        try {
+            List<String> lines = Files.readAllLines(Path.of(fileName));
+            String[] text = lines.toArray(new String[0]);
+            for (String line : text) {
+                String[] temp = line.split(" ");
+                for (String word : temp) {
+                    if (word.toLowerCase().startsWith(WORDS_START)) {
+                        readStrings.append(word).append(",");
                     }
                 }
-            } catch (IOException e) {
-                throw new RuntimeException("Can't read file");
             }
-            String[] result = Stream.of(readStrings.toString().toLowerCase()
-                    .replaceAll("[^a-z,]", "")
-                    .split(",")).sorted().toArray(String[]::new);
-            return (readStrings.length() == 0) ? empty : result;
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
         }
+        String[] result = Stream.of(readStrings.toString().toLowerCase()
+                .replaceAll("[^a-z,]", "")
+                .split(",")).sorted().toArray(String[]::new);
+        return (readStrings.length() == 0) ? empty : result;
     }
 }
