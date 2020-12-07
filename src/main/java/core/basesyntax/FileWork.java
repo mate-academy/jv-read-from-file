0fe354151ea9bc8e4ddf35096d6d0c9b327595b1
AndreiYazadzhi <belator251000@gmail.com>
2020-12-07 12:54:12 +0200

    Edit mistakes

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -7,57 +7,33 @@ import java.util.Arrays;
 
 public class FileWork {
     private static final String SPECIFIED_CHARACTER = "w";
-    private static final int FIRST_UPPERCASE_LETTER_AT_ASCII = 65;
-    private static final int LAST_UPPERCASE_LETTER_AT_ASCII = 90;
-    private static final int FIRST_LOWERCASE_LETTER_AT_ASCII = 97;
-    private static final int LAST_LOWERCASE_LETTER_AT_ASCII = 122;
-    private static final int FIRST_SIGN = 32;
-    private static final int LAST_SIGN = 63;
-    private static final int CARRIAGE_RETURN = 13;
 
     public String[] readFromFile(String fileName) {
-        String data = "";
+        StringBuilder data = new StringBuilder();
         try {
             BufferedReader reader = new BufferedReader(new FileReader(fileName));
-            int value = reader.read();
-            while (value != -1) {
-                if ((value >= FIRST_UPPERCASE_LETTER_AT_ASCII
-                        && value <= LAST_UPPERCASE_LETTER_AT_ASCII)
-                        || (value >= FIRST_LOWERCASE_LETTER_AT_ASCII
-                        && value <= LAST_LOWERCASE_LETTER_AT_ASCII)) {
-                    data = data + (char)value;
-                } else if (value >= FIRST_SIGN && value <= LAST_SIGN || value == CARRIAGE_RETURN) {
-                    data += " ";
-                }
-                value = reader.read();
+            String value = reader.readLine();
+            while (value != null) {
+                data.append(value).append(" ");
+                value = reader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file!");
+            throw new RuntimeException("Can't read file!" + fileName, e);
         }
-        data = data.toLowerCase();
-        String[] stringArrayData = data.split(" ");
-        String[] result = new String[checkQuantityOfRequiredWords(stringArrayData)];
-        if (result.length == 0) {
-            return result;
-        }
-        int count = 0;
+        String[] stringArrayData = data.toString().split("\\W++");
+        StringBuilder stringResult = new StringBuilder();
         for (String word : stringArrayData) {
-            if (word.startsWith(SPECIFIED_CHARACTER)) {
-                result[count] = word;
-                count++;
+            if (word.toLowerCase().startsWith(SPECIFIED_CHARACTER)) {
+                stringResult.append(word.toLowerCase()).append(",");
             }
         }
+        if (stringResult.toString().equals("")) {
+            return new String[0];
+        }
+
+        String[] result = stringResult.toString().split("\\W++");
         Arrays.sort(result);
         return result;
     }
 
-    public int checkQuantityOfRequiredWords(String[] stringArrayData) {
-        int count = 0;
-        for (String word : stringArrayData) {
-            if (word.toLowerCase().startsWith(SPECIFIED_CHARACTER)) {
-                count++;
-            }
-        }
-        return count;
-    }
 }
