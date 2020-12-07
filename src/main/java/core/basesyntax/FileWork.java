d15431a244d27372ffc78349798f1fecd18e2cd9
Volodymyr Lykhvar <vlykhvar@gmail.com>
2020-12-07 13:59:31 +0200

    changed for to toArray changed names of varible changet sout to throw ne

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -4,41 +4,40 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class FileWork {
 
     public String[] readFromFile(String fileName) {
-        StringBuilder file = new StringBuilder();
-        String[] readFromFile = new String[] {};
-        ArrayList<String> read = new ArrayList<>();
+        String[] readFromFile;
+        StringBuilder stringBuilder = new StringBuilder();
+        ArrayList<String> arrayList = new ArrayList<>();
         try (BufferedReader bufferedReader = new BufferedReader(new
                 FileReader((fileName)))) {
             String newLine = bufferedReader.readLine();
             while (newLine != null) {
-                file.append(newLine.toLowerCase()).append(" ");
+                stringBuilder.append(newLine.toLowerCase()).append(" ");
                 newLine = bufferedReader.readLine();
             }
         } catch (FileNotFoundException e) {
-            System.out.println("File was not found");
+           throw new RuntimeException("File was not found");
         } catch (IOException e) {
-            System.out.println("File was not written ");
+            throw new RuntimeException("File was not written ");
         }
-        readFromFile = file.toString().split(" ");
+        readFromFile = stringBuilder.toString().split(" ");
         for (String readFile : readFromFile) {
             if (startsWith(readFile)) {
-                read.add(readFile.replaceAll("[,.!?]", ""));
+                arrayList.add(readFile.replaceAll("[,.!?]", ""));
             }
         }
-        readFromFile = new String[read.size()];
-        Collections.sort(read);
-        read.toArray(readFromFile);
+        readFromFile = new String[arrayList.size()];
+        Collections.sort(arrayList);
+        arrayList.toArray(readFromFile);
         return readFromFile;
     }
 
-    private boolean startsWith(String word){
+    private boolean startsWith(String word) {
 
         return word.indexOf('w') == 0;
     }
