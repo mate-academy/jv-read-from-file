3ddcd67155511fc94092e30f1b00653c51317f05
OleksandrPokotylenko <aln.nko.of@gmail.com>
2020-12-07 16:19:29 +0200

    second fix (code removed from try-catch, splitter changed, unnecessary "replaceAll" removed)

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -13,30 +13,30 @@ public class FileWork {
     public String[] readFromFile(String fileName) {
         File file = new File(fileName);
         StringBuilder stringBuilder = new StringBuilder();
+        List<String> stringList;
         try {
-            List<String> stringList = Files.readAllLines(file.toPath());
-            if (stringList.size() == 0) {
-                return new String[0];
-            }
-            for (String string: stringList) {
-                String[] words = string.split(" ");
-                for (String word : words) {
-                    if (word.toLowerCase().startsWith(SPECIFIED_CHARACTER)) {
-                        stringBuilder.append(word.toLowerCase().replaceAll("[^a-z]", ""))
-                                .append(" ");
-                    }
-                }
-            }
-            if (stringBuilder.toString().isEmpty()) {
-                return new String[0];
-            }
-            String[] sortableStrings = stringBuilder.toString().split(" ");
-            List<String> sortingList = Arrays.asList(sortableStrings);
-            Collections.sort(sortingList);
-            String[] result = new String[sortingList.size()];
-            return sortingList.toArray(result);
+            stringList = Files.readAllLines(file.toPath());
         } catch (IOException e) {
             throw new RuntimeException("Can't read file" + fileName, e);
         }
+        if (stringList.size() == 0) {
+            return new String[0];
+        }
+        for (String string: stringList) {
+            String[] words = string.split("\\W+");
+            for (String word : words) {
+                if (word.toLowerCase().startsWith(SPECIFIED_CHARACTER)) {
+                    stringBuilder.append(word.toLowerCase()).append(" ");
+                }
+            }
+        }
+        if (stringBuilder.toString().isEmpty()) {
+            return new String[0];
+        }
+        String[] sortableStrings = stringBuilder.toString().split(" ");
+        List<String> sortingList = Arrays.asList(sortableStrings);
+        Collections.sort(sortingList);
+        String[] result = new String[sortingList.size()];
+        return sortingList.toArray(result);
     }
 }
