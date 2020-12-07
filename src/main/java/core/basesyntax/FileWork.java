909758d9a7746abf0f9c192caab8f0edcfebd9af
Alex <koleksandr4@gmail.com>
2020-12-07 17:55:58 +0200

    added requested changes

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -8,34 +8,34 @@ import java.util.Arrays;
 
 public class FileWork {
 
-    static final char STARTS_WITH = 'w';
+    private static final char STARTS_WITH = 'w';
 
     public String[] readFromFile(String fileName) {
-        String[] result;
-        StringBuilder allFile = new StringBuilder();
+        StringBuilder infoFromFile = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String value = reader.readLine();
             if (value == null || value.isEmpty()) {
                 return new String[0];
             }
             while (value != null) {
-                allFile.append(value);
-                allFile.append(" ");
+                infoFromFile.append(value);
+                infoFromFile.append(" ");
                 value = reader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read file", e);
         }
-        String[] strArray = allFile.toString().toLowerCase().split(" ");
-        ArrayList<String> stringArrayList = new ArrayList<>();
-        for (String str : strArray) {
+        String[] splitInfo = infoFromFile.toString().toLowerCase().split(" ");
+        ArrayList<String> wordsWithW = new ArrayList<>();
+        for (String str : splitInfo) {
             if (str.charAt(0) == STARTS_WITH) {
                 str = str.replaceAll("[.!?]", "");
-                stringArrayList.add(str);
+                wordsWithW.add(str);
             }
         }
-        result = stringArrayList.toArray(new String[0]);
-        Arrays.sort(result);
-        return result;
+        String[] readFromFile;
+        readFromFile = wordsWithW.toArray(new String[0]);
+        Arrays.sort(readFromFile);
+        return readFromFile;
     }
 }
