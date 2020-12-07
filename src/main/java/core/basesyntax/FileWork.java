238ec568c6eefdaf7eedaae6b732d6511881cba6
YuraBondarenko <yuriybondarenko2001@gmail.com>
2020-12-07 11:27:58 +0200

    split the code into two methods, replaced "read" to "readLine" and changed word sorting implementation add constant "W_CHARACTER" realised method that reads data from a file

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -6,44 +6,34 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class FileWork {
-    private static final char W_CHARACTER = 'w';
+    private static final String W_CHARACTER = "w";
 
     public String[] readFromFile(String fileName) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(fileName));) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             StringBuilder wordsReader = new StringBuilder();
-            int value = reader.read();
-            if (value == -1) {
-                return new String[0];
-            }
-            int i = 0;
+            String readLine = reader.readLine();
             int counter = 0;
-            while (value != -1) {
-                if ((value >= 65 && value <= 122) || (char) value == ' ') {
-                    wordsReader.append((char) value);
-                } else if (value == '\n') {
-                    wordsReader.append(" ");
+            while (readLine != null) {
+                String[] arrReadLine = readLine.split("\\s*(\\s|,|!|\\.|\\?)\\s*");
+                for (int i = 0; i < arrReadLine.length; i++) {
+                    if (arrReadLine[i].toLowerCase().startsWith(W_CHARACTER)) {
+                        wordsReader.append(arrReadLine[i].toLowerCase()).append(" ");
+                    }
                 }
-                value = reader.read();
+                readLine = reader.readLine();
             }
-            String[] arr = wordsReader.toString().toLowerCase().split(" ");
-            for (i = 0; i < arr.length; i++) {
-                if (arr[i].charAt(0) == W_CHARACTER) {
-                    counter++;
-                }
-            }
-            String[] arrW = new String[counter];
-            counter = 0;
-            for (i = 0; i < arr.length; i++) {
-                if (arr[i].charAt(0) == W_CHARACTER) {
-                    arrW[counter] = arr[i].trim();
-                    counter++;
-                }
-            }
-            Arrays.sort(arrW);
-            return arrW;
-
+            return createArrayAndSort(wordsReader);
         } catch (IOException e) {
             throw new RuntimeException("Can't read file", e);
         }
+    }
+
+    private String[] createArrayAndSort(StringBuilder wordsReader) {
+        if (wordsReader.toString().equals("")) {
+            return new String[0];
+        }
+        String[] arrW = wordsReader.toString().split(" ");
+        Arrays.sort(arrW);
+        return arrW;
     }
 }
