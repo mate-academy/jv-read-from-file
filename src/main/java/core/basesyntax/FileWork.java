ca49f2dd3e5ab433cd279fee8f8aa3f9067e7560
egorka <entertainment.veks@gmail.com>
2020-12-07 12:41:12 +0200

    yegor sereda TEST commit

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,27 +17,22 @@ public class FileWork {
         try {
             data = Files.readAllLines(file.toPath());
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file", e);
+            throw new RuntimeException("Can't read file " + fileName, e);
         }
 
         for (int i = 0; i < data.size(); i++) {
             String[] strings = data.get(i).split(" ");
             for (String str : strings) {
-                if (str.charAt(0) == 'w' || str.charAt(0) == 'W') {
+                if (Character.toLowerCase(str.charAt(0)) == 'w') {
                     int lastSymbol = str.length() - 1;
-                    if (str.charAt(lastSymbol) == '!'
-                            || str.charAt(lastSymbol) == '.'
-                            || str.charAt(lastSymbol) == '?'
-                            || str.charAt(lastSymbol) == ',') {
-                        returnStrings.add(str.toLowerCase().substring(0, lastSymbol));
-                    } else {
-                        returnStrings.add(str.toLowerCase());
-                    }
+
+                    str.replaceAll("[!.?,]", "");
+                    returnStrings.add(str.toLowerCase());
                 }
             }
         }
 
-        returnStrings = returnStrings.stream().sorted().collect(Collectors.toList());
+        Collections.sort(returnStrings);
         return returnStrings.toArray(new String[0]);
     }
 }
