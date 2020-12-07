e05a60b3620b39dacba8bf3bf52ee05a3c7f90d0
ivlytvynets <ivlytvynets@users.noreply.github.com>
2020-12-07 23:52:16 +0200

    method `readFromFile` was implemented

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,7 +1,33 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class FileWork {
+    private static final String SPECIFIED_CHARACTER = "w";
+
     public String[] readFromFile(String fileName) {
-        return null;
+        List<String> words = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                String[] wordsArray = value.split(" ");
+                for (String word: wordsArray) {
+                    word = word.toLowerCase();
+                    if (word.startsWith(SPECIFIED_CHARACTER)) {
+                        words.add(word.replaceAll("\\W", ""));
+                    }
+                }
+                value = bufferedReader.readLine();
+            }
+            Collections.sort(words);
+            return words.toArray(new String[0]);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read the file", e);
+        }
     }
 }
