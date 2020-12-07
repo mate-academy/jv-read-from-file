8f3e2d318398eb60722bee0ef6f8033aaa5695b6
engineer027 <dp270597dma@gmail.com>
2020-12-07 11:45:28 +0200

    use TRY-WITH-RESOURCES, added .replaceAll ();

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -6,43 +6,32 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class FileWork {
+    private static final String letter = "w";
+
     public String[] readFromFile(String fileName) {
-        String [] str = new String[0];
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+        StringBuilder stringWords = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             StringBuilder stringBuilder = new StringBuilder();
             String value = reader.readLine();
-            if (value == null) {
-                String [] resultWords = new String[0];
-                return resultWords;
-            }
             while (value != null) {
                 stringBuilder.append(value).append(" ");
                 value = reader.readLine();
             }
-            String [] textString = stringBuilder.toString().split(" ");
-            StringBuilder stringWords = new StringBuilder();
-            boolean getEmplyResult = false;
+            String[] textString = stringBuilder.toString().replaceAll("\\W", " ").split(" ");
             for (int i = 0; i < textString.length; i++) {
-                if (textString[i].charAt(0) == 'w' || textString[i].charAt(0) == 'W') {
-                    getEmplyResult = true;
-                    if (!Character.isLetter(textString[i].charAt(textString[i].length() - 1))) {
-                        stringWords.append(textString[i].toLowerCase()).deleteCharAt(stringWords
-                                .length() - 1).append(" ");
-                    } else {
-                        stringWords.append(textString[i].toLowerCase()).append(" ");
-                    }
+                if (textString[i].toLowerCase().startsWith(letter)) {
+                    stringWords.append(textString[i].toLowerCase()).append(" ");
                 }
             }
-            if (getEmplyResult) {
-                String [] resultWords = stringWords.toString().split(" ");
-                Arrays.sort(resultWords);
-                return resultWords;
-            }
-            String [] resultWords = new String[0];
-            return resultWords;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        if (stringWords.length() == 0) {
+            String [] resultWords = new String[0];
+            return resultWords;
+        }
+        String [] finishWords = stringWords.toString().trim().split(" ");
+        Arrays.sort(finishWords);
+        return finishWords;
     }
 }
