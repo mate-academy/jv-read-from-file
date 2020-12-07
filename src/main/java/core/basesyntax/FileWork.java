bae4cfe5cff80f8419660d2c345134f67a9621f6
Igor Bochkor <igorbochkor@gmail.com>
2020-12-07 11:12:49 +0200

    fixed 1.remove variable file 2. close() BufferedReader with try-with-resources 3. moved main code out of try block

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -10,33 +10,33 @@ public class FileWork {
     private static final String SPEC_CHARACTER = "w";
 
     public String[] readFromFile(String fileName) {
-        File file = new File(fileName);
+        StringBuilder builderWordsStartsWithW = new StringBuilder();
         StringBuilder builderWordsForAllFile = new StringBuilder();
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+
+        try (BufferedReader bufferedReader =
+                     new BufferedReader(new FileReader(new File(fileName)))) {
             String value = bufferedReader.readLine();
             while (value != null) {
                 builderWordsForAllFile.append(value).append(System.lineSeparator());
                 value = bufferedReader.readLine();
             }
-
-            String[] wordsOfFile = builderWordsForAllFile.toString().toLowerCase()
-                    .replaceAll("[,!?\\.]", "")
-                    .split("\\s+");
-            StringBuilder builderWordsStartsWithW = new StringBuilder();
-            for (String st : wordsOfFile) {
-                if (st.startsWith(SPEC_CHARACTER)) {
-                    builderWordsStartsWithW.append(st).append(" ");
-                }
-            }
-            String[] wordsStartsWithW = builderWordsStartsWithW.toString().split(" ");
-            Arrays.sort(wordsStartsWithW);
-            if (builderWordsStartsWithW.toString().isEmpty()) {
-                return new String[0];
-            }
-            return wordsStartsWithW;
         } catch (IOException e) {
             throw new RuntimeException("Can't find file");
         }
+        String[] wordsOfFile = builderWordsForAllFile.toString().toLowerCase()
+                .replaceAll("[,!?\\.]", "")
+                .split("\\s+");
+
+        for (String st : wordsOfFile) {
+            if (st.startsWith(SPEC_CHARACTER)) {
+                builderWordsStartsWithW.append(st).append(" ");
+            }
+        }
+        String[] wordsStartsWithW = builderWordsStartsWithW.toString().split(" ");
+        Arrays.sort(wordsStartsWithW);
+        if (builderWordsStartsWithW.toString().isEmpty()) {
+            return new String[0];
+        }
+        return wordsStartsWithW;
     }
 }
