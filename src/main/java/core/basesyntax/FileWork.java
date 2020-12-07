a318944fd3290212884eddac3eb8a0f31916076a
Alex <koleksandr4@gmail.com>
2020-12-07 11:51:30 +0200

    added requested changes

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -7,34 +7,35 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class FileWork {
+
+    static final char STARTS_WITH = 'w';
+
     public String[] readFromFile(String fileName) {
         String[] result;
+        StringBuilder allFile = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-            StringBuilder allFile = new StringBuilder();
             String value = reader.readLine();
             if (value == null || value.isEmpty()) {
                 return new String[0];
             }
             while (value != null) {
-                if (!value.isEmpty()) {
-                    allFile.append(value);
-                    allFile.append(" ");
-                }
+                allFile.append(value);
+                allFile.append(" ");
                 value = reader.readLine();
             }
-            String[] strArray = allFile.toString().toLowerCase().split(" ");
-            ArrayList<String> stringArrayList = new ArrayList<>();
-            for (int i = 0; i < strArray.length; i++) {
-                if (strArray[i].charAt(0) == 'w') {
-                    strArray[i] = strArray[i].replaceAll("[.!?]", "");
-                    stringArrayList.add(strArray[i]);
-                }
-            }
-            result = stringArrayList.toArray(new String[0]);
-            Arrays.sort(result);
         } catch (IOException e) {
             throw new RuntimeException("Can't read file", e);
         }
+        String[] strArray = allFile.toString().toLowerCase().split(" ");
+        ArrayList<String> stringArrayList = new ArrayList<>();
+        for (String str : strArray) {
+            if (str.charAt(0) == STARTS_WITH) {
+                str = str.replaceAll("[.!?]", "");
+                stringArrayList.add(str);
+            }
+        }
+        result = stringArrayList.toArray(new String[0]);
+        Arrays.sort(result);
         return result;
     }
 }
