4f04222e87678b9916531a49767643da1a31ea81
Maksim <max.freedman.main@gmail.com>
2021-03-30 19:12:21 +0300

    added solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,7 +1,42 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        return null;
+        Path pathToTest = Paths.get(fileName);
+        File fileTo = pathToTest.toFile();
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileTo))) {
+            String value = reader.readLine();
+
+            while (value != null) {
+                for (String line : value.split(" ")) {
+
+                    if (line.toLowerCase().charAt(0) == 'w') {
+                        builder.append(" ").append(line.toLowerCase()
+                                .replace(".", "").replace(",", "")
+                                .replace("!", "").replace("?", ""));
+                    }
+                }
+                value = reader.readLine();
+            }
+
+            if (!builder.toString().equals("")) {
+                String[] result = builder.toString().trim().split(" ");
+                Arrays.sort(result);
+                return result;
+            }
+
+            return new String[0];
+        } catch (IOException e) {
+            throw new RuntimeException("File not found.", e);
+        }
     }
 }
