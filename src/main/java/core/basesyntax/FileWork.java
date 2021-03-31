706cb1988eb4b16ce161eef695247380baf2a70d
safordog <safordog@gmail.com>
2021-03-31 23:21:15 +0300

    added solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,7 +1,33 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
 public class FileWork {
+    private static final String LOOKING_CHARACTER = "w";
+
     public String[] readFromFile(String fileName) {
-        return null;
+        String dataFromFile = "";
+        Path path = Paths.get(fileName);
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = Files.newBufferedReader(path)) {
+            while (bufferedReader.ready()) {
+                stringBuilder.append(bufferedReader.readLine()).append(" ");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        dataFromFile = stringBuilder.toString().replaceAll("[!\\.,?]", "");
+        Stream<String> streamOfString = Pattern.compile(" ").splitAsStream(dataFromFile);
+        return streamOfString
+                .map(String::toLowerCase)
+                .filter(word -> word.startsWith(LOOKING_CHARACTER))
+                .sorted()
+                .toArray(size -> new String[size]);
     }
 }
