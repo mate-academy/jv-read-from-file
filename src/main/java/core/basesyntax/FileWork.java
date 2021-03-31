92dfd4a7a577c6633bdc72ec589070f2ef4a5580
Maksim <max.freedman.main@gmail.com>
2021-03-31 03:05:12 +0300

    code refactoring

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,42 +1,33 @@
 package core.basesyntax;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Arrays;
 
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        Path pathToTest = Paths.get(fileName);
-        File fileTo = pathToTest.toFile();
-        StringBuilder builder = new StringBuilder();
-        try (BufferedReader reader = new BufferedReader(new FileReader(fileTo))) {
-            String value = reader.readLine();
-
-            while (value != null) {
-                for (String line : value.split(" ")) {
-
-                    if (line.toLowerCase().charAt(0) == 'w') {
-                        builder.append(" ").append(line.toLowerCase()
-                                .replace(".", "").replace(",", "")
-                                .replace("!", "").replace("?", ""));
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            StringBuilder text = new StringBuilder();
+            String line = reader.readLine();
+            while (line != null) {
+                for (String word : line.split(" ")) {
+                    if (word.toLowerCase().charAt(0) == 'w') {
+                        text.append(word.toLowerCase().replaceAll("[.,!?]", ""))
+                                .append(System.lineSeparator());
                     }
                 }
-                value = reader.readLine();
+                line = reader.readLine();
             }
-
-            if (!builder.toString().equals("")) {
-                String[] result = builder.toString().trim().split(" ");
+            if (text.length() > 1) {
+                String[] result = text.toString().split(System.lineSeparator());
                 Arrays.sort(result);
                 return result;
             }
-
             return new String[0];
+
         } catch (IOException e) {
-            throw new RuntimeException("File not found.", e);
+            throw new RuntimeException("File not found", e);
         }
     }
 }
