f8344615d056d300b2570caabf83e78d1642b58f
fotomassimo <maksym.pavliuchenko@gmail.com>
2021-04-01 22:04:26 +0300

    Task jv-read-from-file, first attempt

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,7 +1,39 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String SPECIFIED_LETTER = "w";
+
     public String[] readFromFile(String fileName) {
-        return null;
+        File file = new File(fileName);
+        try (BufferedReader textReader = new BufferedReader(new FileReader(file))) {
+            StringBuilder text = new StringBuilder();
+            int value = textReader.read();
+            while (value != -1) {
+                text.append((char) value);
+                value = textReader.read();
+            }
+            String[] words = text.toString().split("[\\W]+");
+            StringBuilder filteredWordsList = new StringBuilder();
+            for (String word : words) {
+                String lowerCaseWord = word.toLowerCase();
+                if (lowerCaseWord.startsWith(SPECIFIED_LETTER)) {
+                    filteredWordsList.append(lowerCaseWord).append(" ");
+                }
+            }
+            if (filteredWordsList.length() == 0) {
+                return new String[0];
+            }
+            String[] filteredWords = filteredWordsList.toString().split(" ");
+            Arrays.sort(filteredWords);
+            return filteredWords;
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read the file", e);
+        }
     }
 }
