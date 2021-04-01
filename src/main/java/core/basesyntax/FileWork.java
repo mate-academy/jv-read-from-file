d6f4b8c2bb86b5773c7ee1a14c3ea2fd9ab916bf
KseniaObertii <ksenia.obertii@users.noreply.github.com>
2021-04-01 15:32:55 +0300

    "added solution"

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,7 +1,32 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class FileWork {
+    private static final char SEARCHED_LETTER = 'w';
+    private static final String SEPARATOR = " ";
+
     public String[] readFromFile(String fileName) {
-        return null;
+        ArrayList<String> result = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line = reader.readLine();
+            while (line != null) {
+                for (String word : line.split(SEPARATOR)) {
+                    if (word.toLowerCase().charAt(0) == SEARCHED_LETTER) {
+                        result.add(word.toLowerCase().replaceAll("\\p{P}", ""));
+                    }
+                }
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        Collections.sort(result);
+
+        return result.toArray(new String[0]);
     }
 }
