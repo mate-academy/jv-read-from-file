4a0638f2c9271a5392649ab5925257cd96776afa
Nazar-yo <bilaknazar@gmail.com>
2021-04-02 18:03:53 +0300

    All done

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,7 +1,41 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Locale;
+
 public class FileWork {
+    private static final String REGEX = "\\s*(\\s|,|\\?|!|\\.)\\s*";
+
     public String[] readFromFile(String fileName) {
-        return null;
+        File file = new File(fileName);
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String line = bufferedReader.readLine();
+            if (line == null) {
+                return new String[0];
+            }
+            StringBuilder builder = new StringBuilder();
+            while (line != null) {
+                String[] words = line.toLowerCase(Locale.ROOT).split(REGEX);
+                for (String word : words) {
+                    if (word.indexOf("w") == 0) {
+                        builder.append(word).append(" ");
+                    }
+                }
+                line = bufferedReader.readLine();
+            }
+            String[] result = builder.toString().split(" ");
+            Arrays.sort(result);
+            if (result.length == 1) {
+                return new String[0];
+            }
+            return result;
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t find or read file", e);
+        }
     }
 }
