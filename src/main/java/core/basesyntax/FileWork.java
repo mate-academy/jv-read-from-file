10835fe7c787405148638510fcb391ed6a6f59c2
dant0n <dmibuc@gmail.com>
2021-04-02 18:56:10 +0300

    Refactor code.

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -6,19 +6,17 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class FileWork {
-    private static final char SPECIFIED_CHARACTER = 'w';
+    private static final String SPECIFIED_CHARACTER = "w";
 
     public String[] readFromFile(String fileName) {
         StringBuilder stringBuilder = new StringBuilder();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
             String line;
             while ((line = bufferedReader.readLine()) != null) {
-                for (String word : line.split(" ")) {
+                for (String word : line.split("\\W+")) {
                     String lowerCaseWord = word.toLowerCase();
-                    if (word.length() > 0 && lowerCaseWord.charAt(0) == SPECIFIED_CHARACTER) {
-                        String cleanWord = lowerCaseWord
-                                .replaceAll("\\s*\\p{Punct}+\\s*$", "");
-                        stringBuilder.append(cleanWord);
+                    if (word.length() > 0 && lowerCaseWord.startsWith(SPECIFIED_CHARACTER)) {
+                        stringBuilder.append(lowerCaseWord);
                         stringBuilder.append(" ");
                     }
                 }
