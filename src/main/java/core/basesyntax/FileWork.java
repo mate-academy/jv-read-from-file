2316b77fecc9cb1e78ac2258d73e7cc1ce5eb4b6
unknown <vovazalli@gmail.com>
2021-04-02 21:53:37 +0300

    File Reader implementation

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,7 +1,40 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        return null;
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+            String readString = bufferedReader.readLine();
+            String[] tempArray;
+
+            List<String> wordList = new ArrayList<>();
+            while (readString != null) {
+                tempArray = readString.split(" ");
+                for (String word : tempArray) {
+                    word = word.toLowerCase();
+                    if (word.startsWith("w")) {
+                        wordList.add(word.replaceAll("[^a-z]", ""));
+                    }
+                }
+                readString = bufferedReader.readLine();
+            }
+            Collections.sort(wordList);
+            String[] resultArray = new String[wordList.size()];
+            int i = 0;
+            for (String word : wordList) {
+                resultArray[i] = word;
+                i++;
+            }
+            return resultArray;
+        } catch (IOException e) {
+            throw new RuntimeException("File does not exist.");
+        }
     }
 }
