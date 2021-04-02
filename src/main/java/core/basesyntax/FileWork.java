e2659e6c230f10a4b2e8b6b8e56e6cb437659e5a
vuyko23 <mykolasakulych@gmail.com>
2021-04-02 20:10:14 +0300

    fixed all problem

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -4,8 +4,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 
 public class FileWork {
@@ -13,24 +13,23 @@ public class FileWork {
 
     public String[] readFromFile(String fileName) {
         File file = new File(fileName);
-        List<String> comparingResult = new LinkedList<>();
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-            StringBuilder builder = new StringBuilder();
+        List<String> comparingResult = new ArrayList<>();
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
             String readerValue = bufferedReader.readLine();
             while (readerValue != null) {
                 builder.append(readerValue).append(System.lineSeparator());
                 readerValue = bufferedReader.readLine();
             }
-            String[] inputArray = builder.toString().split("\\s+");
-            for (String singleWord : inputArray) {
-                singleWord = singleWord.replaceAll("\\W", "").toLowerCase();
-                if (singleWord.startsWith(START_CHARACTER)) {
-                    comparingResult.add(singleWord);
-                }
-            }
         } catch (IOException exception) {
             throw new RuntimeException("Can`t read file, file is Empty!" + exception);
+        }
+        String[] inputArray = builder.toString().split("\\s+");
+        for (String singleWord : inputArray) {
+            singleWord = singleWord.replaceAll("\\W", "").toLowerCase();
+            if (singleWord.startsWith(START_CHARACTER)) {
+                comparingResult.add(singleWord);
+            }
         }
         Collections.sort(comparingResult);
         return comparingResult.toArray(new String[0]);
