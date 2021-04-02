d5eabc48ae1cc8f254c234eae9cb7d8bef8ef458
Den4eg007 <68178197+Den4eg007@users.noreply.github.com>
2021-04-02 23:47:58 +0300

    Made some changes to readFromFile

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -7,35 +7,33 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class FileWork {
+    private static final String REGEX_SPLITTER = "\\W+";
+
     public String[] readFromFile(String fileName) {
         File file = new File(fileName);
         String[] result = {};
-
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
             String currentLine = bufferedReader.readLine();
             StringBuilder stringBuilder = new StringBuilder();
-            String[] currentLineSplit;
-
+            String[] currentLineArray;
             while (currentLine != null) {
-                currentLine = currentLine.toLowerCase();
-                currentLine = currentLine.replaceAll("[^a-zA-Z]", " ");
-                currentLineSplit = currentLine.split(" ");
-                for (String s : currentLineSplit) {
+                currentLineArray = currentLine.toLowerCase().replaceAll(REGEX_SPLITTER, " ")
+                        .split(" ");
+                for (String s : currentLineArray) {
                     if (s.startsWith("w")) {
                         stringBuilder.append(s).append(" ");
                     }
                 }
                 currentLine = bufferedReader.readLine();
             }
-            if (stringBuilder.length() != 0) {
-                result = stringBuilder.toString().split(" ");
-            } else {
+            if (stringBuilder.length() == 0) {
                 return result;
             }
+            result = stringBuilder.toString().split(" ");
+            Arrays.sort(result);
         } catch (IOException e) {
             throw new RuntimeException("Can't read the file", e);
         }
-        Arrays.sort(result);
         return result;
     }
 }
