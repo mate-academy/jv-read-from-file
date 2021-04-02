3fe3050b09197d94ef6c7939832b3dc621bba469
Darina <to.darina.to>
2021-04-03 00:13:19 +0300

    test done

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,7 +1,37 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        return null;
+        String[] changeValue;
+        String[] fileRead = {};
+        File myFile = new File(fileName);
+        try (BufferedReader reader = new BufferedReader(new FileReader(myFile))) {
+            StringBuilder builder = new StringBuilder();
+            String value = reader.readLine();
+            while (value != null) {
+                changeValue = (value.replaceAll("\\W+", " ")
+                        .toLowerCase().split(" "));
+                for (int i = 0; i < changeValue.length; i++) {
+                    if (changeValue[i].charAt(0) == 'w') {
+                        builder.append(changeValue[i]).append(" ");
+                    }
+                }
+                value = reader.readLine();
+            }
+            if (builder.length() == 0) {
+                return fileRead;
+            }
+            fileRead = String.valueOf(builder).split(" ");
+            Arrays.sort(fileRead);
+        } catch (IOException e) {
+            throw new RuntimeException("Something do wrong" + e);
+        }
+        return fileRead;
     }
 }
