93f49f82fbaf719ab5e156323c78923ba2faae72
MakeDreamCode <65960125+MakeDreamCode@users.noreply.github.com>
2021-04-03 20:39:40 +0300

    Initial commit. First version.

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,7 +1,36 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
 public class FileWork {
+    private static final String SPACE_DELIMITER = " ";
+    private static final char LOOKING_CHARACTER = 'w';
+
     public String[] readFromFile(String fileName) {
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        File file = new File(fileName);
+        List<String> stringsFromFile;
+        try {
+            stringsFromFile = Files.readAllLines(file.toPath());
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read a file", e);
+        }
+
+        for (String string : stringsFromFile) {
+            String[] wordsOfString = string.toLowerCase()
+                                           .split(SPACE_DELIMITER);
+            for (String singleWord : wordsOfString) {
+                if (singleWord.charAt(0) == LOOKING_CHARACTER) {
+                    stringBuilder.append(singleWord)
+                                 .append(SPACE_DELIMITER);
+                }
+            }
+        }
+
+        return stringBuilder.toString()
+                            .split(SPACE_DELIMITER);
     }
 }
