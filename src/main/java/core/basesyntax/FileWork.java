e7ce89fa1fc6a72254dba25cfc854fcd0a66c7a2
Anatolii Zarytskyi <tolia.zaritskiy@gmail.com>
2021-04-03 21:29:11 +0300

    hw5 is done

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,7 +1,29 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class FileWork {
+    private static final String SPECIFIED_CHARACTER = "w";
+
     public String[] readFromFile(String fileName) {
-        return null;
+        File file = new File(fileName);
+        List<String> result = new ArrayList<>();
+        try {
+            String data = Files.readString(file.toPath());
+            for (String wordFromData : data.replaceAll("[\\n\\r\\p{P}]", " ").split(" ")) {
+                if (wordFromData.toLowerCase().startsWith(SPECIFIED_CHARACTER)) {
+                    result.add(wordFromData.toLowerCase());
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file ", e);
+        }
+        Collections.sort(result);
+        return result.toArray(new String[0]);
     }
 }
