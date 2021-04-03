3e1c1b5d626c194e08511ee9ce7cd18f36bbc4c4
apyavchik <81320602+apyavchik@users.noreply.github.com>
2021-04-03 21:49:20 +0300

    Add pyavchik homework jv-read-from-file

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,7 +1,59 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.List;
+
 public class FileWork {
+    private static final String SPECIFIED_CHARACTER = "w";
+
     public String[] readFromFile(String fileName) {
-        return null;
+        File file = new File(fileName);
+
+        List<String> strings;
+
+        try {
+            strings = Files.readAllLines(file.toPath());
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String string : strings) {
+            stringBuilder.append(string
+                    .replaceAll("\\.", "")
+                    .replaceAll(",", "")
+                    .replaceAll("!", "")
+                    .replaceAll("\\?", "")
+                    .replaceAll(";", "")).append(" ");
+
+        }
+
+        String resultString = stringBuilder.toString().strip().toLowerCase();
+        String[] tempArray = resultString.split(" ");
+        int count = 0;
+        for (String value : tempArray) {
+            if (startWithLetter(value)) {
+                count++;
+            }
+        }
+
+        String[] resultArray = new String[count];
+        count = 0;
+        for (String s : tempArray) {
+            if (startWithLetter(s)) {
+                resultArray[count] = s.toLowerCase();
+                count++;
+            }
+        }
+        Arrays.sort(resultArray);
+
+        return resultArray;
+    }
+
+    public boolean startWithLetter(String word) {
+        return word.startsWith(SPECIFIED_CHARACTER);
     }
 }
