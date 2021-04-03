5a383888c8d98b399e51c2a832b4c462b669a093
IhorRome <ihor.rome@gmail.com>
2021-04-03 15:37:52 +0300

    fixes added

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -5,21 +5,19 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Locale;
 
 public class FileWork {
-    private static final char SPECIFIED_CHARACTER = 'w';
+    private static final String SPECIFIED_CHARACTER = "w";
 
     public String[] readFromFile(String fileName) {
         File file = new File(fileName);
         StringBuilder builder = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            int value = reader.read();
-            while (value != -1) {
-                if (Character.toLowerCase((char)value) == SPECIFIED_CHARACTER) {
-                    value = pullWord(reader, builder, value);
-                } else {
-                    value = passWord(reader, value);
-                }
+            String value = reader.readLine();
+            while (value != null) {
+                getAllWordsWithW(value, builder);
+                value = reader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("can't read from file");
@@ -27,44 +25,27 @@ public class FileWork {
         return toStringArray(builder);
     }
 
-    public int pullWord(BufferedReader reader, StringBuilder builder, int value)
-            throws IOException {
-        if (builder.length() != 0) {
-            builder.append(' ');
+    private void getAllWordsWithW(String input, StringBuilder builder) {
+        input = input.replaceAll("[^a-z A-Z]", "")
+                .toLowerCase(Locale.ROOT);
+        String[] wordsFromLine = input.split(" ");
+        for (String word : wordsFromLine) {
+            if (word.startsWith(SPECIFIED_CHARACTER)) {
+                if (builder.length() != 0) {
+                    builder.append(' ');
+                }
+                builder.append(word);
+            }
         }
-        do {
-            builder.append(Character.toLowerCase((char) value));
-            value = reader.read();
-        } while (notEndOfWord(value));
-        return value;
     }
 
-    public int passWord(BufferedReader reader, int value) throws IOException {
-        while (notEndOfWord(value)) {
-            value = reader.read();
-        }
-        return reader.read();
-    }
-
-    public boolean notEndOfWord(int value) {
-        return (char) value != ' '
-                && (char) value != '.'
-                && (char) value != ','
-                && (char) value != '-'
-                && (char) value != '!'
-                && (char) value != '?'
-                && (char) value != '\n'
-                && (char) value != '\r'
-                && value != -1;
-    }
-
-    public String [] toStringArray(StringBuilder builder) {
+    public String[] toStringArray(StringBuilder builder) {
         if (builder.length() != 0) {
-            String [] result = builder.toString().split(" ");
+            String[] result = builder.toString().split(" ");
             Arrays.sort(result);
             return result;
         } else {
-            return new String[] {};
+            return new String[0];
         }
     }
 }
