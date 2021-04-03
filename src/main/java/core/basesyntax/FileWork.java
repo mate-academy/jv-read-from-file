ed200707b014389e4494fd927b56ad27d66183ca
Stanislav Aleksandrovych <astanislav2361@gmail.com>
2021-04-03 17:34:01 +0300

    added solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,7 +1,40 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    public static final char GIVEN_CHARACTER = 'w';
+    public static final String WHITE_SPACE = " ";
+    public static final String EMPTY = "";
+
     public String[] readFromFile(String fileName) {
-        return null;
+        File file = new File(fileName);
+        String newString = EMPTY;
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line = reader.readLine();
+            while (line != null) {
+                String[] words = line.split(WHITE_SPACE);
+                line = reader.readLine();
+                for (String word : words) {
+                    if (word.toLowerCase().charAt(0) == GIVEN_CHARACTER) {
+                        newString += word.toLowerCase().replaceAll("\\p{P}", EMPTY)
+                                + WHITE_SPACE;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        if (newString.equals(EMPTY)) {
+            String[] emptyArray = new String[0];
+            return emptyArray;
+        }
+        String[] answer = newString.split(WHITE_SPACE);
+        Arrays.sort(answer);
+        return answer;
     }
 }
