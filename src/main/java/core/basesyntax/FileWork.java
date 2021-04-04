99fe4d65b218df7cfafdb8ccea5eda0831bd3d9c
anufriiev <dimaanufriev1012@gmail.com>
2021-04-04 23:20:55 +0300

    HW-5. Reading from file and filtering solution added

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,7 +1,40 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class FileWork {
+    public static final String CHARACTER = "w";
+
     public String[] readFromFile(String fileName) {
-        return null;
+
+        StringBuilder stringBuilder = new StringBuilder();
+        String[] finalArray;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            String value = reader.readLine();
+            while (value != null) {
+                stringBuilder.append(value).append(" ");
+                value = reader.readLine();
+            }
+            String result = stringBuilder.toString();
+            String lowerCaseWords = result.toLowerCase();
+            String[] strings = lowerCaseWords.split("\\W");
+            Arrays.sort(strings);
+            ArrayList<String> wordsArray = new ArrayList<>();
+            for (String word : strings) {
+                if (word.toLowerCase()
+                        .startsWith(CHARACTER)) {
+                    wordsArray.add(word);
+                }
+            }
+            finalArray = wordsArray.toArray(new String[wordsArray.size()]);
+        } catch (IOException e) {
+            throw new RuntimeException("File not found", e);
+        }
+        return finalArray;
     }
 }
