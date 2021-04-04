c1a144fb4903ff0ef37671664efa9d3f480c57bc
SerKisJD <sergej.kislyakov@gmail.com>
2021-04-04 22:49:44 +0300

    1. added solution, tests ok, mvn ok

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,7 +1,34 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String START_LETTER = "w";
+
     public String[] readFromFile(String fileName) {
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                for (String word : line.split("\\W+")) {
+                    String wordToLowerCase = word.toLowerCase();
+                    if (wordToLowerCase.startsWith(START_LETTER)) {
+                        stringBuilder.append(wordToLowerCase).append(" ");
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read file");
+        }
+        if (stringBuilder.length() > 0) {
+            String[] resultWords = stringBuilder.toString().split(" ");
+            Arrays.sort(resultWords);
+            return resultWords;
+        }
+        return new String[0];
     }
 }
