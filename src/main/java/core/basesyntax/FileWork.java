d12d3891248b6d9e25e178142962384dbd93cc05
Aalderaan <chyregd@gmail.com>
2021-04-04 21:42:44 +0200

    home-work done

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,7 +1,50 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String SPECIFIED_CHARACTER = "w";
+
     public String[] readFromFile(String fileName) {
-        return null;
+
+        File file = new File(fileName);
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String value = reader.readLine();
+            while (value != null) {
+                stringBuilder.append(value).append(" ");
+                value = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read the file...");
+        }
+
+        int count = 0;
+        String[] arrayOfWords = stringBuilder.toString().toLowerCase()
+                .replaceAll("[^a-z ^A-Z]", "").split(" ");
+        for (String words : arrayOfWords) {
+            if (words.startsWith(SPECIFIED_CHARACTER)) {
+                count++;
+            }
+        }
+        if (count == 0) {
+            return new String[]{};
+        }
+        int index = -1;
+        String[] stringWithSpecifiedLetter = new String[count];
+        for (String word : arrayOfWords) {
+            if (word.startsWith(SPECIFIED_CHARACTER)) {
+                index++;
+                stringWithSpecifiedLetter[index] = word;
+            }
+        }
+        Arrays.sort(stringWithSpecifiedLetter);
+        return stringWithSpecifiedLetter;
     }
 }
