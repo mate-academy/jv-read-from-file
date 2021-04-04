46ff91b74898b414ed5c2a4b56c0e09869a39d9f
Elia2909 <290900rR>
2021-04-04 22:49:11 +0300

    task done. All test successful

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,7 +1,41 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String SPECIAL_SYMBOL = "w";
+
     public String[] readFromFile(String fileName) {
-        return null;
+        File file = new File(fileName);
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            StringBuilder stringBuilder = new StringBuilder();
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                stringBuilder.append(value).append(" ");
+                value = bufferedReader.readLine();
+            }
+            String[] words = stringBuilder.toString().split(" ");
+            StringBuilder filtersWorlds = new StringBuilder();
+            for (String word : words) {
+                String toLowerCaseWord = word.toLowerCase();
+                if (toLowerCaseWord.startsWith("w")) {
+                    toLowerCaseWord = toLowerCaseWord.replaceAll("(\\w+)\\p{Punct}(\\s|$)", "$1$2");
+                    filtersWorlds.append(toLowerCaseWord).append(" ");
+
+                }
+            }
+            if (filtersWorlds.length() == 0) {
+                return new String[0];
+            }
+            String[] filterWorldResult = filtersWorlds.toString().split(" ");
+            Arrays.sort(filterWorldResult);
+            return filterWorldResult;
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
     }
 }
