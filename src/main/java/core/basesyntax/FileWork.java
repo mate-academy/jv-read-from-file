b9b5ba923be501d9c67f6e50db94d229e92c8ed9
Nikita <kupora3@gmail.com>
2021-04-05 13:43:42 +0300

    correct all mistakes

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -7,37 +7,35 @@ import java.util.Arrays;
 import java.util.List;
 
 public class FileWork {
-    public static final char FILTER_CHARACTER = 'w';
-    public static final String SEPARATOR_CHARACTER = "_";
-    public static final String SECOND_SEPARATOR_CHARACTER = " ";
-    public static final String EMPTY_CHARACTER = "";
+    private static final char FILTER_CHARACTER = 'w';
+    private static final String SEPARATOR_CHARACTER = "_";
+    private static final String SECOND_SEPARATOR_CHARACTER = " ";
+    private static final String EMPTY_CHARACTER = "";
 
     public String[] readFromFile(String fileName) {
         try {
             String res = "";
-            String list = "";
-            List files = Files.readAllLines(Paths.get(fileName));
-            if (files.equals("") || files.equals(null) || files.isEmpty()) {
+            StringBuilder list = new StringBuilder();
+            List<String> files = Files.readAllLines(Paths.get(fileName));
+            if (files.isEmpty()) {
                 return new String[0];
             }
             for (int i = 0; i < files.size(); i++) {
-                list += files.get(i) + SECOND_SEPARATOR_CHARACTER;
+                list.append(files.get(i)).append(SECOND_SEPARATOR_CHARACTER);
             }
-            String[] listSplit = list.split(SECOND_SEPARATOR_CHARACTER);
+            String[] listSplit = list.toString().split(SECOND_SEPARATOR_CHARACTER);
             for (int i = 0; i < listSplit.length; i++) {
                 if (listSplit[i].toLowerCase().charAt(0) == FILTER_CHARACTER) {
                     res += listSplit[i].toLowerCase() + SEPARATOR_CHARACTER;
                 }
             }
-            res = res.replaceAll("\\W", EMPTY_CHARACTER);
-
-            String[] result = res.trim().split(SEPARATOR_CHARACTER);
-            Arrays.sort(result);
-            if (result.length == 1) {
+            res = res.trim().replaceAll("\\W", EMPTY_CHARACTER);
+            if (res.isEmpty()) {
                 return new String[0];
-            } else {
-                return result;
             }
+            String[] result = res.split(SEPARATOR_CHARACTER);
+            Arrays.sort(result);
+            return result;
         } catch (IOException e) {
             throw new RuntimeException("Can't read from file", e);
         }
