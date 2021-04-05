2967b8c57b6bed44effe04905feae07dac9bb3c4
Stanislav Aleksandrovych <astanislav2361@gmail.com>
2021-04-05 13:54:34 +0300

    simplified some constructions

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -7,11 +7,9 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class FileWork {
-    private static final String LOWER_GIVEN_CHARACTER = "w";
-    private static final String UPPER_GIVEN_CHARACTER = "W";
+    private static final String GIVEN_CHARACTER = "w";
     private static final String SPACE_REGEX = " ";
-    private static final String EMPTY_REGEX = "";
-    private static final String PUNCTUATION_REGEX = "\\p{P}";
+    private static final String SPLIT_REGEX = "\\W+";
 
     public String[] readFromFile(String fileName) {
         File file = new File(fileName);
@@ -19,20 +17,18 @@ public class FileWork {
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String line = reader.readLine();
             while (line != null) {
-                String[] words = line.split(SPACE_REGEX);
+                String[] words = line.toLowerCase().split(SPLIT_REGEX);
                 line = reader.readLine();
                 for (String word : words) {
-                    if (word.startsWith(LOWER_GIVEN_CHARACTER)
-                            || word.startsWith(UPPER_GIVEN_CHARACTER)) {
-                        targetWords.append(word.toLowerCase()
-                                .replaceAll(PUNCTUATION_REGEX, EMPTY_REGEX)).append(SPACE_REGEX);
+                    if (word.startsWith(GIVEN_CHARACTER)) {
+                        targetWords.append(word).append(SPACE_REGEX);
                     }
                 }
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read file", e);
         }
-        if (targetWords.toString().equals(EMPTY_REGEX)) {
+        if (targetWords.length() == 0) {
             return new String[0];
         }
         String[] answer = targetWords.toString().split(SPACE_REGEX);
