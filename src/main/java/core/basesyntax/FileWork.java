67f62f5a2fd21c52550156350864cf7fbe115056
anufriiev <dimaanufriev1012@gmail.com>
2021-04-05 12:32:00 +0300

    HW-5. Requirements added to solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,40 +1,39 @@
 package core.basesyntax;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
 
 public class FileWork {
-    public static final String CHARACTER = "w";
+    private static final String TARGET_CHARACTER = "w";
+    private static final String REGEX_CONST = "\\W";
 
     public String[] readFromFile(String fileName) {
-
         StringBuilder stringBuilder = new StringBuilder();
-        String[] finalArray;
+        List<String> wordsArray = new ArrayList<>();
         try {
             BufferedReader reader = new BufferedReader(new FileReader(fileName));
-            String value = reader.readLine();
-            while (value != null) {
-                stringBuilder.append(value).append(" ");
-                value = reader.readLine();
-            }
-            String result = stringBuilder.toString();
-            String lowerCaseWords = result.toLowerCase();
-            String[] strings = lowerCaseWords.split("\\W");
-            Arrays.sort(strings);
-            ArrayList<String> wordsArray = new ArrayList<>();
-            for (String word : strings) {
-                if (word.toLowerCase()
-                        .startsWith(CHARACTER)) {
-                    wordsArray.add(word);
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] stringArray = line.split(REGEX_CONST);
+                for (String word : stringArray) {
+                    if (word.toLowerCase().startsWith(TARGET_CHARACTER)) {
+                        wordsArray.add(word.toLowerCase(Locale.ROOT));
+                    }
                 }
             }
-            finalArray = wordsArray.toArray(new String[wordsArray.size()]);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("File not found", e);
         } catch (IOException e) {
             throw new RuntimeException("File not found", e);
         }
+        String[] finalArray = wordsArray.toArray(new String[wordsArray.size()]);
+        Arrays.sort(finalArray);
         return finalArray;
     }
 }
