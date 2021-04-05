c98b7fc46e91a60e6916bbf515eb364c7065590f
Halina Kharlanava <galina.harlanova1@gmail.com>
2021-04-05 12:48:03 +0200

    Implemented suggested changes.

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -9,16 +9,14 @@ public class FileWork {
     private static final String CONSTANT_LETTER = "w";
     private static final String SHOULD_BE_DELETED_AT_FINAL_ARRAY = "(?u)[^\\pL ]";
     private static final String REPLACEMENT_OF_INAPPROPRIATE = " ";
-    private StringBuilder wordsStartingWithConstant = new StringBuilder();
 
     public String[] readFromFile(String fileName) {
+        StringBuilder wordsStartingWithConstant = new StringBuilder();
         int wordsWithConstant = 0;
         try {
             String fileData = Files.readString(Path.of(fileName))
                     .replaceAll(SHOULD_BE_DELETED_AT_FINAL_ARRAY, REPLACEMENT_OF_INAPPROPRIATE);
-            if (fileData.length() == 0) {
-                return new String[0];
-            }
+
             for (String lineData : fileData.split(" ")) {
                 if (lineData.toLowerCase().startsWith(CONSTANT_LETTER)) {
                     wordsStartingWithConstant.append(lineData).append(" ");
@@ -29,14 +27,12 @@ public class FileWork {
             throw new RuntimeException("Error while reading the file", e);
         }
 
-        if (wordsWithConstant == 0) {
+        if (wordsStartingWithConstant.length() == 0) {
             return new String[0];
         }
-        String [] returnArray;
-        returnArray = wordsStartingWithConstant
-                .deleteCharAt(wordsStartingWithConstant.lastIndexOf(" "))
-                .toString().toLowerCase().split(" ");
-        Arrays.sort(returnArray);
-        return returnArray;
+        String [] resultArray;
+        resultArray = wordsStartingWithConstant.toString().trim().toLowerCase().split(" ");
+        Arrays.sort(resultArray);
+        return resultArray;
     }
 }
