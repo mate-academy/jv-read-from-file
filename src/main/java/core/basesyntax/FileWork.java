f5b17b758b43cc1cd850af71353d8c4f3e76810e
roman <nashpatsa@gmai.com>
2021-04-05 15:38:35 +0300

    I changed my second mistakes

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -4,14 +4,11 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class FileWork {
     private static final String START_LETTER = "w";
-    private static final String[] SIGNS = new String[] {
-            ",","!","?","."
-    };
 
     public String[] readFromFile(String fileName) {
         List<String> arrayOfWords = new ArrayList<>();
@@ -20,14 +17,9 @@ public class FileWork {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
             String line = bufferedReader.readLine();
             while (line != null) {
-                for (String word : line.split("\\W+")) {
-                    if (word.toLowerCase().startsWith(START_LETTER)) {
-                        for (String sign : SIGNS) {
-                            if (word.endsWith(sign)) {
-                                word = word.substring(0, word.length() - 1);
-                            }
-                        }
-                        arrayOfWords.add(word.toLowerCase());
+                for (String word : line.toLowerCase().split("\\W+")) {
+                    if (word.startsWith(START_LETTER)) {
+                        arrayOfWords.add(word);
                     }
                 }
                 line = bufferedReader.readLine();
@@ -36,8 +28,7 @@ public class FileWork {
             throw new RuntimeException("Can't read file");
         }
 
-        String[] returnedString = arrayOfWords.toArray(new String[0]);
-        Arrays.sort(returnedString);
-        return returnedString;
+        Collections.sort(arrayOfWords);
+        return arrayOfWords.toArray(new String[0]);
     }
 }
