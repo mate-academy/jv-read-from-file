8c3297bad626c4ff173f14d174fe733f2fcb84db
Ihor-Nykyforov <kifa1203@gmail.com>
2021-04-05 16:35:58 +0300

    New changes

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -7,38 +7,34 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class FileWork {
-
     private static final String DELIMITERS = "[^A-Za-z0-9]";
     private static final String SPECIFIED_CHARACTER = "w";
+    private static final String SPACE = " ";
 
     public String[] readFromFile(String fileName) {
         File file = new File(fileName);
         StringBuilder builder = new StringBuilder();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
-            int value = bufferedReader.read();
-            if (value == -1) {
-                return new String[]{};
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                builder.append(value).append(SPACE);
+                value = bufferedReader.readLine();
             }
-            while (value != -1) {
-                builder.append((char) value);
-                value = bufferedReader.read();
-            }
-            System.out.println(builder.toString());
-            String[] array = builder.toString().toLowerCase().split(DELIMITERS);
-            builder.setLength(0);
-            for (String s : array) {
-                if (s.startsWith(SPECIFIED_CHARACTER)) {
-                    builder.append(s).append(" ");
-                }
-            }
-            if (builder.length() == 0) {
-                return new String[]{};
-            }
-            array = builder.toString().split(" ");
-            Arrays.sort(array);
-            return array;
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file",e);
+            throw new RuntimeException("Can't read " + fileName, e);
         }
+        String[] array = builder.toString().toLowerCase().split(DELIMITERS);
+        builder.setLength(0);
+        for (String word : array) {
+            if (word.startsWith(SPECIFIED_CHARACTER)) {
+                builder.append(word).append(SPACE);
+            }
+        }
+        if (builder.length() == 0) {
+            return new String[]{};
+        }
+        array = builder.toString().split(SPACE);
+        Arrays.sort(array);
+        return array;
     }
 }
