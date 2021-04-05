bf64ec69c5f67df6097a73becf2133dd4b193d55
Ihor-Nykyforov <kifa1203@gmail.com>
2021-04-05 17:36:50 +0300

    New changes №2

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -13,28 +13,27 @@ public class FileWork {
 
     public String[] readFromFile(String fileName) {
         File file = new File(fileName);
+        String[] resultArray;
         StringBuilder builder = new StringBuilder();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
             String value = bufferedReader.readLine();
             while (value != null) {
-                builder.append(value).append(SPACE);
+                resultArray = value.toLowerCase().split(DELIMITERS);
                 value = bufferedReader.readLine();
+                for (String word : resultArray) {
+                    if (word.startsWith(SPECIFIED_CHARACTER)) {
+                        builder.append(word).append(SPACE);
+                    }
+                }
             }
+            if (builder.length() == 0) {
+                return new String[]{};
+            }
+            resultArray = builder.toString().split(SPACE);
+            Arrays.sort(resultArray);
+            return resultArray;
         } catch (IOException e) {
             throw new RuntimeException("Can't read " + fileName, e);
         }
-        String[] array = builder.toString().toLowerCase().split(DELIMITERS);
-        builder.setLength(0);
-        for (String word : array) {
-            if (word.startsWith(SPECIFIED_CHARACTER)) {
-                builder.append(word).append(SPACE);
-            }
-        }
-        if (builder.length() == 0) {
-            return new String[]{};
-        }
-        array = builder.toString().split(SPACE);
-        Arrays.sort(array);
-        return array;
     }
 }
