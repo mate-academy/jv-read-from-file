ab6b67b0c923c6f7a6886b464efd8df468ae787c
Dalai <semenov.dalay@gmail.com>
2021-04-05 13:18:02 +0300

    fix regex and added constant

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class FileWork {
+    private static final char FIRST_LETTER = 'w';
+
     public String[] readFromFile(String fileName) {
         StringBuilder buffer = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
@@ -23,23 +25,16 @@ public class FileWork {
 
         String[] result = (buffer.toString()).split(" ");
         Arrays.sort(result);
-        return removeCharacters(result);
+        return result;
     }
 
     private StringBuilder findWords(String line) {
         StringBuilder result = new StringBuilder();
-        for (String word: (line.toLowerCase()).split(" ")) {
-            if (word.charAt(0) == 'w') {
+        for (String word: line.toLowerCase().replaceAll("[^a-zA-Z\\s]", "").split(" ")) {
+            if (word.charAt(0) == FIRST_LETTER) {
                 result.append(word).append(" ");
             }
         }
         return result;
-    }
-
-    private String[] removeCharacters(String[] buffer) {
-        for (int i = 0; i < buffer.length; i++) {
-            buffer[i] = buffer[i].replaceAll("[^a-zA-Z]", "");
-        }
-        return buffer;
     }
 }
