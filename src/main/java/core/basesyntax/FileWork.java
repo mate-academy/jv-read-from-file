3830d7b1d79c7b6c93ae78c9bab09bf027ffad0e
the5tranger <mishaantonyuk00@gmail.com>
2021-04-05 16:27:13 +0300

    Commit v1.1

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,35 +1,39 @@
 package core.basesyntax;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
 
 public class FileWork {
     private static final char SPECIFIED_CHARACTER = 'w';
+    private static final String REGEX = "[\\W]";
 
     public String[] readFromFile(String fileName) {
-        File file = new File(fileName);
         StringBuilder stringBuilder = new StringBuilder();
-        List<String> strings;
+        List<String> lines;
         String[] result;
 
         try {
-            strings = Files.readAllLines(file.toPath());
+            lines = Files.readAllLines(Path.of(fileName));
         } catch (IOException e) {
             throw new RuntimeException("Can't read from FILE! " + fileName, e);
         }
 
-        for (String word : strings.toString()
-                                  .substring(1, strings.toString().length() - 1)
-                                  .split(" ")) {
-            if (strings.toString().equals("[]")) {
+        for (String line : lines) {
+            if (lines.toString().equals("[]")) {
                 return new String[0];
             }
-            if (SPECIFIED_CHARACTER == word.toLowerCase().charAt(0)) {
-                stringBuilder.append(word.toLowerCase().replaceAll("[\\W]", ""))
-                        .append(" ");
+
+            for (String word : line.toLowerCase().split(REGEX)) {
+                if (word.equals("")) {
+                    continue;
+                }
+                if (SPECIFIED_CHARACTER == word.charAt(0)) {
+                    stringBuilder.append(word)
+                            .append(" ");
+                }
             }
         }
 
