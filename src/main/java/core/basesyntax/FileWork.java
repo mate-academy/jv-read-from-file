2a9e016a5bac44bc9ed5c7eb9bec3f5857ffe470
vuyko23 <mykolasakulych@gmail.com>
2021-04-05 12:59:09 +0300

    fixed solution v2

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,7 +1,6 @@
 package core.basesyntax;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -12,10 +11,9 @@ public class FileWork {
     private static final String START_CHARACTER = "w";
 
     public String[] readFromFile(String fileName) {
-        File file = new File(fileName);
         List<String> comparingResult = new ArrayList<>();
         StringBuilder builder = new StringBuilder();
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
             String readerValue = bufferedReader.readLine();
             while (readerValue != null) {
                 builder.append(readerValue).append(System.lineSeparator());
