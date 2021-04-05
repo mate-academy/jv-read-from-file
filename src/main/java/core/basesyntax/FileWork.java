d03ed916d06e0b93211b70f5c897d4c4bfc9e827
Aalderaan <chyregd@gmail.com>
2021-04-05 11:12:25 +0200

    fixed mistakes. Changed StringBuilder to List<>

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,50 +1,32 @@
 package core.basesyntax;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class FileWork {
-    private static final String SPECIFIED_CHARACTER = "w";
+    private static final char SPECIFIED_CHARACTER = 'w';
 
     public String[] readFromFile(String fileName) {
-
-        File file = new File(fileName);
-        StringBuilder stringBuilder = new StringBuilder();
-
+        List<String> listOfWords = new ArrayList<>();
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(file));
-            String value = reader.readLine();
-            while (value != null) {
-                stringBuilder.append(value).append(" ");
-                value = reader.readLine();
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            String line = reader.readLine();
+            while (line != null) {
+                for (String word : line.split(" ")) {
+                    if (word.toLowerCase().charAt(0) == SPECIFIED_CHARACTER) {
+                        listOfWords.add(word.toLowerCase().replaceAll("\\p{P}", ""));
+                    }
+                }
+                line = reader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read the file...");
         }
-
-        int count = 0;
-        String[] arrayOfWords = stringBuilder.toString().toLowerCase()
-                .replaceAll("[^a-z ^A-Z]", "").split(" ");
-        for (String words : arrayOfWords) {
-            if (words.startsWith(SPECIFIED_CHARACTER)) {
-                count++;
-            }
-        }
-        if (count == 0) {
-            return new String[]{};
-        }
-        int index = -1;
-        String[] stringWithSpecifiedLetter = new String[count];
-        for (String word : arrayOfWords) {
-            if (word.startsWith(SPECIFIED_CHARACTER)) {
-                index++;
-                stringWithSpecifiedLetter[index] = word;
-            }
-        }
-        Arrays.sort(stringWithSpecifiedLetter);
-        return stringWithSpecifiedLetter;
+        Collections.sort(listOfWords);
+        return listOfWords.toArray(new String[]{});
     }
 }
