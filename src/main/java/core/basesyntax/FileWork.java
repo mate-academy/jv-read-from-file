17e9615f6c507cf01b84b430ecc612b2fd6b1e47
Veronika-Alekseienko <alekseienko.n@gmail.com>
2021-04-05 18:18:39 +0300

    fixed solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -6,30 +6,29 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class FileWork {
-    public static final String SPECIFIED_CHARACTER = "w";
-    public static final String SEPARATOR = " ";
+    private static final String SPECIFIED_CHARACTER = "w";
+    private static final String SEPARATOR = "\\W+";
 
     public String[] readFromFile(String fileName) {
+        StringBuilder stringBuilder = new StringBuilder();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
-            StringBuilder stringBuilder = new StringBuilder();
-            for (String value = bufferedReader.readLine();
-                    value != null; value = bufferedReader.readLine()) {
-                for (String word : value.split(SEPARATOR)) {
-                    if (word.toLowerCase().startsWith(SPECIFIED_CHARACTER)) {
-                        stringBuilder.append(word.toLowerCase()
-                                .replaceAll("[.,!?]", ""))
+            for (String line = bufferedReader.readLine();
+                    line != null; line = bufferedReader.readLine()) {
+                for (String word : line.toLowerCase().split(SEPARATOR)) {
+                    if (word.startsWith(SPECIFIED_CHARACTER)) {
+                        stringBuilder.append(word.replace(SEPARATOR, ""))
                                 .append(System.lineSeparator());
                     }
                 }
             }
-            if (stringBuilder.length() > 1) {
-                String[] result = stringBuilder.toString().split(System.lineSeparator());
-                Arrays.sort(result);
-                return result;
-            }
-            return new String[0];
         } catch (IOException e) {
             throw new RuntimeException("Can't read the file", e);
         }
+        if (stringBuilder.length() > 1) {
+            String[] result = stringBuilder.toString().split(System.lineSeparator());
+            Arrays.sort(result);
+            return result;
+        }
+        return new String[0];
     }
 }
