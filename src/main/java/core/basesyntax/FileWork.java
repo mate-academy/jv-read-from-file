a646396acd987ca0c26c324c1d776554d591a436
Bohdan Borsh <borshbohdan@gmail.com>
2021-04-05 03:57:26 +0300

    First try.

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,7 +1,57 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
-    public String[] readFromFile(String fileName) {
-        return null;
+    private static final Character SPECIFIED_CHARACTER_LOWER = 'w';
+    private static final Character SPECIFIED_CHARACTER_UPPER = 'W';
+    private static final int ASCI_UPPER_START = 65;
+    private static final int ASCI_UPPER_END = 90;
+    private static final int ASCI_LOWER_START = 97;
+    private static final int ASCI_LOWER_end = 122;
+
+    public String[] readFromFile(String fileName) throws Exception {
+        StringBuilder wordsArrayBuilder = new StringBuilder();
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+            int value = bufferedReader.read();
+            while (value != -1) {
+                if (isSpecified(value)) {
+                    while (valueIsLetter(value)) {
+                        wordsArrayBuilder.append((char) value);
+                        value = bufferedReader.read();
+                    }
+                    wordsArrayBuilder.append(" ");
+                } else {
+                    while (valueIsLetter(value)) {
+                        value = bufferedReader.read();
+                    }
+                }
+                value = bufferedReader.read();
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to read the file");
+        }
+        if (wordsArrayBuilder.toString().equals("")) {
+            return new String[]{};
+        }
+        String[] wordsStartingWithW = wordsArrayBuilder.toString().toLowerCase().split(" ");
+        Arrays.sort(wordsStartingWithW);
+        return wordsStartingWithW;
+    }
+
+    private boolean isSpecified(int firstChar) {
+        return (char) firstChar == SPECIFIED_CHARACTER_LOWER
+                || firstChar == SPECIFIED_CHARACTER_UPPER;
+    }
+
+    private boolean valueIsLetter(int value) {
+        return value >= ASCI_UPPER_START && value <= ASCI_UPPER_END
+                || value >= ASCI_LOWER_START && value <= ASCI_LOWER_end;
+
     }
 }
--- src/test/java/core/basesyntax/FileWorkTest.java
+++ src/test/java/core/basesyntax/FileWorkTest.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 import org.junit.Assert;
 import org.junit.Test;
+import java.io.IOException;
 
 import java.util.Arrays;
 
@@ -21,7 +22,7 @@ public class FileWorkTest {
             new String[]{"was", "was", "whenever", "which", "which", "worse"};
 
     @Test
-    public void readFromEmptyFile() {
+    public void readFromEmptyFile() throws Exception {
         FileWork fileWork = new FileWork();
         String[] actualResult = fileWork.readFromFile(FIRST_FILE_NAME);
         Assert.assertArrayEquals("Test failed! You should returned empty array.",
@@ -29,7 +30,7 @@ public class FileWorkTest {
     }
 
     @Test
-    public void getLowerCaseResultFromFile() {
+    public void getLowerCaseResultFromFile() throws Exception {
         FileWork fileWork = new FileWork();
         String[] actualResult = fileWork.readFromFile(SECOND_FILE_NAME);
         Assert.assertArrayEquals("Test failed! You should returned next array "
@@ -39,7 +40,7 @@ public class FileWorkTest {
     }
 
     @Test
-    public void getCamelCaseResultFromFile() {
+    public void getCamelCaseResultFromFile() throws Exception {
         FileWork fileWork = new FileWork();
         String[] actualResult = fileWork.readFromFile(FOURS_FILE_NAME);
         Assert.assertArrayEquals("Test failed! You should returned next array "
@@ -49,7 +50,7 @@ public class FileWorkTest {
     }
 
     @Test
-    public void getEmptyResultFromFile() {
+    public void getEmptyResultFromFile() throws Exception {
         FileWork fileWork = new FileWork();
         String[] actualResult = fileWork.readFromFile(THIRD_FILE_NAME);
         Assert.assertArrayEquals("Test failed! You should returned empty array.",
@@ -57,7 +58,7 @@ public class FileWorkTest {
     }
 
     @Test
-    public void getAdjacentWordsResultFromFile() {
+    public void getAdjacentWordsResultFromFile() throws Exception {
         FileWork fileWork = new FileWork();
         String[] actualResult = fileWork.readFromFile(FIFTH_FILE_NAME);
         Assert.assertArrayEquals("Test failed! You should returned next array "
