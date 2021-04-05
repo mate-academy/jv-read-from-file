404b076a80fd343609c778ed756b25f61e7566bc
Vlad-Andr <andreev.v99@gmail.com>
2021-04-05 11:43:42 +0300

    fixed required issues

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -5,21 +5,17 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class FileWork {
     private static final String SPECIFIED_CHARACTER = "w";
-    private static final String SPACE = "";
-    private static final String SPECIAL_CHARACTERS = "[,\\s\\-:\\?]";
-    private static final String ONLY_WORD_PATTERN = "[^a-z A-Z]";
+    private static final String SPECIAL_CHARACTERS = "[,.\\s\\-:?!]";
 
     public String[] readFromFile(String fileName) {
-        StringBuilder stringBuilder = new StringBuilder();
+        StringBuilder filteredWords = new StringBuilder();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
             String value = bufferedReader.readLine();
             while (value != null) {
-                stringBuilder.append(value.toLowerCase()).append(System.lineSeparator());
+                filteredWords.append(value.toLowerCase()).append(System.lineSeparator());
                 value = bufferedReader.readLine();
             }
         } catch (FileNotFoundException e) {
@@ -28,19 +24,19 @@ public class FileWork {
             throw new RuntimeException("Can't read from file", e);
         }
 
-        String[] array = stringBuilder.toString().split(SPECIAL_CHARACTERS);
-        int k = 0;
+        String[] array = filteredWords.toString().split(SPECIAL_CHARACTERS);
+        int countWithLetterW = 0;
         for (String word : array) {
             if (startWithLetter(word)) {
-                k++;
+                countWithLetterW++;
             }
         }
 
         int i = 0;
-        String[] startWithW = new String[k];
+        String[] startWithW = new String[countWithLetterW];
         for (String word : array) {
             if (startWithLetter(word)) {
-                startWithW[i] = getOnlyWord(word);
+                startWithW[i] = word;
                 i++;
             }
         }
@@ -51,11 +47,5 @@ public class FileWork {
 
     private boolean startWithLetter(String word) {
         return word.startsWith(SPECIFIED_CHARACTER);
-    }
-
-    private static String getOnlyWord(String str) {
-        Pattern pattern = Pattern.compile(ONLY_WORD_PATTERN);
-        Matcher matcher = pattern.matcher(str);
-        return matcher.replaceAll(SPACE);
     }
 }
