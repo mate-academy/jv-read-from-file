57b32565c272f4a82b6be75fd6c21ffe790a7bdf
Cyouffy <cyouffy@gmail.com>
2021-04-05 14:11:15 +0300

    fixed suggested changes

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -13,22 +13,20 @@ public class FileWork {
         File file = new File(fileName);
         String[] result = new String[0];
 
-        if (isFileEmpty(file) == false) {
+        if (file.length() != 0) {
             String[] fileDataArray = readFile(file);
             StringBuilder wordsStartsW = new StringBuilder();
 
             for (int i = 0; i < fileDataArray.length; i++) {
                 if (startWithLetter(fileDataArray[i])) {
-                    wordsStartsW.append(fileDataArray[i]).append(" ");
+                    wordsStartsW.append(fileDataArray[i].replaceAll("[^\\p{Alnum}\\s]", ""))
+                            .append(" ");
                 }
             }
 
             if (wordsStartsW.length() > 0) {
                 result = wordsStartsW.toString().split(" ");
                 Arrays.sort(result);
-                for (int i = 0; i < result.length; i++) {
-                    result[i] = result[i].replaceAll("[^\\p{Alnum}\\s]", "");
-                }
             }
         }
         return result;
@@ -36,24 +34,19 @@ public class FileWork {
 
     private String[] readFile(File file) {
         StringBuilder wordsArray = new StringBuilder();
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(file));
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))){
             String value = reader.readLine();
             while (value != null) {
                 wordsArray.append(value).append(" ");
                 value = reader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can't read the file");
+            throw new RuntimeException("Can't read the file", e);
         }
         return wordsArray.toString().toLowerCase().split(" ");
     }
 
     public boolean startWithLetter(String word) {
         return word.startsWith(SPECIFIED_CHARACTER);
-    }
-
-    public boolean isFileEmpty(File file) {
-        return file.length() == 0;
     }
 }
