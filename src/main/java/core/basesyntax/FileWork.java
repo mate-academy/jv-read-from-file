0c996ee34b91305de275c5836faff1de78697897
Anton Haniyev <anton199906@gmail.com>
2021-04-06 15:15:17 +0300

    added solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -8,18 +8,18 @@ import java.util.Collections;
 import java.util.List;
 
 public class FileWork {
-    public String[] readFromFile(String fileName) {
-        List<String> result = new ArrayList<>();
+    private static final char CONDITIONAL_LATTER = 'w';
 
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(fileName));
-            StringBuilder stringBuilder = new StringBuilder();
+    public String[] readFromFile(String fileName) {
+        List<String> filteredWords = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+
             String value = reader.readLine();
             while (value != null) {
 
-                for (String word : value.split(" ")) {
-                    if (word.toLowerCase().charAt(0) == 'w') {
-                        result.add(word.toLowerCase().replaceAll("\\p{P}", ""));
+                for (String word : value.toLowerCase().split("\\W+")) {
+                    if (word.startsWith(String.valueOf(CONDITIONAL_LATTER))) {
+                        filteredWords.add(word.replaceAll("\\p{P}", ""));
                     }
                 }
                 value = reader.readLine();
@@ -28,8 +28,8 @@ public class FileWork {
         } catch (IOException e) {
             throw new RuntimeException("Can't read file", e);
         }
-        Collections.sort(result);
+        Collections.sort(filteredWords);
 
-        return result.toArray(new String[0]);
+        return filteredWords.toArray(new String[0]);
     }
 }
