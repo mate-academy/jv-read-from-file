6a3d9df07bf3c0448f93693751349fe1d37b0b98
l_osyash <los.nazar24@gmail.com>
2022-04-14 19:36:42 +0300

    Implemented method readFromFile in class FileWork.

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,13 +1,40 @@
 package core.basesyntax;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 public class FileWork {
+    private static final char START_LETTER = 'w';
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-
-
-        return null;
+        String fileString = readFile(fileName);
+        if (!fileString.equals("")) {
+            String[] words = splitFileString(fileString);
+            StringBuilder builder = new StringBuilder();
+            for (String word : words) {
+                if (startWithLetter(word)) {
+                    builder.append(word).append(" ");
+                }
+            }
+            if (builder.toString().length() != 0) {
+                String[] resultArray = builder.toString().split(" ");
+                return sortArray(resultArray);
+            }
+        }
+        return new String[]{};
+    }
+    
+    public String[] sortArray(String[] resultArray) {
+        List<String> arrayList = Arrays.asList(resultArray);
+        arrayList.sort(Comparator.naturalOrder());
+        String[] array = new String[arrayList.size()];
+        arrayList.toArray(array);
+        return array;
     }
 
     public String readFile(String fileName) {
@@ -17,10 +44,23 @@ public class FileWork {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
             StringBuilder builder = new StringBuilder();
             string = bufferedReader.readLine();
-            builder.append(string);
+            while (string != null) {
+                builder.append(string).append(System.lineSeparator());
+                string = bufferedReader.readLine();
+            }
+            return builder.toString();
         } catch (IOException e) {
             throw new RuntimeException("Can't read file", e);
         }
-        return string;
+    }
+
+    public String[] splitFileString(String fileString) {
+        fileString = fileString.toLowerCase();
+        return fileString.split("\\W+");
+    }
+
+    public boolean startWithLetter(String word) {
+        char[] wordChars = word.toCharArray();
+        return wordChars[0] == START_LETTER;
     }
 }
