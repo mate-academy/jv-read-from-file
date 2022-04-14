bd6f7df41e74a3cbdad12b0b165fe2acb05b4fef
l_osyash <los.nazar24@gmail.com>
2022-04-14 12:38:30 +0300

    Implemented method readFile

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,26 @@
 package core.basesyntax;
 
+import java.io.*;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
         //write your code here
+
+
         return null;
+    }
+
+    public String readFile(String fileName) {
+        File file = new File(fileName);
+        String string;
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            StringBuilder builder = new StringBuilder();
+            string = bufferedReader.readLine();
+            builder.append(string);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        return string;
     }
 }
