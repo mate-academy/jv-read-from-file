fab78bf7d7b710534ae09bd5a593b0a7d951c7e8
Dmytro Getmanskyy <getmanskyy.dmitry@gmail.com>
2022-05-03 11:59:11 +0300

    implement task
    - add public method readFromFile() in class ReadDataFromFile, which read data from file and return a string;
    - add public method processData() in class ProcessData, which process input string and return filtered and sorted array;
    - add public method readFromFile() in class FileWork, which join above functional.

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,16 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+
 public class FileWork {
+    public static void main(String[] args) {
+        FileWork data = new FileWork();
+        System.out.println("Output array of strings: "
+                + Arrays.toString(data.readFromFile("test2")));
+    }
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        String data = new ReadDataFromFile().readFromFile(fileName);
+        return new ProcessData().processData(data);
     }
 }
