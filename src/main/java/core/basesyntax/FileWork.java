f96699a1008f975d9095a9a06ce46cc47817c7e5
Oleksii Mospan <alexey.mospan@gmail.com>
2022-05-09 18:11:22 +0300

    Changed order of imports

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,10 +1,10 @@
 package core.basesyntax;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.File;
-import java.io.IOException;
+import java.io.FileReader;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Arrays;
 
 public class FileWork {
