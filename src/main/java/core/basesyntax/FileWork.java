760ba13149efe112304df9acb2eafeebb3b4bb89
Viacheslav Bondar <mcx.bondar@outlook.com>
2022-05-11 15:23:03 +0300

    done hw-read-from-file-solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,42 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final int EMPTY_ARRAY_LENGTH = 0;
+    private static final int EMPTY_BUILDER = 0;
+    private final StringBuilder builder = new StringBuilder();
+    private BufferedReader reader;
+    private String[] resultArray;
+    private String resultString = "";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        try {
+            reader = new BufferedReader(new FileReader(fileName));
+            String value = reader.readLine();
+            while (value != null) {
+                builder.append(value).append(System.lineSeparator());
+                value = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read file", e);
+        }
+        resultArray = builder.toString().toLowerCase().split("\\W+");
+        builder.setLength(EMPTY_BUILDER);
+        for (String str : resultArray) {
+            if (str.startsWith("w")) {
+                builder.append(str).append(" ");
+            }
+        }
+        resultString = builder.toString();
+        if (resultString.isEmpty()) {
+            return new String[EMPTY_ARRAY_LENGTH];
+        }
+        resultArray = resultString.split(" ");
+        Arrays.sort(resultArray);
+        return resultArray;
     }
 }
