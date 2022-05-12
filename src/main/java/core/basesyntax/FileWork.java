e53f0520c6a1c41f5cdfd486af0846ab0242c30d
Benatti <ssunica0@gmail.com>
2022-05-13 00:24:30 +0300

    Complete Reading from file Task

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,43 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Locale;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+
+        StringBuilder stringBuilder = new StringBuilder();
+        String[] arr = new String[0];
+        try (BufferedReader buffer = new BufferedReader(new FileReader(fileName))) {
+            int value = buffer.read();
+            while (value != -1) {
+                if ((char)value == 'W' || (char)value == 'w') {
+                    while (value >= 65 && value <= 90 || value >= 97 && value <= 122) {
+                        stringBuilder.append((char)value);
+                        value = buffer.read();
+                    }
+                    arr = Arrays.copyOf(arr, arr.length + 1);
+                    arr[arr.length - 1] = stringBuilder.toString().toLowerCase(Locale.ROOT);
+                    stringBuilder.setLength(0);
+                }
+                else if (value >= 65 && value <= 90 || value >= 97 && value <= 122) {
+                    while (value >= 65 && value <= 90 || value >= 97 && value <= 122) {
+                        value = buffer.read();
+                    }
+                } else {
+                    value = buffer.read();
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Can't find file", e);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to file", e);
+        }
+        Arrays.sort(arr);
+        return arr;
     }
 }
