fb846c06f7ad50ba592cd3878011277c822be86b
Viacheslav Bondar <mcx.bondar@outlook.com>
2022-05-12 11:27:52 +0300

    fixed hw-read-from-file-solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -9,13 +9,10 @@ public class FileWork {
     private static final int EMPTY_ARRAY_LENGTH = 0;
     private static final int EMPTY_BUILDER = 0;
     private final StringBuilder builder = new StringBuilder();
-    private BufferedReader reader;
-    private String[] resultArray;
-    private String resultString = "";
 
     public String[] readFromFile(String fileName) {
         try {
-            reader = new BufferedReader(new FileReader(fileName));
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
             String value = reader.readLine();
             while (value != null) {
                 builder.append(value).append(System.lineSeparator());
@@ -24,14 +21,14 @@ public class FileWork {
         } catch (IOException e) {
             throw new RuntimeException("Can`t read file", e);
         }
-        resultArray = builder.toString().toLowerCase().split("\\W+");
+        String[] resultArray = builder.toString().toLowerCase().split("\\W+");
         builder.setLength(EMPTY_BUILDER);
         for (String str : resultArray) {
             if (str.startsWith("w")) {
                 builder.append(str).append(" ");
             }
         }
-        resultString = builder.toString();
+        String resultString = builder.toString();
         if (resultString.isEmpty()) {
             return new String[EMPTY_ARRAY_LENGTH];
         }
