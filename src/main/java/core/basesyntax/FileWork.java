75d1dc9bac013d93e0f3277db687ce6cc6a26460
Pavlo99-gif <kurulchyk7355608@gmail.com>
2022-05-18 14:32:09 +0300

    The task is completed, avoid common mistakes

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,32 +1,37 @@
 package core.basesyntax;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Arrays;
-import java.util.Locale;
 
 public class FileWork {
+    public static final char SPECIFIED_CHARACTER = 'w';
+    public static final int FIRST_LATER = 0;
+
     public String[] readFromFile(String fileName) {
         File file = new File(fileName);
         StringBuilder builder = new StringBuilder();
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(file));
-            int value = reader.read();
-            while (value != -1) {
-                builder.append((char) value);
-                value = reader.read();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            String fileString = bufferedReader.readLine();
+            if (fileString == null) {
+                return new String[0];
+            }
+            while (fileString != null) {
+                builder.append(fileString)
+                        .append(" ");
+                fileString = bufferedReader.readLine();
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can`t read file");
+            throw new RuntimeException("Can`t read file ", e);
         }
-        if (builder.toString().isEmpty()) {
-            return new String[0];
-        }
-        String[] strings = builder.toString().replaceAll(System.lineSeparator(), " ")
-                .replaceAll("[^a-zA-Z ]", "").toLowerCase().split(" ");
+        String[] strings = builder.toString().replaceAll("[^a-zA-Z ]", "")
+                .toLowerCase().split(" ");
         builder = new StringBuilder();
-        for (String string: strings) {
-            if (string.charAt(0) == 'w') {
-                builder.append(string)
+        for (String word: strings) {
+            if (word.charAt(FIRST_LATER) == SPECIFIED_CHARACTER) {
+                builder.append(word)
                         .append(" ");
             }
         }
