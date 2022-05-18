b38d7b6ccb97ff4e5d3807a253f5f2c34b7016e9
Pavlo99-gif <kurulchyk7355608@gmail.com>
2022-05-18 13:44:06 +0300

    first commit

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,40 @@
 package core.basesyntax;
 
+import java.io.*;
+import java.util.Arrays;
+import java.util.Locale;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        StringBuilder builder = new StringBuilder();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            int value = reader.read();
+            while (value != -1) {
+                builder.append((char) value);
+                value = reader.read();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read file");
+        }
+        if (builder.toString().isEmpty()) {
+            return new String[0];
+        }
+        String[] strings = builder.toString().replaceAll(System.lineSeparator(), " ")
+                .replaceAll("[^a-zA-Z ]", "").toLowerCase().split(" ");
+        builder = new StringBuilder();
+        for (String string: strings) {
+            if (string.charAt(0) == 'w') {
+                builder.append(string)
+                        .append(" ");
+            }
+        }
+        if (builder.toString().isEmpty()) {
+            return new String[0];
+        }
+        strings = builder.toString().split(" ");
+        Arrays.sort(strings);
+        return strings;
     }
 }
