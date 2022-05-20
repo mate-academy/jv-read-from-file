567bdbdd452a8b5985190659895a3b39b5e3cbd6
Zinchuk-Yevhenii-Mate <zenya87@gmail.com>
2022-05-20 17:33:40 +0300

    realized metod readfromfile which returned array with words

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,35 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String LETTER = "w";
+    private static final String SEPARATOR = " ";
+    private static final String DELETED_SYMBOL = "(?u)[^\\pL ]";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            String stringFromFile = Files.readString(Path.of(fileName));
+            String allString = stringFromFile.replaceAll(DELETED_SYMBOL, SEPARATOR);
+            String[] allStringArray = allString.toLowerCase().split(" ");
+            for (String stringFile : allStringArray) {
+                if (stringFile.startsWith(LETTER)) {
+                    stringBuilder.append(stringFile).append(" ");
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        if (stringBuilder.length() == 0) {
+            return new String[0];
+        }
+        String [] resultArray = stringBuilder.toString().split(" ");
+        Arrays.sort(resultArray);
+        return resultArray;
     }
 }
+
