3bbfe6052cf45c6ed1aa4ec90174a04c7140753e
Алена <258258>
2022-05-21 18:21:31 +0300

    Added filter words by letter "w".

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,36 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        String SPECIFIED_CHARACTER = "w";
+        File file = new File(fileName);
+        StringBuilder stringLine = new StringBuilder();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String value = reader.readLine();
+            while (value != null) {
+                String[] arrayWords = value.toLowerCase().split(" ");
+                for (String word : arrayWords) {
+                    if (word.startsWith(SPECIFIED_CHARACTER)) {
+                        stringLine.append(word).append(" ");
+                    }
+                }
+                value = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read the file: " + fileName);
+        }
+        String clearValue = stringLine.toString().replaceAll("\\pP", "");
+        String[] wordsWithW = clearValue.split(" ");
+        Arrays.sort(wordsWithW);
+        return stringLine.length() != 0 ? wordsWithW : new String[]{};
     }
 }
