ad0f9f74d81f248612d6a1081b8203df441e5e77
Ivan Kunitskiy <kunitskivan@gmail.com>
2022-05-22 10:13:23 +0300

    Added a function that reads files content and copies all words that start with "w" into an array

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,34 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String WORD_IDENTIFIER = "w";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            StringBuilder builder = new StringBuilder();
+            String value;
+            while ((value = bufferedReader.readLine()) != null) {
+                String[] rowArray = value.toLowerCase().split("\\W");
+                for (String word : rowArray) {
+                    if (word.startsWith(WORD_IDENTIFIER)) {
+                        builder.append(word.toLowerCase())
+                                .append(" ");
+                    }
+                }
+            }
+            if (builder.length() == 0) {
+                return new String[]{};
+            }
+            String[] specificWordArray = builder.toString().trim().split(" ");
+            Arrays.sort(specificWordArray);
+            return specificWordArray;
+        } catch (IOException e) {
+            throw new RuntimeException("The file is blank!", e);
+        }
     }
 }
