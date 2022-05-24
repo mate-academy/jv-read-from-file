1e663d7f297972c86d78efe776c90bb9d30509b2
Illia Khomych <illia.homych@knu.ua>
2022-05-24 15:04:02 +0300

    Homework task implemented

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,10 +1,10 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class FileWork {
