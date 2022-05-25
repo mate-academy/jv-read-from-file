8bc1efe7f05fafd9e1d7dbee00544b344f2858cd
Ilya <zimmboco@gmail.com>
2022-05-25 15:06:53 +0300

    hw-6

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,30 @@
 package core.basesyntax;
 
+import java.io.*;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String SEARCH_W = "w";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        StringBuilder builder = new StringBuilder();
+        File file = new File(fileName);
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] words = line.toLowerCase().split(" ");
+                for (String word : words) {
+                    if (word.startsWith(SEARCH_W)) {
+                        builder.append(word).append(" ");
+                    }
+                }
+            }
+        }  catch (IOException e) {
+            throw new RuntimeException("Can not read the files:" + fileName);
+        }
+        String clearWord = builder.toString().replaceAll("[!-?-,]", "");
+        String[] result = clearWord.split(" ");
+        Arrays.sort(result);
+        return builder.length() != 0 ? result : new String[]{};
     }
 }
