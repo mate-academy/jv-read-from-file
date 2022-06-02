3b097e4a58ac0cc5cda5c496332927c2956f6db2
Sergey Gordeychuk <ya.gordeichuk@gmail.com>
2022-06-03 00:06:42 +0300

    made -read-from-file

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,51 @@
 package core.basesyntax;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        String fileSepar = "";
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            StringBuilder stringBuilder = new StringBuilder();
+            String value = reader.readLine();
+            while (value != null) {
+                stringBuilder.append(value).append(System.lineSeparator());
+                value = reader.readLine();
+            }
+            if (stringBuilder.length() == 0) {
+                return new String[]{};
+            }
+            fileSepar = stringBuilder.toString();
+
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        int count = 0;
+        String fileNameLowerCase = fileSepar.toLowerCase();
+        String[] stringList1 = fileNameLowerCase.split("\\W+");
+        for (String word : stringList1) {
+            char[] x = word.toCharArray();
+            if (x[0] == 'w') {
+                count++;
+            }
+        }
+        String[] stringList = new String[count];
+        int j = 0;
+        for (int i = 0; i < stringList1.length; i++) {
+            char[] x = stringList1[i].toCharArray();
+            if (x[0] == 'w') {
+                stringList[j] = stringList1[i];
+                j++;
+            }
+        }
+        Arrays.sort(stringList);
+        return stringList;
     }
 }
