760b40b22e570c173df58d0a49dcb88b4bdff3d7
Serhii Buria <sergeiburya@gmail.com>
2022-06-27 12:32:52 +0300

    initial new solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,35 @@
 package core.basesyntax;
 
+import java.io.*;
+
 public class FileWork {
+    private static final String SPECIFIED_CHARACTER = "w";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        String[] fileWork = null;
+        File file = new File(fileName);
+        if (fileIsNotEmpty(fileName)) {
+            try {
+                BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+                System.out.println(bufferedReader.read());
+
+
+            } catch (IOException e) {
+                throw new RuntimeException("Can't to read file", e);
+            }
+
+
+            return null;
+        }
+        return fileWork;
+    }
+
+    private boolean fileIsNotEmpty(String fileName) {
+        File file = new File(fileName);
+        return !file.exists();
+    }
+
+    private boolean startWithLetter(String word) {
+        return word.startsWith(SPECIFIED_CHARACTER);
     }
 }
---
+++ src/main/java/core/basesyntax/MainFileWork.java
@@ -0,0 +1,27 @@
+package core.basesyntax;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class MainFileWork {
+    public static void main(String[] args) {
+
+      FileWork fileWork = new FileWork();
+      System.out.println(fileWork.readFromFile("test1"));
+
+
+//        File file = new File("test2");
+//      try {
+//            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+//            System.out.println(bufferedReader.read());
+//        } catch (IOException e) {
+//            throw new RuntimeException("No file", e);
+//     }
+//      Path parentDir = Paths.get("test2");
+//      System.out.println(parentDir.getParent());
+//      System.out.println(parentDir.toAbsolutePath().toString());
+
+
+    }
+}
