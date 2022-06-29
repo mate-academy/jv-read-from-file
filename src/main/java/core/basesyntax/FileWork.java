62505e8639b94cf9db4012f3654fd1db7636c9e6
Serhii Buria <sergeiburya@gmail.com>
2022-06-29 20:43:02 +0300

    bugs fixed aabout test

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class FileWork {
 
@@ -11,6 +13,9 @@ public class FileWork {
         File file = new File(fileName);
         StringBuilder readFile = new StringBuilder();
         StringBuilder sortWords = new StringBuilder();
+        String[] resultReadFile = {};
+        String[] sortReadFile = {};
+        boolean isNoEmpty = false;
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
             int value = bufferedReader.read();
@@ -20,17 +25,25 @@ public class FileWork {
             }
             String [] wordsReadFile = readFile.toString().toLowerCase().trim()
                         .split("[?|!|-|(|)|:|;|'|\"| |.|,|\\s+|\\t|\\n|\\r|\\f]");
+            Arrays.sort(wordsReadFile, Comparator.naturalOrder());
             int index;
             for (int i = 0; i < wordsReadFile.length; i++) {
                 index = i;
-                if (wordsReadFile[i].startsWith("w") || wordsReadFile[i].startsWith("W")) {
+                if (isSymbol(wordsReadFile[i])) {
+                    isNoEmpty = true;
                     sortWords.append(wordsReadFile[i]).append(" ");
                 }
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't to read file", e);
         }
-        String[] resultReadFile = sortWords.toString().split(" ");
+        if (isNoEmpty) {
+            return resultReadFile = sortWords.toString().split(" ");
+        }
         return resultReadFile;
+    }
+
+    private boolean isSymbol(String words){
+        return (words.startsWith("w") || words.startsWith("W"));
     }
 }
