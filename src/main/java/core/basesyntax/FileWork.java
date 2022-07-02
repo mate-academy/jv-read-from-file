3c58c62dfdb6f2b9f7f4ac710be380f2fd32669d
Oleksandr <dovy_al@ukr.net>
2022-07-02 14:51:51 +0300

    hw solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,46 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            int value = reader.read();
+            while (value != -1) {
+                stringBuilder.append((char) value);
+                value = reader.read();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("file not found", e);
+        }
+        if (stringBuilder.isEmpty()) {
+            return new String[0];
+        }
+        String[] split = stringBuilder.toString().toLowerCase().split("\\W+");
+        int n = 0;
+        for (String abc : split) {
+            char[] abcChar = abc.toCharArray();
+            if (abcChar[0] == 'w') {
+                n++;
+            }
+        }
+        String[] onlyW = new String[n];
+        n = 0;
+        for (String abc : split) {
+            char[] abcChar = abc.toCharArray();
+            if (abcChar[0] == 'w') {
+                onlyW[n] = abc;
+                n++;
+            }
+        }
+        Arrays.sort(onlyW);
+        return onlyW;
     }
 }
