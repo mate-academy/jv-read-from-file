807345e811b5621bd5b60c66b867edde71d955c3
Azarov Artur <azarovartyr@gmail.com>
2022-07-14 01:01:38 +0300

    done

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,46 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String SPECIFIED_CHARACTER = "w";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        StringBuilder stringBuilder = new StringBuilder();
+        String temporaryString;
+        String[] result = new String[0];
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            temporaryString = bufferedReader.readLine();
+            while (temporaryString != null) {
+                stringBuilder.append(temporaryString);
+                stringBuilder.append(" ");
+                temporaryString = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read a file", e);
+        }
+        temporaryString = stringBuilder.toString().toLowerCase();
+        String[] array = temporaryString.split("\\W+");
+        stringBuilder.setLength(0);
+
+        for (String word : array) {
+            if (word.startsWith(SPECIFIED_CHARACTER)) {
+                stringBuilder.append(word);
+                stringBuilder.append(" ");
+            }
+        }
+
+        temporaryString = stringBuilder.toString();
+        if (temporaryString.length() > 0) {
+            result = temporaryString.split(" ");
+            Arrays.sort(result);
+        }
+        return result;
     }
 }
+
