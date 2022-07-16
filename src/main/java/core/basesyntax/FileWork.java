eca18c628d150610b9851fa56da754deb3af63c0
Serhii Nazarenko <nasarenko82@gmail.com>
2022-07-16 18:55:40 +0300

    performed for the first time

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,45 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private int length;
+    private int count = 0;
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File myFile = new File(fileName);
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(myFile));
+            int value = reader.read();
+            if (value == -1) {
+                return new String[0];
+            }
+            while (value != -1) {
+                stringBuilder.append((char) value);
+                value = reader.read();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read file", e);
+        }
+        String[] strings = stringBuilder.toString().split("\\W+");
+        for (String string : strings) {
+            if (string.substring(0, 1).toLowerCase().equals("w")) {
+                length++;
+            }
+        }
+        String[] arrayString = new String[length];
+        for (String string : strings) {
+            if (string.substring(0, 1).toLowerCase().equals("w")) {
+                arrayString[count] = string.toLowerCase();
+                count++;
+            }
+        }
+        Arrays.sort(arrayString);
+        return arrayString;
     }
 }
