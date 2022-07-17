7375420a3ff16b786aec13fec9c59d612bf59401
Serhii Davydov <serhii.davydov@gmail.com>
2022-07-17 16:45:40 +0300

    Fix issues and refactoring

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -8,34 +8,30 @@ import java.util.Arrays;
 public class FileWork {
 
     public String[] readFromFile(String fileName) {
-        StringBuilder builder = null;
+        StringBuilder builder = new StringBuilder();
         try {
             BufferedReader reader = new BufferedReader(new FileReader(fileName));
-            builder = new StringBuilder();
-            int value = reader.read();
-            while (value != -1) {
-                if (value == 87 || value == 119) {
-                    builder.append((char) value);
-                    value = reader.read();
-                    while (value != 20 && value != 44 && value != 46 && value != 33 && value != 40
-                            && value != 41 && value != 45 && value != 34 && value != 32
-                            && value != 63) {
-                        builder.append((char) value);
-                        value = reader.read();
+            while (reader.ready()) {
+                String[] line = reader.readLine().split("\\s");
+                for (String word : line) {
+                    if (word.charAt(0) == 'W' || word.charAt(0) == 'w') {
+                        word = word.replaceAll("[^a-zA-Z]", "");
+                        builder.append(word).append(" ");
                     }
-                    builder.append(" ");
                 }
-                value = reader.read();
             }
         } catch (IOException e) {
-            throw new RuntimeException("File can't read.", e);
+            throw new RuntimeException(e);
         }
-        String[] wordsStartW = builder.toString().split(" ");
+        if (builder.length() == 0) {
+            return new String[0];
+        }
+        String[] wordsStartW = builder.toString().trim().split("\\s");
+        System.out.println(wordsStartW.length);
         for (int i = 0; i < wordsStartW.length; i++) {
             wordsStartW[i] = wordsStartW[i].toLowerCase();
         }
         Arrays.sort(wordsStartW);
         return wordsStartW;
     }
-
 }
