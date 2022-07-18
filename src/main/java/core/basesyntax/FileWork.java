17dee44b9b2949e214b78601dc65c3e6af576b15
Dmytro Stakhiv <dstahiv@gmail.com>
2022-07-19 00:22:14 +0300

    Solution FileWork

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,45 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class FileWork {
+    private static final String SPECIFIED_CHARACTER = "w";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+            StringBuilder builder = new StringBuilder();
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                builder.append(value).append(" ");
+                value = bufferedReader.readLine();
+            }
+            Pattern re = Pattern.compile("[^A-Za-z0-9 ]+",Pattern.CASE_INSENSITIVE);
+            Matcher m = re.matcher(builder.toString());
+            String [] anotherValue = m.replaceAll("").toLowerCase().split(" ");
+            StringBuilder stringBuilder = new StringBuilder();
+            for (String start : anotherValue) {
+                if (start.startsWith(SPECIFIED_CHARACTER)) {
+                    stringBuilder.append(start).append(System.lineSeparator());
+                }
+            }
+            if (stringBuilder.length() != 0) {
+                String[] result = stringBuilder.toString().split(System.lineSeparator());
+                Arrays.sort(result);
+                return result;
+            } else {
+                String[] result = new String[0];
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        return new String[0];
     }
 }
+
+
