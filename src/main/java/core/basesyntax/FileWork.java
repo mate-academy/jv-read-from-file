b986abdd81821a4925d8ea95bb0a0f408c3dcdbc
Mykyta Hulianytskyi <yadiver82@gmail.com>
2022-07-24 20:21:51 +0300

    v0.0 FileReader was created all files were read, all words with 'w'-starting were found and sorted, and added to one String array.

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,42 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                value = value.toLowerCase().replaceAll("\\b(?!w)\\w+\\W*", "");
+                value = value.replaceAll("[.]","");
+                if (value != "") {
+                    String[] words = value.split("[\\W++&&[^-]]");
+                    stringBuilder.append(Arrays.toString(words));
+                }
+                value = bufferedReader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Can't find the file", e);
+        } catch (IOException ex) {
+            throw new RuntimeException("Can't read the file", ex);
+        }
+        String word = stringBuilder.toString().replaceAll("\\s++","");
+        if (word == "") {
+            return new String[]{};
+        }
+        word = word.replaceAll("[\\W++&&[^-]]",".");
+        word = word.replaceAll("\\B[.]","");
+        String[] sorted = word.split("[.]++");
+        Arrays.sort(sorted);
+        System.out.println(Arrays.toString(sorted));
+        return sorted;
     }
 }
