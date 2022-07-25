f431296fb808d791784202c9e7412e7b7f298953
Sasha Vradiy <alexvradii@gmail.com>
2022-07-25 13:21:45 +0300

    My solution of tusk jv-read-from-file.

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,28 @@
 package core.basesyntax;
 
+import java.io.*;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
+        File file = new File(fileName);
+        StringBuilder stringBuilder = new StringBuilder();
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                value = value.toLowerCase().replaceAll("\\b(?!w)\\w+\\W*", "");
+                value = value.replaceAll("[.]","");
+                if (value.equals("")) {
+                    String[] words = value.split("[\\W++&&[^-]]");
+                    stringBuilder.append(Arrays.toString(words));
+                }
+                value = bufferedReader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Can't find file", e);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
         return null;
     }
 }
