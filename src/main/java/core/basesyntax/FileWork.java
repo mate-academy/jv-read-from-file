91394f6ba065159ac084f732aedf58bd9b1e50d1
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-07-27 20:51:40 +0200

    add checkArrayForWords()

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,35 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+import java.util.Locale;
+
 public class FileWork {
+    private static final String REGEX_DELETE_CONDITION = "[^a-z]";
+    private static final String START_OF_WORD_CHARACTER = "w";
+    StringBuilder stringBuilder = new StringBuilder();
+
     public String[] readFromFile(String fileName) {
-        //write your code here
+
         return null;
     }
+
+    private String[] checkArrayForWords() {
+        String[] checkedWords = new String[0];
+        if (stringBuilder.length() == 0) {
+            Arrays.sort(checkedWords = stringBuilder.toString().split(" "));
+        }
+        return checkedWords;
+    }
+
+    private void getStringFromArray(String string) {
+        for (String wordFromArray : string.split(" ")) {
+            if (wordFromArray.toLowerCase().startsWith(START_OF_WORD_CHARACTER)) {
+                String resultWordFromArray = wordFromArray.toLowerCase().replaceAll(REGEX_DELETE_CONDITION, "");
+                stringBuilder.append(resultWordFromArray).append(" ");
+            }
+        }
+    }
+
+
 }
+
