8534173ed8a7c18e2e5b01ab2140d84634f2416b
Vasyl Shevtsiv <vasyl.shevtsiv@gmail.com>
2022-07-30 03:26:27 +0300

    read-from-file-hw-solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,39 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String SPECIFIED_CHARACTER = "w";
+
+    public boolean startWithLetter(String word) {
+        return word.startsWith(SPECIFIED_CHARACTER);
+    }
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        ArrayList<String> words = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+
+            String currentLine;
+
+            while ((currentLine = bufferedReader.readLine()) != null) {
+                String[] split = currentLine.split("\\W+");
+                for (String word : split) {
+                    word = word.toLowerCase();
+                    if (startWithLetter(word)) {
+                        words.add(word);
+                    }
+                }
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String[] result = words.toArray(new String[0]);
+        Arrays.sort(result);
+        return result;
     }
 }
