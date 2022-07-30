f1d837fc3f9b3d0611ee59b57c8fb7d29eba2a41
Alexhqolma <66127994+Alexhqolma@users.noreply.github.com>
2022-07-30 15:33:26 +0300

    Realised sorted Array from File

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,41 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        try {
+            BufferedReader fileRead = new BufferedReader(new FileReader(file));
+            if (fileRead.readLine() != null) {
+                String allFile = Files.readAllLines(file.toPath()).toString().toLowerCase();
+                String[] res = allFile.split("\\W+");
+                String cuttedArray = "";
+                for (int i = 0; i < res.length; i++) {
+                    if (res[i].startsWith("w")) {
+                        cuttedArray += res[i] + " ";
+                    }
+                }
+                if (!cuttedArray.isEmpty()) {
+                    String[] result = cuttedArray.split(" ");
+                    Arrays.sort(result);
+                    return result;
+                } else {
+                    String[] result = new String[0];
+                    return result;
+                }
+
+            } else {
+                String[] result = new String[0];
+                return result;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file.");
+        }
     }
 }
