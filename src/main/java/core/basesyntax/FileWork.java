ea547b00b71d87ff9a6b2e0930bbf35472d68d89
Volodymyr Barasiuk <barmalej23@gmail.com>
2022-07-31 16:53:58 +0300

    code updated with suggestions from common mistakes file

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,40 +1,43 @@
 package core.basesyntax;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.stream.Stream;
 
 public class FileWork {
-
+    private static final char UPPER_W = 'W';
+    private static final char LOWER_W = 'w';
     private static final String[] EMPTY_ARRAY = new String[0];
 
     public String[] readFromFile(String fileName) {
-        StringBuilder input = new StringBuilder();
-        StringBuilder output = new StringBuilder();
+        StringBuilder inputReader = new StringBuilder();
+        StringBuilder outputReader = new StringBuilder();
         File file = new File(fileName);
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
             int temp = bufferedReader.read();
             while (temp != -1) {
-                input.append((char) temp);
+                inputReader.append((char) temp);
                 temp = bufferedReader.read();
             }
-            String[] array = input.toString().split("[ ',.?!\n]");
+            String[] array = inputReader.toString().split("[ ',.?!\n]");
             for (String word : array) {
                 if (word.equals("")) {
                     continue;
                 }
                 char[] chars = word.toCharArray();
-                if (chars[0] == 'w' || chars[0] == 'W') {
-                    output.append(word.toLowerCase()).append(" ");
+                if (chars[0] == LOWER_W || chars[0] == UPPER_W) {
+                    outputReader.append(word.toLowerCase()).append(" ");
                 }
             }
-            bufferedReader.close();
         } catch (FileNotFoundException e) {
             throw new RuntimeException("File not found", e);
         } catch (IOException e) {
-            throw new RuntimeException("Can't read the file");
+            throw new RuntimeException("Can't read the file", e);
         }
-        String[] strings = (output.toString().split(" "));
+        String[] strings = (outputReader.toString().split(" "));
         if (strings.length == 1) {
             return EMPTY_ARRAY;
         }
