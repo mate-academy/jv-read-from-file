4e2abc31c2eb406df69e9c14657c0eaf8e530135
buuufy <s1mpleworkpost@gmail.com>
2022-08-02 16:44:30 +0300

    added solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,35 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.Locale;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        StringBuilder builder = new StringBuilder();
+        try {
+            String[] sortedArr = Files
+                    .readAllLines(file.toPath())
+                    .toString()
+                    .toLowerCase(Locale.ROOT)
+                    .split("\\W+");
+            for (int i = 1; i < sortedArr.length; i++) {
+                if (sortedArr[i].charAt(0) == 'w') {
+                    builder.append(sortedArr[i]).append(" ");
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        if (builder.toString().isEmpty()) {
+            return new String[]{};
+        } else {
+            String[] finalArr = builder.toString().split(" ");
+            Arrays.sort(finalArr);
+            return finalArr;
+        }
+
     }
 }
