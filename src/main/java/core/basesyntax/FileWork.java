8bdaa457c73c16830241011869112815be0e1f3c
Ilja Nekrituk <Nekrit99@gmail.com>
2022-08-02 23:08:41 +0300

    add catch and split

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,35 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.regex.Pattern;
+
 public class FileWork {
-    public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+    public static void main (String[]args){
+        FileWork ff = new FileWork();
+       ff.readFromFile("test2");
+    }
+    public String [] readFromFile(String fileName) {
+        File file = new File (fileName);
+        String [] allWords;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String readed = reader.readLine().toLowerCase(Locale.ROOT);
+
+            allWords = readed.split("[\\W]");
+
+            System.out.println(Arrays.toString(allWords));
+
+
+
+
+            }catch (IOException e) {
+            throw new RuntimeException(" ddd ");
+        }
+            return allWords;
     }
 }
