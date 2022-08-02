32533aca9ebd4cd5e732a75787e378818776d638
Alex Alexandrov <alexandrov.alex.al@gmail.com>
2022-08-02 17:08:50 +0300

    The suggested solution was added.

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,33 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        Queue<String> queue = new LinkedList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            do {
+                line = bufferedReader.readLine();
+                if (line != null) {
+                    String[] split = line.split("\\W+");
+                    for (String string : split) {
+                        if (string.toLowerCase().charAt(0) == 'w') {
+                            queue.add(string.toLowerCase());
+                        }
+                    }
+                }
+            } while (line != null);
+        } catch (IOException e) {
+            throw new RuntimeException("Is not possible to find the file");
+        }
+        String[] result = queue.toArray(new String[0]);
+        Arrays.sort(result);
+        return result;
     }
 }
