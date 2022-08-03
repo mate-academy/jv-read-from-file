c1e3fc2f92044b6794b5fa1d58c35304b0e300ce
Julia Kononenko <munguryan13@gmail.com>
2022-08-03 12:55:04 +0300

    implement read data from some files and work with input data

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,39 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String SPECIFIED_CHARACTER = "w";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
+        StringBuilder stringBuilder = new StringBuilder();
+        StringBuilder readValue = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            int value = bufferedReader.read();
+            while (value != -1) {
+                readValue.append((char) value);
+                value = bufferedReader.read();
+            }
+            String[] lineFromFileName = readValue.toString().split("[\\W\\s]");
+            for (String file : lineFromFileName) {
+                if (file.startsWith(SPECIFIED_CHARACTER)
+                        || file.startsWith(SPECIFIED_CHARACTER.toUpperCase())) {
+                    stringBuilder.append(file.toLowerCase()).append(" ");
+                }
+            }
+            if (stringBuilder.toString().isEmpty()) {
+                return new String[]{};
+            }
+            String[] lineWithoutW = stringBuilder.toString().split("[\\W\\s]");
+            Arrays.sort(lineWithoutW);
+            return lineWithoutW;
+
+        } catch (IOException ex) {
+            System.out.println("Exception");
+        }
         return null;
     }
 }
