1263131f37b8abe20a2a2c8ef33cada5bccef248
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-08-03 17:29:22 +0300

    one test does not pass

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,45 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        Path path = file.toPath();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        List<String> strings = null;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            strings = Files.readAllLines(path);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        if (strings.size() == 0) {
+            return new String[0];
+        }
+        for (String str : strings) {
+            String replaceAll = str.toLowerCase().replaceAll("[^a-z0-9 ]", "");
+
+//            System.out.println("replaceAll = " + replaceAll);
+
+            for (String s : replaceAll.split(" ")) {
+                if (s.charAt(0) == 'w') {
+                    stringBuilder.append(s).append(" ");
+                }
+            }
+        }
+//        System.out.println("stringBuilder.toString() = " + stringBuilder.toString().toLowerCase());
+
+        String[] split = stringBuilder.toString().split(" ");
+        Arrays.sort(split);
+        return split;
     }
 }
