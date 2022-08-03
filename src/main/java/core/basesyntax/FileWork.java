47fc9f9108b480af4cd5936b6ad5ff139d173bc1
UmyaFamilia <umyafamilya1974@gmail.com>
2022-08-03 15:16:52 +0300

    FileWork

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,38 +1,36 @@
 package core.basesyntax;
 
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public class FileWork {
-    public String[] readFromFile(String fileName)  {
-        //write your code here
-        String [] emptyArray = new String [0] ;
-        StringBuilder stringBuilder=new StringBuilder();
-       try{ File file =new File(fileName);
-        BufferedReader bufferedReader= new BufferedReader(new FileReader(file));
-        String text = bufferedReader.readLine();
-        while (text!=null){
-            stringBuilder.append(text).append(" ");
-            text=bufferedReader.readLine();
-        }
+    public String[] readFromFile(String fileName) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            File file = new File(fileName);
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String text = bufferedReader.readLine();
+            while (text != null) {
+                stringBuilder.append(text).append(" ");
+                text = bufferedReader.readLine();
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-       String [] array=stringBuilder.toString().toLowerCase().split("[^A-Za-z0-9]+");
-       String line="";
+        String [] array = stringBuilder.toString().toLowerCase().split("[^A-Za-z0-9]+");
+        String line = "";
         for (int i = 0; i < array.length; i++) {
-            if(array[i].startsWith("w"))
-               line= line.concat(array[i]).concat(" ");
+            if (array[i].startsWith("w")) {
+                line = line.concat(array[i]).concat(" ");
+            }
         }
-
-        String []result=line.split(" ");
-        if(result.length==1)
+        String [] result = line.split(" ");
+        if (result.length == 1) {
             return new String[0];
+        }
         Arrays.sort(result);
         return result;
     }
