51187ba4118f09eb0131c1e92ec990989c71f6a4
Thousandfold <you@example.com>
2022-08-03 14:29:23 +0300

    create_word_filter_read_from_file

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,38 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
         //write your code here
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+       try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+           String value = bufferedReader.readLine();
+           if (value == null) {
+               String[] nullArray = new String[0];
+           }
+           while (value != null) {
+               stringBuilder.append(value).append(System.lineSeparator());
+               value = bufferedReader.readLine();
+           }
+       } catch (IOException e) {
+           throw new RuntimeException("File was not found!");
+       }
+       String[] split = stringBuilder.toString().split("\\W+");
+       StringBuilder builder = new StringBuilder();
+        for (String word : split) {
+            if (word.substring(0,1).equals("w")) {
+                builder.append(word).append(" ");
+            } else if (word.substring(0,1).equals("W")) {
+                builder.append(word.toLowerCase()).append(" ");
+            }
+        }
+        String[] arrayW = builder.toString().split(" ");
+        Arrays.sort(arrayW);
+        return arrayW;
     }
+
 }
