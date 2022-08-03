561e80ff5db6cc51520e640fdd3faa64df0ee454
Thousandfold <you@example.com>
2022-08-03 14:45:22 +0300

    fix_mvn_checkstyle

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -9,20 +9,22 @@ public class FileWork {
     public String[] readFromFile(String fileName) {
         //write your code here
         StringBuilder stringBuilder = new StringBuilder();
-       try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
-           String value = bufferedReader.readLine();
-           if (value == null) {
-               String[] nullArray = new String[0];
-           }
-           while (value != null) {
-               stringBuilder.append(value).append(System.lineSeparator());
-               value = bufferedReader.readLine();
-           }
-       } catch (IOException e) {
-           throw new RuntimeException("File was not found!");
-       }
-       String[] split = stringBuilder.toString().split("\\W+");
-       StringBuilder builder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String value = bufferedReader.readLine();
+            if (value == null) {
+                String[] nullArray = new String[]{};
+                return nullArray;
+            }
+            while (value != null) {
+                stringBuilder.append(value).append(System.lineSeparator());
+                value = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("File was not found!");
+        }
+
+        String[] split = stringBuilder.toString().split("\\W+");
+        StringBuilder builder = new StringBuilder();
         for (String word : split) {
             if (word.substring(0,1).equals("w")) {
                 builder.append(word).append(" ");
