89da1254549b109bc92da4f5fb95be66d3b1d4fb
Yevhen Kupriiuk <kupriuk@gmail.com>
2022-08-03 15:21:57 +0300

    all words sorted arrays are crated

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,44 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String LETTER_LOW = "w";
+    private static final String LETTER_UPPER = "W";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        int countWords = 0;
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String string = reader.readLine();
+            while (string != null) {
+                builder.append(string).append(" ");
+                string = reader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("File not found", e);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        String[] array = builder.toString().toLowerCase().split("\\W+");
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].startsWith(LETTER_LOW) || array[i].startsWith(LETTER_UPPER)) {
+                countWords++;
+            }
+        }
+        String[] result = new String[countWords];
+        int index = 0;
+        for (String array1 : array) {
+            if (array1.startsWith(LETTER_LOW) || array1.startsWith(LETTER_UPPER)) {
+                result[index] = array1;
+                index++;
+            }
+        }
+        Arrays.sort(result);
+        return result;
     }
 }
