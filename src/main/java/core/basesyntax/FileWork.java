10235b41346771a0d17ad2af21c4aa9fb749cef7
Danylo Frolov <frolovdanilik@gmail.com>
2022-08-03 14:11:34 +0300

    solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,42 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        String[] resultArray = new String[0];
+        File file = new File(fileName);
+        StringBuilder builder = new StringBuilder();
+        try {
+            FileReader reader = new FileReader(file);
+            FileReader readerCheck = new FileReader(file);
+            if (readerCheck.read() == -1) {
+                return resultArray;
+            }
+            int value = reader.read();
+            while (value != -1) {
+                builder.append((char) value);
+                value = reader.read();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t open file");
+        }
+
+        String[] split = builder.toString().split("\\W+");
+        StringBuilder result = new StringBuilder();
+        for (String string : split) {
+            if (string.toLowerCase().charAt(0) == 'w') {
+                result.append(string).append(" ");
+            }
+        }
+        if (result.toString() == "") {
+            return resultArray;
+        }
+        resultArray = result.toString().toLowerCase().split("\\W+");
+        Arrays.sort(resultArray);
+        return resultArray;
     }
 }
