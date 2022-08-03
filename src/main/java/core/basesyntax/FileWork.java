f23a3ffd13b0b78b58d5fa9eefec09cce60a73bf
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-08-03 18:36:03 +0300

    worked code

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -7,39 +7,36 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
-import java.util.List;
 
 public class FileWork {
     public String[] readFromFile(String fileName) {
         File file = new File(fileName);
         Path path = file.toPath();
-        StringBuilder stringBuilder = new StringBuilder();
-
-        List<String> strings = null;
+        String[] words;
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
-            strings = Files.readAllLines(path);
+            words = Files.readAllLines(path)
+                    .toString()
+                    .toLowerCase()
+                    .replaceAll("[^a-z0-9 ]", "")
+                    .split(" ");
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Problem with read file ", e);
         }
 
-        if (strings.size() == 0) {
-            return new String[0];
-        }
-        for (String str : strings) {
-            String replaceAll = str.toLowerCase().replaceAll("[^a-z0-9 ]", "");
-
-//            System.out.println("replaceAll = " + replaceAll);
-
-            for (String s : replaceAll.split(" ")) {
-                if (s.charAt(0) == 'w') {
-                    stringBuilder.append(s).append(" ");
-                }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String word : words) {
+            if (word.startsWith("w")) {
+                stringBuilder.append(word)
+                        .append(" ");
             }
         }
-//        System.out.println("stringBuilder.toString() = " + stringBuilder.toString().toLowerCase());
+        words = stringBuilder.toString()
+                .split(" ");
 
-        String[] split = stringBuilder.toString().split(" ");
-        Arrays.sort(split);
-        return split;
+        if (words.length <= 1) {
+            return new String[0];
+        }
+        Arrays.sort(words);
+        return words;
     }
 }
