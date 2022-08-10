59199f25c6b272bcc1eb86d6407df05e6a2cc69b
Sasha Vradiy <alexvradii@gmail.com>
2022-08-10 14:27:48 +0300

    My solution of tusk jv-read-from-file.

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -4,25 +4,44 @@ import java.io.*;
 import java.util.Arrays;
 
 public class FileWork {
+    private static final String SPECIFIED_CHARACTER = "w";
+
     public String[] readFromFile(String fileName) {
         File file = new File(fileName);
-        StringBuilder stringBuilder = new StringBuilder();
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+        String[] sortArrayWithOnlyW = new String[0];
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            StringBuilder reader = new StringBuilder();
             String value = bufferedReader.readLine();
+            String textFromFile = null;
             while (value != null) {
-                value = value.toLowerCase().replaceAll("\\b(?!w)\\w+\\W*", "");
-                value = value.replaceAll("[.]", "");
-                if (value.equals("")) {
-                    String[] words = value.split("[\\W++&&[^-]]");
-                    stringBuilder.append(Arrays.toString(words));
-                }
+                reader.append(value).append(" ");
                 value = bufferedReader.readLine();
+                textFromFile = reader.toString();
+            }
+            String[] arrayWithoutPunctuationInLowerCase = new String[0];
+            if (textFromFile != null) {
+                arrayWithoutPunctuationInLowerCase = textFromFile
+                        .replaceAll("[?.!,-]", "")
+                        .toLowerCase()
+                        .split(" ");
+            }
+            StringBuilder stringBuilder = new StringBuilder();
+            for (String string : arrayWithoutPunctuationInLowerCase) {
+                if (string.startsWith(SPECIFIED_CHARACTER)) {
+                    stringBuilder.append(" ").append(string);
+                }
+            }
+            String stringWithWordsW = stringBuilder.toString();
+            if (!stringWithWordsW.isEmpty()) {
+                sortArrayWithOnlyW = stringWithWordsW.strip().split(" ");
+                Arrays.sort(sortArrayWithOnlyW);
             }
         } catch (FileNotFoundException e) {
             throw new RuntimeException("Can't find file", e);
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file", e);
+            throw new RuntimeException("Can't read file!", e);
         }
-        return null;
+        return sortArrayWithOnlyW;
     }
 }
