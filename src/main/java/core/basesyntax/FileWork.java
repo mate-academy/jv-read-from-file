18c416262a0aa9bca676d026454586453cca100e
Maksym Ivanov <ivanov1maxim@gmail.com>
2022-08-31 11:22:45 +0300

    Implement search words starts with a special char logic

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,54 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.StringJoiner;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        return this.readFromFile(fileName,'w');
+    }
+
+    public String[] readFromFile(String fileName, char firstLetter) {
+        File file = new File(fileName);
+        StringJoiner joiner = new StringJoiner(" ");
+
+        try (
+                FileReader fileReader = new FileReader(file);
+                BufferedReader bufferedReader = new BufferedReader(fileReader)
+        ) {
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                String[] words = line.split(" ");
+                for (final String word : words) {
+                    String lowercaseString = word.toLowerCase();
+                    if (lowercaseString.charAt(0) == firstLetter) {
+                        String normalizeString = lowercaseString.replaceAll("[^a-zA-Z0-9]", "");
+                        joiner.add(normalizeString);
+                    }
+                }
+                line = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("File not found", e);
+        }
+
+        if (joiner.length() > 0) {
+            String[] resultWords = joiner.toString().split(" ");
+            for (int i = 0; i < resultWords.length - 1; i++) {
+                for (int j = i + 1; j < resultWords.length; j++) {
+                    if (resultWords[i].compareTo(resultWords[j]) > 0) {
+                        String temp = resultWords[i];
+                        resultWords[i] = resultWords[j];
+                        resultWords[j] = temp;
+                    }
+                }
+            }
+            return resultWords;
+        } else {
+            return new String[0];
+        }
     }
 }
