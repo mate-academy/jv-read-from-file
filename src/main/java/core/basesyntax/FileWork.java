14d12bc6d00f5e4dd1c735445a0e826bea0ecd97
Oleg Pruh <olegpruh1@gmail.com>
2022-09-01 11:53:29 +0100

    The code for reading and filtering is written

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,30 @@
 package core.basesyntax;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.util.*;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        List<String> result;
+        try {
+            result = Files.readAllLines(file.toPath());
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read the file", e);
+        }
+        String[] wordsArray = result.toString().split("\\W+");
+        var filteredWordsList = new ArrayList<String>();
+        for (String word : wordsArray) {
+            if (word.toLowerCase().startsWith("w")) {
+                filteredWordsList.add(word.toLowerCase());
+            }
+        }
+        String[] filteredWordsArray = new String[filteredWordsList.size()];
+        for (int i = 0; i < filteredWordsList.size(); i++) {
+            filteredWordsArray[i] = filteredWordsList.get(i);
+        }
+        Arrays.sort(filteredWordsArray);
+        return filteredWordsArray;
     }
 }
