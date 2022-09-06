8f0cf23c104fbb7a93bfee575c4f652e41149cd6
Anastasiia Chumak <nastya.ch@mate.academy>
2022-09-06 18:17:33 +0300

    Adding application fro File work

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,33 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        List<String> readLinesList;
+        List<String> resultList = new ArrayList<>();
+        File file = new File(fileName);
+        try {
+            readLinesList = Files.readAllLines(file.toPath());
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot read lines from file!");
+        }
+
+        for (String line : readLinesList) {
+            for (String word : line.split("\\W+")) {
+                if (word.startsWith("W") || word.startsWith("w")) {
+                    resultList.add(word.toLowerCase());
+                }
+            }
+        }
+        String[] finalArray = new String[resultList.size()];
+        resultList.toArray(finalArray);
+        Arrays.sort(finalArray);
+        return finalArray;
     }
 }
