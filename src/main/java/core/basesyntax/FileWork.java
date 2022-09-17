a154b61d044eb641d780ba6d351309e46a72b645
sasha <radovietsoleksandr@gmail.com>
2022-09-17 13:10:14 +0300

    file1

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,30 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class FileWork {
-    public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+    public static String[] readFromFile(String fileName) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));) {
+            String readString = bufferedReader.readLine();
+            while (readString != null) {
+                String[] as = readString.toLowerCase().trim().replaceAll("[^A-Za-zА,-]", " ").trim().split(" ");
+                for (String asd : as) {
+                    if (asd.indexOf("w") == 0) {
+                        stringBuilder.append(asd + " ");
+                    }
+                }
+                readString = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("don't directory file");
+        }
+        if (stringBuilder.length() == 0) {
+            stringBuilder.append(" ");
+        }
+        return String.valueOf(stringBuilder).split(" ");
     }
 }
+
