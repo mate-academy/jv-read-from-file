a01272a12b01109e66183f165a2537f224860a04
Dima Holovko <holovkod12@gmail.com>
2022-09-27 22:20:11 +0300

    all done

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,38 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String PREFIX = "w";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String readFile = bufferedReader.readLine();
+            while (readFile != null) {
+                stringBuilder.append(readFile).append(" ");
+                readFile = bufferedReader.readLine();
+            }
+            String dataOfReadFile = stringBuilder.toString().toLowerCase();
+            StringBuilder builder = new StringBuilder();
+            String[]newArray = dataOfReadFile.split("\\W+");
+            for (String word : newArray) {
+                if (word.startsWith(PREFIX)) {
+                    builder.append(word).append(" ");
+                }
+            }
+            String result = builder.toString();
+            if (result.length() == 0) {
+                return new String[]{};
+            } else {
+                String[] outputs = result.split(" ");
+                Arrays.sort(outputs);
+                return outputs;
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Error", e);
+        }
     }
 }
