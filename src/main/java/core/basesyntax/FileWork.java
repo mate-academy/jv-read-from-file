47de27597875740be88e62d811a13a7c5fe28a42
shmax <mshveda28@gmail.com>
2022-09-28 12:03:13 +0300

    Introduce constants

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -7,14 +7,17 @@ import java.util.Arrays;
 import java.util.List;
 
 public class FileWork {
+    public static final String SPECIFIED_LETTER = "w";
+    public static final String SPLIT_REGEX = "\\W";
+
     public String[] readFromFile(String fileName) {
         //write your code here
         try {
             List<String> lines = Files.readAllLines(Path.of(fileName));
             return lines.stream()
-                    .flatMap(line -> Arrays.stream(line.split("\\W")))
+                    .flatMap(line -> Arrays.stream(line.split(SPLIT_REGEX)))
                     .map(String::toLowerCase)
-                    .filter(word -> word.matches("^w.+"))
+                    .filter(word -> word.startsWith(SPECIFIED_LETTER))
                     .sorted()
                     .toArray(String[]::new);
         } catch (IOException e) {
