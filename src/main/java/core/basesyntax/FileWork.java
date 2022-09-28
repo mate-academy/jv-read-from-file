c0ad86266160e192dd1c74289113c986904cadb8
shmax <mshveda28@gmail.com>
2022-09-28 11:53:33 +0300

    Implement file reading logic according to reqs

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,26 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
         //write your code here
-        return null;
+        try {
+            List<String> lines = Files.readAllLines(Path.of(fileName));
+            return lines.stream()
+                    .flatMap(line -> Arrays.stream(line.split("\\W")))
+                    .map(String::toLowerCase)
+                    .filter(word -> word.matches("^w.+"))
+                    .sorted()
+                    .toList()
+                    .toArray(new String[0]);
+        } catch (IOException e) {
+            System.out.println("Can't read file!");
+        }
+        return new String[0];
     }
 }
