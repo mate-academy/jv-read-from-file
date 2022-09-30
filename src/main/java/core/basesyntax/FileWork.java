551d2b480ada1d3175f4a09220dcc7271064f5be
Anton Pavliuk <anton.pavliuk20@gmail.com>
2022-09-30 18:19:10 +0300

    Solution for task. All test passed

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,44 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
         //write your code here
-        return null;
+        File file = new File(fileName);
+        String[] result;
+        String[] words = null;
+        Path path = Paths.get(fileName);
+        try {
+            String readed = Files.readString(path);
+            words = readed.split("\\W+");
+            result = filter(words);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(Arrays.toString(result));
+        Arrays.sort(result);
+        return result;
+    }
+
+    private String[] filter(String[] words) {
+        ArrayList<String> result = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+
+        for (String s : words) {
+            s = s.toLowerCase();
+            if (s.length() > 1 && s.charAt(0) == 'w') {
+                result.add(s);
+                sb.append(s).append(" ");
+            }
+        }
+
+        return result.toArray(new String[0]);
     }
 }
