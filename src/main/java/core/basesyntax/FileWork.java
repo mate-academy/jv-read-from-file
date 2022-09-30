4b60952b03c1de1dd9f3f43f239b7bfc723ed4a2
Anton Pavliuk <anton.pavliuk20@gmail.com>
2022-09-30 18:26:35 +0300

    Some corretion`s according to Common Mistakes and Maven builed

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,41 +1,37 @@
 package core.basesyntax;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class FileWork {
+    private static final char SPECIFIED_CHARACTER = 'w';
+
     public String[] readFromFile(String fileName) {
         //write your code here
-        File file = new File(fileName);
         String[] result;
-        String[] words = null;
-        Path path = Paths.get(fileName);
+
         try {
-            String readed = Files.readString(path);
-            words = readed.split("\\W+");
+            String readed = Files.readString(Paths.get(fileName));
+            String[] words = readed.split("\\W+");
             result = filter(words);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("File reading error", e);
         }
-        System.out.println(Arrays.toString(result));
+
         Arrays.sort(result);
         return result;
     }
 
     private String[] filter(String[] words) {
         ArrayList<String> result = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
 
         for (String s : words) {
             s = s.toLowerCase();
-            if (s.length() > 1 && s.charAt(0) == 'w') {
+            if (s.length() > 1 && s.charAt(0) == SPECIFIED_CHARACTER) {
                 result.add(s);
-                sb.append(s).append(" ");
             }
         }
 
