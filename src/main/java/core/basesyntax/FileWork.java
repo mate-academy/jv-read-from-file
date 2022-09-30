3b92f527c05167f6e10ddf39bce09d0e8cbb6d59
Anastasia Vasylyk <anastasiavasylykv@gmail.com>
2022-09-30 15:06:13 +0300

    Reading from file and filtering was realized

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,43 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final char FILTER_VALUE = 'w';
+    private static final String REGEX_FOR_WORDS = "[$&+,:;=?@#|'<>.-^*()%!\\r\\n ]+";
+    private int countOfFilteredWords = 0;
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        StringBuilder resultStringBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                resultStringBuilder.append(line).append(System.lineSeparator());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read text from file", e);
+        }
+        if (resultStringBuilder.length() == 0) {
+            return new String[] {};
+        }
+        String[] wordsFromFile = resultStringBuilder.toString()
+                .toLowerCase().split(REGEX_FOR_WORDS);
+        for (int i = 0; i < wordsFromFile.length; i++) {
+            if (wordsFromFile[i].charAt(0) == FILTER_VALUE) {
+                countOfFilteredWords += 1;
+            }
+        }
+        String[] filteredWords = new String[countOfFilteredWords];
+        for (int i = 0, j = 0; i < wordsFromFile.length; i++) {
+            if (wordsFromFile[i].charAt(0) == FILTER_VALUE) {
+                filteredWords[j] = wordsFromFile[i];
+                j++;
+            }
+        }
+        Arrays.sort(filteredWords);
+        return filteredWords;
     }
 }
