679d298d07256517bef10be7abdb52da3baf925a
Denis Kalyakin <denis.kalyakin999@gmail.com>
2022-10-02 21:56:21 +0300

    task jv-read-from-file is completed

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,48 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        StringBuilder builder = new StringBuilder();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String value = reader.readLine();
+            if (value == null) {
+                return new String[0];
+            }
+            while (value != null) {
+                builder.append(value).append(System.lineSeparator());
+                value = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read file", e);
+        }
+        String[] split = builder.toString().toLowerCase().split("\\W+");
+        String[] splitW = new String[split.length];
+        int j = 0;
+
+        for (int i = 0; i < split.length; i++) {
+            splitW[i] = String.valueOf(split[i].charAt(0));
+            if (splitW[i].equals("w")) {
+                j++;
+            }
+        }
+        String[] result = new String[j];
+        j -= j;
+        for (int i = 0; i < split.length; i++) {
+            splitW[i] = String.valueOf(split[i].charAt(0));
+            if (splitW[i].equals("w")) {
+                result[j] = split[i];
+                j++;
+            }
+        }
+        Arrays.sort(result);
+        return result;
     }
 }
