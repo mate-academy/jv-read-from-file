8612fb783fde9c8ef85771f34de18325c3274643
Oleksiy Masaltsev <a.o.masaltsev@gmail.com>
2022-10-03 23:14:18 +0300

    getFilteredData implemented

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -3,24 +3,41 @@ package core.basesyntax;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class FileWork {
+    public static final String REGEX = "\\W+";
+    public static final String MESSAGE = "Cannot read the file";
+
     public String[] readFromFile(String fileName) {
         StringBuilder stringBuilder;
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             stringBuilder = new StringBuilder();
             String line;
             while ((line = reader.readLine()) != null) {
-                stringBuilder.append(line);
+                stringBuilder.append(line).append(System.lineSeparator());
             }
         } catch (IOException e) {
-            throw new RuntimeException("Cannot read the file", e);
+            throw new RuntimeException(MESSAGE, e);
         }
         return getFilteredData(stringBuilder);
     }
 
     private String[] getFilteredData(StringBuilder stringBuilder) {
-        String[] words = stringBuilder.toString().toLowerCase().split("\\W+");
-        return words;
+        if (stringBuilder.length() == 0) {
+            return new String[0];
+        }
+        String[] words = stringBuilder.toString().toLowerCase().split(REGEX);
+        String[] filteredWords = new String[words.length];
+        int index = 0;
+        for (String word : words) {
+            if (word != null && word.charAt(0) == 'w') {
+                filteredWords[index] = word;
+                index++;
+            }
+        }
+        filteredWords = Arrays.copyOfRange(filteredWords, 0, index);
+        Arrays.sort(filteredWords);
+        return filteredWords;
     }
 }
