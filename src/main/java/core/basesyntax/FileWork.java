9d97d9e8d35bb9d7fb42ace01b00ee63de537979
Oleksiy Masaltsev <a.o.masaltsev@gmail.com>
2022-10-03 19:25:44 +0300

    readFromFile implemented, getFilteredData method is not ready

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,26 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        StringBuilder stringBuilder;
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            stringBuilder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot read the file", e);
+        }
+        return getFilteredData(stringBuilder);
+    }
+
+    private String[] getFilteredData(StringBuilder stringBuilder) {
+        String[] words = stringBuilder.toString().toLowerCase().split("\\W+");
+        return words;
     }
 }
