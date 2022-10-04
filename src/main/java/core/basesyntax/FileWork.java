fa09cb6cb7abacfc44c22aea2cd139b94116285b
yehbreus <yeh.breus@gmail.com>
2022-10-04 16:26:55 +0300

    fix after mvn clean package

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,6 +1,9 @@
 package core.basesyntax;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Arrays;
 
 public class FileWork {
@@ -19,7 +22,9 @@ public class FileWork {
             bufferedReader = new BufferedReader(new FileReader(file));
             while (true) {
                 int c = bufferedReader.read();
-                if (c == END_OF_FILE) break;
+                if (c == END_OF_FILE) {
+                    break;
+                }
                 String fileLine = bufferedReader.readLine();
                 fileLine = ((char) c + fileLine).toLowerCase();
                 String[] wordsFromLine = fileLine.split(LINE_SEPARATOR);
@@ -27,8 +32,9 @@ public class FileWork {
                     if (word.startsWith(LETTER_WORD_START)) {
                         StringBuilder sb = new StringBuilder();
                         for (int i = 0; i < word.length(); i++) {
-                            if (Character.isLetterOrDigit(word.charAt(i)))
+                            if (Character.isLetterOrDigit(word.charAt(i))) {
                                 sb.append(word.charAt(i));
+                            }
                         }
                         word = sb.toString();
                         result.append(word).append(LINE_SEPARATOR);
@@ -40,6 +46,7 @@ public class FileWork {
         }
         String[] resultArray = result.toString().split(LINE_SEPARATOR);
         Arrays.sort(resultArray);
-        return result.toString().length() > LENGTH_EMPTY_STRING ? resultArray : new String[LENGTH_EMPTY_ARRAY];
+        return result.toString().length() > LENGTH_EMPTY_STRING ? resultArray
+                : new String[LENGTH_EMPTY_ARRAY];
     }
 }
