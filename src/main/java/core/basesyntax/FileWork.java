bf5a8fbaadd507f7bb0a209f07f10a05be9830bd
MuzykaVP <muzykavp.work@gmail.com>
2022-10-04 19:22:07 +0300

    Implemeted readFromFile method

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,33 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class FileWork {
-    public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+    private static final String SPLITTERATOR = "[!?.,;:\\-_ `'\n\r]";
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(readFromFile("test2")));
+    }
+
+    public static String[] readFromFile(String fileName) {
+        ArrayList<String> wordsList = new ArrayList<String>();
+        File file = new File(fileName);
+        try {
+            for (String word : Files.readString(Path.of(file.getPath())).split(SPLITTERATOR)) {
+                if (word.matches("(w|W).*")) {
+                    wordsList.add(word.toLowerCase());
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String[] words = wordsList.toArray(String[]::new);
+        Arrays.sort(words);
+        return words;
     }
 }
