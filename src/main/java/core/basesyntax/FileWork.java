d6b832421c6fdaf343eb003436e7cd9baae17be8
Veronika Tretiak <nikatretiak9@gmail.com>
2022-10-04 20:18:40 +0200

    implemented task

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,46 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String REGEX = "\\W+";
+    private static final char FIRST_LETTER = 'w';
+    private int countOfFilterWords = 0;
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String value = reader.readLine();
+            while (value != null) {
+                stringBuilder.append(value).append(System.lineSeparator());
+                value = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        if (stringBuilder.length() == 0) {
+            return new String[] {};
+        }
+        String[] words = stringBuilder.toString().toLowerCase().split(REGEX);
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].charAt(0) == FIRST_LETTER) {
+                countOfFilterWords++;
+            }
+        }
+        String[] filterWords = new String[countOfFilterWords];
+        for (int i = 0; i < words.length; i++) {
+            for (int j = 0; i < words.length; i++) {
+                if (words[i].charAt(0) == FIRST_LETTER) {
+                    filterWords[j] = words[i];
+                    j++;
+                }
+            }
+        }
+        Arrays.sort(filterWords);
+        return filterWords;
     }
 }
+
