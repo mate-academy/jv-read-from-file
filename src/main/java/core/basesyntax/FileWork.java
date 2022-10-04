48213b274be84bcdf447d3be48917d87190d063b
Tymur Prokhorov <tymurprokhorov@gmail.com>
2022-10-04 13:47:55 +0300

    fixes added

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,26 +12,35 @@ public class FileWork {
 
     public String[] readFromFile(String fileName) {
         String[] words = new String[fileName.length()];
+        StringBuilder stringBuilder = new StringBuilder(" ");
         File file = new File(fileName);
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-            StringBuilder stringBuilder = new StringBuilder();
-            int value = bufferedReader.read();
-            while (value != -1) {
-                stringBuilder.append((char) value);
-                value = bufferedReader.read();
-            }
-            String a = stringBuilder.toString().toLowerCase();
-            String regex = "w[a-z]";
-            Pattern pattern = Pattern.compile(regex);
-            Matcher matcher = pattern.matcher(a);
-            while (matcher.find()) {
-                words = pattern.split(a);
-            }
 
+            String readLine = bufferedReader.readLine();
+            while (readLine != null) {
+                stringBuilder.append(readLine).append(System.lineSeparator());
+                readLine = bufferedReader.readLine();
+            }
         } catch (IOException e) {
             throw new RuntimeException("Can`t read data from file", e);
         }
-        return words;
+        String readStringBuilder = stringBuilder.toString();
+        stringBuilder = new StringBuilder();
+
+        String regex = "\\W[Ww][A-z]*";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(readStringBuilder);
+        String lineSeparator = "";
+        while (matcher.find()) {
+            String curWord = readStringBuilder.substring(matcher.start(), matcher.end());
+            stringBuilder.append(lineSeparator).append(curWord.toLowerCase().trim());
+            lineSeparator = System.lineSeparator();
+        }
+        String result = stringBuilder.toString();
+        String[] array = result.isEmpty() ? new String[]{} : result.split(lineSeparator);
+        Arrays.sort(array);
+
+        return array;
     }
 }
