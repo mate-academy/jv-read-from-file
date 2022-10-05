54567a0bd74fa4f2f9d1d8e1087d45a84d11da1a
Anastasiia Romanova <ana.roma.vi@gmail.com>
2022-10-05 15:07:41 +0300

    read and filtered all files

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,36 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+
 public class FileWork {
+    public static final String SPECIFIED_CHARACTER = "w";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        String[] value;
+
+        try {
+            value = String.valueOf(Files.readAllLines(file.toPath())).split("\\W+");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String v : value) {
+            v = v.toLowerCase();
+            if (v.startsWith(SPECIFIED_CHARACTER)) {
+                stringBuilder.append(v).append(" ");
+            }
+        }
+        String result = stringBuilder.toString();
+        if (result.isEmpty()) {
+            return new String[0];
+        }
+        String[] array = result.split(" ");
+        Arrays.sort(array);
+        return array;
     }
 }
