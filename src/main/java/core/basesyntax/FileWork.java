5fb0c6b99d47d1b99b0823dcb50a7a22e29c8867
Roman Novosad <roman.novosad@yahoo.com>
2022-10-06 23:59:53 +0200

    amended solution after review of article "Try to avoid these common mistakes, while solving task"

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -6,14 +6,16 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class FileWork {
+    private static final String SPECIFIED_CHARACTER = "w";
+    private static final String SPECIFIED_REGEX = "\\W+";
+
     public String[] readFromFile(String fileName) {
         StringBuilder builder;
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             builder = new StringBuilder();
             String value = reader.readLine();
             while (value != null) {
-                builder.append(value).append(" ");
+                builder.append(value).append(System.lineSeparator());
                 value = reader.readLine();
             }
         } catch (IOException e) {
@@ -23,17 +25,17 @@ public class FileWork {
         if (builder.toString().length() == 0) {
             return sortedData;
         }
-        String[] splitData = builder.toString().split("\\W+");
+        String[] splitData = builder.toString().split(SPECIFIED_REGEX);
         int count = 0;
         for (String datum : splitData) {
-            if (datum.toLowerCase().charAt(0) == 'w') {
+            if (datum.toLowerCase().startsWith(SPECIFIED_CHARACTER)) {
                 count++;
             }
         }
         sortedData = new String[count];
         int index = 0;
         for (String datum : splitData) {
-            if (datum.toLowerCase().charAt(0) == 'w') {
+            if (datum.toLowerCase().startsWith(SPECIFIED_CHARACTER)) {
                 sortedData[index] = datum.toLowerCase();
                 index++;
             }
