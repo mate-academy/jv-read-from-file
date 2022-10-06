9ad7a607f644b44917ff2c39a5afe299431ac4c0
Roman Novosad <roman.novosad@yahoo.com>
2022-10-06 23:35:35 +0200

    added to solution to task jv-read-from-file / Files / Java Core

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,44 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        StringBuilder builder;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            builder = new StringBuilder();
+            String value = reader.readLine();
+            while (value != null) {
+                builder.append(value).append(" ");
+                value = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read the file", e);
+        }
+        String[] sortedData = new String[]{};
+        if (builder.toString().length() == 0) {
+            return sortedData;
+        }
+        String[] splitData = builder.toString().split("\\W+");
+        int count = 0;
+        for (String datum : splitData) {
+            if (datum.toLowerCase().charAt(0) == 'w') {
+                count++;
+            }
+        }
+        sortedData = new String[count];
+        int index = 0;
+        for (String datum : splitData) {
+            if (datum.toLowerCase().charAt(0) == 'w') {
+                sortedData[index] = datum.toLowerCase();
+                index++;
+            }
+        }
+        Arrays.sort(sortedData);
+        return sortedData;
     }
 }
