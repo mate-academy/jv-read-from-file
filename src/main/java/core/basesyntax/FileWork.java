f7b7aa7802f7e55ba79489af63de07ede4a27d9e
Dmytro Busyhin <zza4etik@gmail.com>
2022-10-19 23:34:51 +0300

    Implementation of method readFromFile

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,41 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String SPECIFIED_CHARACTER = "w";
+    private StringBuilder stringBuilder = new StringBuilder();
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String valueOfLane = bufferedReader.readLine();
+            if (valueOfLane == null) {
+                return new String[0];
+            }
+            String[] words;
+            while (valueOfLane != null) {
+                words = valueOfLane.toLowerCase().split("\\W+");
+                for (String word : words) {
+                    if (word.startsWith(SPECIFIED_CHARACTER)) {
+                        stringBuilder.append(word).append(System.lineSeparator());
+                    }
+                }
+                valueOfLane = bufferedReader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("File not found", e);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        if (stringBuilder.length() == 0) {
+            return new String[0];
+        }
+        String[] result = stringBuilder.toString().split(System.lineSeparator());
+        Arrays.sort(result);
+        return result;
     }
 }
