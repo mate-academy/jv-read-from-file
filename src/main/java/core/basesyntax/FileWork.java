6a616a2fb744949bc6f1e0bb994b4eeec9fb2abc
Vyacheslav Sukhov <makasin_12@hotmail.com>
2022-10-20 21:17:42 +0200

    first try

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -3,27 +3,25 @@ package core.basesyntax;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        String[] result = new String[fileName.length()];
+        ArrayList<String> list = new ArrayList<>();
+        String[] result = new String[list.size()];
 
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                int count = 0;
                 for (String word : line.split("\\W+")) {
                     if (word.startsWith("w")) {
-                        result[count] = word.toLowerCase();
-                        count++;
+                        list.add(word.toLowerCase());
                     }
                 }
             }
+            return list.toArray(result);
         } catch (IOException a) {
             throw new RuntimeException("there is no such file");
         }
-        Arrays.sort(result);
-        return result;
     }
 }
