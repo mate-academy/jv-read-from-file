c33dda26ae4edf414d28d99ada6b3201faf382fb
Vyacheslav Sukhov <makasin_12@hotmail.com>
2022-10-20 15:22:33 +0200

    first try

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,29 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        String[] result = new String[fileName.length()];
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                int count = 0;
+                for (String word : line.split("\\W+")) {
+                    if (word.startsWith("w")) {
+                        result[count] = word.toLowerCase();
+                        count++;
+                    }
+                }
+            }
+        } catch (IOException a) {
+            throw new RuntimeException("there is no such file");
+        }
+        Arrays.sort(result);
+        return result;
     }
 }
