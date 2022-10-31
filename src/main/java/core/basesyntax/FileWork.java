40c40aa994fb2df8e8ad521a1bf870196a95f071
Kateryna Takhtai <ktakhtay22@gmail.com>
2022-10-31 18:28:42 +0100

    whitespace

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,12 +1,14 @@
 package core.basesyntax;
 
-import java.io.*;
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        File file=new File(fileName);
+        File file = new File(fileName);
         String[] newText;
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
@@ -15,10 +17,10 @@ public class FileWork {
             if (value == null) {
                 return new String[0];
             }
-            while (value != null) {
+                while (value != null) {
             String sentence = value.toLowerCase();
             String[] split = sentence.split("\\W+");
-            for (String words: split) {
+                for (String words: split) {
                 if(words.charAt(0) == 'w') {
                     builder.append(words).append(" ");
 
@@ -29,7 +31,7 @@ public class FileWork {
             value = bufferedReader.readLine();
 
             }
-            if(builder.toString().length() == 0) {
+            if (builder.toString().length() == 0) {
                 return new String[0];
             } else {
 
