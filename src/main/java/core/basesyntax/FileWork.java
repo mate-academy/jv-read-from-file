bdc6f567a9f134c1fe5e2360d364e0aa052c4837
ArtemMakovskyy <photomak79@gmail.com>
2022-11-08 18:16:01 +0200

    start

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -3,6 +3,7 @@ package core.basesyntax;
 public class FileWork {
     public String[] readFromFile(String fileName) {
         //write your code here
+        System.out.println("start");
         return null;
     }
 }
