45c5f136c485f01f5fe45ecfe09951d340538a5b
Igor Puhach <igorpuhach21@gmail.com>
2022-11-26 10:03:41 +0200

    added read on the line,created new array with letter "w" only.

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,39 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class FileWork {
+    private static final String SPECIFIED_LETTER = "w";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            StringBuilder stringBuilder = new StringBuilder();
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                stringBuilder.append(value).append(System.lineSeparator());
+                value = bufferedReader.readLine();
+            }
+            String[] words = stringBuilder.toString().toLowerCase().split("//W+");
+            List<String> wwwList = new ArrayList<>();
+            for (String word : words) {
+                if (word.startsWith(SPECIFIED_LETTER)) {
+                    wwwList.add(word);
+                }
+            }
+            Collections.sort(wwwList);
+            return wwwList.toArray(new String[0]);
+
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file",e);
+        }
     }
 }
+
+
+
+
