ab8d3c9b17732da57c42706d79d10c1c7d4f6b4f
Yurii Storozhuk <95564174+urlo0k@users.noreply.github.com>
2022-11-28 20:59:59 +0200

    finish(task): read from file

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,34 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            StringBuilder builder = new StringBuilder();
+            String input = bufferedReader.readLine();
+            if (input != null) {
+                while (input != null) {
+                    String[] split = input.toLowerCase().split("\\W+");
+                    for (String item : split) {
+                        if (item.charAt(0) == 'w') {
+                            builder.append(item).append(" ");
+                        }
+                    }
+                    input = bufferedReader.readLine();
+                }
+                String[] result = builder.toString().split(" ");
+                Arrays.sort(result);
+                if (result.length != 1) {
+                    return result;
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read the file ", e);
+        }
+        return new String[0];
     }
 }
