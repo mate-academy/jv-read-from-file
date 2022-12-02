dc9e028d02214ef7a929a74ac1c6746fe445d8a9
Yevhenii Kilovyi <jenya.kilevoy@gmail.com>
2022-12-02 14:20:56 +0200

    first try of solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,38 @@
 package core.basesyntax;
 
+import java.io.*;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        int counter = 1;
+        File file = new File(fileName);
+        if (file.length() == 0) {
+            return new String[0];
+        }
+        String[] sortedWords = new String[counter];
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String value = reader.readLine();
+            StringBuilder stringBuilder = new StringBuilder();
+            while (value != null) {
+                value = reader.readLine();
+                stringBuilder.append(value).append(System.lineSeparator());
+            }
+            String wordsFromFile = stringBuilder.toString();
+            String[] splitted = wordsFromFile.split("\\W+");
+            for (int i = 0; i < splitted.length; i++) {
+                if (splitted[i].charAt(0) == 'w') {
+                    counter = counter + 1;
+                    for (int j = 0; j < sortedWords.length; j++) {
+                        sortedWords[j] = splitted[i];
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("File not found", e);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read from file", e);
+        }
+        return  sortedWords;
     }
 }
