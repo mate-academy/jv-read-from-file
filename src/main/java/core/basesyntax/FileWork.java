8ea36cf247e0751b975f318a3bf02913ce01e85f
Yevhenii Kilovyi <jenya.kilevoy@gmail.com>
2022-12-02 15:41:34 +0200

    first try of solution1

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,15 +1,20 @@
 package core.basesyntax;
 
-import java.io.*;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        int counter = 1;
+        int indexCounter = 0;
+        int counter = 0;
         File file = new File(fileName);
         if (file.length() == 0) {
             return new String[0];
         }
-        String[] sortedWords = new String[counter];
+        String[] sortedWords;
         try {
             BufferedReader reader = new BufferedReader(new FileReader(file));
             String value = reader.readLine();
@@ -22,10 +27,14 @@ public class FileWork {
             String[] splitted = wordsFromFile.split("\\W+");
             for (int i = 0; i < splitted.length; i++) {
                 if (splitted[i].charAt(0) == 'w') {
-                    counter = counter + 1;
-                    for (int j = 0; j < sortedWords.length; j++) {
-                        sortedWords[j] = splitted[i];
-                    }
+                    counter++;
+                }
+            }
+            sortedWords = new String[counter];
+            for (int i = 0; i < splitted.length; i++) {
+                if (splitted[i].charAt(0) == 'w') {
+                    sortedWords[indexCounter] = splitted[i];
+                    indexCounter++;
                 }
             }
         } catch (FileNotFoundException e) {
@@ -33,6 +42,6 @@ public class FileWork {
         } catch (IOException e) {
             throw new RuntimeException("Can't read from file", e);
         }
-        return  sortedWords;
+        return sortedWords;
     }
 }
