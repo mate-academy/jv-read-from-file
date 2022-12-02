823eb6038bbdd89f4210fec9f1d06c37edaed014
Yevhenii Kilovyi <jenya.kilevoy@gmail.com>
2022-12-02 15:43:05 +0200

    first try of solution2

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,9 +1,9 @@
 package core.basesyntax;
 
-import java.io.File;
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.BufferedReader
+import java.io.File;;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class FileWork {
