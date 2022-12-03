f695937295f065dca5fa186b58656110e8275ca5
Oleksii Parkhomenko <kcas.pilot@gmail.com>
2022-12-03 04:02:14 +0200

    jv-read-from-file first solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,42 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String PATTERN = "\\W+";
+    private static final String SEPARATOR = "-";
+    private static final char SYMBOL = 'w';
+    private static final int INDEX = 0;
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            String stringLine;
+            String[] lineArray;
+            while ((stringLine = bufferedReader.readLine()) != null) {
+                lineArray = stringLine.toLowerCase().split(PATTERN);
+                for (String word : lineArray) {
+                    if (word.charAt(INDEX) != SYMBOL) {
+                        continue;
+                    }
+                    stringBuilder.append(word).append(SEPARATOR);
+                }
+            }
+
+            if (stringBuilder.length() == 0) {
+                return new String[0];
+            }
+            lineArray = stringBuilder.toString().split(SEPARATOR);
+            Arrays.sort(lineArray);
+            return lineArray;
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
     }
 }
