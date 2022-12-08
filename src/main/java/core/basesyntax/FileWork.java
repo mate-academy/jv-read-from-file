b598f6c3350936e7d673bec141a923e6f38006ca
Denys Yastrebov <programmer_p@outlook.com>
2022-12-08 11:49:38 +0200

    second try

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,37 +1,43 @@
 package core.basesyntax;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class FileWork {
     public String[] readFromFile(String fileName) {
         //write your code here
 
-        final int ARR_SIZE = 100;
+        final int arrSize = 100;
+        final char symbol = (char) 39;
 
         File file = new File(fileName);
-        String[] result = new String[ARR_SIZE];
+        String[] result = new String[arrSize];
         StringBuilder string = new StringBuilder();
         int wordCounter = 0;
 
         try {
             BufferedReader buffReader = new BufferedReader(new FileReader(file));
             String value = buffReader.readLine();
-            String[] lines = new String[ARR_SIZE];
+            String[] lines = new String[arrSize];
             while (value != null) {
                 lines = value.split(" ");
                 for (String checker : lines) {
-                    char[] charArr = new char[ARR_SIZE];
-                    charArr = checker.toCharArray();
-                    for (int i = 0; i < charArr.length(); i++) {
-                        if (charArr[i] != "." || charArr[i] != "," || charArr[i] != "!"
-                                || charArr[i] != "?" || charArr[i] != "'" || charArr[i] != ":"
-                                || charArr[i] != ";") {
-                            string.append(charArr[i]);
+                    char[] charArr = checker.toCharArray();
+                    if (charArr[0] == 'w' || charArr[0] == 'W') {
+                        for (int i = 0; i < charArr.length; i++) {
+                            if (charArr[i] != '.' || charArr[i] != ',' || charArr[i] != '!'
+                                    || charArr[i] != '?' || charArr[i] != symbol
+                                    || charArr[i] != ':' || charArr[i] != ';') {
+                                string.append(charArr[i]);
+                            }
                         }
+                        result[wordCounter] = string.toString().toLowerCase();
+                        string.setLength(0);
+                        wordCounter++;
                     }
-                    result[wordCounter] = string.toString().toLowerCase();
-                    string.setLength(0);
-                    wordCounter++;
                 }
                 value = buffReader.readLine();
             }
