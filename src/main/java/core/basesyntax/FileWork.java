749c020a68943b751e0eb5f4f74a25f5ef6917db
Denys Yastrebov <programmer_p@outlook.com>
2022-12-08 10:33:39 +0200

    first try

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,46 @@
 package core.basesyntax;
 
+import java.io.*;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
         //write your code here
-        return null;
+
+        final int ARR_SIZE = 100;
+
+        File file = new File(fileName);
+        String[] result = new String[ARR_SIZE];
+        StringBuilder string = new StringBuilder();
+        int wordCounter = 0;
+
+        try {
+            BufferedReader buffReader = new BufferedReader(new FileReader(file));
+            String value = buffReader.readLine();
+            String[] lines = new String[ARR_SIZE];
+            while (value != null) {
+                lines = value.split(" ");
+                for (String checker : lines) {
+                    char[] charArr = new char[ARR_SIZE];
+                    charArr = checker.toCharArray();
+                    for (int i = 0; i < charArr.length(); i++) {
+                        if (charArr[i] != "." || charArr[i] != "," || charArr[i] != "!"
+                                || charArr[i] != "?" || charArr[i] != "'" || charArr[i] != ":"
+                                || charArr[i] != ";") {
+                            string.append(charArr[i]);
+                        }
+                    }
+                    result[wordCounter] = string.toString().toLowerCase();
+                    string.setLength(0);
+                    wordCounter++;
+                }
+                value = buffReader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Cannot open file", e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return result;
     }
 }
