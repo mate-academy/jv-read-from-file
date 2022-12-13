1a2259627dd5e3ca0df5c9a8331740c126f2dcf4
Bohdan Halitskyi <b.a.galitskiy@gmail.com>
2022-12-13 18:58:59 +0200

    partial realisaton (will be fixed soon)

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,37 @@
 package core.basesyntax;
 
+import java.io.*;
+import java.util.Arrays;
+
 public class FileWork {
+    final char CORRECT_CAPITAL_LETTER = 'W';
+    final char CORRECT_LETTER = 'w';
     public String[] readFromFile(String fileName) {
         //write your code here
-        return null;
+        File file = new File(fileName);
+        StringBuilder builder = new StringBuilder();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String value = reader.readLine();
+            if (value == null) {
+                return new String[0];
+            }
+            while (value != null) {
+                    String[] words = value.split("\\W+");
+                    for (String word : words) {
+                        if (word.length() > 0 && (word.charAt(0) == CORRECT_CAPITAL_LETTER || word.charAt(0) == CORRECT_LETTER)) {
+                            builder.append(word.toLowerCase()).append(" ");
+                            value = reader.readLine();
+                        }
+                    }
+                }
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String[]correctWords = builder.toString().split(" ");
+        Arrays.sort(correctWords);
+        return correctWords;
     }
 }
