f2417db671d68ca15014b1cf9eb31a63a6bb281c
Maksim Surzhik <kabaneri15@gmail.com>
2022-12-14 15:23:54 +0200

    done

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,34 @@
 package core.basesyntax;
 
-public class FileWork {
-    public String[] readFromFile(String fileName) {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
 
-        return null;
+public class FileWork {
+    private static final char SORTED_SYMBOL = 'w';
+
+    public String[] readFromFile(String fileName) {
+        String[] dataList = new String[0];
+        try {
+            String data = Files.readAllLines(Path.of(fileName)).toString().toLowerCase();
+            if (data.equals("")) {
+                return dataList;
+            }
+            String[] dataListUnsorted = data.split("[\\p{Punct}|\\p{Space}]");
+            for (String element : dataListUnsorted) {
+                if (!element.equals("")){
+                    if (element.toCharArray()[0] == SORTED_SYMBOL) {
+                        String[] newArray = Arrays.copyOf(dataList, dataList.length + 1);
+                        newArray[newArray.length - 1] = element;
+                        dataList = newArray;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("HELP");
+        }
+        Arrays.sort(dataList);
+        return dataList;
     }
 }
