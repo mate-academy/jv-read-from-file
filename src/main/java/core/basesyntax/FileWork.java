ef2e7ec20ede5623447326f18008b62ac0b40573
Olexiy <korm2909@gmail.com>
2022-12-14 13:23:47 +0200

    Finally working app

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -3,35 +3,37 @@ package core.basesyntax;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class FileWork {
-
     private static final String FIRST_LETTER = "w";
 
     public String[] readFromFile(String fileName) {
-        StringBuilder builder = new StringBuilder();
         File file = new File(fileName);
+        List<String> words;
+        StringBuilder builder = new StringBuilder();
         try {
-            builder.append(Files.readAllLines(file.toPath()));
+            words = Files.readAllLines(file.toPath());
         } catch (IOException e) {
-            throw new RuntimeException();
+            throw new RuntimeException("Reading failed", e);
         }
-        String[] separatedFileTxt = builder.toString().toLowerCase().split("\\W+");
-        String[] answer = new String[separatedFileTxt.length];
-        int counter = 0;
-        for (String word : separatedFileTxt) {
-            if (word.startsWith(FIRST_LETTER)) {
-                answer[counter] = word;
-                counter++;
+        List<String> listAnswer = new ArrayList<>();
+        for (String word : words) {
+            String[] wordsOnly = word.toLowerCase().split("\\W+");
+            for (int i = 0; i < wordsOnly.length; i++) {
+                if (wordsOnly[i].startsWith(FIRST_LETTER)) {
+                    listAnswer.add(wordsOnly[i]);
+                }
+            }
+            if (listAnswer == null) {
+                return new String[0];
             }
         }
-
-        if (answer == null) {
-            return new String[0];
-        }
-        Arrays.sort(answer);
+        String [] strAnswer = listAnswer.toArray(new String[0]);
+        Arrays.sort(strAnswer);
         //write your code here
-        return answer;
+        return strAnswer;
     }
 }
