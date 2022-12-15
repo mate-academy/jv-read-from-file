520694ee9dc745add403ff3177a15d2c6e79f45a
Alina Smirnova <0970352020a@gmail.com>
2022-12-15 23:04:06 +0300

    jv-read-from-file developAV

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,36 +1,30 @@
 package core.basesyntax;
 
-import java.util.Arrays;
 import java.io.*;
+import java.util.Arrays;
 
 public class FileWork {
     private static final String SPECIAL_LETTER = "w";
-    private String[] resultArrOfWords = {};
-    private String[] emptyArr = new String[0];
-    private String[] splitArr;
-    private String lowerStrOfWords;
-    private String value;
-    private String firstLetter;
-    private String resultStrOfWords;
-    private StringBuilder builder = new StringBuilder();
+    private final String[] emptyArr = new String[0];
+    private final StringBuilder builder = new StringBuilder();
 
     public String[] readFromFile(String fileName) {
         File file = new File(fileName);
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             if (file.exists() && file.length() != 0) {
-                value = reader.readLine();
-                lowerStrOfWords = value.toLowerCase();
-                splitArr = lowerStrOfWords.split("\\W+");
+                String value = reader.readLine();
+                String lowerStrOfWords = value.toLowerCase();
+                String[] splitArr = lowerStrOfWords.split("\\W+");
                 for (String words : splitArr) {
-                    firstLetter = String.valueOf(words.charAt(0));
+                    String firstLetter = String.valueOf(words.charAt(0));
                     if (firstLetter.equals(SPECIAL_LETTER)) {
                         builder.append(words).append(" ");
                     } else {
                         return emptyArr;
                     }
                 }
-                resultStrOfWords = builder.toString();
-                resultArrOfWords = resultStrOfWords.split(" ");
+                String resultStrOfWords = builder.toString();
+                String[] resultArrOfWords = resultStrOfWords.split(" ");
                 Arrays.sort(resultArrOfWords);
                 return resultArrOfWords;
             } else {
