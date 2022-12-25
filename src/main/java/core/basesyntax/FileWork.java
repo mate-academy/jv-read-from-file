2f0ac942d0977bd9d55a836d5284aa1158e3232a
Andrei_Bondarenko <bondorol007@gmail.com>
2022-12-25 17:47:07 +0200

    commit3

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,8 @@
 package core.basesyntax;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
 
