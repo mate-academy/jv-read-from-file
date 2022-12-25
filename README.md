b8d402b1c2f7ffc908be67816e7f86b9381493ef
imhatep <axeshock322@gmail.com>
2022-12-25 23:10:51 +0200

    jv-read-from-fileo solution

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # jv-read-from-file
+
 You are given a file that contains different words as well as punctuation.
 
 You need to filter out only the words starting with `w`, and remove any punctuation if necessary.
@@ -9,6 +10,7 @@ All words should be lower-case.
 If the file does not contain the necessary words, return an empty array.
 
 Examples:
+
 ```
 "Width world Wide web"
 Result: ["web", "wide", "width", "world"]
@@ -19,4 +21,5 @@ Result: ["wave", "www"]
 
 Hint: try to split Strings with a regular expression that includes whitespace and punctuation characters.
 Here is a good [article](https://stackoverflow.com/questions/13225175/java-string-split-with-a-regex).
+
 #### [Try to avoid these common mistakes, while solving task](https://mate-academy.github.io/jv-program-common-mistakes/java-core/builder-file/read-from-file.html)
--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,69 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        // Declare a File object to represent the file
+        File file = new File(fileName);
+        // Create a StringBuilder to store the cleaned words
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        try {
+            // Create a BufferedReader to read the file
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            // Create a StringBuilder to store the contents of the file
+            StringBuilder stringBuilder = new StringBuilder();
+            // Read the first line of the file
+            String value = bufferedReader.readLine();
+            // Continue reading lines until there are no more lines to read
+            while (value != null) {
+                // Append the current line to the StringBuilder, followed by a line separator
+                stringBuilder.append(value).append(System.lineSeparator());
+                // Read the next line
+                value = bufferedReader.readLine();
+            }
+            // If the StringBuilder is empty (i.e. the file was empty), return an empty array
+            if (stringBuilder.length() < 1) {
+                return new String[0];
+            }
+            // Initialize a variable to store the cleaned version of each word
+            String cleanedWord = "";
+            // Split the contents of the file into an array of words,
+            // removing punctuation and converting to lowercase
+            String[] words = stringBuilder.toString()
+                    .replaceAll("\\p{Punct}", "")
+                    .toLowerCase().split(" ");
+            for (String word : words) {
+                // Remove any words that contain non-word characters (except for the letter 'w')
+                cleanedWord = word.replaceAll("\\b(?!w)\\w+\\b", "");
+                // If the cleaned word is more than one character,
+                // add it to the StringBuilder and increment the count
+                if (cleanedWord.length() > 1) {
+                    count++;
+                    sb.append(cleanedWord + "\n");
+                }
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        // If the StringBuilder is empty (i.e. no words were added to it), return an empty array
+        if (sb.length() < 1) {
+            return new String[0];
+        }
+        // Remove the final line separator from the StringBuilder
+        sb.substring(0, sb.length() - 1);
+        // Initialize an array to hold the cleaned words
+        String[] result = new String[count];
+        // Split the StringBuilder into an array of strings using the line separator
+        result = sb.toString().split(System.lineSeparator());
+        // Sort the array of strings alphabetically
+        Arrays.sort(result);
+        return result;
     }
 }
