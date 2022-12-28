5ad14b23cbc727b503e69a49337dd799c8fdb54c
Ilko Good <ilko.engineering@gmail.com>
2022-12-28 18:18:31 +0200

    file w-reader success

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,43 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        StringBuilder sb = new StringBuilder();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            int value = br.read();
+            while (value != -1) {
+                sb.append((char) value);
+                value = br.read();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Check your file", e);
+        }
+        if (sb.length() == 0) {
+            return new String [0];
+        }
+        String text = sb.toString();
+        String[] splits = text.split("\\W+");
+        int counter = 0;
+        for (String str : splits) {
+            if (str.toLowerCase().charAt(0) == 'w') {
+                counter++;
+            }
+        }
+        String [] arr = new String[counter];
+        counter = 0;
+        for (String str : splits) {
+            if (str.toLowerCase().charAt(0) == 'w') {
+                arr[counter] = str.toLowerCase();
+                counter++;
+            }
+        }
+        Arrays.sort(arr);
+        return arr;
     }
 }
