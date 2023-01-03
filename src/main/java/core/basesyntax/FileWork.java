b77b99bad16479f384d9478326cb46ff2d083ebc
Oleksandr Kravchenko <oleksandr.kravchenko@spirent.com>
2023-01-03 15:52:13 +0200

    feat: jv-read-from-file #comment was implemented jv-read-from-file

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,40 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String pattern = "\\W+\\s|\\s|\\W$";
+    private static final char FIRST_LETTER = 'w';
+    private static final String SPLIT_STRING = " ";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line = reader.readLine();
+            while (line != null) {
+                line = line.toLowerCase();
+                String[] splitLine = line.split(pattern);
+                for (String word : splitLine) {
+                    if (word.toCharArray()[0] == FIRST_LETTER) {
+                        builder.append(word).append(SPLIT_STRING);
+                    }
+                }
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("file was not found");
+        }
+
+        String values = builder.toString();
+        if (builder.length() == 0) {
+            return new String[0];
+        }
+
+        String[] wordList = builder.toString().split(SPLIT_STRING);
+        Arrays.sort(wordList);
+        return wordList;
     }
 }
