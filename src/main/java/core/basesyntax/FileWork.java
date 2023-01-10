64b7fe61f256ac4fe35620aff70074b428c71ea3
carbo <carbon7557@gmail.com>
2023-01-10 18:34:33 +0000

    Task is done.

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,54 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+            File file = new File(fileName);
+            int index = 0;
+            String[] returnArray;
+            try {
+                BufferedReader reader = null;
+                reader = new BufferedReader(new FileReader(file));
+                StringBuilder builder = new StringBuilder();
+                String value = reader.readLine();
+                if (value == null) {
+                    returnArray = new String[0];
+                    return returnArray;
+                }
+                while (value != null) {
+                    builder.append(value).append(" ");
+                    value = reader.readLine();
+                }
+                String[] splitFile = builder.toString().toLowerCase().split("\\W ");
+                for (String word : splitFile) {
+                    String[] splitFile1 = word.split(" ");
+                    for (String word1 : splitFile1) {
+                        char charAtIndexOne = word1.charAt(0);
+                        if (charAtIndexOne == 'w') {
+                            index++;
+                        }
+                    }
+                }
+                returnArray = new String[index];
+                int i = 0;
+                for (String word : splitFile) {
+                    String[] splitFile1 = word.split(" ");
+                    for (String word1 : splitFile1) {
+                        char charAtIndexOne = word1.charAt(0);
+                        if (charAtIndexOne == 'w') {
+                            returnArray[i++] = word1;
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                throw new RuntimeException("Can't read the file", e);
+            }
+            Arrays.sort(returnArray);
+            return returnArray;
+        }
     }
-}
