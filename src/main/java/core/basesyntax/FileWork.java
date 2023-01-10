f4e20f307fff34f6240c42c6117163ee64bfd61a
carbo <carbon7557@gmail.com>
2023-01-10 18:43:02 +0000

    Task is done.

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -8,47 +8,47 @@ import java.util.Arrays;
 
 public class FileWork {
     public String[] readFromFile(String fileName) {
-            File file = new File(fileName);
-            int index = 0;
-            String[] returnArray;
-            try {
-                BufferedReader reader = null;
-                reader = new BufferedReader(new FileReader(file));
-                StringBuilder builder = new StringBuilder();
-                String value = reader.readLine();
-                if (value == null) {
-                    returnArray = new String[0];
-                    return returnArray;
-                }
-                while (value != null) {
-                    builder.append(value).append(" ");
-                    value = reader.readLine();
-                }
-                String[] splitFile = builder.toString().toLowerCase().split("\\W ");
-                for (String word : splitFile) {
-                    String[] splitFile1 = word.split(" ");
-                    for (String word1 : splitFile1) {
-                        char charAtIndexOne = word1.charAt(0);
-                        if (charAtIndexOne == 'w') {
-                            index++;
-                        }
-                    }
-                }
-                returnArray = new String[index];
-                int i = 0;
-                for (String word : splitFile) {
-                    String[] splitFile1 = word.split(" ");
-                    for (String word1 : splitFile1) {
-                        char charAtIndexOne = word1.charAt(0);
-                        if (charAtIndexOne == 'w') {
-                            returnArray[i++] = word1;
-                        }
-                    }
-                }
-            } catch (IOException e) {
-                throw new RuntimeException("Can't read the file", e);
+        File file = new File(fileName);
+        int index = 0;
+        String[] returnArray;
+        try {
+            BufferedReader reader = null;
+            reader = new BufferedReader(new FileReader(file));
+            StringBuilder builder = new StringBuilder();
+            String value = reader.readLine();
+            if (value == null) {
+                returnArray = new String[0];
+                return returnArray;
             }
-            Arrays.sort(returnArray);
-            return returnArray;
+            while (value != null) {
+                builder.append(value).append(" ");
+                value = reader.readLine();
+            }
+            String[] splitFile = builder.toString().toLowerCase().split("\\W ");
+            for (String word : splitFile) {
+                String[] splitFile1 = word.split(" ");
+                for (String word1 : splitFile1) {
+                    char charAtIndexOne = word1.charAt(0);
+                    if (charAtIndexOne == 'w') {
+                        index++;
+                    }
+                }
+            }
+            returnArray = new String[index];
+            int i = 0;
+            for (String word : splitFile) {
+                String[] splitFile1 = word.split(" ");
+                for (String word1 : splitFile1) {
+                    char charAtIndexOne = word1.charAt(0);
+                    if (charAtIndexOne == 'w') {
+                        returnArray[i++] = word1;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read the file", e);
         }
+        Arrays.sort(returnArray);
+        return returnArray;
     }
+}
