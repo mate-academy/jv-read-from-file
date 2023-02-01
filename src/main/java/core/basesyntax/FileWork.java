42433b0fac2b631e3a7eccc672ae3b8e6dbdd9e0
kromchk <kromchyk@yahoo.com>
2023-02-02 00:27:15 +0200

    Working result

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,33 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.ArrayList;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        Path path = Paths.get(fileName);
+        ArrayList<String> res = new ArrayList<>();
+        try (BufferedReader reader = Files.newBufferedReader(path)) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.trim().length() > 0) {
+                    String[] tmp = line.split("\\W");
+                    for (String word : tmp) {
+                        if (word.trim().length() > 0 && word.trim().toLowerCase().charAt(0) == 'w') {
+                            res.add(word.trim().toLowerCase());
+                        }
+                    }
+                }
+            }
+
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        return res.isEmpty() ? null: Arrays.copyOf(res.stream().sorted().toArray(), res.size(), String[].class);
     }
 }
