528a46d45c42da41a8f94e97c554a3bab495b71a
MaksymHorielikov <hmv.fermats@gmail.com>
2023-02-05 00:55:49 +0200

    first commit (create method readFromFile)

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,31 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        File file = new File(fileName);
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line = reader.readLine();
+            while (line != null) {
+                stringBuilder.append(line.toLowerCase()).append(System.lineSeparator());
+                line = reader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("File not found", e);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        return Arrays.stream(stringBuilder.toString().split("\\W+"))
+                .filter(word -> word.startsWith("w"))
+                .sorted()
+                .toArray(String[]::new);
     }
 }
