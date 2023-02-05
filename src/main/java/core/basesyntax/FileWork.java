8c7174c7f8844abcd24d993950379224348cc95c
Volodymyr Romanyshyn <Romanvlad000@gmail.com>
2023-02-05 11:04:57 +0200

    Initial commit

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,18 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+        } catch (IOException e){
+            throw new RuntimeException("Can't reade from file!");
+        }
+        return ;
     }
 }
