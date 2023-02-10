6eef30774b95cadf00d87dc06ad0a12a887f3acc
Denys Dominskyi <dominskyj@gmail.com>
2023-02-10 12:27:10 +0200

    Initial commit

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,39 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.List;
+
 public class FileWork {
+    private static final String SPECIFIED_CHARACTER = "w";
+    private final StringBuilder stringBuilder = new StringBuilder();
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        List<String> lines;
+        try {
+            lines = Files.readAllLines(file.toPath());
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read the file ", e);
+        }
+        if (lines.size() == 0) {
+            return new String[]{};
+        }
+        String[] linesToArray = lines.toString()
+                .toLowerCase()
+                .split("\\W+");
+        for (String word : linesToArray) {
+            if (word.startsWith(SPECIFIED_CHARACTER)) {
+                stringBuilder.append(word).append(" ");
+            }
+        }
+        if (stringBuilder.length() == 0) {
+            return new String[]{};
+        }
+        String[] result = stringBuilder.toString().split(" ");
+        Arrays.sort(result);
+        return result;
     }
 }
