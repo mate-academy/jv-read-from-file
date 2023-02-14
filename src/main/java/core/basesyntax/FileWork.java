fde5e57f442e496ff89d14d67d5fa38608bf6958
Nickolay Voloshchuk <124300295+NickolayVoloshchuk@users.noreply.github.com>
2023-02-14 21:34:15 +0200

    HomeWork JavaCore/Readind from file and filtering

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,29 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.List;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        List<String> list;
+        String[] wordsArray;
+        File file = new File(fileName);
+        StringBuilder words = new StringBuilder();
+        try {
+            list = Files.readAllLines(file.toPath());
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        for (String word : list.toString().toLowerCase().trim().split("\\W+")) {
+            if (word.toLowerCase().startsWith("w")) {
+                words.append(word).append(",");
+            }
+        }
+        wordsArray = words.toString().toLowerCase().split(",");
+        Arrays.sort(wordsArray);
+        return words.length() > 0 ? wordsArray : new String[0];
     }
 }
