733311b0f3e832f1d37adadd8bf42d295d9c0944
Oleksandr Krasnov <redman2775@gmail.com>
2023-02-14 22:36:39 +0200

    realized method "readFromFile";

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,40 @@
 package core.basesyntax;
 
+import java.io.*;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String[] EMPTY_ARRAY = new String[0];
+    private static final char STARTING_WITH = 'w';
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        String[] result;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            StringBuilder stringBuilder = new StringBuilder();
+            int value = reader.read();
+            if (value == -1) {
+                return EMPTY_ARRAY;
+            }
+            while (value != -1) {
+                stringBuilder.append((char) value);
+                value = reader.read();
+            }
+            String[] words = stringBuilder.toString().toLowerCase().split("\\W+");
+            StringBuilder builder = new StringBuilder();
+            for (String word : words) {
+                if (word.charAt(0) == STARTING_WITH) {
+                    builder.append(word).append("=");
+                }
+            }
+            result = builder.toString().split("=");
+            Arrays.sort(result);
+            return builder.length() == 0 ? EMPTY_ARRAY : result;
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("File not found", e);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file: " + file, e);
+        }
     }
 }
