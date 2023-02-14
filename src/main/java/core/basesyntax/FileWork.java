727fa972e5ea1ccf3ddac4e38262989f226e79f8
Daunito <gatne2004@gmail.com>
2023-02-15 00:15:35 +0100

    added solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,34 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    public static final String FIRST_LETTER_IN_WORD = "w";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                builder.append(value).append(" ");
+                value = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot read file", e);
+        }
+        String regex = String.format("\\b(?![%s%s])\\w+",
+                FIRST_LETTER_IN_WORD, FIRST_LETTER_IN_WORD.toUpperCase());
+        String[] words = builder.toString()
+                .replaceAll(regex, "")
+                .replaceAll("[^\\w\\s]", "")
+                .replaceAll("\\s+", " ")
+                .trim()
+                .toLowerCase()
+                .split(" ");
+        Arrays.sort(words);
+        return words[0].length() == 0 ? new String[0] : words;
     }
 }
