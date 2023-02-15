829349d3f7e215de167aebd2dd3d76321c51e815
Volodymyr Prysiazhniuk <verticalsyu@gmail.com>
2023-02-15 14:39:14 +0200

    release

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,54 +1,39 @@
 package core.basesyntax;
 
-import org.jetbrains.annotations.NotNull;
-
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
 
 public class FileWork {
     private static final String SPECIFIED_CHARACTER = "w";
-    private static final String PUNCTUATION_PATTERN = "[.,!?-]";
+    private static final String EXCEPTION_WORDS_PATTERN = "[^a-z ]";
+    private static final String WORDS_SEPARATOR = " ";
 
     public String[] readFromFile(String fileName) {
-        File file = new File(fileName);
-        StringBuilder stringBuilder = new StringBuilder();
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(file));
-            int value = reader.read();
-            while (value != -1) {
-                stringBuilder.append((char) value);
-                value = reader.read();
+        StringBuilder builder = new StringBuilder();
+        String[] result;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String lines = bufferedReader.readLine();
+            while (lines != null) {
+                String[] wordsFromLine = lines.toLowerCase()
+                        .replaceAll(EXCEPTION_WORDS_PATTERN, WORDS_SEPARATOR)
+                        .split(WORDS_SEPARATOR);
+                lines = bufferedReader.readLine();
+                for (String word : wordsFromLine) {
+                    if (word.startsWith(SPECIFIED_CHARACTER)) {
+                        builder.append(word).append(WORDS_SEPARATOR);
+                    }
+                }
             }
-            String cleanedString = stringBuilder.toString().toLowerCase().replaceAll(PUNCTUATION_PATTERN,"");
-            String[] splitArray = cleanedString.split(" ");
-            return bySymbolWordFinder(splitArray, SPECIFIED_CHARACTER);
-        }
-        catch (IOException e) {
-            throw new RuntimeException("Can't read file", e);
-        }
-    }
-
-    public String[] bySymbolWordFinder (String[] strings, String character) {
-        int size = 0;
-        if (strings.length == 0) {
-            return new String[] {};
-        }
-        for (String string : strings) {
-            if (string.startsWith(character)) {
-                size++;
+            result = builder.toString().split(WORDS_SEPARATOR);
+            Arrays.sort(result);
+            if (builder.toString().length() == 0) {
+                return new String[] {};
             }
+            return result;
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read this file",e);
         }
-        String[] result = new String[size];
-        for (String string : strings) {
-            int i = 0;
-            if (string.startsWith(character)) {
-                result[i] = string;
-                i++;
-            }
-        }
-        return Arrays.sort();
     }
 }
