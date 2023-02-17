b3cf096562351c0c9c66aa2aab72245e866b2b5e
Anatolii Nazarov <anatoly_n@ukr.net>
2023-02-17 10:01:03 +0200

    Done

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,42 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class FileWork {
+    private static final String WHITESPACE = " ";
+    private static final String NOTWORD = "\\W+";
+    StringBuilder builderWordsW = new StringBuilder();
+    StringBuilder builderWordsAll = new StringBuilder();
+    int count = 0;
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        if ((fileName == "") || (fileName == null)) {
+            return null;
+        }
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            String value = reader.readLine();
+            while (value != null) {
+                    builderWordsAll.append(value).append(System.lineSeparator());
+                    value = reader.readLine();
+            }
+        }
+        catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        String[] words = builderWordsAll.toString().toLowerCase().split(NOTWORD);
+        for (int i = 0; i < words.length; i++) {
+            if ((words[i].length() >= 1) && (words[i].charAt(0) == ('w'))) {
+                builderWordsW.append(words[i] + " ");
+                count += 1;
+            } else if (i == words.length - 1 && count == 0) {
+                return new String[0];
+            }
+        }
+        String[] wordsW = builderWordsW.toString().split(WHITESPACE);
+        Arrays.sort(wordsW);
+        return wordsW;
     }
 }
