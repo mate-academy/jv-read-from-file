0f0bdb88b0cf6592815b465d0f73ffc7351ed719
Anastasiia Saprykina <n.pukshyn1@gmail.com>
2023-02-27 17:33:41 +0200

    change names for more informative

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -9,34 +9,35 @@ public class FileWork {
     private static final String DELIMITER = "\\W+";
     private static final String DELIMITER_FOR_RESULT = " ";
     private static final char LETTER = 'w';
+    private static final int INDEX_FOR_NEW_FILE = 0;
 
     public String[] readFromFile(String fileName) {
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(fileName));
-            StringBuilder stringBuilder = new StringBuilder();
-            String value = reader.readLine();
+        try (BufferedReader readerFromFile = new BufferedReader(new FileReader(fileName))) {
+            StringBuilder fileContent = new StringBuilder();
+            String value = readerFromFile.readLine();
             if (value == null) {
                 return new String[] {};
             }
             while (value != null) {
-                stringBuilder.append(value).append(System.lineSeparator());
-                value = reader.readLine();
+                fileContent.append(value).append(System.lineSeparator());
+                value = readerFromFile.readLine();
             }
-            String[] split = stringBuilder.toString().toLowerCase().split(DELIMITER);
-            Arrays.sort(split);
-            StringBuilder builder = new StringBuilder();
-            for (int i = 0; i < split.length; i++) {
-                if (split[i].charAt(0) == LETTER) {
-                    builder.append(split[i]).append(DELIMITER_FOR_RESULT);
+            String[] fileContentByWords = fileContent.toString().toLowerCase().split(DELIMITER);
+            Arrays.sort(fileContentByWords);
+            StringBuilder sortedDataFromFile = new StringBuilder();
+            for (int i = 0; i < fileContentByWords.length; i++) {
+                if (fileContentByWords[i].charAt(INDEX_FOR_NEW_FILE) == LETTER) {
+                    sortedDataFromFile.append(fileContentByWords[i])
+                            .append(DELIMITER_FOR_RESULT);
                 }
             }
-            if (builder.toString().split(DELIMITER_FOR_RESULT).length == 1) {
+            if (sortedDataFromFile.toString().split(DELIMITER_FOR_RESULT).length == 1) {
                 return new String[] {};
             } else {
-                return builder.toString().split(DELIMITER_FOR_RESULT);
+                return sortedDataFromFile.toString().split(DELIMITER_FOR_RESULT);
             }
         } catch (IOException e) {
-            throw new RuntimeException("Can`t read file", e);
+            throw new RuntimeException("Can`t read file " + fileName, e);
         }
     }
 }
