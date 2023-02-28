5b64bffec2e7ee79219a64665c6ccc60d0a2715a
Oleg Ravlyk <oleg.ravlyk2005@gmail.com>
2023-02-28 02:22:30 +0200

    added and checked solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,11 +1,8 @@
 package core.basesyntax;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Arrays;
 
 public class FileWork {
@@ -13,15 +10,20 @@ public class FileWork {
         File file = new File(fileName);
         try {
             String fileString = new String(Files.readAllBytes(file.toPath()));
-            String[] array = fileString.split(" ");
+
+            String[] array = fileString.replaceAll("\n", " ").split(" ");
             StringBuilder sortedString = new StringBuilder();
             for (String str : array) {
                 if (str.toLowerCase().replaceAll("\\W", "").startsWith("w")) {
-                    sortedString.append(str.toLowerCase().replaceAll("\\W", "")).append(System.lineSeparator());
+                    sortedString.append(str.toLowerCase().replaceAll("\\W", ""))
+                            .append(System.lineSeparator());
                 }
             }
             String[] requiredArray = sortedString.toString().split(System.lineSeparator());
             Arrays.sort(requiredArray);
+            if (requiredArray[0].equals("")) {
+                return new String[0];
+            }
             return requiredArray;
         } catch (IOException e) {
             throw new RuntimeException("Can't read a file", e);
