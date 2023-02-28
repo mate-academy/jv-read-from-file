968b1a538e7a369a079aa01ea07adc77ebcb7b02
Oleg Ravlyk <oleg.ravlyk2005@gmail.com>
2023-02-28 02:06:35 +0200

    initial commit, afirst Ñsolution version

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,30 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
 public class FileWork {
-    public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+    public String[] readFromFile(String fileName) throws IOException {
+        File file = new File(fileName);
+        try {
+            String fileString = new String(Files.readAllBytes(file.toPath()));
+            String[] array = fileString.split(" ");
+            StringBuilder sortedString = new StringBuilder();
+            for (String str : array) {
+                if (str.toLowerCase().replaceAll("\\W", "").startsWith("w")) {
+                    sortedString.append(str.toLowerCase().replaceAll("\\W", "")).append(System.lineSeparator());
+                }
+            }
+            String[] requiredArray = sortedString.toString().split(System.lineSeparator());
+            Arrays.sort(requiredArray);
+            return requiredArray;
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read a file", e);
+        }
     }
 }
