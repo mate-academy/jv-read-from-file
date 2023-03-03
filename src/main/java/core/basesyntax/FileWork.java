24f98717647db79cf14b2fae0a4ff71f3bdd9914
Malinskyi Daniil <danmalina2707@gmail.com>
2023-03-03 10:18:05 +0200

    jv-read-from-file done; I used regular expression to split the text \\p{Punct} and \\s; converted words array into a stream and methods to filter words starting with "w" ; removed empty line

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -6,7 +6,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 
-
 public class FileWork {
     public String [] readFromFile(String fileName) {
         String text = "";
