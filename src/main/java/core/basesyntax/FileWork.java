1da45aed2c475153e0479bc7faf92722f9fd3f53
Volodymyr Davidov <volodymyrdavidov1@gmail.com>
2023-03-03 11:29:12 +0200

    hw jv-read-from-file

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,6 +1,5 @@
 package core.basesyntax;
 
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -11,7 +10,6 @@ public class FileWork {
     private static final int INDEX_OF_FIRST_LETTER = 0;
     private static final char FIRST_LETTER_OF_THE_WORD = 'w';
 
-
     public String[] readFromFile(String fileName) {
         File file = new File(fileName);
         StringBuilder builder = new StringBuilder();
@@ -20,14 +18,11 @@ public class FileWork {
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
             String value = bufferedReader.readLine();
 
-
             while (value != null) {
                 String[] words = value.toLowerCase().split("\\W+");
 
-
                 for (int i = 0; i < words.length; i++) {
                     char[] word = words[i].toCharArray();
-
 
                     if (word[INDEX_OF_FIRST_LETTER] == FIRST_LETTER_OF_THE_WORD) {
                         builder.append(words[i]).append(" ");
@@ -39,12 +34,10 @@ public class FileWork {
             throw new RuntimeException("Can't read file" + file, e);
         }
 
-
         if (builder.length() == 0) {
             return new String[0];
         }
         String[] result = builder.toString().split(" ");
-
 
         for (int i = 0; i < result.length; i++) {
             for (int k = i + 1; k < result.length; k++) {
