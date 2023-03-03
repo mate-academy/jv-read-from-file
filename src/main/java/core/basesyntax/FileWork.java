5bbd8eb12b8f91f008d2a3e057f9eda85444f2a8
Volodymyr Davidov <volodymyrdavidov1@gmail.com>
2023-03-03 11:24:17 +0200

    hw jv-read-from-file

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,46 +1,57 @@
 package core.basesyntax;
 
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
+
 public class FileWork {
-    private static final int LITERAL_POSITION = 0;
-    private static final char REQUIRED_CHARACTER = 'w';
+    private static final int INDEX_OF_FIRST_LETTER = 0;
+    private static final char FIRST_LETTER_OF_THE_WORD = 'w';
+
 
     public String[] readFromFile(String fileName) {
         File file = new File(fileName);
-        StringBuilder stringBuilder = new StringBuilder();
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
-            String string = bufferedReader.readLine();
+        StringBuilder builder = new StringBuilder();
 
-            while (string != null) {
-                String[] words = string.toLowerCase().split("\\W+");
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            String value = bufferedReader.readLine();
+
+
+            while (value != null) {
+                String[] words = value.toLowerCase().split("\\W+");
+
 
                 for (int i = 0; i < words.length; i++) {
                     char[] word = words[i].toCharArray();
-                    if (word[LITERAL_POSITION] == REQUIRED_CHARACTER) {
-                        stringBuilder.append(words[i]).append(" ");
+
+
+                    if (word[INDEX_OF_FIRST_LETTER] == FIRST_LETTER_OF_THE_WORD) {
+                        builder.append(words[i]).append(" ");
                     }
                 }
-                string = bufferedReader.readLine();
+                value = bufferedReader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't read file" + file, e);
         }
 
-        if (stringBuilder.length() == 0) {
+
+        if (builder.length() == 0) {
             return new String[0];
         }
-        String[] result = stringBuilder.toString().split(" ");
+        String[] result = builder.toString().split(" ");
+
 
         for (int i = 0; i < result.length; i++) {
-            for (int j = 0; j < result.length; j++) {
-                if (result[i].compareTo(result[j]) > 0) {
+            for (int k = i + 1; k < result.length; k++) {
+                if (result[i].compareTo(result[k]) > 0) {
                     String temp = result[i];
-                    result[i] = result[j];
-                    result[j] = temp;
+                    result[i] = result[k];
+                    result[k] = temp;
                 }
             }
         }
