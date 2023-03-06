4addf2cdc9eca1268c16c684b3e33c5f88ff2e21
Pavlo Golibroda <p.golibroda@gmail.com>
2023-03-06 22:28:24 +0200

    implemented method which return all words starting with w

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -2,7 +2,13 @@ package core.basesyntax;
 
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        String[] words = fileName.split("\\W+");
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String word : words) {
+            if (word.startsWith("w") || word.startsWith("W")) {
+                stringBuilder.append(word.toLowerCase()).append(" ");
+            }
+        }
+        return stringBuilder.toString().trim().split(" ");
     }
 }
