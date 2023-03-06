7a77ed40b180a9d8db5e8e97f03de92153db4c94
Rule <amirparadice@gmail.com>
2023-03-06 14:56:54 -0500

    1.Created Static fields. 2.Created FileReader, activate String builder. 3.Wrapped all into try/catch, made a String from a builder with all minusculas letters. 4.Made an array from the String, with split method. 5.Created two loops for filter and correct an array buy a firstletter and compress. 6.sort with Array.sort method and return result

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,45 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String First_Letter = "w";
+    private static final int ZERO = 0;
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            StringBuilder builder = new StringBuilder();
+            int value = reader.read();
+            while (value != -1) {
+                builder.append((char) value);
+                value = reader.read();
+            }
+
+            String text = builder.toString().toLowerCase();
+            String[] split = text.split("\\W+");
+            int count = ZERO;
+
+            for (String words : split) {
+                if (words.startsWith(First_Letter)) {
+                    count++;
+                }
+            }
+            String[] result = new String[count];
+            int resultIndex = ZERO;
+            for (String array : split) {
+                if (array.startsWith(First_Letter)) {
+                    result[resultIndex] = array;
+                    resultIndex++;
+                }
+            }
+            Arrays.sort(result);
+            return result;
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to file" + fileName, e);
+        }
     }
 }
