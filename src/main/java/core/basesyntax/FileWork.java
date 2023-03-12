f3accb608696e821c6fc00ab9dfaf5f03e87c1b5
Victor Rudkovsky <vpcomtek@gmail.com>
2023-03-12 15:49:30 +0200

    HW jv-read-from-file

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,33 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            String line = reader.readLine();
+
+            while (line != null) {
+                String[] words = words(line);
+                for (String word : words) {
+                    if (word.charAt(0) == 'w') {
+                        stringBuilder.append(word).append(" ");
+                    }
+                }
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return stringBuilder.toString().split(" ");
+    }
+
+    private String[] words(String line) {
+        return line.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+");
     }
 }
