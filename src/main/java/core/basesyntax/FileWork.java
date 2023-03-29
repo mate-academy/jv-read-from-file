36c0baab2134cf06d68b0b6aa3da49f335550c40
Serhii Serdiuk <serhii.serdiuk@gmail.com>
2023-03-29 16:47:24 +0200

    Create readFromFile method

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,35 @@
 package core.basesyntax;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class FileWork {
+    private static final String SPECIFIED_CHARACTER = "w";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        String check;
+        String[] strings;
+        try {
+            InputStream inputStream = new FileInputStream(fileName);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            ArrayList<String> array = new ArrayList<>();
+            while (bufferedReader.ready()) {
+                check = (bufferedReader.readLine()).toLowerCase();
+                String[] line = check.split(" ");
+                for (String s : line) {
+                    if (s.startsWith(SPECIFIED_CHARACTER)) {
+                        array.add(s.replaceAll("\\p{Punct}", ""));
+                    }
+                }
+            }
+            inputStream.close();
+            bufferedReader.close();
+            Collections.sort(array);
+            strings = array.toArray(new String[0]);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        return strings;
     }
 }
