323ffc4e9adbcdd1b3cec3118d15ac67e133a314
Dmytro Sokolovskyi <dmytro.sokolovskyi@icloud.com>
2023-03-29 22:59:02 -0400

    added implementation to FileWork class

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,40 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String SPECIFIED_CHARACTER = "w";
+    private static final String REGEX = "\\W";
+    private static final String WHITESPACE = " ";
+    private static final String[] EMPTY_ARRAY = new String[]{};
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String item = bufferedReader.readLine();
+            if (item == null) {
+                return EMPTY_ARRAY;
+            }
+            while (item != null) {
+                String[] data = item.toLowerCase().split(REGEX);
+                for (String word : data) {
+                    if (word.startsWith(SPECIFIED_CHARACTER)) {
+                        stringBuilder.append(word).append(WHITESPACE);
+                    }
+                }
+                item = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file: " + fileName, e);
+        }
+        if (stringBuilder.length() == 0) {
+            return EMPTY_ARRAY;
+        }
+        String[] stringOut = stringBuilder.toString().split(WHITESPACE);
+        Arrays.sort(stringOut);
+        return stringOut;
     }
 }
