dd5338f03215880f7aa23c0411be91fb637727cd
Serj <pandastars535@gmail.com>
2023-04-05 00:08:11 +0300

    initial commit

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,28 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+
+            String[] total = new String[]{};
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader(fileName));
+                StringBuilder stringBuilder = new StringBuilder();
+                String value = reader.readLine();
+                while (value != null) {
+                    stringBuilder.append(value);
+                    System.out.println(stringBuilder);
+                    value = reader.readLine();
+                }
+
+
+            } catch (IOException e) {
+                throw new RuntimeException("cant read the file", e);
+            }
+
+            return null;
     }
 }
