2eedb88b6e1349793ca80bd65c8f085a7fc0e6cd
Serj <pandastars535@gmail.com>
2023-04-09 00:56:19 +0300

    sent solution to menthor to check , and ask her help

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -7,28 +7,30 @@ import java.util.Arrays;
 
 public class FileWork {
     private static final String SPECIFIED_CHARACTER = "w";
-    public String[] readFromFile(String fileName) {
-        try {
-                BufferedReader reader = new BufferedReader(new FileReader(fileName));
-                StringBuilder stringBuilder = new StringBuilder();
-                String value = reader.readLine();
-                while (value != null) {
-                        stringBuilder.append(value);
-                        value = reader.readLine();
-                }
-                String[] examples = stringBuilder.toString().toLowerCase().split("\\W+");
-                StringBuilder word = new StringBuilder();
-                for (String exam: examples) {
-                    if (exam.startsWith(SPECIFIED_CHARACTER)) {
-                        word.append(exam).append(" ");
-                    }
-                }
-                String[] total = word.toString().split(" ");
-            System.out.println(Arrays.asList(total));
-                return  total;
 
-            } catch (IOException e) {
-                throw new RuntimeException("cant read the file", e);
+    public String[] readFromFile(String fileName) {
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            StringBuilder stringBuilder = new StringBuilder();
+            String value = reader.readLine();
+            while (value != null) {
+                stringBuilder.append(value);
+                value = reader.readLine();
             }
+            String[] examples = stringBuilder.toString().toLowerCase().split("\\W+");
+            StringBuilder word = new StringBuilder();
+            for (String exam: examples) {
+                if (exam.startsWith(SPECIFIED_CHARACTER)) {
+                    word.append(exam).append(" ");
+                }
+            }
+            String[] total = word.toString().split(" ");
+            System.out.println(Arrays.asList(total));
+            return total;
+
+        } catch (IOException e) {
+            throw new RuntimeException("cant read the file", e);
+        }
     }
 }
