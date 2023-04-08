b327ff3e3b0830a36b1dad3689460c80e4b9a017
Serj <pandastars535@gmail.com>
2023-04-09 00:39:03 +0300

    sent solution to menthor to check , and ask her help

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -6,8 +6,9 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class FileWork {
+    private static final String SPECIFIED_CHARACTER = "w";
     public String[] readFromFile(String fileName) {
-            try {
+        try {
                 BufferedReader reader = new BufferedReader(new FileReader(fileName));
                 StringBuilder stringBuilder = new StringBuilder();
                 String value = reader.readLine();
@@ -15,26 +16,19 @@ public class FileWork {
                         stringBuilder.append(value);
                         value = reader.readLine();
                 }
-                String[] examples = stringBuilder.toString().toLowerCase().split(" ");
+                String[] examples = stringBuilder.toString().toLowerCase().split("\\W+");
                 StringBuilder word = new StringBuilder();
                 for (String exam: examples) {
-                    if (exam.contains("w")) {
+                    if (exam.startsWith(SPECIFIED_CHARACTER)) {
                         word.append(exam).append(" ");
                     }
                 }
-                String[] total = word.toString().split("\\W+");
-                StringBuilder builder = new StringBuilder();
-                for (String string : total) {
-                    if (string.startsWith("w")) {
-                        builder.append(string).append(" ");
-                    }
-                }
-                String[] result = builder.toString().toLowerCase().split(" ");
-                System.out.println(Arrays.toString(result));
+                String[] total = word.toString().split(" ");
+            System.out.println(Arrays.asList(total));
+                return  total;
 
             } catch (IOException e) {
                 throw new RuntimeException("cant read the file", e);
             }
-            return null;
     }
 }
--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,13 +1,14 @@
 package core.basesyntax;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
 
 public class Main {
-
     public static void main(String[] args) {
         FileWork fileWork = new FileWork();
+        fileWork.readFromFile("test1");
+        fileWork.readFromFile("test2");
+        fileWork.readFromFile("test3");
         fileWork.readFromFile("test4");
-
+        fileWork.readFromFile("test5");
     }
+
 }
