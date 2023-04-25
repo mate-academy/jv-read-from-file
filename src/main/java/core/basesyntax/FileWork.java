780fe2e0b8f8de594b0a2074429b58ca02cc2e31
Yevhen Tolkunov <yevhentolkunov@gmail.com>
2023-04-25 17:48:56 +0300

    first commit

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,38 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String value = reader.readLine();
+            if (value == null) {
+                return new String[0];
+            }
+            while (value != null) {
+                stringBuilder.append(value).append(System.lineSeparator());
+                value = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        String sentence = stringBuilder.toString();
+        String[] split = sentence.toLowerCase().split("\\W+");
+        ArrayList<String> words = new ArrayList();
+        for (int index = 0; index < split.length; index++) {
+            if (split[index].charAt(0) == 'w') {
+                words.add(split[index]);
+            }
+        }
+        Collections.sort(words);
+        return words.toArray(new String[0]);
     }
 }
