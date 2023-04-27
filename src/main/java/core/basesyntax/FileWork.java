7bf75cb8fbe545abd166546effd63e2e31650140
Arina Polna <polna.arina@gmail.com>
2023-04-27 17:32:04 +0300

    creating array with only words that start with a letter Ñ w

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,34 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String FIRST_LETTER = "w";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File myFile = new File(fileName);
+        try (BufferedReader reader = new BufferedReader(new FileReader(myFile))) {
+            StringBuilder builder = new StringBuilder();
+            String value = reader.readLine();
+            while (value != null) {
+                builder.append(value).append(System.lineSeparator());
+                value = reader.readLine();
+            }
+            String content = builder.toString();
+            String contentLowerCase = content.replaceAll("[^\\w\\s]", "").toLowerCase();
+            String[] words = contentLowerCase.split("\\s+");
+            words = Arrays.stream(words)
+              .filter(word -> word.startsWith(FIRST_LETTER))
+              .toArray(String[]::new);
+            Arrays.sort(words);
+            return words;
+
+        } catch (IOException e) {
+            throw new RuntimeException("Cant read file:", e);
+        }
     }
 }
