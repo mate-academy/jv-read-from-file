05c159a56830e7c7f584ee45235302365d90d1cd
Igor Maksimov <maksigors@gmail.com>
2023-04-27 22:41:36 +0300

    second build of project

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,35 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String SPECIFIED_CHARACTER = "w";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        StringBuilder strBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(
+                new FileReader(new File(fileName)))) {
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                for (String word : value.toLowerCase().split(" ")) {
+                    if (word.startsWith(SPECIFIED_CHARACTER)) {
+                        strBuilder.append(word.toLowerCase().replaceAll("[!?,.]", "")).append(" ");
+                    }
+                }
+                value = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        String[] wordsResult = strBuilder.toString().split(" ");
+        Arrays.sort(wordsResult);
+        if (wordsResult.length == 0) {
+            return new String[] {};
+        }
+        return wordsResult;
     }
 }
