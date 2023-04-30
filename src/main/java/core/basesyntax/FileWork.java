3cb8e76e859e32e4ba0a5a13deb57cbee0c63d87
ivan hromovyi <gromovoy1994@gmail.com>
2023-04-30 15:25:23 +0200

    read from file, looped through values in a string to find words which start with 'w'. Created new String arr with 'w' words

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,47 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final char SPECIFIED_CHARACTER = 'w';
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        StringBuilder builder = new StringBuilder();
+        File file = new File(fileName);
+        if (file.length() == 0) {
+            return new String[0];
+        }
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            int value = bufferedReader.read();
+            while (value != -1) {
+                builder.append((char) value);
+                value = bufferedReader.read();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Couldn't write data to the file " + "'" + fileName + "'");
+        }
+        String[] splitedBuilder = builder.toString()
+                .replaceAll("W", "w")
+                .replaceAll("[,.!?]", "")
+                .split("\\s+");
+        int count = 0;
+        for (String newSplitedString : splitedBuilder) {
+            if (newSplitedString.charAt(0) == SPECIFIED_CHARACTER) {
+                count++;
+            }
+        }
+        String[] resultArr = new String[count];
+        int index = 0;
+        for (String resultString : splitedBuilder) {
+            if (resultString.charAt(0) == SPECIFIED_CHARACTER) {
+                resultArr[index++] = resultString;
+            }
+        }
+        Arrays.sort(resultArr);
+        return resultArr;
     }
 }
