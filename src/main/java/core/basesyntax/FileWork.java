07773e48a7bad063fcd6d956ea907f64e231dc02
marinakusniruk <Kushnirukmv@gmail.com>
2023-05-12 16:41:15 +0200

    Read-from-file hometask completed

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,48 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String SPECIFIED_CHARACTER = "w";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        String readedText = " ";
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+            StringBuilder builder = new StringBuilder();
+            String words = bufferedReader.readLine();
+            while (words != null) {
+                builder.append(words).append(" ");
+                words = bufferedReader.readLine();
+            }
+            readedText = builder.toString();
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+
+        String[] words = readedText.split("\\W+");
+        int count = 0;
+        for (String word : words) {
+            if (word.toLowerCase().startsWith(SPECIFIED_CHARACTER)) {
+                count++;
+            }
+        }
+        int countIndex = 0;
+        String[] arrayToReturn = new String[count];
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].toLowerCase().startsWith(SPECIFIED_CHARACTER)) {
+                arrayToReturn[countIndex] = words[i].toLowerCase();
+                countIndex++;
+            }
+        }
+        if (arrayToReturn.length == 0) {
+            return arrayToReturn;
+        }
+        Arrays.sort(arrayToReturn);
+
+        return arrayToReturn;
     }
 }
