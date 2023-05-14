daa65bc978e19d6d786822c6ee256603ec673715
pochtalon <pochtalon1010@gmail.com>
2023-05-14 14:06:02 +0300

    Created Path object Created String allText for reading whole text from the file Slitted text to array by single words (used regex) Created ArrayList for sorting words Filled Arraylist by lowercase words, what start with 'w' or 'W' Checked if Arraylist is empty for return empty String array Sorted List Wrote all words to string used StringBuilder and ',' delimiter Separated string to string array

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,37 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        Path path = Paths.get(fileName);
+        String allText = null;
+        try {
+            allText = Files.readAllLines(path).toString();
+        } catch (IOException e) {
+            throw new RuntimeException("List was not created", e);
+        }
+        String[] words = allText.split("[^\\p{L}]");
+        ArrayList<String> wList = new ArrayList<>();
+        for (String word : words) {
+            if (word.toLowerCase().startsWith("w")) {
+                wList.add(word.toLowerCase());
+            }
+        }
+        if (wList.size() == 0) {
+            return new String[0];
+        }
+        Collections.sort(wList);
+        StringBuilder builder = new StringBuilder();
+        String delimiter = ",";
+        for (String word : wList) {
+            builder.append(word).append(delimiter);
+        }
+        return builder.toString().split(delimiter);
     }
 }
