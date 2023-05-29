44d7160be089cab3ad20fd30ac690bb383026332
Andrii Khomenko <andpey.homenko@gmail.com>
2023-05-29 23:26:15 +0300

    added the solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,47 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String SPECIAL_CHARACTER = "w";
+    private String [] filtered;
+
     public String[] readFromFile(String fileName) {
+
         //write your code here
-        return null;
+        StringBuilder builder = new StringBuilder();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            String value = reader.readLine();
+            while (value != null) {
+                builder.append(value).append(" ");
+                value = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        StringBuilder newBuilder = new StringBuilder();
+        String [] newArray = builder.toString().toLowerCase().replaceAll("\\W+", " ").split(" ");
+        if (builder.toString().isEmpty()) {
+            filtered = new String[0];
+            return filtered;
+        } else {
+            for (String element : newArray) {
+                if (element.startsWith(SPECIAL_CHARACTER)) {
+                    newBuilder.append(element).append(" ");
+                }
+            }
+        }
+        if (newBuilder.toString().isEmpty()) {
+            filtered = new String[0];
+            return filtered;
+        }
+        filtered = newBuilder.toString().trim().split(" ");
+        Arrays.sort(filtered);
+        return filtered;
     }
 }
+
