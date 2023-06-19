92a592966c11dde59626c568683d5ef004278cfa
Vitalii <stelmaschyk.vg@gmail.com>
2023-06-19 10:57:33 +0300

    Writed solution fo task Read-from-file

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,40 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    static final char SPECIFIED_CHARACTER = 'w';
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        int count = 0;
+        String[] result = new String[count];
+        File file = new File(fileName);
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String value = reader.readLine();
+            while (value != null) {
+                String[] splitFile = value.toLowerCase().split("\\W+");
+                for (String i : splitFile) {
+                    if (i.charAt(0) == SPECIFIED_CHARACTER) {
+                        count++;
+                    }
+                }
+                int temp = 0;
+                for (String i : splitFile) {
+                    if (i.charAt(0) == SPECIFIED_CHARACTER) {
+                        result[temp] = i;
+                        temp++;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("File can't find" + fileName + e);
+        }
+        Arrays.sort(result);
+        return result;
     }
 }
