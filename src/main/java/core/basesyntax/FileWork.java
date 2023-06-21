eba713d01a5c497a6c39402d36a414879af0e8b5
ANDREY <andrey.kiyas@gmail.com>
2023-06-21 22:03:57 +0300

    hw-jv-read-from-file-solution-andrey-kiyas

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,44 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.List;
+
 public class FileWork {
+    private static final String CERTAIN_LETTER = "w";
+    static final int STRING_INDEX = 0;
+    static final int FILE_LENGTH = 0;
+    static final int RESULT_LENGTH = 1;
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        if (file.length() == FILE_LENGTH) {
+            return new String[STRING_INDEX];
+        }
+        List<String> strings;
+        try {
+            strings = Files.readAllLines(file.toPath());
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read from file " + file.getName(), e);
+        }
+        String[] stringsSplit = String.valueOf(strings)
+                .replaceAll("\\p{Punct}", "")
+                .toLowerCase()
+                .split(" ");
+        StringBuilder builderOfW = new StringBuilder();
+        for (String word : stringsSplit) {
+            if (!word.startsWith(CERTAIN_LETTER)) {
+                continue;
+            }
+            builderOfW.append(word).append(" ");
+        }
+        String[] result = String.valueOf(builderOfW).split(" ");
+        Arrays.sort(result);
+        if (result.length == RESULT_LENGTH) {
+            return new String[STRING_INDEX];
+        }
+        return result;
     }
 }
