bf891ccf462dbf446846781f6d6e06c418f6849b
yermilov.m <yermilov.m@hotmail.com>
2023-06-24 14:23:24 +0200

    solution is ready for checking

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,4 +1,5 @@
 package core.basesyntax;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -9,34 +10,37 @@ import java.util.regex.Pattern;
 
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        public static String[] filterWordsFromFile(String fileName) {
-            // Regular expression pattern to match words starting with 'w'
-            Pattern pattern = Pattern.compile("\\b[wW]\\w*\\b");
+        // Regular expression pattern to match words starting with 'w'
+        Pattern pattern = Pattern.compile("\\b[wW]\\w*\\b");
 
-            List<String> wordsList = new ArrayList<>();
+        List<String> words = new ArrayList<>();
 
-            try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    String[] words = line.replaceAll("\\p{Punct}", "").toLowerCase().split("\\s+");
-                    wordsList.addAll(Arrays.asList(words));
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+        // Step 1: Read the text file and extract words
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Remove punctuation and convert to lowercase
+                String[] lineWords = line.replaceAll("\\p{Punct}", "").toLowerCase().split("\\s+");
+                words.addAll(Arrays.asList(lineWords));
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new String[0]; // Return an empty array if there is an error reading the file
+        }
 
-            // Filter words starting with 'w'
-            List<String> filteredWords = new ArrayList<>();
-            for (String word : wordsList) {
-                if (pattern.matcher(word).matches()) {
-                    filteredWords.add(word);
-                }
+        // Step 2: Filter words starting with 'w'
+        List<String> filteredWords = new ArrayList<>();
+        for (String word : words) {
+            if (pattern.matcher(word).matches()) {
+                filteredWords.add(word);
             }
+        }
 
-            // Sort the filtered words naturally
-            String[] result = filteredWords.toArray(new String[0]);
-            Arrays.sort(result, String.CASE_INSENSITIVE_ORDER);
+        // Step 3: Sort the filtered words naturally
+        String[] result = filteredWords.toArray(new String[0]);
+        Arrays.sort(result, String.CASE_INSENSITIVE_ORDER);
 
-            return result;
+        return result;
     }
 }
+
