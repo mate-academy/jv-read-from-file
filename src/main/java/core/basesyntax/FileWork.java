f3f8cf7b31ed0bd67a76263afb8f59c64752e5bb
Hlib Bykovskyi <minexgleb@gmail.com>
2023-06-25 19:05:30 +0300

    Implement FileWork

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,37 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        if (fileName.isEmpty()) {
+            return new String[0];
+        }
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String[] wordsStartsWithW = new String[0];
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] words = line.split("[\\s.,]+");
+
+                for (String word : words) {
+                    String cleanedWord = word.replaceAll("[^a-zA-Z]", "");
+
+                    if (!cleanedWord.isEmpty() && cleanedWord.toLowerCase().startsWith("w")) {
+                        wordsStartsWithW = Arrays.copyOf(wordsStartsWithW, wordsStartsWithW.length + 1);
+                        wordsStartsWithW[wordsStartsWithW.length - 1] = cleanedWord.toLowerCase();
+                    }
+                }
+            }
+
+            Arrays.sort(wordsStartsWithW);
+            return wordsStartsWithW;
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading file", e);
+        }
     }
 }
