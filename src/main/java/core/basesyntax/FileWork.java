8c4ab051d66b3d08b52c0786dc087c4cf72937f6
Leshchenko <411100253g@gmail.com>
2023-06-28 09:28:31 +0300

    implemented readFromFile in file work

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,26 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        String[] list = null;
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));) {
+            list = bufferedReader
+                    .lines()
+                    .flatMap(a -> Arrays.stream(a.split(" ")))
+                    .filter(s -> s.startsWith("w") || s.startsWith("W"))
+                    .map(s -> s.replaceAll("[^A-Za-z0-9]+", ""))
+                    .map(String::toLowerCase)
+                    .sorted()
+                    .toArray(String[]::new);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return list;
     }
 }
