1f15080068a06c5272f82c96fadbdd25c4948ad3
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-06-29 13:23:43 +0300

    added changes to the class FileWork

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -7,33 +7,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FileWork {
+    private static final String STARTING_LETTER = "w";
+    private static final String LINE_SEPARATOR = System.lineSeparator();
+
     public String[] readFromFile(String fileName) {
-        BufferedReader br = null;
-        try {
-            br = new BufferedReader(new FileReader(fileName));
-            StringBuilder sb = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            StringBuilder fileContent = new StringBuilder();
             String line;
             while ((line = br.readLine()) != null) {
-                sb.append(line).append(" ");
+                fileContent.append(line).append(LINE_SEPARATOR);
             }
             String[] words = fileName.split("\\W+");
             List<String> filteredWords = new ArrayList<>();
             for (String word : words) {
-                if (word.startsWith("w")) {
+                if (word.startsWith(STARTING_LETTER)) {
                     filteredWords.add(word);
                 }
             }
             return filteredWords.toArray(new String[0]);
         } catch (IOException e) {
-            throw new RuntimeException("can`t reach file", e);
-        } finally {
-            if (br != null) {
-                try {
-                    br.close();
-                } catch (IOException e) {
-                    throw new RuntimeException("can`t reach file", e);
-                }
-            }
+            throw new RuntimeException("can`t reach file: " + fileName, e);
         }
     }
 }
