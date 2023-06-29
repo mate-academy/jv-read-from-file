269fbbb581018753efd45793b1c8670a6ead8584
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-06-29 12:22:22 +0300

    added changes to the class FileWork and the code was written

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,37 @@
 package core.basesyntax;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(fileName));
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line).append(" ");
+            }
+            String[] words = fileName.split("\\W+");
+            StringBuilder filteredWords = new StringBuilder();
+            for (String word : words) {
+                if (word.toLowerCase().startsWith("w")) {
+                    filteredWords.append(word.toLowerCase()).append(" ");
+                }
+            }
+            String[] result = filteredWords.toString().trim().split(" ");
+            return result;
+        } catch (IOException e) {
+            throw new RuntimeException("can`t reach file", e);
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    throw new RuntimeException("can`t reach file", e);
+                }
+            }
+        }
     }
 }
