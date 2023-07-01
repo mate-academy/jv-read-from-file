b228ed3b8f7477ae272462595dd48e45c3a4b7b3
Bohdan Poliienko <mignatov756@gmail.com>
2023-07-01 23:40:33 +0300

    reading from file class

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,36 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                stringBuilder.append(value).append(System.lineSeparator());
+                value = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        String[] words = stringBuilder.toString().toLowerCase().split("\\W+");
+        StringBuilder builder = new StringBuilder();
+        String[] result = null;
+        for (String word : words) {
+            if (word.startsWith("w")) {
+                builder.append(word).append(" ");
+                result = builder.toString().split(" ");
+                Arrays.sort(result);
+            }
+        }
+        if (builder.length() == 0) {
+            return new String[]{};
+        }
+        return result;
     }
 }
