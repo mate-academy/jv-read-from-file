83ef53b49d2d8a96e7f5bdb6109b47aa7024b0d8
Visqe <veetee@tutanota.com>
2023-07-02 22:24:18 +0300

    added soolution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,32 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class FileWork {
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        List<String> listStartsFromW = new ArrayList<>();
+        String[] words = linesFromFile(fileName).toLowerCase().split("\\W+");
+        for (String word: words) {
+            if (word.startsWith("w")) {
+                listStartsFromW.add(word);
+            }
+        }
+        Collections.sort(listStartsFromW);
+        return listStartsFromW.toArray(new String[0]);
+    }
+
+    public static String linesFromFile(String filename) {
+        try {
+            List<String> listOfLines = Files.readAllLines(Path.of(filename));
+            return String.valueOf(listOfLines);
+        } catch (IOException e) {
+            throw new RuntimeException("Cant read data from file", e);
+        }
     }
 }
