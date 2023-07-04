5b45aca50b2fc1011156e169227e441740975fc5
Arsenii Mehdalskyi <arsen2506@knu.ua>
2023-07-04 21:21:59 +0300

    implemented readFromFile

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,30 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class FileWork {
+    private static final char SPECIFIED_CHARACTER = 'w';
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        ArrayList<String> wordsList = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] wordsFromLine = line.split("\\W+");
+                for (String word : wordsFromLine) {
+                    if (word.toLowerCase().startsWith(String.valueOf(SPECIFIED_CHARACTER))) {
+                        wordsList.add(word.toLowerCase());
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("File does not exit", e);
+        }
+        Collections.sort(wordsList);
+        return wordsList.toArray(new String[0]);
     }
 }
