a07044f1bf833d8b3dbf343051730a04556509aa
Serhiy Priymak <serhiy2506@gmail.com>
2023-07-05 21:22:19 +0300

    complete the task from README

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,39 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final char SPECIFIED_CHARACTER = 'w';
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        StringBuilder stringBuilder = new StringBuilder();
+        String[] sortedArray;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))){
+            String text = bufferedReader.readLine();
+            while (text != null) {
+                String[] textArr = text.split("\\W+");
+                for (String string : textArr) {
+                    String lowerCaseWord = string.toLowerCase();
+                    if (lowerCaseWord.charAt(0) == SPECIFIED_CHARACTER) {
+                        stringBuilder.append(lowerCaseWord).append(" ");
+                    }
+                }
+                text = bufferedReader.readLine();
+            }
+            String specialWords = stringBuilder.toString();
+            if (specialWords.length() == 0) {
+                return new String[0];
+            }
+            sortedArray = specialWords.split(" ");
+            Arrays.sort(sortedArray);
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read the file", e);
+        }
+        return sortedArray;
     }
 }
