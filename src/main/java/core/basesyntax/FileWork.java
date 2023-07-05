a88a02423c5980cf078796315eb40fdead33f781
Volodymyr Lazarchuk <lazarchuk.v.u@gmail.com>
2023-07-06 01:00:47 +0300

    Initial commit

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,45 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            String lineValue = bufferedReader.readLine();
+            while (lineValue != null) {
+                char targetChar = 'w';
+                String[] splittedWords = lineValue.split("\\W+");
+
+                for (String splittedWord : splittedWords) {
+                    String word = splittedWord.toLowerCase();
+                    char[] chars = word.toCharArray();
+                    if (chars[0] == targetChar) {
+                        stringBuilder.append(word).append(" ");
+                    }
+                }
+                lineValue = bufferedReader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Can't find file", e);
+        } catch (IOException e) {
+            throw new RuntimeException("RuntimeException", e);
+        }
+
+        String result = stringBuilder.toString().trim();
+
+        if (result.length() == 0) {
+            return new String[0];
+        }
+
+        String[] split = result.split(" ");
+        Arrays.sort(split);
+        return split;
     }
 }
