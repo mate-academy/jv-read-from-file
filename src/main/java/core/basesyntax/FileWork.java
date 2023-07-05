cff58b82220006cc424ec6bb58b369f6ab930686
Mykyta Zuber <nikitazuber@gmail.com>
2023-07-05 11:38:08 +0300

    readFromFile function

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,50 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    public static final char W_CHAR = 'w';
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        StringBuilder resultString = new StringBuilder();
+        String[] resultArray;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String value = reader.readLine();
+
+            while (value != null) {
+                String[] currentValueArray = value.split(" ");
+
+                for (int i = 0; i < currentValueArray.length; ++i) {
+                    String currentValueString = currentValueArray[i].toLowerCase();
+                    if (currentValueString.charAt(0) == W_CHAR) {
+                        if (resultString.length() >= 1) {
+                            resultString.append(" ");
+                        }
+                        resultString.append(cleanWord(currentValueString));
+                    }
+                }
+                value = reader.readLine();
+            }
+
+            if (resultString.length() < 1) {
+                return new String[0];
+            }
+            resultArray = resultString.toString().split(" ");
+            Arrays.sort(resultArray);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+
+        return resultArray;
+    }
+
+    public String cleanWord(String word) {
+        return word.replaceAll("[\\p{Punct}]", "");
     }
 }
