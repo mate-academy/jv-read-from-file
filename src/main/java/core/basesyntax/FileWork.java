ca6d0226f1119f339dbf98789c49d61612dee7e7
Oleg Turyk <oleh.turyk.ki.2019@lpnu.ua>
2023-07-06 22:48:41 +0300

    added implementation readFromFile method

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,31 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        List<String> filteredWords = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File(fileName)))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] words = line.split("[\\s\\p{Punct}]+");
+                for (String word : words) {
+                    if (word.toLowerCase().startsWith("w")) {
+                        filteredWords.add(word.toLowerCase());
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String[] result = filteredWords.toArray(new String[0]);
+        Arrays.sort(result, String.CASE_INSENSITIVE_ORDER);
+        return result;
     }
 }
