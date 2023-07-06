cb3e5815b1d50a6f62cfc8daff003d3c6ff7cef8
Olexander Parkhomovskyi <sanekparkhomovsky@gmail.com>
2023-07-06 08:51:37 +0300

    implemented read file and sorting words

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,2 +1,10 @@
-package core.basesyntax;public class Main {
+package core.basesyntax;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        FileWork fileWork = new FileWork();
+        System.out.println(Arrays.toString(fileWork.readFromFile("test4")));
+    }
 }
