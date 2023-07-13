ec3d63d1ef76b982825e015e83c19d9745eb513a
LTX <48835997+Lin-Rin@users.noreply.github.com>
2023-07-13 16:34:48 +0300

    done task

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,66 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        String fileContent = getStringFromFile(fileName);
+        String filterResultString;
+        String[] filterResultArray;
+
+        if (fileContent.length() == 0) {
+            return new String[0];
+        }
+
+        filterResultString = filterContent(fileContent);
+
+        if (filterResultString.length() == 0) {
+            return new String[0];
+        }
+
+        filterResultArray = filterResultString.split(" ");
+        Arrays.sort(filterResultArray);
+
+        return filterResultArray;
     }
+
+    private String getStringFromFile(String fileName) {
+        StringBuilder stringBuilder = new StringBuilder();
+        File file = new File(fileName);
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String value = reader.readLine();
+
+            while (value != null) {
+                stringBuilder.append(value).append(System.lineSeparator());
+                value = reader.readLine();
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file, ", e);
+        }
+
+        return stringBuilder.toString();
+    }
+
+    private String filterContent(String fileContent) {
+        StringBuilder stringBuilder = new StringBuilder();
+        String[] splits = fileContent.split("\\W+");
+
+        for (String split : splits) {
+            String lowerCaseSplit = split.toLowerCase();
+
+            if (lowerCaseSplit.charAt(0) == 'w') {
+                stringBuilder.append(lowerCaseSplit).append(" ");
+            }
+        }
+
+        return stringBuilder.toString();
+    }
+
 }
