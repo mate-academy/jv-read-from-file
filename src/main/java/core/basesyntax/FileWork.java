f63b644c161889aa7e8c8299920ea436856fb0fb
ruslanPonomarev <ruslan.ponomarev@wearebrain.com>
2023-07-28 15:21:59 +0300

    RP_Solution jv-read-from-file

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,35 @@
 package core.basesyntax;
 
+import java.io.*;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File myFile = new File(fileName);
+        StringBuilder sb = new StringBuilder();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(myFile));
+            String value = reader.readLine();
+            while (value != null) {
+                sb.append(value).append(System.lineSeparator());
+                value = reader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("No file found: ", e);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file: ", e);
+        }
+        String[] split = sb.toString().split("\\W+");
+        int sum = 0;
+        for (int i = 0; i < split.length; i++) {
+            char aimChar = 'w';
+            if (split[i].toLowerCase().startsWith(String.valueOf(aimChar))) {
+                split[sum] = split[i].toLowerCase();
+                sum++;
+            }
+        }
+        String[] res = Arrays.copyOf(split, sum);
+        Arrays.sort(res);
+        return res;
     }
 }
