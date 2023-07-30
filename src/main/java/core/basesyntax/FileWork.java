ea3dda681ffa81087bd1eb8d3e1744d5ffcd51ce
katym <katy.mozharovska@gmail.com>
2023-07-30 15:38:41 +0300

    readFromFile method implemented

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,8 @@
 package core.basesyntax;
 
 import java.io.File;
-import java.io.FileReader;
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class FileWork {
