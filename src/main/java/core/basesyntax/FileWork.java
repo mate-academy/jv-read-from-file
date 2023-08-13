5b83e8fe85a2e26041f8490bf10dc07a29d04fb5
Misha Kazarian <misha.kazaryan2@gmail.com>
2023-08-13 16:21:12 +0300

    Solution of jw-read-from-file task

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,39 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class FileWork {
+    private static final Pattern WORD_WITH_W_PATTERN = Pattern.compile("\\bw\\w*\\b");
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        ArrayList<String> listWordsWithW = new ArrayList<>();
+        String[] wordsWithW;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            int value = reader.read();
+            while (value != -1) {
+                stringBuilder.append((char) value);
+                value = reader.read();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read data from file " + fileName, e);
+        }
+
+        Matcher matcher =
+                WORD_WITH_W_PATTERN.matcher(stringBuilder.toString().toLowerCase());
+        while (matcher.find()) {
+            listWordsWithW.add(matcher.group());
+        }
+
+        wordsWithW = listWordsWithW.toArray(new String[0]);
+        Arrays.sort(wordsWithW);
+        return wordsWithW;
     }
 }
