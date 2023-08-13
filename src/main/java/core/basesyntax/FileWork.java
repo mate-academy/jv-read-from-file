5b33a50c12da5361d616db23e29fdc45a39fb093
Vlad Rublenko <vladik.rar@ukr.net>
2023-08-13 19:44:17 +0300

    solved task by using try-catch-with resources, constant of letter "W", method "split" and regex [^A-Za-z0-9 ] and made natural sorting by using method of class Arrays - .sort

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,52 @@
 package core.basesyntax;
 
+import java.io.*;
+import java.util.Arrays;
+
 public class FileWork {
+
+    private static final String LETTER_W = "w";
+    private static final int START_INDEX = 0;
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+
+        File myFile = new File(fileName);
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(myFile))) {
+
+            String word = bufferedReader.readLine();
+            StringBuilder contentOfFile = new StringBuilder();
+
+            while (word != null) {
+                contentOfFile.append(word).append(" ");
+                word = bufferedReader.readLine();
+            }
+
+            String[] splittedWords = contentOfFile.toString().split(" ");
+
+            StringBuilder wordWithW = new StringBuilder();
+            String tempWord;
+
+            for (int i = START_INDEX; i < splittedWords.length; i++) {
+                tempWord = splittedWords[i].toLowerCase();
+                if (tempWord.startsWith(LETTER_W)) {
+                    wordWithW.append(tempWord).append(" ");
+                }
+            }
+
+            String[] splittedWordWithW = wordWithW.toString().split(" ");
+            StringBuilder wordsWithoutSymbols = new StringBuilder();
+
+            for (int i = START_INDEX; i < splittedWordWithW.length; i++) {
+                wordsWithoutSymbols.append(splittedWordWithW[i].replaceAll("[^A-Za-z0-9 ]","")).append(" ");
+            }
+
+            String[] result = wordsWithoutSymbols.toString().split(" ");
+            Arrays.sort(result);
+            return result;
+
+            } catch (IOException e) {
+            throw new RuntimeException("can't read a file", e);
+        }
     }
 }
