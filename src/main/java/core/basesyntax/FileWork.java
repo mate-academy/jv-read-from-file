1f41d3b95d9677267b8219166b49da8f5a7a1b28
Oleksandr Chernyonkov <cayman_raw@ukr.net>
2023-08-15 23:53:02 +0300

    I have implemented the readFromFile method to retrieve an array of words from the file, where each word starts with the letter 'w'

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,39 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class FileWork {
+    private static final char FIRST_LETTER = 'w';
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        StringBuilder wordsFromFile = new StringBuilder();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            int value = reader.read();
+            while (value != -1) {
+                wordsFromFile.append((char) value);
+                value = reader.read();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("You have a IOException", e);
+        }
+        if (wordsFromFile.toString().isEmpty()) {
+            return new String[0];
+        }
+        String[] arrayOfWords = wordsFromFile.toString().split("[\\s\\p{Punct}]+");
+        ArrayList<String> finalArray = new ArrayList<>();
+        for (String word : arrayOfWords) {
+            if (word.toLowerCase().charAt(0) == FIRST_LETTER) {
+                finalArray.add(word.toLowerCase());
+            }
+        }
+        Collections.sort(finalArray);
+        return finalArray.toArray(new String[0]);
     }
 }
