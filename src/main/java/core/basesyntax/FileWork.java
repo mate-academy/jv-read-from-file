a8ddbb4516a645361b0056e9ab4beca55dbc9d5d
Volodymyr Hryhoryk <volodymyrhryhoryk@gmail.com>
2023-08-16 13:29:41 +0300

    created readFromFile() method

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,32 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class FileWork {
+    public static final String SPECIFIED_CHARACTER = "w";
+    public static final String REGEX = "\\W+";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        List<String> result = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] splitWords = line.split(REGEX);
+                for (String word : splitWords) {
+                    if (word.toLowerCase().startsWith(SPECIFIED_CHARACTER)) {
+                        result.add(word.toLowerCase());
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read this file", e);
+        }
+        Collections.sort(result);
+        return result.toArray(new String[]{});
     }
 }
