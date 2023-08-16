59b460adc1217865629d945a08bf546c39f4a6ad
Oleksandr Mykhailov <ososkdkc@gmai.com>
2023-08-16 15:19:50 +0300

    Added full solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,7 +1,6 @@
 package core.basesyntax;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -15,15 +14,19 @@ public class FileWork {
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String temp;
             while ((temp = reader.readLine()) != null) {
-                String[] tokens = temp.toLowerCase().split("\\s+w|(?=\\p{Punct})|(?<=\\p{Punct})");
+                String[] tokens = temp.toLowerCase().split("\\s+|(?=\\p{Punct})|(?<=\\p{Punct})");
                 for (String token : tokens) {
-                    if (token.startsWith("w"))
+                    if (token.startsWith("w")) {
+                        words.add(token);
+                    }
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        return null;
+        Collections.sort(words);
+
+        return words.toArray(new String[0]);
     }
 }
