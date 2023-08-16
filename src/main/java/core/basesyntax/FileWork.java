09efc2080c97a575b1a0bb8388f7de2b2fe2d3b7
Yevhen Kukharchuk <jancker19@ukr.net>
2023-08-17 02:15:07 +0300

    implemented method for file reading and searching words with special requirements

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,41 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        StringBuilder fullContent = new StringBuilder();
+        StringBuilder resultContent = new StringBuilder();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String value;
+            while ((value = reader.readLine()) != null) {
+                fullContent.append(value).append(" ");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        String fullString = fullContent.toString().trim().toLowerCase();
+        if (fullString.length() > 0) {
+            String [] splitFullString = fullString.split("\\W+");
+            for (String split : splitFullString) {
+                if (split.charAt(0) == 'w') {
+                    resultContent.append(split).append(" ");
+                }
+            }
+        }
+
+        if (resultContent.toString().length() == 0) {
+            return new String[0];
+        } else {
+            String[] result = resultContent.toString().trim().split(" ");
+            Arrays.sort(result);
+            return result;
+        }
     }
 }
