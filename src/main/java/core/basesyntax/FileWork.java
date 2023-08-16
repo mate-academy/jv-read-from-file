f864b827319561338bf39972706dc8551b8b4b57
Oleksandr Mykhailov <ososkdkc@gmai.com>
2023-08-16 15:12:49 +0300

    Fixing branches

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,29 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
+        List<String> words = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String temp;
+            while ((temp = reader.readLine()) != null) {
+                String[] tokens = temp.toLowerCase().split("\\s+w|(?=\\p{Punct})|(?<=\\p{Punct})");
+                for (String token : tokens) {
+                    if (token.startsWith("w"))
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 }
