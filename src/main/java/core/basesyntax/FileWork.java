6504787a1c82b47095b11e8e6eab0d38720f2476
hp <volodya.gata@gmail.com>
2023-08-17 20:56:21 +0300

    made an ArrayList to add words with special character, added try with resources and bufferreader inside it,while loop for iterating each line and for-each for words inside words array

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,32 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class FileWork {
+    private static final String SPECIFIED_CHARACTER = "w";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        List<String> result = new ArrayList<>();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                String[]words = fileName.split("\\W+");
+                for (String word : words) {
+                    if (word.toLowerCase().startsWith(SPECIFIED_CHARACTER)) {
+                        result.add(word.toLowerCase());
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read a file", e);
+        }
+        Collections.sort(result);
+        return result.toArray(new String[]{});
     }
 }
