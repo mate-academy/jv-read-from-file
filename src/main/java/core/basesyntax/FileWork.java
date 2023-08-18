7b71f112eed3ec74ae10491f7e8a0e363b77a3e4
Yana Bilyk <bilyk.yn@gmail.com>
2023-08-18 18:26:01 -0400

    Created method that read whole file and writes it in an array, then filtered this array for needed words

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,7 +1,10 @@
 package core.basesyntax;
 
-import java.io.*;
-import java.lang.reflect.Array;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Comparator;
 
