e5ce9888a65a54cd7c9311a556228b5bb423c36d
Stanislav Viner <stanislav.viner@gmail.com>
2023-08-24 20:36:46 +0300

    Added jv-read-from-file solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,46 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final int INDEX = -1;
+    private static final String REG_EX = "\\W+";
+    private static final String WHITESPACE = " ";
+    private static final String START_LETTER = "w";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            int value = reader.read();
+            while (value != INDEX) {
+                stringBuilder.append((char) value);
+                value = reader.read();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+
+        return filterFile(stringBuilder);
+    }
+
+    private String[] filterFile(StringBuilder fileName) {
+        StringBuilder result = new StringBuilder();
+        String[] splitFileName = fileName.toString().toLowerCase().split(REG_EX);
+        Arrays.sort(splitFileName);
+
+        for (String word : splitFileName) {
+            if (word.startsWith(START_LETTER)) {
+                result.append(word).append(WHITESPACE);
+            }
+        }
+
+        return result.length() > 0 ? result.toString().split(WHITESPACE) : new String[0];
     }
 }
