07e5fac01dab50a4f60030ffd3227be01c141e32
Yurii <wervmer>
2023-09-03 19:25:53 +0300

    implemented readFromFile method

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,23 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.stream.Stream;
+
 public class FileWork {
+    private static final String WORDS_SPLIT = "\\W+";
+    private static final String SPECIFIED_CHARACTER = "w";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            return bufferedReader.lines()
+                    .flatMap(l -> Stream.of(l.toLowerCase().split(WORDS_SPLIT)))
+                    .filter(w -> w.startsWith(SPECIFIED_CHARACTER))
+                    .sorted()
+                    .toArray(String[]::new);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read data from file " + fileName, e);
+        }
     }
 }
