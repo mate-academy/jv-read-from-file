9f795be31491c4de988673f60f2fcc91d7ad25de
Andrii Bozhenko <a.s.bozhenko@gmail.com>
2023-09-20 12:58:16 +0200

    HM_CreatFile_1.01

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,33 @@
 package core.basesyntax;
 
+import java.io.*;
+
 public class FileWork {
+    public static final String SPECIFIC_CHAR = "w";
+
     public String[] readFromFile(String fileName) {
         //write your code here
-        return null;
+      //  return null;
+        File file = new File(fileName);
+        String readFrom = null;
+        StringBuilder builder = new StringBuilder();
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String value = reader.readLine();
+            while (value != null){
+                builder.append(value).append(System.lineSeparator());
+                value = reader.readLine();
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException("Can't find the file, or read file");
+        }
+        readFrom = builder.toString();
+
+        if (readFrom.isEmpty()) {
+            return new String[0];
+        }
+
     }
 }
