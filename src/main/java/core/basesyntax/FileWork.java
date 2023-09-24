8877302107d0bec1f7cae814ec12e1e0d777fb14
unknown <liubchenkalexander@gmail.com>
2023-09-24 17:08:15 +0300

    test done

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,55 @@
 package core.basesyntax;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class FileWork {
-    public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+    public static String[] readFromFile(String fileName) throws IOException {
+        if (fileName.isEmpty()) {
+            return new String[0];
+        }
+        FileInputStream fis = new FileInputStream(fileName);
+        byte[] buffer = new byte[10];
+        StringBuilder sb = new StringBuilder();
+        while (fis.read(buffer) != -1) {
+            sb.append(new String(buffer));
+            buffer = new byte[10];
+        }
+        fis.close();
+
+        String content = sb.toString();
+
+        String nw = content.replaceAll("[.!/?*']", "").toLowerCase();
+        String n = nw.replaceAll("[\n\r]", " no ").toLowerCase();
+        String[] splt = n.split(" ");
+        int amountFound = 0;
+
+        for (String word : splt) {
+            if (word.startsWith("w")) { //check if word starts with searchWord
+                amountFound++;
+            }
+        }
+        if (amountFound == 0) {
+            return new String[0];
+        }
+        int u = 0;
+        String[] s = new String[amountFound];
+        for (String string : splt) {
+            if (string.startsWith("w")) {
+                s[u] = string;
+                u++;
+            }
+        }
+        for (int i = 0; i < s.length; i++) {
+            for (int j = i + 1; j < s.length; j++) {
+                if (s[i].compareTo(s[j]) > 0) {
+                    String temp = s[i];
+                    s[i] = s[j];
+                    s[j] = temp;
+                }
+            }
+        }
+
+        return s;
     }
 }
