ed0aa817949c52a71481d7902e6a7f7d85aeec48
Vadym Hurnik <vad.com.z@gmail.com>
2023-10-03 22:10:07 +0300

    added logic to FileWork class

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,51 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        String[] strings = getLowerCaseStrings(fileName);
+        int counter = 0;
+
+        if (strings[0] == "") {
+            return new String[]{};
+        }
+
+        for (String str : strings) {
+            if (str.charAt(0) == 'w') {
+                counter++;
+            }
+        }
+
+        String[] result = new String[counter];
+        counter = 0;
+
+        for (String str : strings) {
+            if (str.charAt(0) == 'w') {
+                result[counter++] = str;
+            }
+        }
+
+        Arrays.sort(result);
+        return result;
+    }
+
+    public String[] getLowerCaseStrings(String fileName) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String fileStr;
+            while ((fileStr = bufferedReader.readLine()) != null) {
+                stringBuilder.append(fileStr).append(" ");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read data from file " + fileName, e);
+        }
+
+        String[] strings = stringBuilder.toString().toLowerCase().split("\\W+");
+        return strings;
     }
 }
