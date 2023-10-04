b1afecdc6e4450a337db59f6f8b243187f0ebaa8
Danil Ponomarenko <akame.dante13@gmail.com>
2023-10-04 15:28:57 +0300

    Created first version of program

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,37 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        StringBuilder filteredWords = new StringBuilder();
+        File file = new File(fileName);
+        if (file.length() <= 0) {
+            return new String[]{};
+        }
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] words = line.split(" ");
+                for (String word : words) {
+                    word = word.toLowerCase().replaceAll("\\W+", "");
+                    if (word.startsWith("w")) {
+                        filteredWords.append(word).append(" ");
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String[] res = filteredWords.toString().split(" ");
+        if (res[0].equals("")) {
+            return new String[]{};
+        }
+        Arrays.sort(res);
+        return res;
     }
 }
