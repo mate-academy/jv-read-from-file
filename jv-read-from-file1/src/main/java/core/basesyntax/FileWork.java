a363a8c5c38ac7f73e23ac93ae81288e0c2d87d7
DankevichMisha <83833016+DankevichMisha@users.noreply.github.com>
2023-10-09 09:56:51 +0300

    Add files via upload

--- jv-read-from-file1/src/main/java/core/basesyntax/FileWork.java
+++ jv-read-from-file1/src/main/java/core/basesyntax/FileWork.java
@@ -1,19 +1,28 @@
 package core.basesyntax;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-
+import java.io.BufferedReader;import java.io.FileReader;
+import java.io.IOException;import java.util.Arrays;
 public class FileWork {
-    public String[] readFromFile(String fileName) throws FileNotFoundException {
-        File m = new File("test1");
-        File my = new File("test2");
-        File myf = new File("test3");
-        File myfi = new File("test4");
-        File myfil = new File("test5");
-        FileReader fileReader = new FileReader(m);
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-
+    private static final String SEPARATOR_OF_WORDS = " ";
+    public String[] readFromFile(String fileName) {        //write your code here
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            int value = reader.read();
+            StringBuilder builder = new StringBuilder();
+            while (value != -1) {
+                builder.append((char) value);
+                value = reader.read();            }
+            String [] words = builder.toString().toLowerCase().split("\\W+");
+            StringBuilder resultBuilder = new StringBuilder();
+            for (String word : words) {                if (word.charAt(0) == 'w') {
+                resultBuilder.append(word).append(SEPARATOR_OF_WORDS);                }
+            }            if (resultBuilder.toString().isEmpty()) {
+                return new String[0];            }
+            String [] appropriateWords = resultBuilder.toString().trim().split(SEPARATOR_OF_WORDS);
+            Arrays.sort(appropriateWords);
+            return appropriateWords;        } catch (IOException e) {
+            throw new RuntimeException("Can't read file " + e);
+        } catch (StringIndexOutOfBoundsException e) {
+            return new String[0];
+        }
     }
 }
