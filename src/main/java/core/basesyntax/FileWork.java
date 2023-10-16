b008802b774aae49b957a8c0859412ee0ab75069
Roman Kuzyk <kuzya228337@mail.com>
2023-10-16 18:16:05 +0300

    It imports the necessary Java classes for file handling and collections. The FileWork class has a readFromFile method, which takes a fileName as a parameter. Inside the method: It creates a File object based on the provided fileName. Initializes an empty List named wordsStartingWithW to store words that start with the letter 'w'. It then uses a try-with-resources block to open and read the file using a BufferedReader. It reads the first line from the file. If the line is not null, it splits the line into words based on whitespace using the split method, and then iterates through these words. For each word, it converts it to lowercase using word.toLowerCase() and checks if it starts with the letter 'w'. If it does, the word is added to the wordsStartingWithW list. If any IOException occurs while reading the file, it is caught, and a RuntimeException is thrown, indicating the error. Finally, the method returns an array of words that start with 'w' by converting the List to an array using toArray.

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,28 @@
 package core.basesyntax;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileWork {
-    public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+    public String[] readFromFile(String fileName){
+        File file = new File(fileName);
+        List<String> wordsStartingWithW = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader (file))){
+            String line = bufferedReader.readLine();
+
+            if (line != null) {
+                String[] words = line.split("\\s+");
+                for (String word: words) {
+                    word = word.toLowerCase();
+                    if(word.startsWith("w")) {
+                        wordsStartingWithW.add(word);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return wordsStartingWithW.toArray(new String[0]);
     }
 }
