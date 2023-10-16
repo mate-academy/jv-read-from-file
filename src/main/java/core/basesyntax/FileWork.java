a879efbcdad51b12645136237f57bd688b9b54b9
Roman Kuzyk <kuzya228337@mail.com>
2023-10-16 18:56:35 +0300

    added fix builder test

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,21 +1,30 @@
 package core.basesyntax;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class FileWork {
-    public String[] readFromFile(String fileName){
-        File file = new File(fileName);
-        List<String> wordsStartingWithW = new ArrayList<>();
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader (file))){
-            String line = bufferedReader.readLine();
+    private static final String SPECIFIED_CHARACTER = "w";
 
-            if (line != null) {
+    public String[] readFromFile(String fileName) {
+        File file = new File(fileName);
+        Pattern nonAlphabeticPattern = Pattern.compile("[^a-zA-Z]");
+        List<String> wordsStartingWithW = new ArrayList<>();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
                 String[] words = line.split("\\s+");
-                for (String word: words) {
+                for (String word : words) {
                     word = word.toLowerCase();
-                    if(word.startsWith("w")) {
+                    word = nonAlphabeticPattern.matcher(word).replaceAll("");
+                    if (word.startsWith(SPECIFIED_CHARACTER)) {
                         wordsStartingWithW.add(word);
                     }
                 }
@@ -23,6 +32,8 @@ public class FileWork {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        Collections.sort(wordsStartingWithW);
+
         return wordsStartingWithW.toArray(new String[0]);
     }
 }
