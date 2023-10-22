0a55a89a9668b415a0706531feb7fb9226198081
Ілля Лазарев <108271589+bagouser123@users.noreply.github.com>
2023-10-22 19:05:15 +0300

    add word starts with only w words

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,41 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            StringBuilder content = new StringBuilder();
+            String line;
+
+            while ((line = reader.readLine()) != null){
+                content.append(line).append(" ");
+            }
+            reader.close();
+
+            String text = content.toString().toLowerCase();
+
+            Pattern pattern = Pattern.compile("\\b\\w+\\b");
+            Matcher matcher = pattern.matcher(text);
+
+            HashSet<String> filteredWords = new HashSet<>();
+            while(matcher.find()) {
+                String word = matcher.group();
+                if (word.startsWith("w")) {
+                    filteredWords.add(word);
+                }
+            }
+            String[] result = filteredWords.toArray(new String[0]);
+            return result;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new String[0];
+        }
     }
 }
