19ec278dbe0d96279653bf722f8bda0ff8aee146
Ілля Лазарев <108271589+bagouser123@users.noreply.github.com>
2023-10-22 20:19:26 +0300

    add word starts with only w words

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -3,45 +3,40 @@ package core.basesyntax;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
+import java.util.Collections;
 
 public class FileWork {
     public String[] readFromFile(String fileName) {
         try {
             BufferedReader reader = new BufferedReader(new FileReader(fileName));
-            StringBuilder content = new StringBuilder();
             String line;
+            List<String> words = new ArrayList<>();
 
             while ((line = reader.readLine()) != null) {
-                content.append(line).append(" ");
-            }
-            reader.close();
+                Pattern pattern = Pattern.compile("\\b\\w+\\b");
+                Matcher matcher = pattern.matcher(line.toLowerCase());
 
-            String text = content.toString().toLowerCase();
-
-            Pattern pattern = Pattern.compile("\\b\\w+\\b");
-            Matcher matcher = pattern.matcher(text);
-
-            HashSet<String> filteredWords = new HashSet<>();
-            while (matcher.find()) {
-                String word = matcher.group();
-                if (word.startsWith("w")) {
-                    filteredWords.add(word);
+                while (matcher.find()) {
+                    String word = matcher.group();
+                    if (word.startsWith("w")) {
+                        words.add(word);
+                    }
                 }
             }
-            List<String> sortedWords = filteredWords.stream()
-                    .distinct() // Remove duplicates
-                    .sorted()
-                    .collect(Collectors.toList());
 
+            reader.close();
+
+            // Sort the filtered words naturally
+            Collections.sort(words);
+
+            return words.toArray(new String[0]);
         } catch (IOException e) {
             e.printStackTrace();
             return new String[0];
         }
-        return new String[0];
     }
 }
