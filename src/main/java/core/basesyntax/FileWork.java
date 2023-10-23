acf876830198bb91bdbce5b3bea19faaff20e5b1
Eugene Korniuk <korniuk77@gmail.com>
2023-10-23 17:37:10 +0300

    add eadFromFile method implementation

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,41 @@
 package core.basesyntax;
 
+import java.io.*;
+import java.nio.Buffer;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        String [] wordsFromText;
+        File file = new File(fileName);
+        if (file.length() == 0) {
+            return new String[0];
+        }
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String value = reader.readLine();
+            while (value != null) {
+                builder.append(value);
+                builder.append(" ");
+                value = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read from file " + fileName + e);
+        }
+        String textFromFile = builder.toString();
+        wordsFromText = textFromFile.split("\\W+");
+        builder = new StringBuilder();
+        for (String s : wordsFromText) {
+            if (s.toLowerCase().charAt(0) == 'w') {
+                builder.append(s.toLowerCase());
+                builder.append(" ");
+            }
+        }
+        if(builder.toString().isEmpty()) {
+            return new String[0];
+        }
+        String [] wordStartsWithW = builder.toString().split(" ");
+        Arrays.sort(wordStartsWithW);
+        return wordStartsWithW;
     }
 }
