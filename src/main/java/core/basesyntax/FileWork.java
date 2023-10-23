ddc49f3747012ec4e5009b1937cae40863f56cac
Ілля Лазарев <108271589+bagouser123@users.noreply.github.com>
2023-10-23 17:03:07 +0300

    add word starts with only w words

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -6,8 +6,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.Collections;
+import java.util.regex.Pattern;
 
 public class FileWork {
     public String[] readFromFile(String fileName) {
