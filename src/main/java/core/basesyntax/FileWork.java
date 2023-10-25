ff4242e0235658154126a8adbc251c48fda13887
Dmitriy Sumaroka <sumaroka.dmitriy@gmail.com>
2023-10-25 21:14:44 +0300

    used Files_readAllLines, checkstyle passed

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,41 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.List;
+
 public class FileWork {
+
     public String[] readFromFile(String fileName) {
         //write your code here
-        return null;
+
+        File file = new File(fileName);
+
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            List<String> strings = Files.readAllLines(file.toPath());
+
+            String string = strings.toString().toLowerCase();
+            String[] strings1 = string.split("\\W+");
+            Arrays.sort(strings1);
+            if (strings1.length == 0) {
+                return new String[0];
+            }
+            for (int i = 1; i < strings1.length; i++) {
+                if (strings1[i].charAt(0) == 'w') {
+                    stringBuilder.append(strings1[i]).append(" ");
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String result = stringBuilder.toString();
+        if (result.length() == 0) {
+            return new String[0];
+        }
+        return result.split(" ");
     }
 }
