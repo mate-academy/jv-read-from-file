742fb2defdd5e24a087d4e00d672b8c426ead219
Bohdan Kost <kostbogdan@icloud.com>
2023-10-30 00:02:16 +0200

    readFromFile has been implemented

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,26 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String SPLITTER = "\\W+";
+    private static final char LETTER_TO_CHECK = 'w';
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            return bufferedReader.lines()
+                    .flatMap(line -> Arrays.stream(line.split(SPLITTER)))
+                    .map(String::toLowerCase)
+                    .filter(word -> word.startsWith(String.valueOf(LETTER_TO_CHECK)))
+                    .sorted()
+                    .toArray(String[]::new);
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot read the file", e);
+        }
     }
 }
