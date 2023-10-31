70198c3d4692c1f0c57c21b0ffe73cdd2685e0b9
Slava_Moiseev <qualityengineerinkom@gmail.com>
2023-10-31 18:08:32 +0200

    Can`t find error

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,30 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        String str = "";
+        String[] finalArray;
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            while (bufferedReader.ready()) {
+                str = bufferedReader.readLine().toLowerCase();
+            }
+            String[] firstArr = str.split("\\W+");
+            for (String s : firstArr) {
+                if (s.startsWith("w")) {
+                    builder.append(s).append(" ");
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        finalArray = builder.toString().split(" ");
+        Arrays.sort(finalArray);
+        return finalArray;
     }
 }
