a8f636fbd018ae0db14d9c1d5e3014c5522294c9
Slava_Moiseev <qualityengineerinkom@gmail.com>
2023-11-03 18:21:52 +0200

    It is done

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -8,22 +8,28 @@ import java.util.Arrays;
 public class FileWork {
     public String[] readFromFile(String fileName) {
         String str = "";
-        String[] finalArray;
+        String[] finalArray = new String[0];
         StringBuilder builder = new StringBuilder();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
             while (bufferedReader.ready()) {
-                str = bufferedReader.readLine().toLowerCase();
-            }
-            String[] firstArr = str.split("\\W+");
-            for (String s : firstArr) {
-                if (s.startsWith("w")) {
-                    builder.append(s).append(" ");
+                str = bufferedReader.readLine();
+                if (str.isEmpty()) {
+                    break;
+                } else {
+                    String[] firstArr = str.toLowerCase().split("\\W+");
+                    for (String s : firstArr) {
+                        if (s.toLowerCase().startsWith("w")) {
+                            builder.append(s).append(" ");
+                        }
+                    }
+                    if (!builder.isEmpty()) {
+                        finalArray = builder.toString().split(" ");
+                    }
                 }
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        finalArray = builder.toString().split(" ");
         Arrays.sort(finalArray);
         return finalArray;
     }
