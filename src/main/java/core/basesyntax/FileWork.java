fae6ad7b30dfd5c6f964222bc777a8b74cd48f3b
MaksOsypchuk <Promaks_ua@icloud.com>
2023-11-07 18:14:50 +0200

    I filtered out words starting with w

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,29 @@
 package core.basesyntax;
 
+import java.io.*;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            StringBuilder stringBuilder = new StringBuilder();
+            String[] resultArray;
+            String value;
+            while ((value = reader.readLine()) != null) {
+                String[] split = value.split("\\W+");
+                for (String string : split) {
+                    if (string.toLowerCase().startsWith("w")) {
+                        stringBuilder.append(string.toLowerCase() + ",");
+                    }
+                }
+            }
+            resultArray = stringBuilder.toString().split(",");
+            Arrays.sort(resultArray);
+            return resultArray;
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
     }
 }
