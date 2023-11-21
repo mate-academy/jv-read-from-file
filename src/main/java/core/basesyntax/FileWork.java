c2bb31f281e6455dcc4cb6c80351863f0a3051bd
trokhim03 <trokhimchuk.003@gmail.com>
2023-11-21 13:34:15 +0200

    solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,47 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        StringBuilder stringBuilder = new StringBuilder();
+        int firstCount = 0;
+        int secondCount = 0;
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String pull = reader.readLine();
+            if (pull == null) {
+                return new String[0];
+            }
+            while (pull != null) {
+                stringBuilder.append(pull).append(System.lineSeparator());
+                pull = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String[] split = stringBuilder.toString().split("\\W+");
+
+        for (String index: split) {
+            if (index.charAt(0) == 'w' | index.charAt(0) == 'W') {
+                firstCount++;
+            }
+        }
+        String[] result = new String[firstCount];
+
+        for (String index: split) {
+            if (index.charAt(0) == 'w' | index.charAt(0) == 'W') {
+                result[secondCount] = index.toLowerCase();
+                secondCount++;
+            }
+        }
+        Arrays.sort(result);
+        return result;
     }
 }
