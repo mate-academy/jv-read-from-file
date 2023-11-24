1ff5b56c3306e436e266769fe21c3d4dac703f6a
Yaroslav Berezhniak <yaroslavix100@gmail.com>
2023-11-24 11:16:17 +0200

    task completed

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,30 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        try {
+            String content = new String(Files.readAllBytes(Path.of(fileName)));
+            String []words = content.split("[\\s\\p{Punct}]+");
+            String []filteredWords = new String[words.length];
+            int count = 0;
+
+            for (String word : words) {
+                if (word.startsWith("w") || word.startsWith("W")) {
+                    filteredWords[count++] = word.toLowerCase();
+                }
+            }
+
+            filteredWords = Arrays.copyOf(filteredWords, count);
+            Arrays.sort(filteredWords);
+            return filteredWords;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new String[0];
+        }
     }
 }
