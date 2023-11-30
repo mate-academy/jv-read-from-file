1926cbcbf7faf1b624bd7c9d291614ad3f82a8c5
oKukHtina <olakukhtina95@gmail.com>
2023-11-30 17:37:55 +0100

    create task,read file and wrote all words which starts on "w" letter

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,51 @@
 package core.basesyntax;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        String data = readFile(fileName);
+
+        String separateOnlyWordsForResult = generateReport(data);
+
+        if (separateOnlyWordsForResult.isEmpty()) {
+            return new String[0];
+        }
+
+        String[] resultArray = separateOnlyWordsForResult.toLowerCase()
+                .split(System.lineSeparator());
+        Arrays.sort(resultArray);
+
+        return resultArray;
+    }
+
+    private static String generateReport(String data) {
+        String[] splitWordsWithoutPunctuationMarks = data.split("\\W+");
+        StringBuilder separateOnlyWordsForResult = new StringBuilder();
+
+        for (String word : splitWordsWithoutPunctuationMarks) {
+            String[] splitWord = word.split("");
+
+            if ("w".equals(splitWord[0]) || "W".equals(splitWord[0])) {
+                separateOnlyWordsForResult.append(word).append(System.lineSeparator());
+            }
+        }
+        return separateOnlyWordsForResult.toString();
+    }
+
+    private String readFile(String fileName) {
+        StringBuilder data = new StringBuilder();
+
+        try (FileReader reader = new FileReader(fileName)) {
+            int value;
+            while ((value = reader.read()) != -1) {
+                data.append((char)value);
+            }
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+        }
+        return data.toString();
     }
 }
