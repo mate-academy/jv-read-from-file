8ebe750e0b0d58fce33d3aeb4893d9afba1aa177
Dominik Gągało <cliffcamer@gmail.com>
2023-12-09 11:41:31 +0100

    reading from file homework

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,39 @@
 package core.basesyntax;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
 public class FileWork {
-    public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+    public String[] readFromFile(String fileName) throws FileNotFoundException {
+        List<String> resultList = new ArrayList<>();
+
+        try {
+            BufferedReader buffReader = new BufferedReader(new FileReader(fileName));
+            String value = buffReader.readLine();
+            if (value == null) {
+                return new String[0];
+            }
+            while (value != null) {
+                value = value.toLowerCase().replaceAll("[^\\sa-zA-Z]", "");
+                String[] words = value.split(" ");
+
+                for (String word : words) {
+                    if (word.startsWith("w")) {
+                        resultList.add(word);
+                    }
+                }
+                value = buffReader.readLine();
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read from file.", e);
+        }
+
+        Collections.sort(resultList);
+
+        return resultList.toArray(new String[0]);
     }
 }
