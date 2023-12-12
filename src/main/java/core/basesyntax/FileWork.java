d3e53fd7b62535cde4ec9fd522254f6893578123
Kate892F <tophours13@gmail.com>
2023-12-12 10:49:21 +0200

    completed task

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,38 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            int value = reader.read();
+            while (value != -1) {
+                stringBuilder.append((char) value);
+                value = reader.read();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read the file", e);
+        }
+        String string = stringBuilder.toString();
+
+        String[] split = string.split("\\W+");
+        List<String> lst = new ArrayList<>();
+        for (int i = 0; i < split.length; i++) {
+            String s = split[i];
+            if (s.toLowerCase().startsWith("w")) {
+                lst.add(s.toLowerCase());
+            }
+        }
+        lst.sort(Comparator.naturalOrder());
+        return lst.toArray(new String[0]);
     }
 }
