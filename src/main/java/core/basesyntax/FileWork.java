309c6c18b4af015742b4faa6eb9aa2083d3da236
Oleksandr Kalenyk <carbonmail88@gmail.com>
2024-01-02 20:10:08 +0200

    jv-read-from-file solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,55 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String SPECIFIED_CHARACTER = "w";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+
+        try {
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            StringBuilder sentence = new StringBuilder();
+            String value = bufferedReader.readLine();
+
+            while (value != null) {
+                sentence.append(value).append(System.lineSeparator());
+                value = bufferedReader.readLine();
+            }
+
+            String[] list = sentence.toString().toLowerCase().split("\\W+");
+            int count = 0;
+
+            for (String word : list) {
+                if (word.startsWith(SPECIFIED_CHARACTER)) {
+                    count++;
+                }
+            }
+
+            String[] result = new String[count];
+            int index = 0;
+
+            for (String word : list) {
+                if (word.startsWith(SPECIFIED_CHARACTER)) {
+                    result[index++] = word;
+                }
+            }
+
+            Arrays.sort(result);
+
+            bufferedReader.close();
+            return result;
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
