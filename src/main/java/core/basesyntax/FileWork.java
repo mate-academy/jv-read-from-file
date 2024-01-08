210836806ea73cac6993277ea90c3fa92a6e4366
Vova Maryniak <volodimirmarinak27@gmail.com>
2024-01-08 19:35:04 +0200

    hw-jv-read-from-file

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,30 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class FileWork {
-    public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+    private static final String SPECIFIED_CHARACTER = "w";
+
+    public String[] readFromFile(String filePath) {
+        List<String> wordStartingWithW = new ArrayList<>();
+
+        try (Scanner fileScanner = new Scanner(new File(filePath))) {
+            while (fileScanner.hasNext()) {
+                String word = fileScanner.next().toLowerCase().replaceAll("[^a-zA-Z]", "");
+                if (!word.isEmpty() && word.startsWith(SPECIFIED_CHARACTER)) {
+                    wordStartingWithW.add(word);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("File not found", e);
+        }
+
+        Collections.sort(wordStartingWithW);
+        return wordStartingWithW.toArray(new String[0]);
     }
 }
