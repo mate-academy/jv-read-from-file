0c59021259e6fb76fcd33fb321dac818526dce54
Liza <lizabalashova7@gmail.com>
2024-01-11 19:16:31 +0200

    fixed

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -21,7 +21,6 @@ public class FileWork {
                     }
                 }
             }
-
             Collections.sort(wordsStartingWithW);
             return wordsStartingWithW.toArray(new String[0]);
         } catch (IOException e) {
@@ -29,30 +28,3 @@ public class FileWork {
         }
     }
 }
-
-
-
-
-
-        /*
-
-        String fileUpper = fileName.toUpperCase();
-        char c1;
-        char c2;
-
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(fileName));
-            StringBuilder stringBuilder = new StringBuilder();
-            String value = reader.readLine();
-            while (value != null){
-                stringBuilder.append(value).append(System.lineSeparator());
-                value = reader.readLine();
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("Can't read file " + fileName, e);
-        }
-
-        String[] words = fileName.split("[\\s\\p{Punct}]+");
-
-        */
-
