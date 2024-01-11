c99fafe481930bb73d90e64806f108c2fb0865c8
MarynaBodina <bananafox2002@gmail.com>
2024-01-11 17:34:21 +0200

    "all words is done"

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,28 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String sentence = reader.lines().collect(Collectors.joining(" "));
+            String[] split = sentence.replaceAll("\\p{Punct}", "")
+                    .toLowerCase()
+                    .split("\\s+");
+            String[] result = Arrays.stream(split)
+                    .filter(word -> word.startsWith("w"))
+                    .distinct()
+                    .sorted()
+                    .toArray(String[]::new);
+
+            System.out.println(Arrays.asList(result));
+            return result;
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading from file: " + e.getMessage(), e);
+        }
     }
 }
