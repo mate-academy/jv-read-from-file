e62bacda0fe6dde77d4dce39fd23bec46272b6f9
NekitCool1 <gnnikita97@gmail.com>
2024-01-11 16:27:57 +0200

    Words starting with w were filtered, and removed any punctuation

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,39 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class FileWork {
+
+    private StringBuilder stringBuilder = new StringBuilder();
+    private List<String> result = new ArrayList<>();
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                String[] line = value.split(" ");
+                value = bufferedReader.readLine();
+                for (int i = 0; i < line.length; i++) {
+                    if (line[i].startsWith("w") || line[i].startsWith("W")) {
+                        line[i].toLowerCase();
+                        result.add(line[i].toLowerCase().replaceAll("[^a-zA-Z[wW]]", ""));
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        Collections.sort(result);
+        String[] finalArray = new String[result.size()];
+        for (int i = 0; i < finalArray.length; i++) {
+            finalArray[i] = result.get(i);
+        }
+        return finalArray;
     }
 }
