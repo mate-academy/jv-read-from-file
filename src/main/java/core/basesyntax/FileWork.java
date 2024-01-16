a09c1ce6cbfcc5abcce0e0eab0a0658f287a333f
Vitaliy <vitaliyfilimonov3@gmail.com>
2024-01-16 15:11:20 +0200

    implemented readFromFile method which return string array

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,45 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final int FIRST_CHAR = 0;
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        if (file.length() == 0) {
+            return new String[0];
+        } else {
+            StringBuilder stringContent = new StringBuilder();
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader(file));
+                String readLine = reader.readLine();
+                while (readLine != null) {
+                    stringContent.append(readLine).append(" ");
+                    readLine = reader.readLine();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException("Can't read the file", e);
+            }
+            StringBuilder supportString = new StringBuilder();
+            String[] lookingArray;
+            String[] contentArray = stringContent.toString().toLowerCase().split("\\W+");
+            for (String content : contentArray) {
+                if (content.charAt(FIRST_CHAR) == 'w') {
+                    supportString.append(content).append(" ");
+                }
+            }
+            if (supportString.length() == 0) {
+                return new String[0];
+            } else {
+                lookingArray = supportString.toString().split(" ");
+                Arrays.sort(lookingArray);
+                return lookingArray;
+            }
+        }
     }
 }
