8604014dc064a527b8db822ee7e8dcf64ec9d287
YEvhen Korobka <korobkaevgeny@gmail.com>
2024-01-17 20:56:26 +0200

    Optimize word extraction and filtering logic in FileWork

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,47 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String WORDS = "\\W+";
+    private static final String SEPARATOR = " ";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String fileString;
+            do {
+                fileString = reader.readLine();
+                if (fileString != null) {
+                    stringBuilder.append(fileString).append(System.lineSeparator());
+                }
+            } while (fileString != null);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        if (stringBuilder.toString().isEmpty()) {
+            return new String[0];
+        }
+        String fileString = stringBuilder.toString().toLowerCase();
+        String[] words = fileString.split(WORDS);
+        stringBuilder.setLength(0);
+        for (String word: words) {
+            if (word.charAt(0) == 'w') {
+                stringBuilder.append(word).append(SEPARATOR);
+            }
+        }
+        if (stringBuilder.toString().isEmpty()) {
+            return new String[0];
+        }
+        String[] result = stringBuilder.toString().split(SEPARATOR);
+        Arrays.sort(result);
+        return result;
     }
 }
