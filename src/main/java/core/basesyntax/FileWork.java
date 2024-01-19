8ebac6a9a49651a358edc59656acb76022e8d01b
Marharyta Salii <margarita12122015@gmail.com>
2024-01-19 20:14:35 +0200

    read-from-file-done

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,41 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String SEPARATOR = " ";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+            StringBuilder stringBuilder = new StringBuilder();
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                stringBuilder.append(value).append(SEPARATOR);
+                value = bufferedReader.readLine();
+            }
+            String words = stringBuilder.toString();
+            String[] strings = stringBuilder.toString().split(SEPARATOR);
+            if (words.isEmpty()) {
+                String[] empty = {};
+                return empty;
+            }
+            StringBuilder result = new StringBuilder();
+            for (String string : strings) {
+                if (string.charAt(0) == 'w' || string.charAt(0) == 'W') {
+                    string = string.replaceAll("[^a-zA-Z]", "");
+                    string = string.toLowerCase();
+                    result.append(string).append(SEPARATOR);
+                }
+            }
+            String[] newResult = result.toString().split(SEPARATOR);
+            Arrays.sort(newResult);
+            return newResult;
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read this file", e);
+        }
     }
 }
