17a941bfe5573e79bb242649150010d674b7b51c
Marharyta Salii <margarita12122015@gmail.com>
2024-01-19 20:25:41 +0200

    read-from-file-done

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -13,23 +13,26 @@ public class FileWork {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
             StringBuilder stringBuilder = new StringBuilder();
             String value = bufferedReader.readLine();
+            if (value == null) {
+                return new String[0];
+            }
             while (value != null) {
                 stringBuilder.append(value).append(SEPARATOR);
                 value = bufferedReader.readLine();
             }
-            String words = stringBuilder.toString();
             String[] strings = stringBuilder.toString().split(SEPARATOR);
-            if (words.isEmpty()) {
-                String[] empty = {};
-                return empty;
-            }
             StringBuilder result = new StringBuilder();
+            int count = 0;
             for (String string : strings) {
                 if (string.charAt(0) == 'w' || string.charAt(0) == 'W') {
                     string = string.replaceAll("[^a-zA-Z]", "");
                     string = string.toLowerCase();
                     result.append(string).append(SEPARATOR);
+                    count++;
                 }
+            }
+            if (count == 0) {
+                return new String[0];
             }
             String[] newResult = result.toString().split(SEPARATOR);
             Arrays.sort(newResult);
