ad09989b20fb672d19578fdfc82e9d8562b7586c
Zorii Bohdan <bodkazorii@gmail.com>
2024-01-26 10:51:44 +0200

    implemented file reading, strings filtering and sorting

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,32 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+
 public class FileWork {
+    public static final String W_START_REGEX = "[w][a-z]*";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        String sentence;
+        try {
+            sentence = Files.readString(new File(fileName).toPath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String [] substrings = sentence.toLowerCase().split("\\W+");
+        String [] machedSubstrings = new String[substrings.length];
+        int index = 0;
+        for (String substring : substrings) {
+            if (substring.matches(W_START_REGEX)) {
+                machedSubstrings[index++] = substring;
+            }
+        }
+        machedSubstrings = Arrays.copyOf(machedSubstrings, index);
+        Arrays.sort(machedSubstrings);
+
+        return machedSubstrings;
     }
 }
