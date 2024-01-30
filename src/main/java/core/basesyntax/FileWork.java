bc99610c439a268fbd3a4ce90ecb711471883dbb
Andrii Hryniuk <grinuk0706@gmail.com>
2024-01-30 22:09:24 +0200

    First working solution.

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,47 @@
 package core.basesyntax;
 
+import java.io.*;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        if (file.length() == 0) {
+            return new String[0];
+        }
+        try {
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            StringBuilder allLine = new StringBuilder();
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                allLine.append(line).append(System.lineSeparator());
+                line = bufferedReader.readLine();
+            }
+            String[] words = allLine.toString().replace("?","").replace("!","")
+                    .replace(",", "").replace(".", "").replace("/","")
+                    .replace(";","").replace(":", "").replace("-", "")
+                    .replace(System.lineSeparator(), " ").toLowerCase().split(" ");
+            int size = 0;
+            for (int i = 0; i < words.length; i++) {
+                if (words[i].charAt(0) == 'w') {
+                    size++;
+                } else {
+                    words[i] = null;
+                }
+            }
+            int i = 0;
+            String[] result = new String[size];
+            for (String word : words) {
+                if (word != null) {
+                    result[i] = word;
+                    i++;
+                }
+            }
+            Arrays.sort(result);
+            return result;
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read from file", e);
+        }
     }
 }
