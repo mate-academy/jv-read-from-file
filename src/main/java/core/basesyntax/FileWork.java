e8a9bcf494f487d96c4d78a9c07551805dba8f12
Vitaliy <vitaliyufc@gmail.com>
2024-01-30 18:39:02 +0300

    TotalResult

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,30 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileWork {
+    private final List<String> totalResult = new ArrayList<>();
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] separatedWords = line.split("[^a-zA-Z]");
+                for (String word : separatedWords) {
+                    if (!word.isEmpty()) {
+                        totalResult.add(word.toLowerCase());
+                    }
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        totalResult.sort(String::compareToIgnoreCase);
+        return totalResult.toArray(new String[0]);
     }
 }
