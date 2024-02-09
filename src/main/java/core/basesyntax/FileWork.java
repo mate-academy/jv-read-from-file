eae5758fdbb8e1945d16bbef86a8e91b88eaf9ba
DmitryLazyuka <83352819+DmitryLazyuka@users.noreply.github.com>
2024-02-09 08:47:45 +0200

    Solution jv_read_from_file task

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,35 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        StringBuilder text = new StringBuilder();
+        StringBuilder result = new StringBuilder();
+        String[] wordsStartsWithW;
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            while (br.ready()) {
+                text.append(br.readLine()).append(" ");
+            }
+            String textWithoutPunctuation = text.toString().toLowerCase()
+                    .replaceAll("[^\\w\\s]+", " ");
+            String[] array = textWithoutPunctuation.split(" ");
+            for (String s : array) {
+                if (s.startsWith("w")) {
+                    result.append(" ").append(s);
+                }
+            }
+            wordsStartsWithW = result.toString().trim().split(" ");
+            if (wordsStartsWithW.length == 1 && wordsStartsWithW[0].isEmpty()) {
+                return new String[0];
+            }
+            Arrays.sort(wordsStartsWithW);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return wordsStartsWithW;
     }
 }
