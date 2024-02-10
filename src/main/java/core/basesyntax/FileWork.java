2db8c0b0545ab1f06c9a4d0383b64036c73d24a7
Oleksandr Samoylenko <ntskazhu1985@gmail.com>
2024-02-10 19:40:18 +0200

    Revert "File work class was implemented, build success."
    This reverts commit 87cd491c5d63a4edcf660f75f05db777e9282363.

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,46 +1,8 @@
 package core.basesyntax;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Locale;
-
 public class FileWork {
-    private static final String[] EMPTY_ARRAY = new String[0];
-
     public String[] readFromFile(String fileName) {
-        StringBuilder stringOfWords = new StringBuilder();
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(fileName));
-            String value = reader.readLine();
-            while (value != null) {
-                stringOfWords.append(value).append(" ");
-                value = reader.readLine();
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("Can't read file", e);
-        }
-        if (stringOfWords.length() == 0) {
-            return EMPTY_ARRAY;
-        }
-        String[] wordsArray = stringOfWords
-                .toString()
-                .toLowerCase(Locale.ROOT)
-                .split("\\W+");
-        stringOfWords.setLength(0);
-        if (wordsArray.length > 0) {
-            for (String word : wordsArray) {
-                if (word.charAt(0) == 'w') {
-                    stringOfWords.append(word).append(' ');
-                }
-            }
-        }
-        if (stringOfWords.length() == 0) {
-            return EMPTY_ARRAY;
-        }
-        wordsArray = stringOfWords.toString().split(" ");
-        Arrays.sort(wordsArray);
-        return wordsArray;
+        //write your code here
+        return null;
     }
 }
