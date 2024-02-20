f7ea70078de5039fde378d2ce06a4b7a231d4ab8
Viktor Kushnir <viktor.kushnir404@gmail.com>
2024-02-20 15:56:03 +0200

    jv-read-from-file solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,43 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String SPECIFIED_CHARACTER = "w";
+    private static final String SPACE = " ";
+    private StringBuilder stringBuilder;
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            stringBuilder = new StringBuilder();
+            String value = reader.readLine();
+            while (value != null) {
+                stringBuilder.append(value).append(System.lineSeparator());
+                value = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return processText(String.valueOf(stringBuilder));
+    }
+
+    private String[] processText(String text) {
+        String[] words = stringBuilder.toString().split("\\W+");
+        stringBuilder = new StringBuilder();
+        for (String word : words) {
+            word = word.toLowerCase();
+            if (word.startsWith(SPECIFIED_CHARACTER)) {
+                stringBuilder.append(word).append(SPACE);
+            }
+        }
+        if (stringBuilder.length() == 0) {
+            return new String[0];
+        }
+        words = stringBuilder.toString().split(SPACE);
+        Arrays.sort(words);
+        return words;
     }
 }
