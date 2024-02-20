f0863e616835752b010c9a5583efe6eea4c78c90
Shr1mpa <aminakrevetka@gmail.com>
2024-02-20 11:29:40 +0200

    solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,36 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class FileWork {
+    private static String FIND_WORD_REGEX = "\\b[wW]\\w+";
+
     public String[] readFromFile(String fileName) {
         //write your code here
-        return null;
+        File file = new File(fileName);
+        List<String> resultList = new ArrayList<>();
+        try {
+            List<String> strings = Files.readAllLines(file.toPath());
+            Pattern pattern = Pattern.compile(FIND_WORD_REGEX);
+
+            for (String string: strings) {
+                Matcher matcher = pattern.matcher(string);
+                while (matcher.find()) {
+                    resultList.add(matcher.group().toLowerCase());
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Cant`t read file", e);
+        }
+        Collections.sort(resultList);
+        String[] result = resultList.toArray(new String[0]);
+        return result;
     }
 }
