02c801d55266c50a3a34bd06875def27b5b2761c
Pavlo-Home-PC <serikovpavel99@gmail.com>
2024-02-21 22:58:20 +0200

    fix: initial comit was a mistake
    Passed test and Build.

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,20 +1,32 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class FileWork {
     public String[] readFromFile(String fileName) {
-
-        try {
-            FileReader fileReader = new FileReader(fileName);
-            String[] separated = fileReader.read();
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String temp;
+            while ((temp = bufferedReader.readLine()) != null) {
+                String[] spitedBySpaceAndPunctuation = temp.split("[\\s\\p{Punct}]+");
+                for (String tempWord : spitedBySpaceAndPunctuation) {
+                    if (tempWord.toLowerCase().startsWith("w")) {
+                        sb.append(tempWord.toLowerCase());
+                        sb.append(" ");
+                    }
+                }
+            }
+            if (sb.isEmpty()) {
+                return new String[0];
+            }
+            String[] result = sb.toString().trim().split(" ");
+            Arrays.sort(result);
+            return result;
         } catch (IOException e) {
             throw new RuntimeException("Can't read the file" + e);
         }
-        return null;
-    }
-    public boolean startWithLetterW(String word) {
-        return true;
     }
 }
