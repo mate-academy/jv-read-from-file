a29ae431b1547e3ae043e9466cf8b5da14f7b3be
Pavlo-Home-PC <serikovpavel99@gmail.com>
2024-02-21 18:52:47 +0200

    feat : first-try-commit
    Succesfull buid and passed test.

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,20 @@
 package core.basesyntax;
 
+import java.io.FileReader;
+import java.io.IOException;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
+
+        try {
+            FileReader fileReader = new FileReader(fileName);
+            String[] separated = fileReader.read();
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read the file" + e);
+        }
         return null;
+    }
+    public boolean startWithLetterW(String word) {
+        return true;
     }
 }
