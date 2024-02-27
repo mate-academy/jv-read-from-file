ebdf0d1c37eb5e21781c074022c83c26c768c400
Wojtas1996PL <mat.wojtkowiak@gmail.com>
2024-02-27 14:21:14 +0100

    First solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,50 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
         //write your code here
-        return null;
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(fileName));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        StringBuilder content = new StringBuilder();
+        String line;
+
+        while (true) {
+            try {
+                if (!((line = reader.readLine()) != null)) {
+                    break;
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            content.append(line);
+            content.append(System.lineSeparator());
+        }
+        String toSplit = content.toString();
+        String[] tab = toSplit.split("([.,!?:;'\"-]|\\s)");
+        int index = 0;
+        for (int i = 0; i < tab.length; i++) {
+            if (tab[i].indexOf("w") == 0 || tab[i].indexOf("W") == 0) {
+                index++;
+            }
+        }
+        String[] endTab = new String[index];
+        for (int i = 0, y = 0; i < tab.length && y < endTab.length; i++) {
+            if (tab[i].indexOf("w") == 0 || tab[i].indexOf("W") == 0) {
+                endTab[y] = tab[i].toLowerCase();
+                y++;
+            }
+        }
+        Arrays.sort(endTab);
+        return endTab;
     }
 }
