2db466419bb3c447ecbfe32aa8633894ef1a8cee
Lolita <ilolita796@gmail.com>
2024-03-09 14:42:39 +0200

    fixed issues

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -7,18 +7,20 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class FileWork {
+    private static final int ARRAY_LENGTH = 10;
+    private static final String SPECIFIED_CHARACTER = "w";
+
     public String[] readFromFile(String fileName) {
         //write your code here
         File file = new File(fileName);
-        String[] results = new String[10];
+        String[] results = new String[ARRAY_LENGTH];
         int i = 0;
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(file));
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String value = reader.readLine();
             while (value != null) {
                 String[] words = value.toLowerCase().split("\\W+");
                 for (String word : words) {
-                    if (word.charAt(0) == 'w') {
+                    if (word.startsWith(SPECIFIED_CHARACTER)) {
                         results[i++] = word;
                     }
                 }
@@ -28,22 +30,22 @@ public class FileWork {
             throw new RuntimeException("Can't read file", e);
         }
         if (file.length() == 0 || results[0] == null) {
-            results = new String[0];
-            return results;
+            return new String[0];
         }
-        return getNewArray(results);
+        return getSortedArray(results);
     }
 
-    public String[] getNewArray(String[] array) {
-        int newArrayLength = 0;
-        for (String s : array) {
-            if (s != null) {
-                newArrayLength++;
+    public String[] getSortedArray(String[] words) {
+        int sortedArrayLength = 0;
+        for (String word : words) {
+            if (word == null) {
+                break;
             }
+            sortedArrayLength++;
         }
-        String[] newArray = new String[newArrayLength];
-        System.arraycopy(array, 0, newArray, 0, newArray.length);
-        Arrays.sort(newArray);
-        return newArray;
+        String[] sortedArray = new String[sortedArrayLength];
+        System.arraycopy(words, 0, sortedArray, 0, sortedArray.length);
+        Arrays.sort(sortedArray);
+        return sortedArray;
     }
 }
