48db16ea29aea0cb9c6cf3f019226940b9057379
Artem Hurievskyi <gurievsky.artem@gmail.com>
2024-03-19 14:24:30 +0200

    you never know

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,38 @@
 package core.basesyntax;
 
+
+import java.io.*;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] split = line.split(" ");
+                for (String word : split) {
+                    String cleanWord = word.toLowerCase().replaceAll("[^a-zA-Z]", "");
+                    if (cleanWord.startsWith("w")) {
+                        stringBuilder.append(cleanWord).append(" ");
+                    }
+
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+
+        }
+
+        if (stringBuilder.length() == 0) {
+            return new String[0];
+        } else {
+            String[] splitBuilder = stringBuilder.toString().split(" ");
+            return splitBuilder;
+        }
+
+
+
     }
 }
