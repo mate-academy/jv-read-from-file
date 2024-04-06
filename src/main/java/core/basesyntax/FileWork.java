89da66ec92b200228f5251803d0c8a43ab45d662
krudeN <wetwettew@gmail.com>
2024-04-06 16:18:56 +0300

    added solution for the task

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,47 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String SPECIFIED_CHARACTER = "w";
+    private static final String[] EMPTY_ARRAY = new String[0];
+    private final StringBuilder builder = new StringBuilder();
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        int arrayLength = 0;
+        int startIndex = 0;
+        File myFile = new File(fileName);
+        try (BufferedReader reader = new BufferedReader(new FileReader(myFile))) {
+            String value = reader.readLine();
+            if (value == null) {
+                return EMPTY_ARRAY;
+            }
+            while (value != null) {
+                builder.append(value).append(" ");
+                value = reader.readLine();
+            }
+            String[] words = builder.toString().replaceAll("\\W", " ").split(" ");
+            for (String word : words) {
+                String lowerCaseWord = word.toLowerCase();
+                if (lowerCaseWord.startsWith(SPECIFIED_CHARACTER)) {
+                    arrayLength++;
+                }
+            }
+            String[] resultArray = new String[arrayLength];
+            for (String word : words) {
+                if (word.toLowerCase().startsWith(SPECIFIED_CHARACTER)) {
+                    resultArray[startIndex] = word.toLowerCase();
+                    startIndex++;
+                }
+            }
+            Arrays.sort(resultArray, String.CASE_INSENSITIVE_ORDER);
+            return resultArray;
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read the file", e);
+        }
     }
 }
