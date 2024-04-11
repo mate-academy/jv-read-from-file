b139dc352fd79852b6f4a34374b9533ffbe4aa7b
Tomasz Baran <MarcusOfficial@tuta.io>
2024-04-11 17:28:43 +0200

    Maven code improvements

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,8 @@
 package core.basesyntax;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
