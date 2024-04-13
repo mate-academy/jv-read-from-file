775c13a8c6670e803f079040336bda95e5557f89
Volodymyr Kulesha <v.s.kulesha@gmail.com>
2024-04-13 19:07:11 +0300

    implemented reading from file and searching words with special first character

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,45 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String CHARACTER_FOR_FILTER = "w";
+    private StringBuilder stringBuilder = new StringBuilder();
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String lineContent = reader.readLine();
+            while (lineContent != null) {
+                stringBuilder.append(System.lineSeparator())
+                        .append(lineContent);
+                lineContent = reader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("File not found", e);
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read a file", e);
+        }
+
+        String[] words = stringBuilder
+                .toString()
+                .toLowerCase()
+                .split("\\W+");
+        stringBuilder = new StringBuilder();
+        for (String word: words) {
+            if (word.startsWith(CHARACTER_FOR_FILTER)) {
+                stringBuilder.append(word).append(System.lineSeparator());
+            }
+        }
+        String[] result = stringBuilder.isEmpty()
+                ? new String[]{}
+                : stringBuilder.toString().split(System.lineSeparator());
+        Arrays.sort(result);
+
+        return result;
     }
 }
