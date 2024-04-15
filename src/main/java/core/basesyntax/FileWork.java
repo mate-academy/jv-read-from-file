1c77881ce51cf1e4d9b727671a3b4242f4fbb680
AnnaAAA3432 <144123194+AnnaAAA3432@users.noreply.github.com>
2024-04-15 17:24:46 +0200

    task solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,49 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        try {
+            StringBuilder stringBuilder = new StringBuilder();
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+
+            String line = bufferedReader.readLine();
+
+            while (line != null) {
+                stringBuilder.append(line).append("\n");
+                line = bufferedReader.readLine();
+            }
+
+            bufferedReader.close();
+
+            String resould = stringBuilder.toString();
+
+            String[] array = resould.split("\\W+");
+            List<String> list = new ArrayList<String>();
+            for (int i = 0; i < array.length; i++) {
+                if (array[i].startsWith("w") || array[i].startsWith("W")) {
+                    list.add(array[i].toLowerCase(Locale.ROOT));
+                }
+            }
+
+            String[] arrays = new String[list.size()];
+
+            Arrays.sort(list.toArray(arrays));
+
+            return arrays;
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
+
 }
