900cde5a26313f0dbb0576c34793f0d5597c84eb
Dmytro Lapikov <it.lapikov@gmail.com>
2024-04-16 17:50:28 +0300

    task done, test passed

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,43 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        StringBuilder addWords = new StringBuilder();
+
+        if (file.length() == 0) {
+            return new String[]{};
+        }
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[]tempWords = line.toLowerCase().split(" ");
+                for (String tempWord : tempWords) {
+                    if (tempWord.charAt(0) == 'w') {
+                        if (tempWord.contains(".") || tempWord.contains(",")
+                                || tempWord.contains("?") || tempWord.contains("!")) {
+                            addWords.append(tempWord.substring(0, tempWord.length() - 1))
+                                    .append(" ");
+                        } else {
+                            addWords.append(tempWord).append(" ");
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read the file.: " + e);
+        }
+        if (addWords.length() > 0) {
+            String[] finalArray = addWords.toString().split(" ");
+            Arrays.sort(finalArray);
+            return finalArray;
+        }
+        return new String[]{};
     }
 }
