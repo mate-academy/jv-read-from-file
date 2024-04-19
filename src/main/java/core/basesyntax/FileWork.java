6c3b6989cec6ea15047a80c6e6b648a62849ad0e
flamehost <flamehost@syneforge.com>
2024-04-19 23:25:40 +0300

    implement readFromFile method

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,35 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        String[] result;
+        try {
+            StringBuilder builder = new StringBuilder();
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] words = line.split(" ");
+                for (String word : words) {
+                    word = word.replaceAll("[^a-zA-Z]", "").toLowerCase();
+                    if (word.toLowerCase().startsWith("w")) {
+                        builder.append(word).append(" ");
+                    }
+                }
+            }
+            result = builder.toString().split(" ");
+            Arrays.sort(result);
+            if (result[0].isEmpty()) {
+                return new String[0];
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Cant read file", e);
+        }
+        return result;
     }
 }
