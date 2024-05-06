30f6e63b9ff62c0c74ee173d8b7bcb9fd199287d
olexanderbondarenko <bondarenkoov.dev@gmail.com>
2024-05-06 11:19:23 +0200

    solved the task

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,34 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        try {
+            String fileContent = new String(Files.readAllBytes(Paths.get(fileName)));
+            String wordsStartsWith = filteredByStartLetter(fileContent, "w");
+            String[] result = wordsStartsWith.isEmpty()
+                    ? new String[0]
+                    : wordsStartsWith.split(" ");
+            Arrays.sort(result);
+
+            return result;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String filteredByStartLetter(String string, String startWith) {
+        StringBuilder wordsStartsWith = new StringBuilder();
+        for (String word : string.toLowerCase().split("\\W+")) {
+            if (word.startsWith(startWith)) {
+                wordsStartsWith.append(word).append(" ");
+            }
+        }
+
+        return wordsStartsWith.toString();
     }
 }
