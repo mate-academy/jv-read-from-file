46dd7f58af507ab8b4a54318c0557794d80944ac
Ievgenii Kazaov <Jacalius1@gmail.com>
2024-05-07 16:11:51 -0600

    Wrote a filter and sorting for the incoming file according to the assignment.

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,35 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        String[] wordWithW = new String[100];
+        int countWords = 0;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                line = line.replaceAll("[^a-zA-Z\\s]", "");
+                String[] wods = line.split("\\s+");
+                for (String word : wods) {
+                    if (word.toLowerCase().startsWith("w")) {
+                        wordWithW[countWords++] = word.toLowerCase();
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file!", e);
+        }
+
+        String[] result = new String[countWords];
+        for (int i = 0; i < countWords; i++) {
+            result[i] = wordWithW[i];
+        }
+        Arrays.sort(result);
+        return result;
     }
 }
