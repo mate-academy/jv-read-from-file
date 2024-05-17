8187f1ee34436305b4d996c5518923b20beb163b
Anton <panyutin15@gmail.com>
2024-05-17 07:01:56 +0300

    added solution of task

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,11 @@
+package core.basesyntax;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        FileWork fileWork = new FileWork();
+
+        System.out.println(Arrays.toString(fileWork.readFromFile("test1")));
+    }
+}
