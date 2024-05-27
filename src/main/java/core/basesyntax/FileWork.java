836e76329b8808214fd474782cfdb797949de2d9
smagles <lesialist@icloud.com>
2024-05-27 19:34:24 +0200

    Read words from a file, filter them, and sort them

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,30 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        List<String> list = new ArrayList<>();
+
+        try {
+            Scanner scanner = new Scanner(new File(fileName));
+            while (scanner.hasNext()) {
+                String word = scanner.next().replaceAll("\\p{Punct}", "").toLowerCase();
+                if (word.startsWith("w")) {
+                    list.add(word);
+                }
+
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read from file");
+        }
+        String[] result = list.toArray(list.toArray(new String[]{}));
+        Arrays.sort(result);
+        return result;
     }
 }
