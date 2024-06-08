859a8ea9edd60b6687bc90aa243a83f4c8cfd746
Oleh <oleh.vodolazov@gmail.com>
2024-06-08 20:56:39 +0300

    read data from a file, filtered out words starting with 'w' and removed punctuation from them

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,44 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                stringBuilder.append(value).append(" ");
+                value = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        String fileContent = stringBuilder.toString().toLowerCase();
+        if (fileContent.length() == 0) {
+            return new String[0];
+        }
+        String[] parts = fileContent.split(" ");
+        int arrayLength = 0;
+        for (String part : parts) {
+            if (part.charAt(0) == 'w') {
+                arrayLength++;
+            }
+        }
+        String[] result = new String[arrayLength];
+        int count = 0;
+        for (String part : parts) {
+            if (part.charAt(0) == 'w') {
+                result[count++] = part.replaceAll("\\p{Punct}", "");
+            }
+        }
+        Arrays.sort(result);
+        return result;
     }
 }
