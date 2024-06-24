c507f385f3174719d7f4d434e77741d263146cd4
Anastasiia Nikita <anastasiya.folk@gmail.com>
2024-06-24 16:59:50 +0300

    Read from file

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,38 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String SPECIFIED_CHARACTER = "w";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        try {
+            File file = new File(fileName);
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            StringBuilder stringBuilder = new StringBuilder();
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                stringBuilder.append(value).append(System.lineSeparator());
+                value = bufferedReader.readLine();
+            }
+            String[] wordsArray = stringBuilder.toString().split("\\W+");
+            String[] resultWordsArray = new String[wordsArray.length];
+            int index = 0;
+            for (String word : wordsArray) {
+                if (word.toLowerCase().startsWith(SPECIFIED_CHARACTER)) {
+                    resultWordsArray[index] = word.toLowerCase();
+                    index++;
+                }
+            }
+            String[] result = Arrays.copyOf(resultWordsArray, index);
+            Arrays.sort(result);
+            return result;
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read from file", e);
+        }
     }
 }
