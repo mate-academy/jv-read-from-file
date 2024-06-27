709c7607a2a8b04c852662fbb6211b67aeb6a81c
Denys Mazurenko <0gur4ik3992@gmail.com>
2024-06-27 16:56:07 +0300

    Have failed tests

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,44 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class FileWork {
+    private static final String SPECIFIED_CHARACTER = "w";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        String readText = "";
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            StringBuilder stringBuilder = new StringBuilder();
+            int value = bufferedReader.read();
+            while (value != -1) {
+                readText = String.valueOf(stringBuilder.append((char) value)).toLowerCase();
+                value = bufferedReader.read();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Error ", e);
+        }
+
+        String [] parts = readText.split("\\W+");
+        int counter = 0;
+
+        for (String word: parts) {
+            if (word.startsWith(SPECIFIED_CHARACTER)) {
+                counter++;
+            }
+        }
+
+        String [] sortedTextBySpecifiedChar = new String[counter];
+        int index = 0;
+        for (int i = 0; i < parts.length; i++) {
+            if (parts[i].startsWith(SPECIFIED_CHARACTER)) {
+                sortedTextBySpecifiedChar[index++] = parts[i];
+            }
+        }
+        return sortedTextBySpecifiedChar.length > 1 ? sortedTextBySpecifiedChar : new String[]{};
     }
 }
