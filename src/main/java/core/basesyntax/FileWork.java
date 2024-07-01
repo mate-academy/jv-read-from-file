5532809bc473b690723a1b5a62b35835cd47bd31
Denys Mazurenko <0gur4ik3992@gmail.com>
2024-07-01 22:43:13 +0300

    Home work solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -4,41 +4,34 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class FileWork {
     private static final String SPECIFIED_CHARACTER = "w";
 
     public String[] readFromFile(String fileName) {
         File file = new File(fileName);
-        String readText = "";
+        StringBuilder stringBuilder = new StringBuilder();
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-            StringBuilder stringBuilder = new StringBuilder();
             int value = bufferedReader.read();
             while (value != -1) {
-                readText = String.valueOf(stringBuilder.append((char) value)).toLowerCase();
+                stringBuilder.append((char) value);
                 value = bufferedReader.read();
             }
         } catch (IOException e) {
             throw new RuntimeException("Error ", e);
         }
 
-        String [] parts = readText.split("\\W+");
-        int counter = 0;
-
-        for (String word: parts) {
-            if (word.startsWith(SPECIFIED_CHARACTER)) {
-                counter++;
-            }
-        }
-
-        String [] sortedTextBySpecifiedChar = new String[counter];
-        int index = 0;
+        String [] parts = stringBuilder.toString().toLowerCase().split("\\W+");
+        ArrayList<String> sortedText = new ArrayList<>();
         for (int i = 0; i < parts.length; i++) {
             if (parts[i].startsWith(SPECIFIED_CHARACTER)) {
-                sortedTextBySpecifiedChar[index++] = parts[i];
+                sortedText.add(parts[i]);
             }
         }
-        return sortedTextBySpecifiedChar.length > 1 ? sortedTextBySpecifiedChar : new String[]{};
+        Collections.sort(sortedText);
+        return sortedText.toArray(new String[0]);
     }
 }
