3e3ea0a27eee48a355fcd29e9ef683556bf0ca64
YevhenSrdk <eugeneserdyuk23@gmail.com>
2024-07-06 19:21:59 +0300

    Added functionality that returns words starting with the letter w

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,35 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
         //write your code here
-        return null;
+        File file = new File(fileName);
+        List<String> wordsList = new ArrayList<>();
+        try {
+            List<String> strings = Files.readAllLines(file.toPath());
+            for (String line : strings) {
+                String[] words = line.toLowerCase().split("\\W+");
+                for (String word : words) {
+                    if (word.startsWith("w")) {
+                        wordsList.add(word);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read file", e);
+        }
+
+        String[] filteredWords = wordsList.toArray(new String[0]);
+
+        Arrays.sort(filteredWords);
+
+        return filteredWords;
     }
 }
