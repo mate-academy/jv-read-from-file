dbda092a298f2d1a56f6cb7ac60934ac76a7aeef
Artem Stadnik <artem.stadnik.contact@gmail.com>
2024-07-15 17:39:11 +0300

    first attempt

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,31 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        try {
+            List<String> strings = Files.readAllLines(file.toPath());
+            ArrayList<String> filteredWords = new ArrayList<>();
+            for (String string: strings) {
+                String[] words = string.split("\\W+");
+                for (String eachWord: words) {
+                    eachWord = eachWord.toLowerCase();
+                    if (eachWord.startsWith("w")) {
+                        filteredWords.add(eachWord);
+                    }
+                }
+            }
+            Collections.sort(filteredWords);
+            return filteredWords.toArray(new String[0]);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
     }
 }
