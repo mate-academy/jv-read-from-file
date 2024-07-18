5c33ef197a24d4f9f5192b564ae2c31c04ae0751
Oleksii <obabych1@stu.vistula.edu.pl>
2024-07-18 21:59:56 +0200

    commit homework read from file

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,36 @@
 package core.basesyntax;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
 public class FileWork {
+    private static final String CHARACTER = "w";
+    private static final Pattern PATTERN = Pattern.compile("\\W+");
+    private static final List<String> RESULT = new ArrayList<>();
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+
+        if (!file.exists()) {
+            return null;
+        }
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] words = PATTERN.split(line.toLowerCase());
+                for (String word: words) {
+                    if (word.startsWith(CHARACTER)) {
+                        RESULT.add(word);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return RESULT.toArray(new String[0]);
     }
 }
