f71dbaf2d8033f4c922465126064d3257d48f438
Misha Doroshenko <doroshenko.m@ajax.systems>
2024-07-20 17:05:14 +0300

    Solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,37 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        List<String> startsWithW = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File(fileName)))) {
+            String line = reader.readLine();
+
+            while (line != null) {
+                String[] splittedLine = line.split(" ");
+                for (String l: splittedLine) {
+                    l = l.toLowerCase();
+                    if (l.startsWith("w")) {
+                        l = l.replaceAll("[^a-zA-Z0-9\\s]", "");
+                        startsWithW.add(l);
+                    }
+                }
+                line = reader.readLine();
+            }
+            Collections.sort(startsWithW);
+            return startsWithW.toArray(new String[0]);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("File " + fileName + " not found", e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
