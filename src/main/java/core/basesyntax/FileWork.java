15f1215682241bdba6afe95031415aeae4dc5fd6
Dmytro Tkachuk <dmitrotkacuk071@gmail.com>
2024-07-23 12:33:32 +0300

    Created the code

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -2,7 +2,17 @@ package core.basesyntax;
 
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        String [] readFromFile = fileName.split(" ");
+        if (readFromFile.length == 0) {
+            return null;
+        }
+        int i = 0;
+        for (String readFromFil : readFromFile) {
+            readFromFile[i] = new StringBuilder(readFromFil).toString();
+            i++;
+            //write your code here
+            return null;
+        }
+        return readFromFile;
     }
 }
