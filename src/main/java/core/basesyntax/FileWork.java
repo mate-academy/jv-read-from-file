c1e96f69a4b83c1805bd73fa6a0034bd05d62bd0
Igor Neminuschii <igorgorslav@gmail.com>
2024-08-01 14:35:19 +0300

    implemented method to filter the words from file

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,54 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String SPECIFIED_CHARACTER = "w";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            StringBuilder stringBuilder = new StringBuilder();
+            String value = bufferedReader.readLine();
+
+            while (value != null) {
+                stringBuilder.append(value).append(System.lineSeparator());
+                value = bufferedReader.readLine();
+            }
+
+            String[] wordsFromFile = stringBuilder.toString().split("[^a-zA-Z]+");
+            String[] exclusiveWords = keepRequiredWords(wordsFromFile);
+            Arrays.sort(exclusiveWords);
+            return exclusiveWords;
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("File doesn't exist", e);
+        } catch (IOException e) {
+            throw new RuntimeException("The file is not able to be read", e);
+        }
+    }
+
+    private String[] keepRequiredWords(String[] entireText) {
+        int count = 0;
+        for (String word : entireText) {
+            if (word.toLowerCase().startsWith("w")) {
+                count++;
+            }
+        }
+
+        String[] specificWords = new String[count];
+        int index = 0;
+
+        for (String word : entireText) {
+            if (word.toLowerCase().startsWith(SPECIFIED_CHARACTER)) {
+                specificWords[index] = word.toLowerCase();
+                index++;
+            }
+        }
+
+        return specificWords;
     }
 }
