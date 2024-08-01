54444646899a58da1a92c742064f9ff466a720ce
xxWersebuxx <cz.damax@gmail.com>
2024-08-01 12:46:44 +0200

    Test passed. Code is Creating dinamic list of whanted words depends of WANTED_LETTER. Returning corrent message if file is empty and also code is sorting words before returning words.

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,43 @@
 package core.basesyntax;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class FileWork {
+    private static final String WANTED_LETTER = "w";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+
+        List<String> resultList = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            int value = reader.read();
+            StringBuilder builder = new StringBuilder();
+            while (value != -1) {
+                builder.append((char) value);
+                value = reader.read();
+            }
+
+            String text = builder.toString();
+            String[] wordsArray = text.split("\\s+");
+
+            for (String s : wordsArray) {
+                String cleanWord = s.replaceAll("[^a-zA-Z]", "").toLowerCase();
+                if (cleanWord.startsWith(WANTED_LETTER)) {
+                    resultList.add(cleanWord);
+                }
+            }
+
+            String[] result = resultList.toArray(new String[0]);
+            Arrays.sort(result);
+
+            return result;
+
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read from file", e);
+        }
     }
+
 }
