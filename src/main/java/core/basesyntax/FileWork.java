9db234f01e6bd79dd598d80938d861c09f2b70d8
Artem Lavrivskyi <lavrivskyi@gmail.com>
2024-08-15 20:15:30 +0300

    readFromFile method logic implemented

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,26 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        String input;
+        try {
+            input = Files.readString(Path.of(fileName));
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        String onlyWordsString = input.replaceAll("\\b[^a-zA-Z]+", " ").toLowerCase();
+        String[] onlyWordsArr = onlyWordsString.split(" ");
+        Pattern filterPattern = Pattern.compile("\\b[Ww][a-zA-Z]+");
+        String[] finalArray = Arrays.stream(onlyWordsArr)
+                .filter(filterPattern.asPredicate())
+                .toArray(String[]::new);
+        Arrays.sort(finalArray);
+        return finalArray;
     }
 }
