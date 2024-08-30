0a431e681ff858a1c50f559860c6b47ab7504077
fn1z <mr.artemrud2005@gmail.com>
2024-08-30 08:42:21 +0300

    readme completed

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,34 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        List<String> wordsList = new ArrayList<>();
+
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(fileName));
+
+            for (String line : lines) {
+                String[] words = line.toLowerCase().split("\\W+");
+
+                for (String word : words) {
+                    if (word.startsWith("W")) {
+                        wordsList.add(word);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Can't filter file w/ name - " + fileName);
+        }
+        String[] result = wordsList.toArray(new String[0]);
+        Arrays.sort(result);
+
+        return result;
     }
 }
