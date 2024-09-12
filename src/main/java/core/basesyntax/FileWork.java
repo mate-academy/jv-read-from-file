9e0a9580c4a72adc2440208597cd083dc4665e3c
Maks <petliukofficial@gmail.com>
2024-09-12 21:37:12 +0300

    Extracted and sorted words from the file starting with 'w

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,35 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        StringBuilder result = new StringBuilder();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] words = line.toLowerCase().split("\\W+");
+                for (String word : words) {
+                    if (word.startsWith("w")) {
+                        result.append(word).append(" ");
+                    }
+                }
+            }
+            if (result.length() == 0) {
+                return new String[0];
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String[] filteredWords = result.toString().split("\\s+");
+
+        Arrays.sort(filteredWords);
+
+        return filteredWords;
     }
 }
