08b0f1db9b95ba58115fcc917433baa03c3f4520
Olha Oleshchuk <olha.oleshchuk@gmail.com>
2024-09-13 13:38:54 +0300

    read from file (second try)

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -3,40 +3,32 @@ package core.basesyntax;
 import java.io.*;
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
 
 public class FileWork {
     String fileForRead = "fileForRead.txt";
     String lowerString = "";
 
     public String[] readFromFile(String fileForRead) {
-        StringBuilder stringBuilder = new StringBuilder();
-        File myFile = new File(fileForRead);
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileForRead))) {
-            FileReader fileReader = new FileReader(myFile);
-            // BufferedReader bufferedReader = new BufferedReader(fileReader);
+        File myFile = new File("fileForRead.txt");
+        List<String> wordList = new ArrayList<>();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(myFile))) {
             String value = bufferedReader.readLine();
             while (value != null) {
-                stringBuilder.append(lowerString).append(System.lineSeparator());
-                System.out.println(value);
+                String[] split = value.split("\\W+");
+                for(String word : split) {
+                    wordList.add(word.toLowerCase());
+                }
                 value = bufferedReader.readLine();
-                if(value != null) {
-                    String[] split = value.split("\\W+");
-                    String string = Arrays.toString(split);
-                    lowerString = string.toLowerCase();
-                }
             }
-        } catch(IOException e) {
+        } catch (IOException e) {
             throw new RuntimeException("Can't read file", e);
-        } finally {
-            try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileForRead))) {
-                bufferedReader.close();
-                return readFromFile(fileForRead);
-                } catch(IOException e) {
-                    throw new RuntimeException("Can't read file", e);
-                }
-            }
         }
+        return wordList.toArray(new String[0]);
     }
+}
 
