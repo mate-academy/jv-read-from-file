8e9e115ff27d32c6c5426216878193783d8a2e2a
Heorhii Tsarenko <heorhii.tsarenko@gmail.com>
2024-09-18 22:13:34 +0300

    Complete task according to the conditions

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,44 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        try {
+            String content = Files.readString(Path.of(fileName));
+
+            String[] words = content.split("\\W+");
+
+            //convert to lowerCase
+            for (int i = 0; i < words.length; i++) {
+                words[i] = words[i].toLowerCase();
+            }
+
+            int countOfWords = 0;
+            for (String word : words) {
+                if (word.startsWith("w")) {
+                    countOfWords++;
+                }
+            }
+
+            String[] filteredWords = new String[countOfWords];
+
+            int indexArray = 0;
+            for (String word : words) {
+                if (word.startsWith("w")) {
+                    filteredWords[indexArray++] = word;
+                }
+            }
+
+            Arrays.sort(filteredWords);
+
+            return filteredWords;
+
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read information from file");
+        }
     }
 }
