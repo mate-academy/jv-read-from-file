f45c544d5416e7c7dfdd544f39491ca1990b9f07
DariaBakal <bakal.dasha01@gmail.com>
2024-09-26 16:42:10 +0300

    implemented readFromFile method

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,39 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String SPECIFIED_CHARACTER = "w";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            String fileContent = Files.readString(file.toPath());
+            String[] words = fileContent.toLowerCase().split("[\\s\\p{Punct}]+");
+
+            for (int i = 0; i < words.length; i++) {
+                if (words[i].startsWith(SPECIFIED_CHARACTER)) {
+                    if (i < words.length - 1) {
+                        stringBuilder.append(words[i]).append(" ");
+                    } else {
+                        stringBuilder.append(words[i]);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read this file", e);
+        }
+
+        if (stringBuilder.length() == 0) {
+            return new String[0];
+        }
+
+        String[] resultArray = stringBuilder.toString().split(" ");
+        Arrays.sort(resultArray);
+        return resultArray;
     }
 }
