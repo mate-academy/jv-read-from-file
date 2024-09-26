1a327aba1556c1bd33594d5aa32546afab459474
Pawel <capiukpawel@gmail.com>
2024-09-26 16:39:26 +0200

    First approach to the read-form-file task.

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,36 @@
 package core.basesyntax;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String SPECIFIED_CHARACTER = "w";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+
+        StringBuilder result = new StringBuilder();
+        Path path = Path.of(fileName);
+        try {
+            String content = Files.readString(path);
+            String[] words = content.toLowerCase().split("[\\s\\p{Punct}]+");
+
+            for (int i = 0; i < words.length; i++) {
+                if (words[i].startsWith(SPECIFIED_CHARACTER)) {
+                    result.append(words[i]).append(" ");
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        if (result.isEmpty()) {
+            return new String[0];
+        }
+
+        String[] resultArray = result.toString().split(" ");
+        Arrays.sort(resultArray);
+        return resultArray;
+
     }
 }
