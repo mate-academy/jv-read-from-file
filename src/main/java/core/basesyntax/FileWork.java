0b1252177c77abb9c44cead1724a61e0d6e9f6da
iurii <kenu21@gmail.com>
2024-10-02 16:49:55 +0200

    READ-FILE: implemented reading file with filtering

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,27 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
 public class FileWork {
+
     public String[] readFromFile(String fileName) {
         //write your code here
-        return null;
+        Path file = Paths.get(fileName);
+        try {
+            String fileString = Files.readString(file);
+            String withoutPunctuation =
+                    fileString.replaceAll("[^a-zA-Z0-9\\s]", "").toLowerCase();
+            String[] words = withoutPunctuation.split("\\s");
+            return Arrays.stream(words)
+                .filter(word -> word.startsWith("w"))
+                .sorted()
+                .toArray(String[]::new);
+        } catch (IOException ioException) {
+            throw new RuntimeException(ioException);
+        }
     }
 }
