6d6d779cc9f3809e0a0bb88e119191892cd0379f
Pavlo Sukhanko <swtlouisiana@gmail.com>
2024-10-04 19:15:01 +0100

    created the sorting logic and decompressed the file

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,40 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        String regex = "\\b[wW]\\w*\\b";
+
+        try {
+            String words = new String(Files.readAllBytes(Paths.get(fileName)));
+
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(words);
+
+            List<String> filteredWords = new ArrayList<>();
+            while (matcher.find()) {
+                filteredWords.add(matcher.group().toLowerCase(Locale.ROOT));
+            }
+
+            if (filteredWords.isEmpty()) {
+                return new String[0];
+            }
+
+            Collections.sort(filteredWords);
+
+            return filteredWords.toArray(new String[0]);
+
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
     }
 }
