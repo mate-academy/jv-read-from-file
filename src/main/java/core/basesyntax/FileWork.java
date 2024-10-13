b7d2c4de0abffd46999fcdd8cb50fb8cba701a58
Alexander Tyschenko <qwitelite@gmail.com>
2024-10-13 21:07:22 +0300

    solved task

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,30 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileWork {
+    public static final String NEED_CHAR = "w";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        List<String> result = new ArrayList<>();
+        File file = new File(fileName);
+        try {
+            List<String> data = Files.readAllLines(file.toPath());
+            String text = data.toString();
+            String[] words = text.toLowerCase().split("\\W+");
+            for (String word : words) {
+                if (word.startsWith(NEED_CHAR)) {
+                    result.add(word);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        result.sort(null);
+        return result.toArray(new String[0]);
     }
 }
