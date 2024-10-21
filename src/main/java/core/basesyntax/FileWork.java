5b6b168f4db690629f5b252cc52f4985fcbc616f
Serhii Senkiv <senkiv34@gmail.com>
2024-10-21 16:24:06 +0200

    Solution for jv-read-from-file

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,38 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
+        List<String> result = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            while (true) {
+                String line = br.readLine();
+                if (line == null) {
+                    break;
+                }
+                String[] words = line.split(" ");
+                for (String word : words) {
+                    if (word.startsWith("w") || word.startsWith("W")) {
+                        result.add(checkForPunctuation(word));
+                    }
+                }
+            }
+            Collections.sort(result);
+            String[] words = result.toArray(new String[result.size()]);
+            return words;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
+    }
+
+    private String checkForPunctuation(String word) {
+        return word.replaceAll("\\p{Punct}", "").toLowerCase();
     }
 }
