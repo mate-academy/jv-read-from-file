fe851e7e22c6b03ec800f841e72e9782df862277
Maks <lisichenkowork@gmail.com>
2024-10-27 11:01:09 +0100

    first commit

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,47 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+
+        int count = 0;
+
+        try {
+            String fileContent = Files.readString(Path.of(fileName));
+            if (fileContent.isEmpty()) {
+                return new String[0];
+            }
+            String[] arrayFromFile = fileContent.split("\\W+");
+            for (String word : arrayFromFile) {
+                if (word.charAt(0) == 'W' || word.charAt(0) == 'w') {
+                    count++;
+                }
+            }
+
+            String[] arrayWithWords = new String[count];
+            int index = 0;
+
+            for (String word : arrayFromFile) {
+                if (!word.isEmpty() && (word.charAt(0) == 'W' || word.charAt(0) == 'w')) {
+                    arrayWithWords[index++] = word;
+                }
+            }
+
+            for (int i = 0; i < arrayWithWords.length; i++) {
+                arrayWithWords[i] = arrayWithWords[i].toLowerCase();
+            }
+
+            Arrays.sort(arrayWithWords);
+            return arrayWithWords;
+
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read this file", e);
+        }
     }
 }
