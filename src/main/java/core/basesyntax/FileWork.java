8c5585757222089b59f58bf8402fcfbeb85cb5c7
Vasyl <vasiliy.azarov.vl1@gmail.com>
2024-11-03 16:57:33 +0100

    implemented task logic

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,59 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class FileWork {
+    private final static String PUNCTUALITY_REG_EX = "[\\p{Punct}\\s]+";
+    private final static String W_WORD_REG_EX = "\\b[wW]\\w*";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        try {
+            File file = new File(fileName);
+            List<String> lines = Files.readAllLines(file.toPath());
+            return getArrayOfNeededWords(lines);
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read file", e);
+        }
     }
+
+    public String[] getArrayOfNeededWords(List<String> lines) {
+        List<String> resultList = new ArrayList<>();
+        for (String line : lines) {
+            resultList.addAll(wordFinder(line));
+        }
+        return resultList.toArray(new String[0]);
+    }
+
+    public List<String> wordFinder(String line) {
+        List<String> result = new ArrayList<>();
+        List<String> words = wordSeparator(line);
+        for (String word : words) {
+            Pattern pattern = Pattern.compile(W_WORD_REG_EX);
+            Matcher matcher = pattern.matcher(word);
+            while (matcher.find()) {
+                result.add(matcher.group());
+            }
+        }
+        return result;
+    }
+
+    public String wordFormatter(String word) {
+        return word.replaceAll(PUNCTUALITY_REG_EX, "");
+    }
+
+    public List<String> wordSeparator(String line) {
+        List<String> words = List.of(line.split(PUNCTUALITY_REG_EX));
+        List<String> formattedWords = new ArrayList<>();
+        for (String word : words) {
+            formattedWords.add(wordFormatter(word));
+        }
+        return formattedWords;
+    }
+
 }
