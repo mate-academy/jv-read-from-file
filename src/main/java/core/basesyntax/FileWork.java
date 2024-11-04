dd5bc86070e3cc5547a7e868dab46165bce72e8b
Sasha Koval <kovalsana40@gmail.com>
2024-11-04 22:02:19 +0200

    I cant pass getCamelCaseResultFromFile test. I just cant understand what wrong with my result

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,57 @@
 package core.basesyntax;
 
+import java.io.*;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
         //write your code here
-        return null;
+        File file = new File(fileName);
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            int value = reader.read();
+            while (value != -1) {
+                stringBuilder.append((char) value);
+                value = reader.read();
+            }
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String str = stringBuilder.toString();
+        if (str.isEmpty()) {
+            return new String[0];
+        }
+        String[] toArray = str.split(" ");
+        StringBuilder stringBuilder1 = new StringBuilder();
+        for (int i = 0; i < toArray.length; i++) {
+            char[] symbols = toArray[i].toCharArray();
+            for (int j = 0; j < symbols.length; j++) {
+                if (symbols[0] == 'w' || symbols[0] == 'W') {
+                    for (int k = 0; k < symbols.length; k++) {
+                        if (symbols[k] != '.' && symbols[k] != ','
+                                && symbols[k] != '?'
+                        && symbols[k] != '!') {
+                            stringBuilder1.append(symbols[k]);
+                        }
+                    }
+                    stringBuilder1.append(" ");
+                    break;
+
+                }
+            }
+        }
+        String string = stringBuilder1.toString();
+        if (string.isEmpty()) {
+            return new String[0];
+        }
+        string = string.toLowerCase();
+        String[] result = string.split(" ");
+        Arrays.sort(result);
+
+        return result;
     }
 }
