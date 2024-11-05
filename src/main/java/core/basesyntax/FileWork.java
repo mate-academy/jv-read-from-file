9fc203768f2b29826ca089c19f4d7c453cd40e33
Vitalii Martynov <vitaliy_m81@ukr.net>
2024-11-05 13:44:54 +0200

    w-word filter from a file was implemented

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,42 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.List;
+
 public class FileWork {
+    private static final char SEEKING_CHAR = 'w';
+    private static final String FILTER_REGEX = "\\W+";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        try {
+            List<String> allLines = Files.readAllLines(file.toPath());
+            int count = 0;
+            for (String line : allLines) {
+                String[] words = line.split(FILTER_REGEX);
+                for (String word : words) {
+                    if (word.toLowerCase().charAt(0) == SEEKING_CHAR) {
+                        count++;
+                    }
+                }
+            }
+            String[] result = new String[count];
+            int i = 0;
+            for (String line : allLines) {
+                String[] words = line.split(FILTER_REGEX);
+                for (String word : words) {
+                    if (word.toLowerCase().charAt(0) == SEEKING_CHAR) {
+                        result[i++] = word.toLowerCase();
+                    }
+                }
+            }
+            Arrays.sort(result);
+            return result;
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
     }
 }
