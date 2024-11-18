ee14ffb27f0733e25993b2d806a38b6fa6ba7ceb
Serhii Slobodianiuk <sergey.slobodianiuk@gmail.com>
2024-11-18 12:38:06 +0200

    java read from file task is done

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,52 @@
 package core.basesyntax;
 
+import java.io.*;
+import java.util.Arrays;
+
 public class FileWork {
-    public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+
+    public static void main(String[] args) {
+        readFromFile("test1");
+    }
+
+    public static String[] readFromFile(String fileName) {
+        File file = new File(fileName);
+        StringBuilder builder = new StringBuilder();
+        if (file.length() == 0) {
+            return new String[0];
+        }
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String value = reader.readLine();
+            while (value != null) {
+                builder.append(value).append(System.lineSeparator());
+                value = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t read from file");
+        }
+
+        String[] splitContent = builder.toString().toLowerCase().split("\\W+");
+        int counter = 0;
+        for (String word : splitContent) {
+            if (word.charAt(0) == 'w') {
+                counter++;
+            }
+        }
+        if (counter == 0) {
+            return new String[0];
+        }
+        String[] result = new String[counter];
+        counter = 0;
+        for (String word : splitContent) {
+            if (word.charAt(0) == 'w') {
+                result[counter] = word;
+                counter++;
+            }
+        }
+        Arrays.sort(result);
+        System.out.println(Arrays.toString(result));
+        return result;
     }
 }
