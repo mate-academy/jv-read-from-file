5145a2f117dae04f558b30c67310a8c227b0e82d
YarikNarozhnyi <gnredit9@gmail.com>
2024-11-24 11:45:46 +0000

    3874834

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,32 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        String[] split = fileName.split("\\W+");
+
+        int count = 0;
+        for (String word : split) {
+            if (word.toLowerCase().startsWith("w")) {
+                count++;
+            }
+        }
+
+        if (count == 0) {
+            return new String[0];
+        }
+
+        String[] filteredWords = new String[count];
+        int index = 0;
+            for (String word : split) {
+            if (word.toLowerCase().startsWith("w")) {
+                filteredWords[index] = word.toLowerCase();
+                index++;
+            }
+          }
+        
+        return filteredWords;
     }
 }
