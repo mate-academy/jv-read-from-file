8a5dbb2f2f8bb856e91a94f22d097e2d84cb4dfa
Mukuta Pyataev <pataevmikita100@gmail.com>
2024-11-26 17:41:09 +0200

    implemented readFromFile method

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,32 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        ArrayList<String> words = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] lineWords = line.toLowerCase().split("\\W+");
+
+                for (String word : lineWords) {
+                    if (!word.isEmpty() && word.startsWith("w")) {
+                        words.add(word);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String[] result = words.toArray(new String[0]);
+        Arrays.sort(result);
+        return result;
     }
 }
