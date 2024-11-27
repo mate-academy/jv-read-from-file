87831c45a8daf6131bfad388916c5b50d5518eeb
Illia <ilya.artnaev@gmail.com>
2024-11-27 20:14:16 +0200

    Implement readFromFile method.

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,30 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileWork {
+    private static final String SPECIFIED_CHARACTER = "w";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        List<String> result = new ArrayList<>();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            bufferedReader.lines().forEach(line -> {
+                for (String word : line.split("\\W+")) {
+                    if (word.toLowerCase().startsWith(SPECIFIED_CHARACTER)) {
+                        result.add(word.toLowerCase());
+                    }
+                }
+            });
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read data from file " + fileName, e);
+        }
+
+        result.sort(String::compareTo);
+        return result.toArray(new String[0]);
     }
 }
