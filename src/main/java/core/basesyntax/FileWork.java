afafe994479728f79a2744e7acb476fe4dcb6308
Andrii Rastorhuiev <114357806+arastorhuiev@users.noreply.github.com>
2024-12-01 14:12:08 +0200

    add base solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,44 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class FileWork {
+    private static final String FILTER_CHARACTER = "w";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        List<String> words = new ArrayList<>();
+        File file = new File(fileName);
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String value = bufferedReader.readLine();
+
+            while (value != null) {
+                String[] line = value.split("\\W+");
+
+                for (String s : line) {
+                    String lowerCaseString = s.toLowerCase();
+
+                    if (lowerCaseString.startsWith(FILTER_CHARACTER)) {
+                        words.add(lowerCaseString);
+                    }
+                }
+
+                value = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String[] wordsArr = words.toArray(new String[0]);
+        Arrays.sort(wordsArr);
+
+        return wordsArr;
     }
 }
