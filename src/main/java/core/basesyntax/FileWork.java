775e88f0bc98bf11fad4af43330da723271b15f6
Vitalii Seleshchuk <seleschuk@gmail.com>
2024-12-06 23:16:11 +0200

    Done homework

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,36 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+//import java.io.FileNotFoundException;
+//import java.io.*;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
         //write your code here
-        return null;
+        List<String> result = new ArrayList<>();
+        Pattern wordPattern = Pattern.compile("\\b[wW]\\w*\\b");
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                Matcher matcher = wordPattern.matcher(line);
+                while (matcher.find()) {
+                    String word = matcher.group().toLowerCase(Locale.ROOT).replaceAll("[^a-z]", "");
+                    result.add(word);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Exception Error", e);
+        }
+        Collections.sort(result);
+        return result.toArray(new String[0]);
     }
 }
