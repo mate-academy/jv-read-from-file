6154de8f75a96cd392243760a785043fc36865c6
Oleksii Petrychenko <apetrychenkoff@gmail.com>
2024-12-10 03:04:07 +0200

    completed the task by reading lines from files and filtering them by the indicated character

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,30 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        List<String> lines;
+        try {
+            lines = Files.readAllLines(file.toPath());
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to read provided file", e);
+        }
+        List<String> filteredWords = new ArrayList<>();
+        for (String line : lines) {
+            filteredWords.addAll(Arrays.stream(line.split("\\W+"))
+                    .map(String::toLowerCase)
+                    .filter(word -> word.startsWith("w"))
+                    .toList());
+        }
+        filteredWords.sort(String::compareTo);
+
+        return filteredWords.toArray(new String[0]);
     }
 }
