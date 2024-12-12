6bce5a608f122cd7859d7c706640598af5805942
eulu <anegamega@gmail.com>
2024-12-12 15:44:33 +0200

    Implement readFromFile method

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,29 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            int value;
+            do {
+                value = br.read();
+                sb.append((char) value);
+            } while (value != -1);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String[] wordsArr = sb.toString().split("\\W+");
+        return Arrays.stream(wordsArr)
+                .map(String::toLowerCase)
+                .filter(w -> w.startsWith("w"))
+                .sorted()
+                .toArray(String[]::new);
     }
 }
