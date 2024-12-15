07cd82abc3e851e11a85201ec43776190c157177
Wojtek <wojka66@gmail.com>
2024-12-15 19:09:27 +0100

    Initial commit.

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,37 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class FileWork {
+    private static final String SPECIFIED_CHARACTER = "w";
+    private static final String EXCEPTION_MESSAGE = "Can't read file";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        Path testFile = Path.of(fileName);
+        String[] resultArray;
+
+        try {
+            List<String> lines = Files.readAllLines(testFile);
+            List<String> linesList = Arrays.asList(String.valueOf(lines)
+                    .toLowerCase().split("\\W+"));
+            List<String> wordsList = new ArrayList<>(List.of());
+            for (String word : linesList) {
+                if (word.startsWith(SPECIFIED_CHARACTER)) {
+                    wordsList.add(word);
+                }
+            }
+            Collections.sort(wordsList);
+            resultArray = wordsList.toArray(new String[]{});
+        } catch (IOException e) {
+            throw new RuntimeException(EXCEPTION_MESSAGE, e);
+        }
+
+        return resultArray;
     }
 }
