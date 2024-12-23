633d6ff68cc3a74c8110c1a3117aafc40695791c
Piotr Hic <piotrhic@gmail.com>
2024-12-23 11:40:13 +0100

    v2

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,15 +1,17 @@
 package core.basesyntax;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class FileWork {
 
     public String[] readFromFile(String fileName) {
         String[] finalWords;
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader (fileName))) {
             String result = bufferedReader.readLine();
             System.out.println(result);
-            String[] words = result.split( "\\W+");
+            String[] words = result.split("\\W+");
             int count = 0;
             for (String s : words) {
                 String first = s.toLowerCase().substring(0,1);
