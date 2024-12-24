8233ea7b4469e821a9c03b3575e5e2b602d20da4
leonid <5744095@stud.nau.edu.ua>
2024-12-24 12:20:07 +0200

    implement task

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,28 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+
 public class FileWork {
+
+    private final char LOWER_W_ASCII_VALUE = 119;
+    private final char UPPER_W_ASCII_VALUE = 87;
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        String s;
+        try {
+            s = Files.readString(Path.of(fileName));
+        } catch (IOException e) {
+            throw new RuntimeException("Problem with file reading");
+        }
+
+        return Arrays.stream(s.split("[ .,'\n;!?]"))
+                .filter(item -> !item.isBlank())
+                .filter(item -> item.charAt(0) == LOWER_W_ASCII_VALUE || item.charAt(0) == UPPER_W_ASCII_VALUE)
+                .map(String::toLowerCase)
+                .sorted()
+                .toArray(String[]::new);
     }
 }
