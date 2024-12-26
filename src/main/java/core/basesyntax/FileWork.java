0cf4197d6c9c20d84dacc343b42cb82468cc6337
Your_Name Your_Surname <your@email>
2024-12-26 20:51:06 +0200

    Read, filtered and sorted information from a file

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,46 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final char SPECIFIED_CHARACTER = 'w';
+    private static final int SPECIFIED_NUMBER = 0;
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            StringBuilder builder = new StringBuilder();
+            String value = reader.readLine();
+            while (value != null) {
+                builder.append(value).append(" ");
+                value = reader.readLine();
+            }
+            if (builder.isEmpty()) {
+                return new String[0];
+            }
+            String[] words = builder.toString().toLowerCase().split("\\W+");
+            int lenght = 0;
+            for (String word: words) {
+                if (word.charAt(SPECIFIED_NUMBER) == SPECIFIED_CHARACTER) {
+                    lenght++;
+                }
+            }
+            String[] wordsWithOnlyW = new String[lenght];
+            int i = 0;
+            for (String word: words) {
+                if (word.charAt(0) == SPECIFIED_CHARACTER) {
+                    wordsWithOnlyW[i] = word;
+                    i++;
+                }
+            }
+            Arrays.sort(wordsWithOnlyW);
+            return wordsWithOnlyW;
+        } catch (Exception e) {
+            throw new RuntimeException("Can't read file", e);
+        }
     }
 }
