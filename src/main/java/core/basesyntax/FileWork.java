2d75da3527417dc80e78c16d84fa285692dab2f6
DmitryShmerchuk <Dmitryvin1992@gmail.com>
2024-12-26 16:46:38 +0200

    Ready

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,52 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        int initialCapacity = 10;
+        String[] filteredWords = new String[initialCapacity];
+        int count = 0;
+        File file = new File(fileName);
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                String[] words = line.split("\\W+");
+
+                for (String word : words) {
+                    if (word.isEmpty()) {
+                        continue;
+                    }
+                    if (word.toLowerCase().startsWith("w")) {
+                        String cleanWord = word.replaceAll("^[^a-zA-Z]+|[^a-zA-Z]+$", "").toLowerCase();
+
+                        if (!cleanWord.isEmpty()) {
+                            if (count == filteredWords.length) {
+                                filteredWords = Arrays.copyOf(filteredWords, filteredWords.length * 2);
+                            }
+
+                            filteredWords[count++] = cleanWord;
+                        }
+                    }
+                }
+            }
+
+
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read data from file: " + fileName, e);
+        }
+        if (count == 0) {
+            return new String[0];
+        }
+
+        String[] result = Arrays.copyOf(filteredWords, count);
+        Arrays.sort(result);
+        return result;
     }
 }
