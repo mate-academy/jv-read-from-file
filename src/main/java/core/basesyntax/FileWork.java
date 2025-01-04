21e2a641372aef78d45779e27a3ad5e5f8bcb1bc
Mykola <mykola.turak.ki.2023@lpnu.ua>
2025-01-04 22:35:24 +0200

    make task

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,29 @@
 package core.basesyntax;
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        ArrayList<String> resultList = new ArrayList<>();
+        File file = new File(fileName);
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] words = line.split("\\W+");
+                for (String word : words) {
+                    if (!word.isEmpty() && word.toLowerCase().startsWith("w")) {
+                        resultList.add(word.toLowerCase());
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Can't open the file", e);
+        } catch (IOException e) {
+            throw new RuntimeException("Error while reading the file", e);
+        }
+
+        return resultList.stream().sorted().toArray(String[]::new);
     }
 }
