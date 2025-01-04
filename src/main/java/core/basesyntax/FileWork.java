b55333d560cd1fa62a9b1f684640902e7f00f47b
Mykola <mykola.turak.ki.2023@lpnu.ua>
2025-01-04 22:40:53 +0200

    solve style problems

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -3,8 +3,8 @@ package core.basesyntax;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class FileWork {
