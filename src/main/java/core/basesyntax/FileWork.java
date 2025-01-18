840190b58fb2e207667b8cdb96a36a9d9a6549fd
Hanna Voloshko <aa.voloshko09@gmail.com>
2025-01-19 00:11:17 +0200

    realised logic from task

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,39 @@
 package core.basesyntax;
 
+import java.io.*;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final char SPECIFIED_CHARACTER = 'w';
     public String[] readFromFile(String fileName) {
         //write your code here
-        return null;
+        File file = new File(fileName);
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            StringBuilder stringBuilder = new StringBuilder();
+            String value = bufferedReader.readLine();
+            if (value == null) {
+                return new String[0];
+            } else {
+                while (value != null) {
+                    stringBuilder.append(value).append(" ");
+                    value = bufferedReader.readLine();
+                }
+                String lineFromFile = stringBuilder.toString();
+                String[] split = lineFromFile.split("\\W+");
+                String[] tempArray = new String[split.length];
+                int index = 0;
+                for (String word: split) {
+                    if (word.toLowerCase().charAt(0) == SPECIFIED_CHARACTER) {
+                        tempArray[index] = word.toLowerCase();
+                        index++;
+                    }
+                }
+                String[] resultArray = Arrays.copyOf(tempArray, index);
+                Arrays.sort(resultArray);
+                return resultArray;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't open file");
+        }
     }
 }
