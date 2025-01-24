1f3c0aa5ce8af956b5839b6a1a341743d4bda73f
Kamil Szerszen <szerszenk1997@gmail.com>
2025-01-24 17:48:42 +0100

    Tasks is ready for checking!

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,47 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+
+        File file = new File(fileName);
+        String fileString;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String value = reader.readLine();
+            StringBuilder builder = new StringBuilder();
+            while (value != null) {
+                builder.append(value).append(System.lineSeparator());
+                value = reader.readLine();
+            }
+            fileString = builder.toString();
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read from file", e);
+        }
+
+        fileString = fileString.replaceAll("[^\\w\\s]", "");
+
+        ArrayList<String> words = new ArrayList<>();
+        String regex = "\\bw\\w*";
+
+        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(fileString);
+
+        while (matcher.find()) {
+            String word = matcher.group().toLowerCase();
+            words.add(word);
+        }
+
+        Collections.sort(words);
+
+        return words.toArray(new String[0]);
     }
 }
