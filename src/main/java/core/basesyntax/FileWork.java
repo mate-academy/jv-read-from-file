e412c565d3e2438b0a7cacbb5b540e9ca2b89794
Andrzej Pietkiewicz <pietan@me.com>
2025-01-25 22:20:03 +0100

    first solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,48 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        List<String> result = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            StringBuilder builder = new StringBuilder();
+            int value;
+            while ((value = reader.read()) != -1) {
+                char currentChar = (char) value;
+
+                if (Character.isLetter(currentChar)) {
+                    builder.append(currentChar);
+                }
+
+                if (Character.isWhitespace(currentChar) && !builder.isEmpty()) {
+                    String word = builder.toString().toLowerCase();
+                    if (word.charAt(0) == 'w') {
+                        result.add(word);
+                    }
+                    builder.delete(0, builder.length());
+                }
+            }
+
+            if (!builder.isEmpty()) {
+                String word = builder.toString().toLowerCase();
+                if (word.charAt(0) == 'w') {
+                    result.add(word);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Collections.sort(result);
+        return result.toArray(new String[0]);
     }
 }
