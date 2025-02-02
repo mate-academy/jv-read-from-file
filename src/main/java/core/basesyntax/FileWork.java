2ebcad72ab47d69ad9309068a7589dff3107df67
Roman Malyarenko <malyarenko.roman@gmail.com>
2025-02-02 20:06:32 +0200

    filtered out only the words starting with "w" and removed any punctuation

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,49 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final char SPECIFIED_CHARACTER = 'w';
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                stringBuilder.append(value).append(System.lineSeparator());
+                value = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        if (stringBuilder.isEmpty()) {
+            return new String[0];
+        }
+        String fileToString = stringBuilder.toString().toLowerCase();
+        String[] fileSplit = fileToString.split("\\W+");
+        int lengthResultArray = 0;
+        for (String word : fileSplit) {
+            if (word.charAt(0) == SPECIFIED_CHARACTER) {
+                lengthResultArray++;
+            }
+        }
+        if (lengthResultArray == 0) {
+            return new String[0];
+        }
+        String[] unsortedResult = new String[lengthResultArray];
+        int indexUnsortedResult = 0;
+        for (int i = 0; i < fileSplit.length; i++) {
+            if (fileSplit[i].charAt(0) == SPECIFIED_CHARACTER) {
+                unsortedResult[indexUnsortedResult] = fileSplit[i];
+                indexUnsortedResult++;
+            }
+        }
+        Arrays.sort(unsortedResult);
+        return unsortedResult;
     }
 }
