c7146358f1824493081e4469974e051bf4f0c82c
Luchko Roman <romander09@gmail.com>
2025-02-06 18:44:16 +0200

    do all

--- src/main/java/core/basesyntax/Main.java
+++
@@ -1,9 +0,0 @@
-package core.basesyntax;
-
-public class Main {
-    public static void main(String[] args) {
-        FileWork fileWork = new FileWork();
-        String name = "test5";
-        fileWork.readFromFile(name);
-    }
-}
