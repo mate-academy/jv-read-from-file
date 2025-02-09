38141935432c3a5644b34b9c7e3f5c4282c1db54
Liudmyla <kuzmenko.lv2017@gmail.com>
2025-02-09 04:46:16 +0200

    Created readFromFile() method

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,59 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String SPECIFIED_CHARACTER = "w";
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        StringBuilder stringBuilder = new StringBuilder();
+        String result = null;
+        String value = null;
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            value = bufferedReader.readLine();
+
+            if (value == null) {
+                return new String[0];
+            }
+
+            while (value != null) {
+                stringBuilder.append(value).append(System.lineSeparator());
+                value = bufferedReader.readLine();
+            }
+
+            result = stringBuilder.toString().toLowerCase();
+        } catch (IOException e) {
+            throw new RuntimeException("There is no such file" + e);
+        }
+
+        String[] splitText = result.split("\\W+");
+        String[] resultArray = new String[splitText.length];
+        int count = 0;
+
+        for (String word: splitText) {
+            if (word.startsWith(SPECIFIED_CHARACTER)) {
+                resultArray[count] = word;
+                count++;
+            }
+        }
+
+        String[] sortedArray = new String[count];
+        int secondCount = 0;
+        for (int i = 0; i < resultArray.length; i++) {
+            if (resultArray[i] != null) {
+                sortedArray[secondCount] = resultArray[i];
+                secondCount++;
+            }
+        }
+
+        Arrays.sort(sortedArray);
+
+        return sortedArray;
     }
 }
