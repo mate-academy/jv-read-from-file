a07409ff843235d9e5a0a816e2cf93749d570d44
Nazar <nazar0302@gmail.com>
2025-02-09 14:42:34 +0200

    implemented readFromFile()

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,25 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
         //write your code here
-        return null;
+        try {
+            String content = Files.readString(Path.of(fileName));
+            String[] words = content.split("[^a-zA-Z]+");
+
+            return Arrays.stream(words)
+                    .map(String::toLowerCase)
+                    .filter(word -> word.startsWith("w"))
+                    .sorted()
+                    .toArray(String[]::new);
+        } catch (IOException e) {
+            System.out.println("read from file error:" + e);
+            return new String[0];
+        }
     }
 }
