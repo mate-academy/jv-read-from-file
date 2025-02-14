4afa4081447e652d0072d856cfa143f20a7b82dd
Jacek Kubiak <jacek.ku@poczta.fm>
2025-02-14 13:01:16 +0100

    Task to check

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,34 @@
 package core.basesyntax;
 
+import java.io.*;
+import java.util.Arrays;
+import java.util.Locale;
+
 public class FileWork {
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        String[] ArrayWordFromFile = new String[5];
+        int i = 0;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] words = line.split("\\s+");
+                for (String word : words) {
+                    String filterWord = word.replaceAll("[\\p{Punct}]", "").toLowerCase(Locale.ROOT);
+                    if (filterWord.startsWith("w")) {
+                        ArrayWordFromFile[i] = filterWord;
+                        i++;
+                    }
+                }
+            }
+            Arrays.sort(ArrayWordFromFile);
+            return ArrayWordFromFile;
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
