7fc7491d4219dbd2bea63f7e7f2ac3b544f96be1
Vitalii Kozovyi <vitalikkozovuy@gmail.com>
2025-02-14 04:28:21 +0100

    solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,26 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        try {
+            List<String> lines = Files.readAllLines(Path.of(fileName));
+
+            return lines.stream()
+                    .flatMap(line -> Arrays.stream(line.toLowerCase().split("\\W+")))
+                    .filter(word -> word.startsWith("w"))
+                    .sorted()
+                    .toArray(String[]::new);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new String[0];
     }
+
 }
