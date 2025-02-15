9c50c5b1ff5c9b37b79cdce8f2d56e4ad6fe9ada
GennadyX7 <gennadyax@gmail.com>
2025-02-15 23:46:02 +0200

    I filtered out only words that start with w and removed any punctuation. I passed the words into an array which I sorted alphabetically.

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,38 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.regex.Pattern;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        String[] emptyArray = new String[0];
+        File file = new File(fileName);
+        List<String> wordsList = new ArrayList<>();
+        Pattern pattern = Pattern.compile("[\\W_]+");
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] words = pattern.split(line.toLowerCase());
+                for (String word : words) {
+                    if (word.startsWith("w") && !word.isEmpty()) {
+                        wordsList.add(word);
+                    }
+                }
+            }
+            String[] wordsArray = new String[wordsList.size()];
+            wordsList.sort(Comparator.naturalOrder());
+            return wordsArray = wordsList.toArray(wordsArray);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("File not found", e);
+        } catch (IOException e) {
+            throw new RuntimeException("Read data from file error", e);
+        }
     }
 }
