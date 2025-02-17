805292b9b4a839ffb8b2bb5be34e57a7d368c570
Jacek Kubiak <jacek.ku@poczta.fm>
2025-02-17 12:47:31 +0100

    task after corrections to control

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,38 +1,36 @@
 package core.basesyntax;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Locale;
 
 public class FileWork {
-
     public String[] readFromFile(String fileName) {
-        String[] arrayWordFromFile = new String[5];
-        int i = 0;
 
+        List<String> wordsList = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] words = line.split("\\s+");
                 for (String word : words) {
-                    String filterWord
-                            = word.replaceAll("[\\p{Punct}]", "").toLowerCase(Locale.ROOT);
+                    String filterWord =
+                            word.replaceAll("[\\p{Punct}]", "").toLowerCase(Locale.ROOT);
                     if (filterWord.startsWith("w")) {
-                        arrayWordFromFile[i] = filterWord;
-                        i++;
+                        wordsList.add(filterWord);
                     }
                 }
             }
-            Arrays.sort(arrayWordFromFile);
-            return arrayWordFromFile;
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        Collections.sort(wordsList);
+        return wordsList.toArray(new String[0]);
     }
 }
+
+
 
