250fe0323a438de4f4a202f45091be10d150fe98
Mateusz <mateusz.kiryk@o2.pl>
2025-02-17 19:07:05 +0100

    Third solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -2,8 +2,8 @@ package core.basesyntax;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
