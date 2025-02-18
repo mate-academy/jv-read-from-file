84d25446b0d0906636422cd76f96c34cbfb6efb8
Irina Levchenko <ira-levch@ukr.net>
2025-02-18 20:38:35 +0200

    Implementedd file reading with method read() (BufferedReader) into StringBuilder followed by spliting with regex "\\W+" insensitive to punctuation marks to the array (String[]). The array is filtered saving only the words that start with "w" or "W" with creation of new StringBuilder result that is splited to a new array with final natural sorting

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,42 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        StringBuilder builder = new StringBuilder();
+        StringBuilder result = new StringBuilder();
+        boolean firstWord = true;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            int value = reader.read();
+            while (value != -1) {
+                builder.append((char) value);
+                value = reader.read();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read from file " + fileName, e);
+        }
+        String[] words = builder.toString().split("\\W+");
+        for (String word : words) {
+            if (word.startsWith("w") || word.startsWith("W")) {
+                if (!firstWord) {
+                    result.append(" ");
+                }
+                result.append(word);
+                firstWord = false;
+            }
+        }
+        if (result.length() == 0) {
+            return new String[0];
+        }
+        String[] resultArray = result.toString().toLowerCase().split(" ");
+        Arrays.sort(resultArray);
+        return resultArray;
     }
 }
