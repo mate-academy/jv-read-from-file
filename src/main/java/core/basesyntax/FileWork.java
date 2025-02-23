c4af616c1d799aad7d209dc66fe0796c283c7a91
zowimar <zowimar@gmail.com>
2025-02-23 01:00:30 +0100

    remove encoding config from checkstyle plugin

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,42 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+
+
+        List<String> result = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                line = line.replaceAll("[^a-zA-Z]", " ");
+                String[] words = line.split("\\s+");
+
+                for (String word : words) {
+                    if (word.startsWith("w")) {
+                        result.add(word.toLowerCase());
+                    }
+
+                }
+
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+            Collections.sort(result);
+            return result.toArray(new String[0]);
+        }
     }
-}
+
+
+
+
+
+
