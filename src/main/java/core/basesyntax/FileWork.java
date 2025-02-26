5ebf3fe0949c627a219ffa792c4bf4db5490af4d
zowimar <zowimar@gmail.com>
2025-02-26 20:15:00 +0100

    Merge remote-tracking branch 'origin/develop' into develop

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -9,8 +9,6 @@ import java.util.List;
 
 public class FileWork {
     public String[] readFromFile(String fileName) {
-
-
         List<String> result = new ArrayList<>();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
             String line;
