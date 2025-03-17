bccc3b750e30448b6cfd26c2332da88c34ecdf42
Illia Dobrovolskyi <illia.dobrovolskyi2@gmail.com>
2025-03-17 21:40:30 +0200

    implemented readFromFile() method

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,49 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    private static final String SEPARATOR = " ";
+    private static final String REGEX = "\\W+";
+    private static final int CHARACTER_INDEX = 0;
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String line = bufferedReader.readLine();
+            if (line == null) {
+                return new String[0];
+            }
+            while (line != null) {
+                stringBuilder.append(line).append(SEPARATOR);
+                line = bufferedReader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Can't find the file", e);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read from file", e);
+        }
+        String[] words = stringBuilder.toString().toLowerCase().split(REGEX);
+        int arraySize = 0;
+        for (String word : words) {
+            if (word.charAt(CHARACTER_INDEX) == 'w') {
+                arraySize++;
+            }
+        }
+        String[] resultArray = new String[arraySize];
+        int index = 0;
+        for (String word : words) {
+            if (word.charAt(CHARACTER_INDEX) == 'w') {
+                resultArray[index] = word;
+                index++;
+            }
+        }
+        Arrays.sort(resultArray);
+
+        return resultArray;
     }
 }
