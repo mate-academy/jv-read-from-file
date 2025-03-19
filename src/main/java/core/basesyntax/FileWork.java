f4c4b2317dbffe875e8cfbb4230bbc0584838f28
Rostyslav Olekha <ros123ole@gmail.com>
2025-03-19 17:40:54 +0200

    first commit

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,36 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        List<String> result = new ArrayList<>();
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] words = line.split("\\s+");
+                for (String word : words) {
+                    word = word.replaceAll("[^a-zA-Z]", "").toLowerCase();
+                    if (word.startsWith("w")) {
+                        result.add(word);
+                    }
+                }
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            throw new RuntimeException("IOException" + e);
+        }
+        if (result.isEmpty()) {
+            return new String[0];
+        }
+        Collections.sort(result);
+        return result.toArray(new String[0]);
     }
 }
