69ec3c3072aba3d0169a65f134cfd836ab7127e1
Iryna Kravchenko <kravchenkoiryna1207@gmail.com>
2025-04-08 14:45:51 +0200

    Have done!

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,59 @@
 package core.basesyntax;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        String fileContent = "";
+        try {
+            fileContent = Files.readString(Path.of(fileName));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Result of file: ");
+        System.out.println(fileContent);
+
+        String[] words = fileContent.split("\\W+");
+        System.out.println("Split words: ");
+        for (String word : words) {
+            System.out.println(word);
+        }
+
+        List<String> result = new ArrayList<>();
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                String lowerCaseWord = word.toLowerCase();
+                if (lowerCaseWord.startsWith("w")) {
+                    result.add(lowerCaseWord);
+                }
+            }
+        }
+
+        System.out.println("After filtering: ");
+        for (String word : result) {
+            System.out.println(word);
+        }
+
+        Collections.sort(result);
+        return result.toArray(new String[0]);
+
     }
+
+    public static void main(String[] args) {
+        FileWork fileWork = new FileWork();
+        String[] result = fileWork.readFromFile("test1");
+
+        System.out.println("Result: ");
+        for (String word : result) {
+            System.out.println(word);
+        }
+    }
+
 }
+
