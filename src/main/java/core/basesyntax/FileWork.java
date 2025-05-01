40ae8552b1b54125b4d888fc37244e33e374ce34
Artem Bohachenko <bogart2012g00g1e@gmail.com>
2025-05-01 23:36:51 +0300

    implemented method FileWork

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,36 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        StringBuilder stringBuilderFile = new StringBuilder();
+        StringBuilder stringBuilderResult = new StringBuilder();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                stringBuilderFile.append(value).append(System.lineSeparator());
+                value = bufferedReader.readLine();
+            }
+            String[] arrayString = stringBuilderFile.toString().split("\\W+");
+            for (String word : arrayString) {
+                if (!word.isEmpty() && word.toLowerCase().toCharArray()[0] == 'w') {
+                    stringBuilderResult.append(word.toLowerCase()).append(" ");
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("File not found!", e);
+        }
+        if (stringBuilderResult.isEmpty()) {
+            return new String[]{};
+        } else {
+            String[] result = stringBuilderResult.toString().split(" ");
+            Arrays.sort(result);
+            return result;
+        }
     }
 }
