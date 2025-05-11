13b4f3b434ed4f56bb9dc76417184c3e0c35c61e
Mwxyz2022 <rozhukvlad@gmail.com>
2025-05-11 13:31:59 +0300

    Implemented the readFromFile method to read all lines from a file. Extracted words that start with the letter 'w' and collected them. Sorted the matching words alphabetically and returned them as an array.

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,50 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.List;
+
 public class FileWork {
-    public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+    private final StringBuilder matchingWordsBuilder = new StringBuilder();
+
+    public String[] readFromFile(String filePath) {
+        File file = new File(filePath);
+
+        try {
+            List<String> lines = Files.readAllLines(file.toPath());
+            System.out.println(lines);
+
+            for (String line : lines) {
+                extractWordsStartingWithW(line);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading file!", e);
+        }
+
+        String allWords = matchingWordsBuilder.toString().trim();
+
+        if (allWords.isEmpty()) {
+            return new String[0];
+        }
+
+        String[] result = allWords.split(" ");
+        Arrays.sort(result);
+        matchingWordsBuilder.setLength(0);
+
+        return result;
+    }
+
+    private void extractWordsStartingWithW(String sentence) {
+        String[] words = sentence.toLowerCase().split("[\\W+]");
+        for (String word : words) {
+            boolean startsWithW = word.matches("w[a-zA-Z]*");
+
+            if (startsWithW) {
+                matchingWordsBuilder.append(word).append(" ");
+            }
+        }
     }
 }
