6d21b04be28d5ead8cab0c1d8c8653e6493d9341
Valeria <valeriaua97@gmail.com>
2025-05-16 17:30:06 +0200

    added solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,34 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileWork {
-    public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+    public String[] readFromFile(String fileName) throws IOException {
+        List<String> result = new ArrayList<>();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                String clearLine = line.toLowerCase().replaceAll("[^a-z\\s]", "");
+                String[] words = clearLine.split("\\s+");
+
+                for (String word : words) {
+                    if (word.startsWith("w")) {
+                        result.add(word);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read from file", e);
+        }
+
+        result.sort(String::compareTo);
+
+        return result.toArray(new String[0]);
     }
 }
